d76c624e51cacd6fb093cbcb2e0abf316f6f13b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:30 +0000

    Add package serverutils/4.0.1/serverutils-4.0.1.tar.gz

---
+++ packages/serverutils/serverutils-4.0.1.tar.gz/serverutils-4.0.1/README.txt
@@ -0,0 +1,3 @@
+A simple library for writing TCP/Websocket servers in Python
+
+Currently about as unstable as it is possible to be in a PyPi project
---
+++ packages/serverutils/serverutils-4.0.1.tar.gz/serverutils-4.0.1/serverutils/__init__.py
@@ -0,0 +1,5 @@
+from .serverlistenable import TCPServer
+from .socketutils import ServerSocket, ClientSocket, TCPSocket
+from .protocols import Protocol_HTTP, HTTPIncoming, HTTPOutgoing, HFE, Protocol_WebSockets
+from .server import *
+from .extensions import *
