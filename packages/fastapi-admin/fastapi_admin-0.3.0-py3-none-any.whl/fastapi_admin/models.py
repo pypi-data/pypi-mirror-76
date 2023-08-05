a892d608b7375f50011b1d415ed7dd9919e0bf7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:37 +0000

    Add package fastapi-admin/0.3.0/fastapi_admin-0.3.0-py3-none-any.whl

---
+++ packages/fastapi-admin/fastapi_admin-0.3.0-py3-none-any.whl/fastapi_admin/models.py
@@ -0,0 +1,42 @@
+from tortoise import Model, fields
+
+from fastapi_admin import enums
+
+
+class AbstractUser(Model):
+    username = fields.CharField(max_length=20, unique=True)
+    password = fields.CharField(
+        max_length=200, description="Will auto hash with raw password when change"
+    )
+    is_active = fields.BooleanField(default=True,)
+    is_superuser = fields.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
+
+
+class AbstractPermission(Model):
+    label = fields.CharField(max_length=50)
+    model = fields.CharField(max_length=50)
+    action: enums.PermissionAction = fields.IntEnumField(
+        enums.PermissionAction, default=enums.PermissionAction.read
+    )
+
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        abstract = True
+
+
+class AbstractRole(Model):
+    label = fields.CharField(max_length=50)
+    users = fields.ManyToManyField("models.User")
+
+    permissions = fields.ManyToManyField("models.Permission")
+
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        abstract = True
