edf45d57e861834b3b2d822a4b77aeba12319837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:48 +0000

    Add package discodo/0.2.1a0/discodo-0.2.1a0.tar.gz

---
+++ packages/discodo/discodo-0.2.1a0.tar.gz/discodo-0.2.1a0/discodo/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+__title__ = "discodo"
+__author__ = "kijk2869"
+__lisence__ = "MIT"
+__version__ = "0.2.1a"
+
+from collections import namedtuple
+
+from .AudioSource import *
+from .gateway import VoiceSocket
+from .manager import AudioManager
+from .natives import *
+from .node import *
+from .planner import *
+from .player import Player
+from .stat import getStat
+from .updater import check_version
+from .utils import *
+from .voice_client import VoiceClient
+from .voice_connector import VoiceConnector
+
+try:
+    import discord
+except ModuleNotFoundError:
+    pass
+else:
+    from .DPYClient import DPYClient
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+version_info = VersionInfo(major=0, minor=2, micro=1, releaselevel="alpha", serial=0)
