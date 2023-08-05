894336afe90a103151cac8a7cdd4d144a92e487b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:00 +0000

    Add package songmam/0.11.0/songmam-0.11.0-py3-none-any.whl

---
+++ packages/songmam/songmam-0.11.0-py3-none-any.whl/songmam/facebook/entries/standby.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel
+
+from songmam.facebook.webhook import Webhook
+
+class StandbyEntry(BaseModel):
+    id: str
+    time: int
+    standby: List[]
+
+class StandbyObject(Webhook):
+    entry: StandbyEntry
+
+# {
+#   "object":"page",
+#   "entry":[
+#     {
+#       "id":"<PAGE_ID>",
+#       "time":1458692752478,
+#       "standby":[
+#         {
+#           "recipient":{
+#             "id":"<USER_ID>"
+#           },
+#           "recipient":{
+#             "id":"<PAGE_ID>"
+#           },
+#
+#           ...
+#         }
+#       ]
+#     }
+#   ]
+# }
