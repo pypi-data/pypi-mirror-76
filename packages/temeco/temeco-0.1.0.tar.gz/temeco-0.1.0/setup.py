9b83cc39f4124b9bc4593670f64161a57ba74e09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:52 +0000

    Add package temeco/0.1.0/temeco-0.1.0.tar.gz

---
+++ packages/temeco/temeco-0.1.0.tar.gz/temeco-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['temeco']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'temeco',
+    'version': '0.1.0',
+    'description': 'A small package handling telegram message copying preserving text entities',
+    'long_description': None,
+    'author': 'monomonedula',
+    'author_email': 'valh@tuta.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
