1b0d1da6bb0d90ed7951d4eaa7c88ec63adf110c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package MacTmp/0.0.3/MacTmp-0.0.3-py3-none-any.whl

---
+++ packages/MacTmp/MacTmp-0.0.3-py3-none-any.whl/MacTmp/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+# sudo powermetrics --samplers smc -i1 -n1
+
+def CPU_Temp():
+    tmp = os.popen('sudo powermetrics --samplers smc -i1 -n1')
+    return tmp
+
+def GPU_Temp():
+    return [i.strip('GPU die temperature: ' for i in os.popen('sudo powermetrics|grep -i "GPU die temperature"').readlines())]
