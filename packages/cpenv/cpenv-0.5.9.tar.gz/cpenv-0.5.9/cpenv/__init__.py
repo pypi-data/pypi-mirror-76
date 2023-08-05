cebcf113375336e4d66b35b45a5c38919ec5d489
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:04 +0000

    Add package cpenv/0.5.9/cpenv-0.5.9.tar.gz

---
+++ packages/cpenv/cpenv-0.5.9.tar.gz/cpenv-0.5.9/cpenv/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+'''isort:skip_file'''
+__title__ = 'cpenv'
+__version__ = '0.5.9'
+__author__ = 'Dan Bradham'
+__email__ = 'danielbradham@gmail.com'
+__url__ = 'http://github.com/cpenv/cpenv'
+__license__ = 'MIT'
+__description__ = 'Cross-platform Python environment management.'
+
+
+# Local imports
+from .api import *
+from .repos import *
+from .reporter import *
+from .resolver import *
+from .versions import *
+from . import vendor
+
+
+# Initialize cpenv
+from .api import _init
+_init()
