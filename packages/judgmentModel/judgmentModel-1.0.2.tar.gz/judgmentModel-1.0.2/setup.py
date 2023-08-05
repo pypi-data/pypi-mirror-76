1e8baccab86863573cd388e3e76ba316834327bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:22 +0000

    Add package judgmentModel/1.0.2/judgmentModel-1.0.2.tar.gz

---
+++ packages/judgmentModel/judgmentModel-1.0.2.tar.gz/judgmentModel-1.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# requirements = list(map(lambda x: x.strip(), open("requirements.txt").readlines()))
+
+setup(
+    name="judgmentModel",
+    version="1.0.2",
+    keywords="wenju xgboost gensim",
+    description="A Chinese question judgment library",
+    long_description="A Chinese question judgment library",
+    license="MIT License",
+    url="https://github.com/chendayin",
+    author="dayin",
+    author_email="760849607@qq.com",
+    packages=find_packages(),
+    package_data={'judgmentModel': ['data/*']},
+    include_package_data=True,
+    platforms='any',
+)
