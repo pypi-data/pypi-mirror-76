c7353e0dbcf1973260e63e96799d25b7526f02fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:09 +0000

    Add package pyPman/1.0.0b3/pyPman-1.0.0b3.tar.gz

---
+++ packages/pyPman/pyPman-1.0.0b3.tar.gz/pyPman-1.0.0b3/pyPman/install/init_builder.py
@@ -0,0 +1,27 @@
+import os
+
+from pyPman.utilities import utils
+from pyPman.install.utils import CommandInitializer
+
+
+class InitCommandGenerator(CommandInitializer):
+    def __init__(self, *args):
+        super(InitCommandGenerator, self).__init__(*args)
+
+    @property
+    def assigner(self):
+        return [
+            "__version__ = '%s'" % self.package_dict['version'],
+            "__name__ = '%s'" % self.package_dict['name'],
+            "__author__ = ['%s']" % self.package_dict['author'],
+            "__author_email__ = ['%s']" % self.package_dict['authorMail']
+        ]
+
+
+def command_builder(package_dict):
+    cmd_gen = InitCommandGenerator(package_dict)
+    init_file = utils.build_cmd_file(cmd_gen.initializer, script_list=[], class_or_def=False)
+    init_file = utils.build_cmd_file(cmd_gen.assigner, script_list=init_file, class_or_def=False)
+    init_file.append("")
+    final_init_file = "\n".join(init_file)
+    return final_init_file
