b062b58c0c454b05973b5bdf823918da25bec01e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:54 +0000

    Add package mlos/0.0.2/mlos-0.0.2.tar.gz

---
+++ packages/mlos/mlos-0.0.2.tar.gz/mlos-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='mlos',
+    version='0.0.2',    
+    description='MLP test Package',
+    author='Padma Polash Paul',
+    author_email='ppaul@dpami.com',    
+    license='BSD 2-clause',
+    packages=['mlos'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)    
