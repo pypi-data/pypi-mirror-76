97340e07749819c5ba977c066a8318d54af7a7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:30 +0000

    Add package test-cj/0.1/test_cj-0.1-py3-none-any.whl

---
+++ packages/test-cj/test_cj-0.1-py3-none-any.whl/test_cj/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 16:44:12 2020
+
+@author: cj
+"""
+def test_function(input_str):
+    print("Hello, I'm beomwoo.moon")
+    print("Your input string is,",input_str)
+    print("Bye!")
+
+
