6b5b5053ca220cbbbdd69c72680654ae5e0e4088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:45 +0000

    Add package lescode/0.2.6/lescode-0.2.6.tar.gz

---
+++ packages/lescode/lescode-0.2.6.tar.gz/lescode-0.2.6/lescode/config/__init__.py
@@ -0,0 +1,2 @@
+from .pool import (Config, asclass, asdict, get_config, itemsof, keysof,
+                   load_config, valuesof)
---
+++ packages/lescode/lescode-0.2.6.tar.gz/lescode-0.2.6/lescode/design_pattern/__init__.py
@@ -0,0 +1,2 @@
+from .behavioral import *
+from .creational import *
