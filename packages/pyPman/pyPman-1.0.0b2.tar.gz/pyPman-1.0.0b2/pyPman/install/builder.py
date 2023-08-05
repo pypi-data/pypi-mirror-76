096295391afaca5ec8857806c6aaaa2237e13a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:58 +0000

    Add package pyPman/1.0.0b2/pyPman-1.0.0b2.tar.gz

---
+++ packages/pyPman/pyPman-1.0.0b2.tar.gz/pyPman-1.0.0b2/pyPman/install/builder.py
@@ -0,0 +1,70 @@
+"""
+Build structure:
+    projectName
+        \____ packageName
+        \       \____ __init__.py
+        \       \____ __main__.py(entry point)
+        \____ README.md
+        \____ LICENSE
+        \____ setup.py
+
+    By building the project with PyProject manager, you can use the project for installing using 'pip'
+
+"""
+import os
+import subprocess
+from pyPman.install import setup_builder, init_builder, main_builder, readme_builder, license_builder
+from pyPman.utilities import utils
+
+
+def setup_structure(output, package_dict):
+    """ Definition to setup the directory and files structure in output folder
+    """
+    directories = [
+        package_dict['name'],
+        os.path.join(package_dict['name'], package_dict['package'])
+    ]
+    user_modules = [os.path.join(
+        package_dict['name'],
+        package_dict['package'],
+        module
+    ) for module in package_dict['module']]
+    directories.extend(user_modules)
+
+    files = [
+        os.path.join(package_dict['name'], "README.md"),
+        os.path.join(package_dict['name'], "LICENSE"),
+        os.path.join(package_dict['name'], "setup.py"),
+        os.path.join(package_dict['name'], os.path.join(package_dict['package'], "__init__.py")),
+        os.path.join(package_dict['name'], os.path.join(package_dict['package'], package_dict['main']))
+    ]
+    user_module_initialise = [os.path.join(module_dir, '__init__.py') for module_dir in user_modules]
+    files.extend(user_module_initialise)
+
+    for directory in directories:
+        utils.create_directory(output, directory)
+
+    for file_name in files:
+        utils.create_file(output, file_name=file_name)
+
+    return files
+
+
+def install(output, package_dict):
+    # Check if the git repository is added
+    if package_dict['url']:
+        commands = [
+            "git clone %s",
+        ]
+        call_repository = subprocess.call()
+    # Setting up the file structure in the output directory
+    script_files = setup_structure(output, package_dict)
+
+    # Generating file scripts
+    init_build_file = init_builder.command_builder(package_dict)
+    main_build_file = main_builder.command_builder(package_dict)
+    setup_build_file = setup_builder.commands(package_dict)
+    print("Writing new files to the scripts")
+    utils.write_file(script_files[3], data=init_build_file)
+    utils.write_file(script_files[4], data=main_build_file)
+    utils.write_file(script_files[2], data=setup_build_file)
---
+++ packages/pyPman/pyPman-1.0.0b2.tar.gz/pyPman-1.0.0b2/setup.py
@@ -0,0 +1,105 @@
+from setuptools import setup
+import os
+from glob import glob
+from shutil import rmtree
+
+packageName = "pyPman"
+try:
+    from setuptools.command.clean import clean as clean_command
+except ImportError as err:
+    from distutils.command.clean import clean as clean_command
+
+
+class Clean(clean_command):
+    def run(self):
+        clean_command.run(self)
+        delete_in_root = ['build', 'dist', '*.egg-info', '.tox', '.eggs', '.cache']
+        delete_everywhere = ['__pycache__', '*.pyc']
+
+        for candidate in delete_in_root:
+            rmtree_glob(candidate)
+
+        for visible_dir in glob("[A-Za-z0-9]*"):
+            for candidate in delete_everywhere:
+                rmtree_glob(os.path.join(visible_dir, candidate))
+                rmtree_glob(os.path.join(visible_dir, "*", candidate))
+
+
+def rmtree_glob(candidate):
+    for fobj in glob(candidate):
+        try:
+            rmtree(fobj)
+            print("%s/ removed from project" % fobj)
+        except OSError:
+            try:
+                os.remove(fobj)
+                print("%s/ removed from project" % fobj)
+            except OSError:
+                print("Testing")
+
+
+def read_file(path=None, file_name=None):
+    path = path or "."
+    file_path = os.path.join(path, file_name)
+    print("Reading file %s" % file_path)
+    with open(file_path) as fd:
+        return fd.read()
+
+
+exec(read_file(path=os.path.join('.', packageName), file_name="__init__.py"))
+version = __version__
+projectName = __name__
+readmeData = read_file(file_name="README.md")
+licenseData = read_file(file_name="LICENSE")
+
+setup(
+    name=packageName,
+    description="Python project manager",
+    version=version,
+    packages=[
+        packageName,
+        "%s.initialize" % packageName,
+        "%s.install" % packageName,
+        "%s.utilities" % packageName,
+    ],
+    install_requires=[
+        "Click",
+        "traVer == 1.1.0",
+        "setuptools",
+        "importlib_metadata"
+    ],
+    license="GNU GENERAL PUBLIC LICENSE",
+    long_description_content_type='text/markdown',
+    long_description=readmeData,
+    author=', '.join(__author__),
+    author_email=', '.join(__author_email__),
+    url="https://github.com/sunilk-n/TraVersion",
+    classifiers=[
+        # See https://pypi.org/pypi?%3Aaction=list_classifiers
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    data_files=[
+        "README.md",
+        "LICENSE"
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyPm=pyPman.__main__:cli'
+        ],
+    },
+    cmdclass={
+        "clean": Clean,
+    },
+)
