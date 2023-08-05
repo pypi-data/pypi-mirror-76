44c9446fb1faf9560c80ade1c1060a5bd4a0931b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package cloudcomponents.cdk-github-webhook/1.0.75/cloudcomponents.cdk_github_webhook-1.0.75-py3-none-any.whl

---
+++ packages/cloudcomponents.cdk-github-webhook/cloudcomponents.cdk_github_webhook-1.0.75-py3-none-any.whl/cloudcomponents/cdk_github_webhook/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.core._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@cloudcomponents/cdk-github-webhook",
+    "1.0.75",
+    __name__[0:-6],
+    "cdk-github-webhook@1.0.75.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
