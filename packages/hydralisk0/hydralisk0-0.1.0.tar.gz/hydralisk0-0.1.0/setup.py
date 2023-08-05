f081f680fd268ec075e01e9249875894b6de0306
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:20 +0000

    Add package hydralisk0/0.1.0/hydralisk0-0.1.0.tar.gz

---
+++ packages/hydralisk0/hydralisk0-0.1.0.tar.gz/hydralisk0-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='hydralisk0',
+    version='0.1.0',
+    author='Hui Wang',
+    author_email='hydra_007@sina.com',
+    description='An example for learning how to publish a Python package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pypa/sampleproject',
+    package=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
