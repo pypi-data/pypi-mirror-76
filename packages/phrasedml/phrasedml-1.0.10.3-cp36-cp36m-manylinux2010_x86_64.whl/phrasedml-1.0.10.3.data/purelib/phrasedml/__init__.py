97240af0c2729d867bb3b8397d2264a8597d215b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:55 +0000

    Add package phrasedml/1.0.10.3/phrasedml-1.0.10.3-cp36-cp36m-manylinux2010_x86_64.whl

---
+++ packages/phrasedml/phrasedml-1.0.10.3-cp36-cp36m-manylinux2010_x86_64.whl/phrasedml-1.0.10.3.data/purelib/phrasedml/__init__.py
@@ -0,0 +1,15 @@
+"""
+The libPhrasedml library, (c) 2014-2015 Lucian Smith, Kiri Choi, J Kyle Medley, and Herbert Sauro
+
+libPhrasedml is a library for Phrasedml, a human-readable,
+human-writeable simulation experiment definition language.
+For more information, see http://phrasedml.sf.net/
+"""
+
+from .phrasedml import *
+from pkg_resources import get_distribution
+
+#__version__ = LIBPHRASEDML_VERSION_STRING[1:]
+
+__version__ = get_distribution('phrasedml').version
+
