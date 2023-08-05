b02374649aa5475bd72b7a7126e8ee3087bc473c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:03 +0000

    Add package osr2mp4app/0.0.4a6/osr2mp4app-0.0.4a6-py3-none-any.whl

---
+++ packages/osr2mp4app/osr2mp4app-0.0.4a6-py3-none-any.whl/osr2mp4app/config_data.py
@@ -0,0 +1,54 @@
+import json
+import os
+from copy import copy
+from abspath import configpath, settingspath, pppath
+from osr2mp4.global_var import defaultsettings, defaultppconfig
+
+from helper.helper import loadsettings
+
+if os.path.isfile(pppath):
+	with open(pppath) as f:
+		current_ppsettings = json.load(f)
+	for key in defaultppconfig:
+		if key not in current_ppsettings:
+			current_ppsettings[key] = defaultppconfig[key]
+else:
+	current_ppsettings = copy(defaultppconfig)
+
+if os.path.isfile(settingspath):
+	with open(settingspath) as f:
+		current_settings = json.load(f)
+
+	for key in defaultsettings:
+		if key not in current_settings:
+			current_settings[key] = defaultsettings[key]
+
+else:
+	current_settings = copy(defaultsettings)
+
+if os.path.isfile(configpath):
+	with open(configpath) as f:
+		current_config = json.load(f)
+	outputpath = current_config["Output path"]
+
+else:
+	current_config = {
+		"osu! path": "",
+		"Skin path": "",
+		"Beatmap path": "",
+		".osr path": "",
+		"Default skin path": "",
+		"Output path": "",
+		"Output name": "",
+		"Width": 1280,
+		"Height": 720,
+		"FPS": 60,
+		"Start time": 0,
+		"End time": -1,
+		"Video codec": "XVID",
+		"Process": 2,
+		"ffmpeg path": "ffmpeg"
+	}
+	# logging.info("Current config is set to default")
+
+loadsettings(current_config, current_settings, current_ppsettings)
