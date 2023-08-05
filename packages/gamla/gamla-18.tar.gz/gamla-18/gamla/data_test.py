48ad8bf24741e05cec6cb6443ade5f9ef73a7135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:40 +0000

    Add package gamla/18/gamla-18.tar.gz

---
+++ packages/gamla/gamla-18.tar.gz/gamla-18/gamla/data_test.py
@@ -0,0 +1,41 @@
+import dataclasses
+from typing import Any
+
+from gamla import data
+
+
+def test_freeze_deep():
+    original = {"1": {"2": "345", "some-string": ["hello"]}}
+    frozen = data.freeze_deep(original)
+    # Check values are intact.
+    assert frozen == data.frozendict(
+        {"1": data.frozendict({"2": "345", "some-string": ("hello",)})},
+    )
+    # Check hashability.
+    {data.freeze_deep(original)}
+
+
+@dataclasses.dataclass(frozen=True)
+class MockDataclassA:
+    a: int
+
+
+@dataclasses.dataclass(frozen=True)
+class MockDataclassB:
+    b: MockDataclassA
+
+
+def test_match_false():
+    assert not data.match(MockDataclassB(MockDataclassA(5)))(
+        MockDataclassB(MockDataclassA(4)),
+    )
+
+
+def test_match_true_deep():
+    assert data.match(MockDataclassB(MockDataclassA(Any)))(
+        MockDataclassB(MockDataclassA(4)),
+    )
+
+
+def test_match_true_shallow():
+    assert data.match(MockDataclassB(Any))(MockDataclassB(MockDataclassA(4)))
