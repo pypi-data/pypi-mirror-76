67f287ceb19ca52f5e3812571269b214fe0cbb1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:00 +0000

    Add package lonny-pg-job/1.1/lonny_pg_job-1.1.tar.gz

---
+++ packages/lonny-pg-job/lonny_pg_job-1.1.tar.gz/lonny_pg_job-1.1/lonny_pg_job/serverless.py
@@ -0,0 +1,39 @@
+from time import sleep
+from os import getenv
+from importlib import import_module
+from datetime import datetime, timedelta
+from .logger import logger
+from .util import Handler, ProcessManager
+from multiprocessing import Process
+from logging import StreamHandler
+import sys
+
+logger.addHandler(StreamHandler())
+logger.setLevel(getenv("LOG_LEVEL", "INFO"))
+
+PROC_MONITOR_CYCLE = 0.5
+
+def _target(registry_str):
+    sys.path.insert(0, "")
+    handler = Handler()
+    handler.setup()
+    module, attr = registry_str.split(":")
+    registry = import_module(module).__getattribute__(attr)
+    while registry.consume() and handler.running:
+        pass
+
+def _mk_proc(registry_str):
+    return Process(
+        target = _target,
+        args = (registry_str,)
+    )
+
+def handler(event, context):
+    kwargs = event.get("kwargs", dict())
+    time_now = datetime.utcnow()
+    procs = [_mk_proc(kwargs["registry_str"]) for _ in range(kwargs.get("workers", 1))]
+    with ProcessManager(procs) as mgr:
+        while mgr.all_running():
+            if (datetime.utcnow() - time_now) > timedelta(seconds = kwargs["stop_after"]):
+                break
+            sleep(PROC_MONITOR_CYCLE)
