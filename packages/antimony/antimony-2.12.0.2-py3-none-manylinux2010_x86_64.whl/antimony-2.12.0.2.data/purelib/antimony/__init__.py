a742216eeac5cd0c94a41e6062e4f903884b81be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:06 +0000

    Add package antimony/2.12.0.2/antimony-2.12.0.2-py3-none-manylinux2010_x86_64.whl

---
+++ packages/antimony/antimony-2.12.0.2-py3-none-manylinux2010_x86_64.whl/antimony-2.12.0.2.data/purelib/antimony/__init__.py
@@ -0,0 +1,13 @@
+"""
+The libAntimony library, (c) 2007-2020 Lucian Smith and Herbert Sauro
+
+libAntimony is a library for Antimony, a human-readable,
+human-writeable model definition language.  For more information,
+see http://antimony.sf.net/
+"""
+
+from .antimony import *
+from pkg_resources import get_distribution
+
+__version__ = get_distribution('antimony').version
+
