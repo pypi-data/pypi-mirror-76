11101d4f314041ef8965ff56b2a275695c341f10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package graia-template/0.0.3/graia-template-0.0.3.tar.gz

---
+++ packages/graia-template/graia-template-0.0.3.tar.gz/graia-template-0.0.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['graia', 'graia.template']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['graia-application-mirai', 'regex>=2020.7.14,<2021.0.0']
+
+setup_kwargs = {
+    'name': 'graia-template',
+    'version': '0.0.3',
+    'description': '',
+    'long_description': None,
+    'author': 'GreyElaina',
+    'author_email': 'GreyElaina@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
