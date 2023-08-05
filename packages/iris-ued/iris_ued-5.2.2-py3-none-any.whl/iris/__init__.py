072debec7aa186cb5d7ff372f086d1859ab6f833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package iris-ued/5.2.2/iris_ued-5.2.2-py3-none-any.whl

---
+++ packages/iris-ued/iris_ued-5.2.2-py3-none-any.whl/iris/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = "Laurent P. René de Cotret"
+__email__ = "laurent.renedecotret@mail.mcgill.ca"
+__license__ = "MIT"
+__version__ = "5.2.2"
+
+from .raw import AbstractRawDataset, check_raw_bounds, open_raw
+from .dataset import DiffractionDataset, PowderDiffractionDataset
+from .meta import ExperimentalParameter
+from .plugins import install_plugin, load_plugin
+
+from . import plugins
