2e24447b3a5c68ba060d0f96a4e24bd1624d2548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package geneutils/0.0.4/geneutils-0.0.4.tar.gz

---
+++ packages/geneutils/geneutils-0.0.4.tar.gz/geneutils-0.0.4/README.md
@@ -0,0 +1,107 @@
+# geneutils
+
+[![PyPI version](https://badge.fury.io/py/geneutils.svg)](https://badge.fury.io/py/geneutils)
+![CI geneutils](https://github.com/samapriya/geneutils/workflows/CI%20geneutils/badge.svg)
+[![DOI](https://zenodo.org/badge/DOI/10.5281/zenodo.3975287.svg)](https://doi.org/10.5281/zenodo.3975287)
+
+This tool was originally inspired by the [work here](https://github.com/Gurdhhu/bioinf_scripts) for dealing with nucleotide sequences and more tools will be added as needed. This toolbox requires python 3 and can be installed.
+
+## Table of contents
+* [Installation](#installation)
+* [geneutils cli tools](#geneutils-cli-tools)
+    * [init](#init)
+    * [blasthit](#blasthit)
+
+
+## Installation
+This assumes that you have native python 3 & pip installed in your system, you can test this by going to the terminal (or windows command prompt) and trying
+
+```python``` and then ```pip list```
+
+If you get no errors and you have python 3 or higher you should be good to go. To install **geneutils** you can install using two methods
+
+```pip install geneutils```
+
+or you can also try
+
+```pip install geneutils --user```
+
+
+To upgrade to a new version from old you can try
+
+```
+pip install geneutils --upgrade
+```
+
+or
+
+```
+pip install geneutils --user --upgrade
+```
+
+or you can also try
+
+```
+git clone https://github.com/samapriya/geneutils.git
+cd geneutils
+python setup.py install
+```
+
+Though there is a conda installer for linux/mac for geneutils
+
+```conda install -c samapriya geneutils```
+
+The recommended way would be to use the conda environment/terminal and then do a ```pip install geneutils```
+
+Installation is an optional step; the application can be also run directly by executing pydrop.py script. The advantage of having it installed is being able to execute porg as any command line tool. I recommend installation within virtual environment. If you don't want to install, browse into the geneutils folder and try ```python geneutils.py -h``` to get to the same result.
+
+## geneutils cli tools
+This is a command line tool and it is designed to simply call the tools you need.
+
+![geneutils_main](https://user-images.githubusercontent.com/6677629/89606925-3f9cc780-d83f-11ea-9a05-6b1c9a31ff68.gif)
+
+### init
+Turns out there are benefits of registering for a NCBI account and to use your email address and your API key. Apart from NCBI having a way of contacting you, the API key raises the rate limit imposed on your queries. This is a recommended step and a user should not skip thought it is possible to use the blashit tool without the API key.
+*From the NCBI account page you can find this information*
+
+**E-utils users are allowed 3 requests/second without an API key. Create an API key to increase your e-utils limit to 10 requests/second......Only one API Key per user. Replacing or deleting will inactivate the current key. Use this key by passing it with api_key=API_KEY parameter.**
+
+Generate your API Key
+![ncbi_apikey](https://user-images.githubusercontent.com/6677629/89606628-67d7f680-d83e-11ea-9c43-328903dcd6b7.gif)
+
+The init tool saves your email address and API key to be saved in your local machine which can be used instead of typing out your email over and over again. The API key is a clear entry meaning you cannot see when you type in or paste your API key for safety.
+![ncbi_cred](https://user-images.githubusercontent.com/6677629/89147373-6c419e00-d524-11ea-8043-58f3e9699b5f.gif)
+
+### blasthit
+This script is intended for taxonomic annotation of **blast** results (blastn, tblastn, blastp or blastx) saved in **Hit Table CSV** format where GenBank accession numbers are in the 4th column. It uses **efetch** function from **Bio.Entrez** package to get information about accessions from **GenBank Nucleotide (Nuccore)** or **Protein** databases.
+The output is an annotated CSV file "*_annotated.csv" with the following columns added:
+
+* Record name
+* Species name
+* Full taxonomy
+* Reference
+* Date of update
+
+![geneutils_bhits](https://user-images.githubusercontent.com/6677629/89607253-0dd83080-d840-11ea-997b-9b69cbb4e8b4.gif)
+
+| arguments | description |
+| --- | --- |
+| path | Pathway to csv-formatted Hit Table file with blastn results. Positional argument |
+| db | For the output of **nucleotide blast** or **tblastn**, use <code>n</code>. For the output of **protein blast** or **blastx**, use <code>p</code>. Positional argument |
+| email | Your NCBI email. Optional argument |
+| -h, --help | Show help message and exit. Optional argument |
+
+
+## Changelog
+
+### 0.0.4
+  - Fixed issue with path basename
+  
+### 0.0.3
+  - Fixed issue with repeating email id for accession blocks
+  - Updated ReadMe to include **geneutils init** as first step.
+  - Fixed CSV write issue outside loop
+
+### 0.0.2
+  - Added credential tool to save NCBI email and API Key
+  - Minor fixes to overall functionality.
---
+++ packages/geneutils/geneutils-0.0.4.tar.gz/geneutils-0.0.4/geneutils/geneutils.py
@@ -0,0 +1,230 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__copyright__ = """
+
+MIT License
+
+Copyright (c) 2020 Samapriya Roy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+__license__ = "MIT License"
+
+import csv
+import sys
+import os
+import json
+import getpass
+import argparse
+from os.path import expanduser
+from Bio import Entrez
+
+accession_list = []
+row_list = []
+acession_annotations = []
+
+# Save default NCBI email and API Key
+def genauth():
+    try:
+        email_id = input("Enter your NCBI email address:  ")
+        api_key = getpass.getpass("Enter your NCBI API Key:  ")
+        if len(api_key) == 0:
+            api_key = "pass"
+    except Exception as e:
+        print(e)
+
+    data = {
+        "api_key": api_key.strip(),
+        "email": email_id.strip(),
+    }
+
+    with open(os.path.join(expanduser("~"), "geneauth.json"), "w") as outfile:
+        json.dump(data, outfile)
+    if len(data["api_key"]) > 0 and len(data["email"]) > 0:
+        print("Credentials created with : email id & api key")
+
+
+def genauth_from_parser(args):
+    genauth()
+
+
+# Merge lists together
+def merge(lst1, lst2):
+    return [a + b for (a, b) in zip(lst1, lst2)]
+
+
+# Get annotate hit table
+def accession_parse(path, db, email):
+    """This script is intended for annotation of blast results saved in Hit Table CSV format.
+       The output is an annotated CSV file "*_annotated.csv" with the following columns added:
+       Record name, Species, Date of update, Reference, Full taxonomy
+
+    [description]
+
+    Arguments:
+        path {[type]} -- [Pathway to csv-formatted Hit Table file with blast results]
+        db {[type]} -- ["For the output of nucleotide blast or tblastn, use 'n'. "
+                         "For the output of protein blast or blastx, use 'p'."]
+        email {[type]} -- [NCBI email]
+    """
+
+    # get existing credentials
+    if os.path.exists(os.path.join(expanduser("~"), "geneauth.json")):
+        with open(os.path.join(expanduser("~"), "geneauth.json")) as json_file:
+            cred = json.load(json_file)
+        email_id = cred["email"].strip()
+        api_key = cred["api_key"].strip()
+    else:
+        email_id = None
+        api_key = None
+
+    n = 200
+    open(os.path.basename(path).split(".")[0] + "_annotated.csv", "w")
+    with open(path, encoding="utf-8-sig") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            accession_list.append(row[1])
+            row_list.append(row)
+    accession_chunk = [
+        accession_list[i * n : (i + 1) * n]
+        for i in range((len(accession_list) + n - 1) // n)
+    ]
+    i = 1
+    if email is not None:
+        Entrez.email = email
+    elif email is None and email_id is not None:
+        Entrez.email = email_id
+    if api_key != "pass" and api_key is not None:
+        Entrez.api_key = api_key
+    if email is None and email_id is not None and api_key != "pass":
+        print("Using both email and api_key")
+    elif email is not None:
+        print("Using email id {}".format(email))
+    elif email is None and email_id is None:
+        sys.exit("Either pass an email or try geneutils init")
+    for subsets in accession_chunk:
+        idlist = ",".join(subsets)
+        dbdict = {"n": "nuccore", "p": "protein"}
+        handle = Entrez.efetch(db=dbdict[db], id=idlist, retmode="xml")
+        data = Entrez.parse(handle)
+        for items in data:
+            try:
+                version = items["GBSeq_accession-version"]
+                definition = items["GBSeq_definition"]
+                organism = items["GBSeq_organism"]
+                taxonomy = items["GBSeq_taxonomy"]
+                date = items["GBSeq_update-date"]
+                ref = ", ".join(items["GBSeq_references"][0]["GBReference_authors"])
+                acession_annotations.append([definition, organism, taxonomy, ref, date])
+            except Exception as e:
+                version = items["GBSeq_accession-version"]
+                definition = items["GBSeq_definition"]
+                organism = items["GBSeq_organism"]
+                taxonomy = items["GBSeq_taxonomy"]
+                date = items["GBSeq_update-date"]
+                ref = "No references found"
+                acession_annotations.append([definition, organism, taxonomy, ref, date])
+            print("Processed a total of {} records".format(i), end="\r")
+            i = i + 1
+        handle.close()
+    for annotated_rows in merge(row_list, acession_annotations):
+        with open(path.split(".")[0] + "_annotated.csv", "a") as csvfile:
+            writer = csv.writer(csvfile, delimiter=",", lineterminator="\n")
+            writer.writerow(annotated_rows)
+        csvfile.close()
+    print("")
+    print("Annotated files written to {}".format(path.split(".")[0] + "_annotated.csv"))
+
+
+def accession_parse_from_parser(args):
+    accession_parse(path=args.path, db=args.db, email=args.email)
+
+
+def main(args=None):
+    parser = argparse.ArgumentParser(
+        description="CLI and utilities for Genetic analysis and database interface"
+    )
+    subparsers = parser.add_subparsers()
+
+    parser_genauth = subparsers.add_parser(
+        "init", help="Setup NCBI email and API key as credentials"
+    )
+    parser_genauth.set_defaults(func=genauth_from_parser)
+
+    parser_accession_parse = subparsers.add_parser(
+        "blasthit", help="Annotation of blast results saved in Hit Table CSV format"
+    )
+    parser_accession_parse.add_argument(
+        "--path", help="Pathway to csv-formatted Hit Table file with blast results"
+    )
+    parser_accession_parse.add_argument(
+        "--db",
+        choices=["n", "p"],
+        help="For the output of nucleotide blast or tblastn, use 'n'. "
+        "For the output of protein blast or blastx, use 'p'.",
+    )
+    optional_named = parser_accession_parse.add_argument_group(
+        "Optional named arguments"
+    )
+    optional_named.add_argument(
+        "--email", help="NCBI email else from credentials", default=None
+    )
+    parser_accession_parse.set_defaults(func=accession_parse_from_parser)
+
+    args = parser.parse_args()
+
+    try:
+        func = args.func
+    except AttributeError:
+        parser.error("too few arguments")
+    func(args)
+
+
+if __name__ == "__main__":
+    main()
+
+
+# liners
+
+# insert -
+
+# chimney liner- utility chimneys
+
+# basement- no liner/ insert
+
+# liner
+
+
+# each
+
+
+# 3 liners
+# direct vent gas fireplace (power to each one ) and gas to eac one ()
+
+# wood fill liner
+
+# 124 sq inch
+
+# no need to parge insert
+
+
+
+
