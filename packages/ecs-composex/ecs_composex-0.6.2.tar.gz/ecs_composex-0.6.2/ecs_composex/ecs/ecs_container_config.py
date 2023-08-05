ced8a3eccca2c305f61784423508548420cd60ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:05 +0000

    Add package ecs-composex/0.6.2/ecs_composex-0.6.2.tar.gz

---
+++ packages/ecs-composex/ecs_composex-0.6.2.tar.gz/ecs_composex-0.6.2/ecs_composex/ecs/ecs_container_config.py
@@ -0,0 +1,92 @@
+﻿#  -*- coding: utf-8 -*-
+#   ECS ComposeX <https://github.com/lambda-my-aws/ecs_composex>
+#   Copyright (C) 2020  John Mille <john@lambda-my-aws.io>
+#  #
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#  #
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#  #
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Module of functions to help with container definition
+"""
+
+
+from troposphere import Ref, GetAtt, ImportValue, Sub
+from troposphere.ecs import ContainerDefinition, Environment
+
+from ecs_composex.common import LOG
+
+
+def import_env_variables(environment):
+    """
+    Function to import Docker compose env variables into ECS Env Variables
+
+    :param environment: Environment variables as defined on the ecs_service definition
+    :type environment: dict
+    :return: list of Environment
+    :rtype: list<troposphere.ecs.Environment>
+    """
+    env_vars = []
+    for key in environment:
+        if not isinstance(environment[key], str):
+            env_vars.append(Environment(Name=key, Value=str(environment[key])))
+        else:
+            env_vars.append(Environment(Name=key, Value=environment[key]))
+    return env_vars
+
+
+def extend_container_secrets(container, secret):
+    """
+    Function to add secrets to a Container definition
+
+    :param container: container definition
+    :type container: troposphere.ecs.ContainerDefinition
+    :param secret: secret to add
+    :type secret: troposphere.ecs.Secret
+    """
+    if hasattr(container, "Secrets"):
+        secrets = getattr(container, "Secrets")
+        if secrets:
+            uniq = [secret.Name for secret in secrets]
+            if secret.Name not in uniq:
+                secrets.append(secret)
+        else:
+            setattr(container, "Secrets", [secret])
+    else:
+        setattr(container, "Secrets", [secret])
+
+
+def extend_container_envvars(container, env_vars):
+    ignored_containers = ["xray-daemon", "envoy"]
+    if (
+        isinstance(container, ContainerDefinition)
+        and not isinstance(container.Name, (Ref, Sub, GetAtt, ImportValue))
+        and container.Name in ignored_containers
+    ):
+        LOG.debug(f"Ignoring AWS Container {container.Name}")
+        return
+    environment = (
+        getattr(container, "Environment")
+        if hasattr(container, "Environment")
+        and not isinstance(getattr(container, "Environment"), Ref)
+        else []
+    )
+    if environment:
+        existing = [var.Name for var in environment]
+        for var in env_vars:
+            if var.Name not in existing:
+                LOG.debug(f"Adding {var.Name} to {existing}")
+                environment.append(var)
+
+    else:
+        setattr(container, "Environment", env_vars)
+    LOG.debug(f"{container.Name}, {[env.Name for env in environment]}")
