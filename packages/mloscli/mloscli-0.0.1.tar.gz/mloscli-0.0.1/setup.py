c7869375f036305f6712b28b79a6d3b14f98a823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package mloscli/0.0.1/mloscli-0.0.1.tar.gz

---
+++ packages/mloscli/mloscli-0.0.1.tar.gz/mloscli-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='mloscli',
+    version='0.0.1',    
+    description='mlOScli Package',
+    author='Padma Polash Paul',
+    author_email='ppaul@dpami.com',    
+    license='BSD 2-clause',
+    packages=['mloscli'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)    
