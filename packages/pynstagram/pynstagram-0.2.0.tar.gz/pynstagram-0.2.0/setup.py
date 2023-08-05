e9c2c819b35dc5dc23be5da926a209f2be74d601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:23 +0000

    Add package pynstagram/0.2.0/pynstagram-0.2.0.tar.gz

---
+++ packages/pynstagram/pynstagram-0.2.0.tar.gz/pynstagram-0.2.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pynstagram',
+    version='0.2.0',
+    packages=['pynstagram'],
+    url='https://github.com/OpenJarbas/pynstagram',
+    license='Apache2',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    install_requires=["instagram_scraper"],
+    description='simple instagram api'
+)
