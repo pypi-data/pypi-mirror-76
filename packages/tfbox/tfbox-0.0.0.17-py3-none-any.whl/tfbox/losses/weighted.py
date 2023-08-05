34d82566e969c90adf25cb266dc6ad8c33d49c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package tfbox/0.0.0.17/tfbox-0.0.0.17-py3-none-any.whl

---
+++ packages/tfbox/tfbox-0.0.0.17-py3-none-any.whl/tfbox/losses/weighted.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import tensorflow.keras.losses as losses
+import tensorflow.keras.backend as K
+
+EPS=1e-8
+
+#
+# CUSTOM LOSS FUNCTIIONS
+#
+def weighted_categorical_crossentropy(weights=None,**kwargs):
+    """ weighted_categorical_crossentropy
+        Args:
+            * weights<ktensor|nparray|list>: crossentropy weights
+        Returns:
+            * weighted categorical crossentropy function
+    """
+    cce=losses.CategoricalCrossentropy(**kwargs)
+    if weights is None:
+        print('WARNING: WCCE called without weights. Defaulting to CCE')
+        return cce
+    else:
+        print('WCCE:',weights,kwargs)
+        if isinstance(weights,list) or isinstance(np.ndarray):
+            weights=[1/(w+EPS) for w in weights]
+            weights=K.variable(weights)
+        def _loss(target,output):
+            unweighted_losses=cce(target,output)
+            pixel_weights=tf.reduce_sum(weights*target, axis=-1)
+            weighted_losses=unweighted_losses*pixel_weights
+            return tf.reduce_mean(weighted_losses)
+    return _loss
+
