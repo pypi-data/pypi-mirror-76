60728e1e7d44f0319aceda199a7f0e05e011b1a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:15 +0000

    Add package Av1an/1.12.8/Av1an-1.12.8.tar.gz

---
+++ packages/Av1an/Av1an-1.12.8.tar.gz/Av1an-1.12.8/Av1an/arg_parse.py
@@ -0,0 +1,126 @@
+#!/bin/env python
+import argparse
+from pathlib import Path
+
+class Args(object):
+
+    def __init__(self, initial_data):
+        # Input/Output/Temp
+        self.input = None
+        self.temp = None
+        self.output_file = None
+
+        # Splitting
+        self.scenes = None
+        self.split_method = None
+        self.extra_split = None
+        self.min_scene_len = None
+
+        # PySceneDetect split
+        self.threshold = None
+
+        # AOM Keyframe split
+        self.reuse_first_pass = None
+
+        # Encoding
+        self.passes = None
+        self.video_params = None
+        self.encoder = None
+        self.workers = None
+        self.config = None
+
+        self.video_params = None
+
+        # FFmpeg params
+        self.ffmpeg_pipe = None
+        self.ffmpeg = None
+        self.audio_params = None
+        self.pix_format = None
+
+        # Misc
+        self.logging = None
+        self.resume = None
+        self.no_check = None
+        self.keep = None
+
+        # Vmaf
+        self.vmaf = None
+        self.vmaf_path = None
+        self.vmaf_res = None
+
+        # Target Vmaf
+        self.vmaf_target = None
+        self.vmaf_steps = None
+        self.min_q = None
+        self.max_q = None
+        self.vmaf_plots = None
+        self.vmaf_rate = None
+        self.n_threads = None
+
+        # VVC
+        self.vvc_conf = None
+        self.video_dimensions = (None, None)
+        self.video_framerate = None
+        for key in initial_data:
+            setattr(self, key, initial_data[key])
+
+def arg_parsing():
+    """Command line parsing"""
+    parser = argparse.ArgumentParser()
+
+    # Input/Output/Temp
+    parser.add_argument('--input', '-i', nargs='+', type=Path, help='Input File')
+    parser.add_argument('--temp', type=Path, default=Path('.temp'), help='Set temp folder path')
+    parser.add_argument('--output_file', '-o', type=Path, default=None, help='Specify output file')
+
+    # Splitting
+    parser.add_argument('--scenes', '-s', type=str, default=None, help='File location for scenes')
+    parser.add_argument('--split_method', type=str, default='pyscene', help='Specify splitting method',
+                        choices=['pyscene', 'aom_keyframes'])
+    parser.add_argument('--extra_split', '-xs', type=int, default=0, help='Number of frames after which make split')
+    parser.add_argument('--min_scene_len', type=int, default=None, help='Minimum number of frames in a split')
+
+    # PySceneDetect split
+    parser.add_argument('--threshold', '-tr', type=float, default=50, help='PySceneDetect Threshold')
+
+    # AOM Keyframe split
+    parser.add_argument('--reuse_first_pass', help='Reuse the first pass from aom_keyframes split on the chunks', action='store_true')
+
+    # Encoding
+    parser.add_argument('--passes', '-p', type=int, default=None, help='Specify encoding passes', choices=[1, 2])
+    parser.add_argument('--video_params', '-v', type=str, default=None, help='encoding settings')
+    parser.add_argument('--encoder', '-enc', type=str, default='aom', help='Choosing encoder',
+                        choices=['aom', 'svt_av1', 'rav1e', 'vpx','x265', 'x264', 'vvc'])
+    parser.add_argument('--workers', '-w', type=int, default=0, help='Number of workers')
+    parser.add_argument('-cfg', '--config', type=Path, help='Parameters file. Save/Read: '
+                                                            'Video, Audio, Encoder, FFmpeg parameteres')
+
+    # FFmpeg params
+    parser.add_argument('--ffmpeg', '-ff', type=str, default='', help='FFmpeg commands')
+    parser.add_argument('--audio_params', '-a', type=str, default='-c:a copy', help='FFmpeg audio settings')
+    parser.add_argument('--pix_format', '-fmt', type=str, default='yuv420p', help='FFmpeg pixel format')
+
+    # Misc
+    parser.add_argument('--logging', '-log', type=str, default=None, help='Enable logging')
+    parser.add_argument('--resume', '-r', help='Resuming previous session', action='store_true')
+    parser.add_argument('--no_check', '-n', help='Do not check encodings', action='store_true')
+    parser.add_argument('--keep', help='Keep temporally folder after encode', action='store_true')
+
+    # Vmaf
+    parser.add_argument('--vmaf', help='Calculating vmaf after encode', action='store_true')
+    parser.add_argument('--vmaf_path', type=Path, default=None, help='Path to vmaf models')
+    parser.add_argument('--vmaf_res', type=str, default="1920x1080", help='Resolution used in vmaf calculation')
+
+    # Target Vmaf
+    parser.add_argument('--vmaf_target', type=float, help='Value of Vmaf to target')
+    parser.add_argument('--vmaf_steps', type=int, default=4, help='Steps between min and max qp for target vmaf')
+    parser.add_argument('--min_q', type=int, default=None, help='Min q for target vmaf')
+    parser.add_argument('--max_q', type=int, default=None, help='Max q for target vmaf')
+    parser.add_argument('--vmaf_plots', help='Make plots of probes in temp folder', action='store_true')
+    parser.add_argument('--vmaf_rate', type=int, default=4, help='Framerate for probes, 0 - original')
+    parser.add_argument('--n_threads', type=int, default=None, help='Threads for vmaf calculation')
+
+    # VVC
+    parser.add_argument('--vvc_conf', type=Path, default=None, help='Path to VVC confing file')
+
+    return Args(vars(parser.parse_args()))
---
+++ packages/Av1an/Av1an-1.12.8.tar.gz/Av1an-1.12.8/Av1an/setup.py
@@ -0,0 +1,153 @@
+#!/bin/env python
+
+import os
+import sys
+import shutil
+import atexit
+from distutils.spawn import find_executable
+from pathlib import Path
+
+from psutil import virtual_memory
+
+from .utils import terminate
+from .compose import get_default_params_for_encoder
+
+
+def set_vmaf(args):
+
+    if args.vmaf_path:
+        if not Path(args.vmaf_path).exists():
+            print(f'No such model: {Path(args.vmaf_path).as_posix()}')
+            terminate()
+
+    if args.vmaf_steps < 4:
+        print('Target vmaf require more than 3 probes/steps')
+        terminate()
+
+
+    defaul_ranges = {'svt_av1': (20, 40), 'rav1e': (70, 150), 'aom': (25, 50), 'vpx': (25, 50),'x265': (20, 40), 'x264': (20, 35), 'vvc': (20, 50)}
+
+    if args.min_q is None or args.max_q is None:
+        args.min_q, args.max_q = defaul_ranges.get(args.encoder)
+
+
+def check_exes(args):
+    if not find_executable('ffmpeg'):
+        print('No ffmpeg')
+        terminate()
+    encoders = {'svt_av1': 'SvtAv1EncApp', 'rav1e': 'rav1e', 'aom': 'aomenc', 'vpx': 'vpxenc','x265': 'x265', 'x264': 'x264', 'vvc': 'vvc_encoder'}
+
+
+    # Check if encoder executable is reachable
+    if args.encoder in encoders:
+        enc = encoders.get(args.encoder)
+
+        if not find_executable(enc):
+            print(f'Encoder {enc} not found')
+            terminate()
+    else:
+        print(f'Not valid encoder {args.encoder}\nValid encoders: "aom rav1e", "svt_av1", "vpx", "x265", "x264" ')
+        terminate()
+
+    if args.encoder == 'vvc':
+        if not find_executable('vvc_concat'):
+            print('vvc concatenation executabe "vvc_concat" not found')
+            terminate()
+
+
+def startup_check(args):
+
+    encoders_default_passes = {'svt_av1': 1, 'rav1e': 1, 'aom': 2, 'vpx': 2,'x265': 1, 'x264': 1, 'vvc':1 }
+
+
+    if sys.version_info < (3, 6):
+        print('Python 3.6+ required')
+        sys.exit()
+    if sys.platform == 'linux':
+        def restore_term():
+            os.system("stty sane")
+        atexit.register(restore_term)
+
+    check_exes(args)
+
+    set_vmaf(args)
+
+    if args.reuse_first_pass and args.encoder != 'aom' and args.split_method != 'aom_keyframes':
+        print('Reusing the first pass is only supported with the aom encoder and aom_keyframes split method.')
+        terminate()
+
+
+    if args.encoder == 'vvc' and not args.vvc_conf:
+        print('Conf file for vvc required')
+        terminate()
+
+    # No check because vvc
+    if args.encoder == 'vvc':
+        args.no_check = True
+
+    if args.passes is None:
+        args.passes = encoders_default_passes.get(args.encoder)
+
+
+    if args.video_params is None and args.encoder == 'vvc':
+        print('VVC require:\n',
+        ' -wdt X - video width\n',
+        ' -hgt X - video height\n',
+        ' -fr X  - framerate\n',
+        ' -q X   - quantizer\n',
+        'Example: -wdt 640 -hgt 360 -fr 23.98 -q 30 '
+        )
+        terminate()
+
+    if args.video_params is None:
+        args.video_params = get_default_params_for_encoder(args.encoder)
+
+
+
+    args.pix_format = f'-strict -1 -pix_fmt {args.pix_format}'
+    args.ffmpeg_pipe = f' {args.ffmpeg} {args.pix_format} -f yuv4mpegpipe - |'
+
+
+def determine_resources(encoder, workers):
+    """Returns number of workers that machine can handle with selected encoder."""
+
+    # If set by user, skip
+    if workers != 0:
+        return workers
+
+    cpu = os.cpu_count()
+    ram = round(virtual_memory().total / 2 ** 30)
+
+    if encoder in ('aom', 'rav1e', 'vpx'):
+        workers =  round(min(cpu / 2, ram / 1.5))
+
+    elif encoder in ('svt_av1', 'x265', 'x264'):
+        workers =  round(min(cpu, ram)) // 8
+
+    elif encoder in ('vvc'):
+        workers = round(min(cpu, ram))
+
+    # fix if workers round up to 0
+    if workers == 0:
+        workers = 1
+
+    return workers
+
+
+def setup(temp: Path, resume):
+    """Creating temporally folders when needed."""
+    # Make temporal directories, and remove them if already presented
+    if not resume:
+        if temp.is_dir():
+            shutil.rmtree(temp)
+
+    (temp / 'split').mkdir(parents=True, exist_ok=True)
+    (temp / 'encode').mkdir(exist_ok=True)
+
+
+def outputs_filenames(inp: Path, out:Path, encoder):
+    suffix = '.mkv'
+    if out:
+        return out.with_suffix(suffix)
+    else:
+        return Path(f'{inp.stem}_av1{suffix}')
