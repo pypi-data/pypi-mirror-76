37dcdae2e35cda21f945491428cac31cc7b531c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:11 +0000

    Add package alacrity/1.3.3/alacrity-1.3.3.tar.gz

---
+++ packages/alacrity/alacrity-1.3.3.tar.gz/alacrity-1.3.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as file_obj:
+    long_description = file_obj.read()
+
+setuptools.setup(
+    name="alacrity",
+    version="1.3.3",
+    author="Vishnuvardhan Kumar",
+    author_email="vishnukumar1997@gmail.com",
+    description="Quickstart your Python development with CLI package "
+                "templating",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=['clint'],
+    entry_points={
+          'console_scripts': [
+              'alacrity = alacrity.core:main'
+          ]
+      },
+    include_package_data=True,
+    classifiers=(
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
