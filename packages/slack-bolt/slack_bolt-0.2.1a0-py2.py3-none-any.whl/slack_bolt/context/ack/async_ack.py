921495bfabea827d0e35fbf5c08462ffd1fd5c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:07 +0000

    Add package slack-bolt/0.2.1a0/slack_bolt-0.2.1a0-py2.py3-none-any.whl

---
+++ packages/slack-bolt/slack_bolt-0.2.1a0-py2.py3-none-any.whl/slack_bolt/context/ack/async_ack.py
@@ -0,0 +1,19 @@
+from typing import List, Optional, Union
+
+from slack_bolt.context.ack.internals import _set_response
+from slack_bolt.response.response import BoltResponse
+from slack_sdk.models.blocks import Block
+
+
+class AsyncAck:
+    response: Optional[BoltResponse]
+
+    def __init__(self):
+        self.response: Optional[BoltResponse] = None
+
+    async def __call__(
+        self,
+        text_or_whole_response: Union[str, dict] = "",
+        blocks: Optional[List[Union[dict, Block]]] = None,
+    ) -> BoltResponse:
+        return _set_response(self, text_or_whole_response, blocks)
