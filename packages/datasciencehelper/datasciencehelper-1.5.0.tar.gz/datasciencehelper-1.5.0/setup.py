bcb8216a45405f6cad824523a8f8ca7cddb29b92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package datasciencehelper/1.5.0/datasciencehelper-1.5.0.tar.gz

---
+++ packages/datasciencehelper/datasciencehelper-1.5.0.tar.gz/datasciencehelper-1.5.0/setup.py
@@ -0,0 +1,12 @@
+
+
+
+from setuptools import setup
+
+setup(
+      name = "datasciencehelper",
+      version = "1.5.0",
+      description = "A helper library that saves time.",
+      py_modules = ["DataScienceHelper"],
+      package_dir = {"": "src"},
+      )
