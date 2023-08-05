1c63d0fc001b742bae8f535fd072b29a5acdd301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:51 +0000

    Add package zxbasic/1.12.0/zxbasic-1.12.0.tar.gz

---
+++ packages/zxbasic/zxbasic-1.12.0.tar.gz/zxbasic-1.12.0/tests/symbols/test_symbolSENTENCE.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from unittest import TestCase
+
+import symbols
+
+
+class TestSymbolSENTENCE(TestCase):
+    def setUp(self):
+        self.TOKEN = 'TOKEN'
+
+    def test_args(self):
+        # Must allow None args (which will be ignored)
+        s = symbols.SENTENCE(self.TOKEN, None, None, symbols.SENTENCE(self.TOKEN), None)
+        self.assertEqual(len(s.args), 1)
+
+    def test_args_fail(self):
+        # Fails for non symbol args
+        self.assertRaises(AssertionError, symbols.SENTENCE, self.TOKEN, 'blah')
+
+    def test_token(self):
+        s = symbols.SENTENCE(self.TOKEN)
+        self.assertEqual(s.token, self.TOKEN)
