17c496786751b9b8414f4942dde56cf2317c6dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:05 +0000

    Add package common-ui-components/2.18.6/common_ui_components-2.18.6-py3-none-any.whl

---
+++ packages/common-ui-components/common_ui_components-2.18.6-py3-none-any.whl/commonuicomponents/__init__.py
@@ -0,0 +1,8 @@
+from .appbuilder import AppBuilder
+from .applauncher import AppLauncher
+from .basetab import BaseTab
+from .basetabloader import BaseTabLoader
+from .json import Config, Storage
+from .periodicalafter import PeriodicalAfter
+from .staticutils import StaticUtils
+from .version import __version__
