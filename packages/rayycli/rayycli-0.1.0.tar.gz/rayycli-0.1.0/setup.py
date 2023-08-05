10ee999b8ce245d5e2f2f77b711e280275a98a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package rayycli/0.1.0/rayycli-0.1.0.tar.gz

---
+++ packages/rayycli/rayycli-0.1.0.tar.gz/rayycli-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rayycli', 'rayycli.ractf', 'rayycli.ractf.helpers', 'rayycli.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['racli = rayycli.racli:cli']}
+
+setup_kwargs = {
+    'name': 'rayycli',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jeremiah Boby',
+    'author_email': 'mail@jeremiahboby.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
