e1a662fe1323ed9040c36d496b2f5c8873548193
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:14 +0000

    Add package lujing-distributions/0.1/lujing_distributions-0.1.tar.gz

---
+++ packages/lujing-distributions/lujing_distributions-0.1.tar.gz/lujing_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lujing_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['lujing_distributions'],
+      author = 'Lujing Yang',
+      author_email = 'lujing@me.com',
+      zip_safe=False)
