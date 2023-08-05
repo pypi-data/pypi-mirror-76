d82c1efe7fabf27bcc8500d19e0d39a950f5357d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:05 +0000

    Add package donjon-painter/0.9.5/donjon_painter-0.9.5-py3-none-any.whl

---
+++ packages/donjon-painter/donjon_painter-0.9.5-py3-none-any.whl/donjon_painter/painter.py
@@ -0,0 +1,101 @@
+import sys
+import argparse
+import time
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent.resolve()))
+import menu
+import themes
+import txtmap
+import imgmap
+import stretch
+
+
+# Handle arguments
+def getArgs(showHelp=0):
+    parser = argparse.ArgumentParser(
+        prog="donjon-painter",
+        description="A tool to create dungeon maps from TSV files \
+        (Donjon's Random Dungeon Generator)"
+    )
+    parser.add_argument(
+        "MAPFILE", help="specify TSV file to parse", nargs='?')
+    parser.add_argument(
+        "-t", "--tileset",
+        help="select the tileset folder to use (interactive mode if not)")
+    parser.add_argument(
+        "-m", "--measure",
+        help="get the time it takes to execute the script",
+        action="store_true")
+    parser.add_argument(
+        "-o", "--output",
+        help="declare name and/or location of map to save",
+        default=str(Path(Path.home(), 'Map.png')))
+    parser.add_argument(
+        "-p", "--pixels",
+        help="specify the size of your map tile assets (default 70)",
+        type=int,
+        default=70)
+    parser.add_argument(
+        "-r", "--randomise",
+        help="rotate the floor tile asset for some variation to the map",
+        action="store_true")
+    parser.add_argument(
+        "-s", "--savetiles",
+        help="generate a theme from a partial one (good for making themes)",
+        action="store_true")
+
+    if not showHelp:
+        return parser.parse_args()
+    else:
+        return parser.print_help()
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    args = getArgs()
+    args = stretch.expandargs(args)
+
+    # Execution Redux (Interactive Mode vs. Single Command Mode)
+    if args.MAPFILE is None:
+        menu.mainmenu(args)
+    else:
+        if args.tileset is None:
+            themes.printThemes()
+            args.tileset = themes.selTheme(args.tileset)
+        if args.savetiles:
+            start = time.time()
+
+            tmpRes = themes.canGenerate(args.tileset, True)
+            tmpRes = themes.generateTheme(tmpRes)
+            if themes.writeTheme(args.tileset, tmpRes) is False:
+                print("Theme cannot be saved - insufficient resources.")
+            else:
+                if args.measure:
+                    end = time.time()
+                    print("Theme generation done in", end - start, "seconds.")
+        if args.measure:
+            start = time.time()
+
+        print("Attempting map generation...")
+
+        tmpTheme = themes.canGenerate(args.tileset, True)
+        tmpTheme = themes.generateTheme(tmpTheme)
+
+        tmpMap = txtmap.readMap(args.MAPFILE)
+        tmpMap = txtmap.parseMap(tmpMap)
+        tmpMap = imgmap.generateMap(args, tmpTheme, tmpMap)
+
+        if imgmap.writeMap(args, tmpMap) is False:
+            print("Map cannot be saved - insufficient resources.")
+        else:
+            if args.measure:
+                end = time.time()
+                print("Map generation done in", end - start, "seconds.")
+            print("Finished!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/donjon-painter/donjon_painter-0.9.5-py3-none-any.whl/donjon_painter/themes.py
@@ -0,0 +1,228 @@
+from pathlib import Path
+from PIL import Image
+import copy
+
+
+# Retrieve list of theme directories
+def getThemes():
+    themeList = []
+
+    imgLoc = Path(__file__).parent.resolve()
+    imgLoc = Path(imgLoc, 'themes')
+
+    for item in imgLoc.iterdir():
+            if item.is_dir():
+                themeList.append(item)
+
+    themeList = sorted(themeList)
+    return themeList
+
+
+# Display all builtin themes
+def printThemes():
+    curList = getThemes()
+    print("===Theme List===")
+    for key, item in enumerate(curList):
+        themeName = item.parts
+        print("[" + str(key) + "]: " + themeName[-1])
+
+
+# Return desired theme folder
+def selTheme(curTile):
+    themes = getThemes()
+    options = range(0, len(themes))
+    themeIndex = list(str(item) for item in options)
+    selection = input()
+
+    if selection in themeIndex:
+        curTile = themes[int(selection)]
+    else:
+        curTile = False
+    return curTile
+
+
+# ==================== Theme Generation Functions ====================
+resources = {
+    'floorAssets': {
+        'floor':                '0-Floor'
+    },
+    'spaceAssets': {
+        'dungeonSpace':         '1-Space'
+    },
+    'wallAssets': {
+        'wallTop':              '2-T_Wall',
+        'wallBottom':           '2-B_Wall',
+        'wallLeft':             '2-L_Wall',
+        'wallRight':            '2-R_Wall'
+    },
+    'inCorners': {
+        'topLeftCornerI':       '3-TL_Corner_I',
+        'bottomRightCornerI':   '3-BR_Corner_I',
+        'bottomLeftCornerI':    '3-BL_Corner_I',
+        'topRightCornerI':      '3-TR_Corner_I'
+    },
+    'outCorners': {
+        'topLeftCornerO':       '4-TL_Corner_O',
+        'bottomRightCornerO':   '4-BR_Corner_O',
+        'bottomLeftCornerO':    '4-BL_Corner_O',
+        'topRightCornerO':      '4-TR_Corner_O'
+    },
+    'doorAssets': {
+        'doorTop':              '5-T_Door',
+        'doorBottom':           '5-B_Door',
+        'doorLeft':             '5-L_Door',
+        'doorRight':            '5-R_Door'
+    },
+    'doorSAssets': {
+        'doorSecretTop':        '6-T_Door_Secret',
+        'doorSecretBottom':     '6-B_Door_Secret',
+        'doorSecretLeft':       '6-L_Door_Secret',
+        'doorSecretRight':      '6-R_Door_Secret'
+    },
+    'doorPAssets': {
+        'doorPortTop':          '7-T_Door_Portcullis',
+        'doorPortBottom':       '7-B_Door_Portcullis',
+        'doorPortLeft':         '7-L_Door_Portcullis',
+        'doorPortRight':        '7-R_Door_Portcullis'
+    },
+    'stairUAssets': {
+        'topStairU':            '8-T_Stair_U',
+        'bottomStairU':         '8-B_Stair_U',
+        'leftStairU':           '8-L_Stair_U',
+        'rightStairU':          '8-R_Stair_U'
+    },
+    'stairUUAssets': {
+        'topStairUU':           '9-T_Stair_UU',
+        'bottomStairUU':        '9-B_Stair_UU',
+        'leftStairUU':          '9-L_Stair_UU',
+        'rightStairUU':         '9-R_Stair_UU'
+    },
+    'stairDAssets': {
+        'topStairD':            '10-T_Stair_D',
+        'bottomStairD':         '10-B_Stair_D',
+        'leftStairD':           '10-L_Stair_D',
+        'rightStairD':          '10-R_Stair_D'
+    },
+    'stairDDAssets': {
+        'topStairDD':           '11-T_Stair_DD',
+        'bottomStairDD':        '11-B_Stair_DD',
+        'leftStairDD':          '11-L_Stair_DD',
+        'rightStairDD':         '11-R_Stair_DD'
+    }
+}
+
+
+# Check if resource generation is possible
+def canGenerate(themePath, openImages=False):
+    tmpRes = copy.deepcopy(resources)
+
+    if Path(themePath).is_dir:
+        sufficient = []
+        fileTypes = ['.png', '.jpg', '.jpeg']
+        # Different asset types
+        for superKey, val in tmpRes.items():
+            # Asset type members
+            state = False
+            for assetKey, fileName in val.items():
+                for suffix in fileTypes:
+                    tmpName = fileName + suffix
+                    if Path(themePath, tmpName).is_file():
+                        if openImages:
+                            tmpRes[superKey][assetKey] = Image.open(
+                                Path(themePath, tmpName)
+                            )
+                        state = True
+            sufficient.append(state)
+
+        if openImages:
+            return [all(sufficient), tmpRes]
+        else:
+            return all(sufficient)
+    else:
+        return False
+
+
+# Supply a fully made theme if possible
+def generateTheme(tmpRes):
+    if tmpRes[0] is not False:
+        # Dictionary with some open images
+        tmpRes = tmpRes[1]
+
+        def rotateNone(im):
+            return im
+
+        def rotateLeft(im):
+            tmpImage = im.transpose(Image.ROTATE_90)
+            tmpImage.format = im.format
+            return tmpImage
+
+        def rotateFlip(im):
+            tmpImage = im.transpose(Image.ROTATE_180)
+            tmpImage.format = im.format
+            return tmpImage
+
+        def rotateRight(im):
+            tmpImage = im.transpose(Image.ROTATE_270)
+            tmpImage.format = im.format
+            return tmpImage
+
+        rotate = {
+            0: {
+                0: rotateNone,
+                1: rotateFlip,
+                2: rotateLeft,
+                3: rotateRight
+            },
+            1: {
+                0: rotateFlip,
+                1: rotateNone,
+                2: rotateRight,
+                3: rotateLeft
+            },
+            2: {
+                0: rotateRight,
+                1: rotateLeft,
+                2: rotateNone,
+                3: rotateFlip
+            },
+            3: {
+                0: rotateLeft,
+                1: rotateRight,
+                2: rotateFlip,
+                3: rotateNone
+            }
+        }
+
+        # Different asset types -> asset permutations
+        for key, val in tmpRes.items():
+            # Get asset to transpose to others
+            imgIndex = ''
+            transposee = ''
+            for i, (assetKey, img) in enumerate(val.items()):
+                if isinstance(img, Image.Image):
+                    imgIndex = i
+                    transposee = img
+                    break
+            # Create other assets from found one
+            for i, (assetKey, img) in enumerate(val.items()):
+                if not isinstance(img, Image.Image):
+                    tmpRes[key][assetKey] = rotate[imgIndex][i](transposee)
+
+        return tmpRes
+    else:
+        return False
+
+
+# Create a theme at the specified path
+def writeTheme(themePath, themeRes):
+    if themeRes is not False:
+        for itemGroup, category in themeRes.items():
+            for item, asset in category.items():
+                imgName = resources[itemGroup][item]
+                imgName = imgName + '.' + asset.format.lower()
+                saveLoc = Path(themePath, imgName)
+                if not saveLoc.is_file():
+                    asset.save(saveLoc)
+        print("Theme saved!")
+    else:
+        return False
