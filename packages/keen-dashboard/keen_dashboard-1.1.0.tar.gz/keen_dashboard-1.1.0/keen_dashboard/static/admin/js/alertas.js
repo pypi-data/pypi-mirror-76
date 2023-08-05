19d110488629ab11f49ddf3a8b91913085366928
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:56 +0000

    Add package keen-dashboard/1.1.0/keen_dashboard-1.1.0.tar.gz

---
+++ packages/keen-dashboard/keen_dashboard-1.1.0.tar.gz/keen_dashboard-1.1.0/keen_dashboard/static/admin/js/alertas.js
@@ -0,0 +1,55 @@
+function alertaSucesso(mensagem)
+{
+    Swal.fire({
+      type: "success",
+      title: mensagem,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+}
+
+function alertaErro(mensagem)
+{
+    Swal.fire({
+      type: 'error',
+      title: 'Oops...',
+      text: mensagem,
+    });
+}
+
+function alertaGenerico(json)
+{
+    Swal.fire({
+      type: json.icon,
+      title: json.mensagem,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+}
+
+(function($) {
+    $('[data-confirm]').click( function() {
+        let element = $(this)
+        event.preventDefault();
+        let text = element.attr('data-confirm-text')
+        let icon = element.attr('data-confirm-icon')
+        let title = element.attr('data-confirm-title')
+        let button_text = element.attr('data-confirm-button-text')
+
+        Swal.fire({
+              title: title,
+              text: text,
+              type: icon,
+              showCancelButton: true,
+              confirmButtonColor: '#5398fb',
+              cancelButtonColor: '#fa6767',
+              confirmButtonText: button_text,
+              cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.value) {
+                location.href = element.attr('href')
+            }
+        });
+
+    });
+})(jQuery);
