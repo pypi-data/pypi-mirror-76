0a6d10ab1f3a28ac43243176b3a651f4e58941d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:36 +0000

    Add package twigs/1.0.49/twigs-1.0.49.tar.gz

---
+++ packages/twigs/twigs-1.0.49.tar.gz/twigs-1.0.49/twigs/gcp_cis_tool/gcp_cis.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+import os
+
+from . import check1 as check1
+from . import check2 as check2
+from . import check3 as check3
+from . import check4 as check4
+from . import check5 as check5
+from . import check6 as check6
+from . import check7 as check7
+
+def run_tests():
+    config_issues = []
+    config_issues.extend(check1.run_checks())
+    config_issues.extend(check2.run_checks())
+    config_issues.extend(check3.run_checks())
+    config_issues.extend(check4.run_checks())
+    config_issues.extend(check5.run_checks())
+    config_issues.extend(check6.run_checks())
+    #config_issues.extend(check7.run_checks())
+    #print config_issues
+    return config_issues
+
+# run_tests()
