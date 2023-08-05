18ff9bf090d79bd18f27c391bad179dee42f46b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:25 +0000

    Add package fa/0.1.9/fa-0.1.9.tar.gz

---
+++ packages/fa/fa-0.1.9.tar.gz/fa-0.1.9/fa/commands/add_offset_range.py
@@ -0,0 +1,32 @@
+from argparse import RawTextHelpFormatter
+from fa import utils
+
+
+DESCRIPTION = '''adds a python-range to resultset
+
+EXAMPLE:
+    result = [0, 0x200]
+    -> add-offset-range 0 4 8
+    result = [0, 4, 8, 0x200, 0x204, 0x208]
+'''
+
+
+def get_parser():
+    p = utils.ArgumentParserNoExit('add-offset-range',
+                                   description=DESCRIPTION,
+                                   formatter_class=RawTextHelpFormatter)
+    p.add_argument('start', type=int)
+    p.add_argument('end', type=int)
+    p.add_argument('step', type=int)
+    return p
+
+
+def add_offset_range(addresses, start, end, step):
+    for ea in addresses:
+        for i in range(start, end, step):
+            yield ea + i
+
+
+def run(segments, args, addresses, interpreter=None, **kwargs):
+    gen = add_offset_range(addresses, args.start, args.end, args.step)
+    return list(gen)
