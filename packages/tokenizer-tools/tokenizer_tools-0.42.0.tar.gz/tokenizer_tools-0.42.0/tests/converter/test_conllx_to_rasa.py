98a833389a67f268e647154789a0285f45bc5d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:26 +0000

    Add package tokenizer-tools/0.42.0/tokenizer_tools-0.42.0.tar.gz

---
+++ packages/tokenizer-tools/tokenizer_tools-0.42.0.tar.gz/tokenizer_tools-0.42.0/tests/converter/test_conllx_to_rasa.py
@@ -0,0 +1,20 @@
+import json
+
+import pytest
+from jsoncomparison import Compare, NO_DIFF
+
+from tokenizer_tools.converter.conllx_to_rasa import conllx_to_rasa
+
+
+def test_conllx_to_rasa(datadir, tmpdir):
+    expected_file = datadir / "output.json"
+    result_file = tmpdir / "output.json"
+
+    rs = conllx_to_rasa(datadir / "input.conllx", result_file)
+    print(rs)
+
+    with open(expected_file) as e_fd, open(result_file) as r_fd:
+        expected = json.load(e_fd)
+        result = json.load(r_fd)
+
+        assert Compare().check(result, expected) == NO_DIFF
