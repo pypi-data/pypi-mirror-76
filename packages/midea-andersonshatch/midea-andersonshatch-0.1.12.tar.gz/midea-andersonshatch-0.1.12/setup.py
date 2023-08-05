6607f49b3427c6954382f295122e8859535a3148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:09 +0000

    Add package midea-andersonshatch/0.1.12/midea-andersonshatch-0.1.12.tar.gz

---
+++ packages/midea-andersonshatch/midea-andersonshatch-0.1.12.tar.gz/midea-andersonshatch-0.1.12/setup.py
@@ -0,0 +1,22 @@
+#!/bin/python
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='midea-andersonshatch',  
+     version='0.1.12',
+     author="NeoAcheron, andersonshatch",
+     author_email="master@neoacheron.com, hello@andersonshatch.com",
+     description="A library to control Midea appliances via the cloud API",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/andersonshatch/midea-ac-lib",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
