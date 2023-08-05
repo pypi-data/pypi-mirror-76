7c5f4a779ced4bf455696cac39c359e1a8a8c84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:59 +0000

    Add package django-s3-file-field/0.0.11/django-s3-file-field-0.0.11.tar.gz

---
+++ packages/django-s3-file-field/django-s3-file-field-0.0.11.tar.gz/django-s3-file-field-0.0.11/s3_file_field/checks.py
@@ -0,0 +1,116 @@
+import io
+import json
+import logging
+from pathlib import PurePosixPath
+from time import time
+from typing import List, Optional
+
+from botocore.client import Config
+from botocore.exceptions import ConnectionError
+from django.core.checks import Error, register, Warning
+
+from . import constants
+from .boto import client_factory
+from .constants import S3FF_STORAGE_PROVIDER, StorageProvider
+
+
+# TODO: this should only add a handler when running the check command
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+
+
+TEST_OBJECT_KEY = str(constants.S3FF_UPLOAD_PREFIX / PurePosixPath('.s3-file-field-test-file'))
+
+
+W001 = Warning(
+    'Unable to determine the underlying storage provider. '
+    's3_file_field will use the filesystem for storing all files.',
+    id='s3_file_field.W001',
+)
+
+E001 = Error('Unable to connect to the specified storage bucket.', id='s3_file_field.E001')
+E002 = Error('Unable to put objects into the specified storage bucket.', id='s3_file_field.E002')
+E003 = Error('Unable to delete objects from the specified storage bucket.', id='s3_file_field.E003')
+E004 = Error(
+    'Unable to assume STS role for issuing temporary credentials.', id='s3_file_field.E004'
+)
+E005 = Error('Unable to determine the underlying storage provider.', id='s3_file_field.E005')
+
+
+@register(deploy=True)
+def check_supported_storage_provider(app_configs: Optional[List], **kwargs) -> List:
+    return [] if S3FF_STORAGE_PROVIDER != StorageProvider.UNSUPPORTED else [E005]
+
+
+@register()
+def determine_storage_provider(app_configs: Optional[List], **kwargs) -> List:
+    return [] if S3FF_STORAGE_PROVIDER != StorageProvider.UNSUPPORTED else [W001]
+
+
+@register()
+def test_bucket_access(app_configs: Optional[List], **kwargs) -> List:
+    # ignore bucket access checks when in development mode
+    if S3FF_STORAGE_PROVIDER == StorageProvider.UNSUPPORTED:
+        return []
+
+    # Use a short timeout to quickly fail on connection misconfigurations
+    client = client_factory('s3', config=Config(connect_timeout=5, retries={'max_attempts': 0}))
+
+    try:
+        client.upload_fileobj(io.BytesIO(), constants.S3FF_BUCKET, TEST_OBJECT_KEY)
+    except ConnectionError:
+        logger.exception('Failed to connect to storage bucket')
+        return [E001]
+    except Exception:
+        logger.exception('Failed to put an object into the storage bucket.')
+        return [E002]
+
+    try:
+        client.delete_object(Bucket=constants.S3FF_BUCKET, Key=TEST_OBJECT_KEY)
+    except ConnectionError:
+        logger.exception('Failed to connect to storage bucket')
+        return [E001]
+    except Exception:
+        logger.exception('Failed to delete an object from the storage bucket.')
+        return [E003]
+
+    return []
+
+
+@register()
+def test_assume_role_configuration(app_configs: Optional[List], **kwargs) -> List:
+    # ignore assume role checks when in development mode
+    if S3FF_STORAGE_PROVIDER == StorageProvider.UNSUPPORTED:
+        return []
+
+    client = client_factory('sts', config=Config(connect_timeout=5, retries={'max_attempts': 0}))
+
+    try:
+        client.assume_role(
+            RoleArn=constants.S3FF_UPLOAD_STS_ARN,
+            RoleSessionName=f'file-upload-{int(time())}',
+            Policy=json.dumps(
+                {
+                    'Version': '2012-10-17',
+                    'Statement': [
+                        {
+                            'Effect': 'Allow',
+                            'Action': ['s3:PutObject'],
+                            'Resource': f'arn:aws:s3:::{constants.S3FF_BUCKET}/{TEST_OBJECT_KEY}',
+                        }
+                    ],
+                }
+            ),
+            DurationSeconds=constants.S3FF_UPLOAD_DURATION,
+        )
+    except ConnectionError:
+        logger.exception('Failed to connect to storage bucket')
+        return [E001]
+    except Exception:
+        logger.exception('Failed to assume STS role.')
+        return [E004]
+
+    return []
+
+
+# TODO: investigate a possible check for CORS misconfigurations
---
+++ packages/django-s3-file-field/django-s3-file-field-0.0.11.tar.gz/django-s3-file-field-0.0.11/s3_file_field/widgets.py
@@ -0,0 +1,86 @@
+import functools
+import json
+import posixpath
+from typing import Any, Dict, Iterable, Mapping
+
+from django.forms import ClearableFileInput
+from django.urls import reverse
+
+from .constants import S3FF_STORAGE_PROVIDER, StorageProvider
+
+
+@functools.lru_cache(maxsize=1)
+def get_base_url() -> str:
+    prepare_url = reverse('s3_file_field:upload-prepare')
+    finalize_url = reverse('s3_file_field:upload-finalize')
+    # Use posixpath to always parse URL paths with forward slashes
+    return posixpath.commonpath([prepare_url, finalize_url])
+
+
+class S3FakeFile:
+    """
+    helper object to act similar to an UploadedFile.
+
+    But without the data since they already have been uploaded.
+    """
+
+    def __init__(self, info: Mapping[str, Any]):
+        super().__init__()
+
+        self.name: str = info['id']
+        self.original_name: str = info['name']
+        self.size: int = info['size']
+        self.signature: str = info['signature']
+        self._committed = True
+
+    def __str__(self):
+        return self.name
+
+    def bool(self):
+        return bool(self.name)
+
+    def len(self):
+        return self.size
+
+
+class S3FileInput(ClearableFileInput):
+    """widget to render the S3 File Input."""
+
+    class Media:
+        js = ['joist/joist.js']
+
+    @property
+    def template_name(self):
+        if S3FF_STORAGE_PROVIDER == StorageProvider.UNSUPPORTED:
+            return 'django/forms/widgets/file.html'
+        else:
+            return 'joist/s3fileinput.html'
+
+    def get_context(self, name: str, value: str, attrs):
+        context = super().get_context(name, value, attrs)
+        context['widget'].update({'baseurl': get_base_url()})
+        return context
+
+    def value_from_datadict(
+        self, data: Dict[str, Any], files: Mapping[str, Iterable[Any]], name: str
+    ):
+        if name not in files and data.get(name):
+            # JSON fake file
+            return S3FakeFile(json.loads(data[name]))
+        return super().value_from_datadict(data, files, name)
+
+    def value_omitted_from_data(self, data: Mapping[str, Any], files: Mapping[str, Any], name: str):
+        return (
+            name not in files and not data.get(name) and self.clear_checkbox_name(name) not in data
+        )
+
+
+class S3AdminFileInput(S3FileInput):
+    """widget used by the admin page."""
+
+    @property
+    def template_name(self):
+        if S3FF_STORAGE_PROVIDER == StorageProvider.UNSUPPORTED:
+            return 'admin/widgets/clearable_file_input.html'
+        else:
+            return 'joist/s3adminfileinput.html'
