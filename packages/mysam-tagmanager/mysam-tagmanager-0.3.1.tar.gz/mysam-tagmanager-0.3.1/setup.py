08168575a436aaf88fca7517890c49ac13f56781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:38 +0000

    Add package mysam-tagmanager/0.3.1/mysam-tagmanager-0.3.1.tar.gz

---
+++ packages/mysam-tagmanager/mysam-tagmanager-0.3.1.tar.gz/mysam-tagmanager-0.3.1/setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python
+from setuptools import setup
+
+# to install type:
+# python setup.py install --root=/
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+        
+setup (name='mysam-tagmanager', version='0.3.1',
+      author='Taha Zerrouki',
+      author_email='taha_zerrouki@hotmail.com',
+      url='http://pypi.python.com/projects/mysam-tagmanager/',
+      license='GPL',
+      description="Mysam: Arabic tags manager",
+      long_description = readme(),
+      package_dir={'mysam': 'mysam'},
+      packages=['mysam'],
+      include_package_data=True,
+      package_data = {
+        'mysam': ['doc/*.*','doc/html/*', "config/*"],
+        },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Natural Language :: Arabic',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Text Processing :: Linguistic',
+          ],
+    );
+
