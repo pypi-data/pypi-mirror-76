e33d97b6aecf1cc50528f0f2dc3f180d4cb12680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:20 +0000

    Add package dfsummarizer/0.1.2/dfsummarizer-0.1.2.tar.gz

---
+++ packages/dfsummarizer/dfsummarizer-0.1.2.tar.gz/dfsummarizer-0.1.2/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+ 
+"""setup.py: setuptools control."""
+ 
+import re
+from setuptools import setup
+ 
+version = re.search(
+        '^__version__\s*=\s*"(.*)"',
+        open('dfsummarizer/dfsummarizer.py').read(),
+        re.M
+    ).group(1)
+ 
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
+with open("markdown_test.md", "rb") as f:
+    example = f.read().decode("utf-8")
+
+long_descr = long_descr + "\n" + example
+
+setup(
+    name = "dfsummarizer",
+    packages = ["dfsummarizer"],
+    install_requires=[
+        'pandas','numpy'
+    ],
+    entry_points = {
+        "console_scripts": ['dfsummarizer = dfsummarizer.dfsummarizer:main']
+        },
+    version = version,
+    description = "Python command line application to summarize a CSV or TSV dataset.",
+    long_description = long_descr,
+    long_description_content_type='text/markdown',
+    author = "John Hawkins",
+    author_email = "hawkins.john.c@gmail.com",
+    url = "http://john-hawkins.github.io",
+    )
+
