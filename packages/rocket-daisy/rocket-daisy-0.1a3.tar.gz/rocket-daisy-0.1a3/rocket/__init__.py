69240b9d7745a4c5bc227c3b51b6f55a5ee5ae5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:49 +0000

    Add package rocket-daisy/0.1a3/rocket-daisy-0.1a3.tar.gz

---
+++ packages/rocket-daisy/rocket-daisy-0.1a3.tar.gz/rocket-daisy-0.1a3/rocket/__init__.py
@@ -0,0 +1,34 @@
+# +-------------------------------------------------------------------------------+
+#
+#      Program:    setup.py
+#
+#      Purpose:    setup for remote open control - key enabling technology (Rocket)
+#
+#      Target:     Maxwell SOC on isar
+#
+#      Author:     Martin Shishkov
+#
+#      License:    GPL 3
+# +-------------------------------------------------------------------------------+
+import os
+
+class Init:
+    def __init__(self):
+        print('Installing rocket-daisy')
+        
+    def get_package(self):
+        linkPyPi = "https://files.pythonhosted.org/packages/79/10/70b87e9562a284c87a8bd67bde1611d4266fa52d7ef3f704ea5774585f06/rocket-daisy-0.1a2.tar.gz"
+        rocket = "rocket-daisy-0.1a2"
+        if not os.path.isdir(rocket):
+            print('downloading rocket-daisy')
+            os.system("wget " + linkPyPi)
+            os.system(("tar xvzf %s.tar.gz" % (rocket)))
+            os.chdir(rocket)
+            os.system("sudo python3 setup.py bdist")
+            
+            
+ 
+ 
+ 
+i = Init()
+i.get_package()    
