3fb094eaed50aa19b75ce1c8e9122f587619159f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:38 +0000

    Add package PyTujian/0.0.45/PyTujian-0.0.45.tar.gz

---
+++ packages/PyTujian/PyTujian-0.0.45.tar.gz/PyTujian-0.0.45/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r", encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyTujian",
+    version="0.0.45",
+    author="gggxbbb",
+    author_email="gamegxb@163.com",
+    description="A simlpe tool for Tujian",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gggxbbb/tujian_python",
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    install_requires=[
+        #'qrcode>=6.0',
+        #'pillow>=6.0.0',
+        #'image>=1.5.0',
+        'PyYAML>=5.1.2',
+        #'dhash>=1.3'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': [
+            'PyTujian = PyTujian.__main__:main'
+        ]
+    }
+)
