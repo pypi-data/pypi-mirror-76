6a73a43d180eda6ef7bc2c90be38536660c0c1f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:52 +0000

    Add package robotframework-reportportal/5.0.3/robotframework-reportportal-5.0.3.tar.gz

---
+++ packages/robotframework-reportportal/robotframework-reportportal-5.0.3.tar.gz/robotframework-reportportal-5.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+__version__ = '5.0.3'
+
+requirements = [
+    "reportportal-client>=5.0.5",
+    "six",
+]
+
+setup(
+    name='robotframework-reportportal',
+    packages=find_packages(),
+    version=__version__,
+    description='Listener for RobotFramework reporting to ReportPortal v5',
+    author_email='SupportEPMC-TSTReportPortal@epam.com',
+    url='https://github.com/reportportal/agent-Python-RobotFramework',
+    download_url=(
+        'https://github.com/reportportal/agent-Python-RobotFramework/'
+        'tarball/{version}'.format(version=__version__)),
+    keywords=['testing', 'reporting', 'robot framework', 'reportportal'],
+    classifiers=[],
+    install_requires=requirements
+)
