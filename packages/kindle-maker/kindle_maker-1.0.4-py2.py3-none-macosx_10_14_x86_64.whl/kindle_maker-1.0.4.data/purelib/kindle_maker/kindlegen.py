306a0f010571e2728a480d3b304694823c024752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:01 +0000

    Add package kindle-maker/1.0.4/kindle_maker-1.0.4-py2.py3-none-macosx_10_14_x86_64.whl

---
+++ packages/kindle-maker/kindle_maker-1.0.4-py2.py3-none-macosx_10_14_x86_64.whl/kindle_maker-1.0.4.data/purelib/kindle_maker/kindlegen.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import platform
+import os
+
+import kindle_maker
+
+path = kindle_maker.__path__[0]
+
+system = platform.system()
+
+if system == 'Linux':
+    kindlegen = os.path.join(path, 'bin/linux/kindlegen')
+elif system == 'Darwin':
+    kindlegen = os.path.join(path, 'bin/mac/kindlegen')
+elif system == 'Windows':
+    kindlegen = os.path.join(path, 'bin/windows/kindlegen.exe')
+else:
+    raise Exception('Not supported system:{}'.format(system))
