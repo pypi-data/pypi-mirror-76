c0cd8814075628de15f42b113184bbfb6b113987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:02 +0000

    Add package sbi/0.11.2/sbi-0.11.2-py2.py3-none-any.whl

---
+++ packages/sbi/sbi-0.11.2-py2.py3-none-any.whl/sbi/inference/snpe/snpe_c.py
@@ -0,0 +1,235 @@
+# This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
+# under the Affero General Public License v3, see <https://www.gnu.org/licenses/>.
+
+
+from typing import Any, Callable, Dict, Optional, Union
+
+import torch
+from torch import Tensor, eye, ones
+
+from sbi.inference.posteriors.base_posterior import NeuralPosterior
+from sbi.inference.snpe.snpe_base import PosteriorEstimator
+from sbi.types import TensorboardSummaryWriter
+from sbi.utils import clamp_and_warn, del_entries, repeat_rows
+
+
+class SNPE_C(PosteriorEstimator):
+    def __init__(
+        self,
+        simulator: Callable,
+        prior,
+        num_workers: int = 1,
+        simulation_batch_size: int = 1,
+        density_estimator: Union[str, Callable] = "maf",
+        sample_with_mcmc: bool = False,
+        mcmc_method: str = "slice_np",
+        mcmc_parameters: Optional[Dict[str, Any]] = None,
+        use_combined_loss: bool = False,
+        device: str = "cpu",
+        logging_level: Union[int, str] = "WARNING",
+        summary_writer: Optional[TensorboardSummaryWriter] = None,
+        show_progress_bars: bool = True,
+        show_round_summary: bool = False,
+    ):
+        r"""SNPE-C / APT [1].
+
+        [1] _Automatic Posterior Transformation for Likelihood-free Inference_,
+            Greenberg et al., ICML 2019, https://arxiv.org/abs/1905.07488.
+
+        Args:
+            simulator: A function that takes parameters $\theta$ and maps them to
+                simulations, or observations, `x`, $\mathrm{sim}(\theta)\to x$. Any
+                regular Python callable (i.e. function or class with `__call__` method)
+                can be used.
+            prior: A probability distribution that expresses prior knowledge about the
+                parameters, e.g. which ranges are meaningful for them. Any
+                object with `.log_prob()`and `.sample()` (for example, a PyTorch
+                distribution) can be used.
+            num_workers: Number of parallel workers to use for simulations.
+            simulation_batch_size: Number of parameter sets that the simulator
+                maps to data x at once. If None, we simulate all parameter sets at the
+                same time. If >= 1, the simulator has to process data of shape
+                (simulation_batch_size, parameter_dimension).
+            density_estimator: If it is a string, use a pre-configured network of the
+                provided type (one of nsf, maf, mdn, made). Alternatively, a function
+                that builds a custom neural network can be provided. The function will
+                be called with the first batch of simulations (theta, x), which can
+                thus be used for shape inference and potentially for z-scoring. It
+                needs to return a PyTorch `nn.Module` implementing the density
+                estimator. The density estimator needs to provide the methods
+                `.log_prob` and `.sample()`.
+            sample_with_mcmc: Whether to sample with MCMC. MCMC can be used to deal
+                with high leakage.
+            mcmc_method: Method used for MCMC sampling, one of `slice_np`, `slice`, `hmc`, `nuts`.
+                Currently defaults to `slice_np` for a custom numpy implementation of
+                slice sampling; select `hmc`, `nuts` or `slice` for Pyro-based sampling.
+            mcmc_parameters: Dictionary overriding the default parameters for MCMC.
+                The following parameters are supported: `thin` to set the thinning
+                factor for the chain, `warmup_steps` to set the initial number of
+                samples to discard, `num_chains` for the number of chains, `init_strategy`
+                for the initialisation strategy for chains; `prior` will draw init
+                locations from prior, whereas `sir` will use Sequential-Importance-
+                Resampling using `init_strategy_num_candidates` to find init
+                locations.
+            use_combined_loss: Whether to train the neural net also on prior samples
+                using maximum likelihood in addition to training it on all samples using
+                atomic loss. The extra MLE loss helps prevent density leaking with
+                bounded priors.
+            device: torch device on which to compute, e.g. gpu, cpu.
+            logging_level: Minimum severity of messages to log. One of the strings
+                INFO, WARNING, DEBUG, ERROR and CRITICAL.
+            summary_writer: A tensorboard `SummaryWriter` to control, among others, log
+                file location (default is `<current working directory>/logs`.)
+            show_progress_bars: Whether to show a progressbar during simulation and
+                sampling.
+            show_round_summary: Whether to show the validation loss and leakage after
+                each round.
+        """
+
+        self._use_combined_loss = use_combined_loss
+
+        kwargs = del_entries(
+            locals(), entries=("self", "__class__", "use_combined_loss")
+        )
+        super().__init__(**kwargs)
+
+    def __call__(
+        self,
+        num_simulations: int,
+        proposal: Optional[Any] = None,
+        num_atoms: int = 10,
+        training_batch_size: int = 50,
+        learning_rate: float = 5e-4,
+        validation_fraction: float = 0.1,
+        stop_after_epochs: int = 20,
+        max_num_epochs: Optional[int] = None,
+        clip_max_norm: Optional[float] = 5.0,
+        calibration_kernel: Optional[Callable] = None,
+        exclude_invalid_x: bool = True,
+        discard_prior_samples: bool = False,
+        retrain_from_scratch_each_round: bool = False,
+    ) -> NeuralPosterior:
+        r"""Run SNPE.
+
+        Return posterior $p(\theta|x)$ after inference.
+
+        Args:
+            num_simulations: Number of simulator calls.
+            proposal: Distribution that the parameters $\theta$ are drawn from.
+                `proposal=None` uses the prior. Setting the proposal to a distribution
+                targeted on a specific observation, e.g. a posterior $p(\theta|x_o)$
+                obtained previously, can lead to less required simulations.
+            num_atoms: Number of atoms to use for classification.
+            training_batch_size: Training batch size.
+            learning_rate: Learning rate for Adam optimizer.
+            validation_fraction: The fraction of data to use for validation.
+            stop_after_epochs: The number of epochs to wait for improvement on the
+                validation set before terminating training.
+            max_num_epochs: Maximum number of epochs to run. If reached, we stop
+                training even when the validation loss is still decreasing. If None, we
+                train until validation loss increases (see also `stop_after_epochs`).
+            clip_max_norm: Value at which to clip the total gradient norm in order to
+                prevent exploding gradients. Use None for no clipping.
+            calibration_kernel: A function to calibrate the loss with respect to the
+                simulations `x`. See Lueckmann, Gonçalves et al., NeurIPS 2017.
+            exclude_invalid_x: Whether to exclude simulation outputs `x=NaN` or `x=±∞`
+                during training. Expect errors, silent or explicit, when `False`.
+            discard_prior_samples: Whether to discard samples simulated in round 1, i.e.
+                from the prior. Training may be sped up by ignoring such less targeted
+                samples.
+            retrain_from_scratch_each_round: Whether to retrain the conditional density
+                estimator for the posterior from scratch each round.
+
+        Returns:
+            Posterior $p(\theta|x)$ that can be sampled and evaluated.
+        """
+
+        # WARNING: sneaky trick ahead. We proxy the parent's `__call__` here,
+        # requiring the signature to have `num_atoms`, save it for use below, and
+        # continue. It's sneaky because we are using the object (self) as a namespace
+        # to pass arguments between functions, and that's implicit state management.
+
+        self._num_atoms = num_atoms
+        kwargs = del_entries(locals(), entries=("self", "__class__", "num_atoms"))
+
+        return super().__call__(**kwargs)
+
+    def _log_prob_proposal_posterior(
+        self, theta: Tensor, x: Tensor, masks: Tensor
+    ) -> Tensor:
+        """
+        Return log probability of the proposal posterior.
+
+        We have two main options when evaluating the proposal posterior.
+            (1) Generate atoms from the proposal prior.
+            (2) Generate atoms from a more targeted distribution, such as the most
+                recent posterior.
+        If we choose the latter, it is likely beneficial not to do this in the first
+        round, since we would be sampling from a randomly-initialized neural density
+        estimator.
+
+        Args:
+            theta: Batch of parameters θ.
+            x: Batch of data.
+            masks: Mask that is True for prior samples in the batch in order to train
+                them with prior loss.
+
+        Returns:
+            Log-probability of the proposal posterior.
+        """
+
+        batch_size = theta.shape[0]
+
+        num_atoms = clamp_and_warn(
+            "num_atoms", self._num_atoms, min_val=2, max_val=batch_size
+        )
+
+        # Each set of parameter atoms is evaluated using the same x,
+        # so we repeat rows of the data x, e.g. [1, 2] -> [1, 1, 2, 2]
+        repeated_x = repeat_rows(x, num_atoms)
+
+        # To generate the full set of atoms for a given item in the batch,
+        # we sample without replacement num_atoms - 1 times from the rest
+        # of the theta in the batch.
+        probs = ones(batch_size, batch_size) * (1 - eye(batch_size)) / (batch_size - 1)
+
+        choices = torch.multinomial(probs, num_samples=num_atoms - 1, replacement=False)
+        contrasting_theta = theta[choices]
+
+        # We can now create our sets of atoms from the contrasting parameter sets
+        # we have generated.
+        atomic_theta = torch.cat((theta[:, None, :], contrasting_theta), dim=1).reshape(
+            batch_size * num_atoms, -1
+        )
+
+        # Evaluate large batch giving (batch_size * num_atoms) log prob posterior evals.
+        log_prob_posterior = self._posterior.net.log_prob(atomic_theta, repeated_x)
+        self._assert_all_finite(log_prob_posterior, "posterior eval")
+        log_prob_posterior = log_prob_posterior.reshape(batch_size, num_atoms)
+
+        # Get (batch_size * num_atoms) log prob prior evals.
+        log_prob_prior = self._prior.log_prob(atomic_theta)
+        log_prob_prior = log_prob_prior.reshape(batch_size, num_atoms)
+        self._assert_all_finite(log_prob_prior, "prior eval")
+
+        # Compute unnormalized proposal posterior.
+        unnormalized_log_prob = log_prob_posterior - log_prob_prior
+
+        # Normalize proposal posterior across discrete set of atoms.
+        log_prob_proposal_posterior = unnormalized_log_prob[:, 0] - torch.logsumexp(
+            unnormalized_log_prob, dim=-1
+        )
+        self._assert_all_finite(log_prob_proposal_posterior, "proposal posterior eval")
+
+        # XXX This evaluates the posterior on _all_ prior samples
+        if self._use_combined_loss:
+            log_prob_posterior_non_atomic = self._posterior.net.log_prob(theta, x)
+            masks = masks.reshape(-1)
+            log_prob_proposal_posterior = (
+                masks * log_prob_posterior_non_atomic + log_prob_proposal_posterior
+            )
+
+        return log_prob_proposal_posterior
+
+    def _log_prob_proposal_MoG(self, theta: Tensor, x: Tensor, masks: Tensor) -> Tensor:
+        raise NotImplementedError
---
+++ packages/sbi/sbi-0.11.2-py2.py3-none-any.whl/sbi/utils/get_nn_models.py
@@ -0,0 +1,61 @@
+# This file is part of sbi, a toolkit for simulation-based inference. sbi is licensed
+# under the Affero General Public License v3, see <https://www.gnu.org/licenses/>.
+
+
+from typing import Callable, Optional, Union, Dict, Any, Tuple, Union, cast, List, Sequence, TypeVar
+from sbi.neural_nets.flow import build_made, build_maf, build_nsf
+from sbi.neural_nets.mdn import build_mdn
+from sbi.neural_nets.classifier import (
+    build_linear_classifier,
+    build_mlp_classifier,
+    build_resnet_classifier,
+)
+
+
+def classifier_nn(model: str, **kwargs: Any) -> Callable:
+    def build_fn(batch_theta, batch_x):
+        if model == "linear":
+            return build_linear_classifier(
+                batch_x=batch_x, batch_y=batch_theta, **kwargs
+            )
+        if model == "mlp":
+            return build_mlp_classifier(batch_x=batch_x, batch_y=batch_theta, **kwargs)
+        if model == "resnet":
+            return build_resnet_classifier(
+                batch_x=batch_x, batch_y=batch_theta, **kwargs
+            )
+            raise NotImplementedError
+
+    return build_fn
+
+
+def likelihood_nn(model: str, **kwargs: Any) -> Callable:
+    def build_fn(batch_theta, batch_x):
+        if model == "mdn":
+            return build_mdn(batch_x=batch_x, batch_y=batch_theta, **kwargs)
+        if model == "made":
+            return build_made(batch_x=batch_x, batch_y=batch_theta, **kwargs)
+        if model == "maf":
+            return build_maf(batch_x=batch_x, batch_y=batch_theta, **kwargs)
+        elif model == "nsf":
+            return build_nsf(batch_x=batch_x, batch_y=batch_theta, **kwargs)
+        else:
+            raise NotImplementedError
+
+    return build_fn
+
+
+def posterior_nn(model: str, **kwargs: Any) -> Callable:
+    def build_fn(batch_theta, batch_x):
+        if model == "mdn":
+            return build_mdn(batch_x=batch_theta, batch_y=batch_x, **kwargs)
+        if model == "made":
+            return build_made(batch_x=batch_theta, batch_y=batch_x, **kwargs)
+        if model == "maf":
+            return build_maf(batch_x=batch_theta, batch_y=batch_x, **kwargs)
+        elif model == "nsf":
+            return build_nsf(batch_x=batch_theta, batch_y=batch_x, **kwargs)
+        else:
+            raise NotImplementedError
+
+    return build_fn
