f704d7f70a5652e6e4340fb1fca21fbf44b4336c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:15 +0000

    Add package xiangshi/1.2.0/xiangshi-1.2.0-py3-none-any.whl

---
+++ packages/xiangshi/xiangshi-1.2.0-py3-none-any.whl/xiangshi/__init__.py
@@ -0,0 +1,8 @@
+from xiangshi.main import calculator
+
+calculator = calculator()
+cal = calculator.cal
+input2list = calculator.input2list
+dict2file = calculator.dict2file
+GetTF = calculator.GetTF
+GetIDF = calculator.GetIDF
