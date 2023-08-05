eaa2ba432e761da796b0f4a60e5e94978808d734
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:00 +0000

    Add package cloudcomponents.cdk-dynamodb-seeder/1.0.6/cloudcomponents.cdk-dynamodb-seeder-1.0.6.tar.gz

---
+++ packages/cloudcomponents.cdk-dynamodb-seeder/cloudcomponents.cdk-dynamodb-seeder-1.0.6.tar.gz/cloudcomponents.cdk-dynamodb-seeder-1.0.6/setup.py
@@ -0,0 +1,61 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cloudcomponents.cdk-dynamodb-seeder",
+    "version": "1.0.6",
+    "description": "A seeder for dynamodb tables",
+    "license": "MIT",
+    "url": "https://github.com/cloudcomponents/cdk-constructs",
+    "long_description_content_type": "text/markdown",
+    "author": "hupe1980",
+    "project_urls": {
+        "Source": "https://github.com/cloudcomponents/cdk-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cloudcomponents.cdk_dynamodb_seeder",
+        "cloudcomponents.cdk_dynamodb_seeder._jsii"
+    ],
+    "package_data": {
+        "cloudcomponents.cdk_dynamodb_seeder._jsii": [
+            "cdk-dynamodb-seeder@1.0.6.jsii.tgz"
+        ],
+        "cloudcomponents.cdk_dynamodb_seeder": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii>=1.10.0, <2.0.0",
+        "publication>=0.0.3",
+        "aws-cdk.aws-dynamodb>=1.57.0, <2.0.0",
+        "aws-cdk.aws-iam>=1.57.0, <2.0.0",
+        "aws-cdk.aws-s3>=1.57.0, <2.0.0",
+        "aws-cdk.aws-s3-assets>=1.57.0, <2.0.0",
+        "aws-cdk.core>=1.57.0, <2.0.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ]
+}
+"""
+)
+
+with open("README.md") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
