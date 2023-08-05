52a0e244edd310233006364ddf951bc1bb6185ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package sylenium/0.1.0/sylenium-0.1.0-py2.py3-none-any.whl

---
+++ packages/sylenium/sylenium-0.1.0-py2.py3-none-any.whl/sylenium/exceptions/exceptions.py
@@ -0,0 +1,7 @@
+class syleniumConfigValueError(ValueError):
+    """
+    When configuration options specified often by a user provided yaml do not adhere to syleniums constraints this is
+    raised.
+    """
+
+    pass
