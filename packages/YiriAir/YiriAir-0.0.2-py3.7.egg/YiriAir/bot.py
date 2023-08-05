84927e0754f02e745ddc0aab5567aa8b12aa797f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:45 +0000

    Add package YiriAir/0.0.2/YiriAir-0.0.2-py3.7.egg

---
+++ packages/YiriAir/YiriAir-0.0.2-py3.7.egg/YiriAir/bot.py
@@ -0,0 +1,34 @@
+# -*- encoding=utf8 -*-
+
+import time
+from .driver import PocoDriver, Session
+
+
+class YiriAir():
+    def __init__(self, device, query_interval=1):
+        self.driver = PocoDriver(device)
+        self.current_session = Session(self.driver)
+        self.query_interval = query_interval
+        self.message_hooks = []
+
+    def on_message(self, message_type='Text'):
+        def decorator(func):
+            def decorated(message, title):
+                txt, msg_type, sender = message
+                if msg_type == message_type:
+                    return func(txt, title, sender)
+            self.message_hooks.append(decorated)
+            return decorated
+        return decorator
+
+    def send_message(self, msg_text):
+        return self.current_session.send_message(msg_text)
+
+    def run(self):
+        while True:
+            messages = self.current_session.check_latest_message()
+            for msg in messages:
+                for hook in self.message_hooks:
+                    hook(msg, self.current_session.title)
+            self.current_session = self.current_session.jump_to_new_session()
+            time.sleep(self.query_interval)
