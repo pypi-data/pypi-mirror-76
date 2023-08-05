008d642ed2778d9358e8d2d37bb17f5cfabcce16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:57 +0000

    Add package mootdx/0.6.4/mootdx-0.6.4-py2.py3-none-any.whl

---
+++ packages/mootdx/mootdx-0.6.4-py2.py3-none-any.whl/mootdx/contrib/pytdx/util/__init__.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+
+from .date_util import get_real_trade_date
+from .trade_date import trade_date_sse
+# from pytdx.util.best_ip import select_best_ip, ping
+# comment to avoid recycle ref
