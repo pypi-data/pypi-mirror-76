d44594ac15cb488564555bf2feebbda9482c9cf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package scikit-clean/0.1.2/scikit_clean-0.1.2-py3-none-any.whl

---
+++ packages/scikit-clean/scikit_clean-0.1.2-py3-none-any.whl/skclean/__init__.py
@@ -0,0 +1,9 @@
+from . import datasets
+from . import detectors
+from . import handlers
+from . import models
+from . import pipeline
+from . import simulate_noise
+
+# TODO: Remove this __version__ coupling with setup.py
+__version__ = "0.1.2"
