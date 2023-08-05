f0845978a5e185b4612e6c8ea6b29744088a0ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:25 +0000

    Add package sphinx-sitemap/2.2.0/sphinx-sitemap-2.2.0.tar.gz

---
+++ packages/sphinx-sitemap/sphinx-sitemap-2.2.0.tar.gz/sphinx-sitemap-2.2.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import os
+
+long_description = open('README.rst' if os.path.exists('README.rst') else 'README.md').read()
+exec(compile(
+    open('sphinx_sitemap/version.py').read(), 'sphinx_sitemap/version.py', 'exec'))
+
+setup(
+    name='sphinx-sitemap',
+    description='Sitemap generator for Sphinx',
+    long_description=long_description,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Documentation',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+      ],
+    version=__version__,
+    author='Jared Dillard',
+    author_email='jared.dillard@gmail.com',
+    install_requires=['six', 'sphinx >= 1.2'],
+    url="https://github.com/jdillard/sphinx-sitemap",
+    license='MIT',
+    download_url="https://github.com/jdillard/sphinx-sitemap/archive/v2.2.0.tar.gz",
+    packages=['sphinx_sitemap'],
+ )
