622e18b58b7d793b1b8d69c30e4d00e5f0d4300e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:14 +0000

    Add package fcp/0.29/fcp-0.29.tar.gz

---
+++ packages/fcp/fcp-0.29.tar.gz/fcp-0.29/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open("fcp/version.py") as f:
+    version = f.read()
+
+_, version = version.split("=")
+version = version.strip()
+
+version = version[1:-1]
+
+setup(
+    name="fcp",
+    description="CAN bus manager",
+    version=version,
+    author="Joao Freitas",
+    author_email="joaj.freitas@gmail.com",
+    license="GPLv3",
+    url="https://gitlab.com/joajfreitas/fcp-core",
+    download_url="https://gitlab.com/joajfreitas/fcp-core/-/archive/v0.29/fcp-core-v0.29.tar.gz",
+    packages=find_packages(),
+    entry_points={"console_scripts": ["fcp = fcp.__main__:main",],},
+    install_requires=["click", "PySide2", "jinja2"],
+)
