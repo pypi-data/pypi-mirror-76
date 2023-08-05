9da0f4d83b4976a0f549a171be479942785c2af4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:38 +0000

    Add package senderman/0.0.1/senderman-0.0.1.tar.gz

---
+++ packages/senderman/senderman-0.0.1.tar.gz/senderman-0.0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name='senderman',
+    version='0.0.1',
+    author='SNBCK',
+    author_email='snbckcode@gmail.com',
+    url='https://zhuanlan.zhihu.com/p/26159930',
+    description=u'',
+    install_requires=[],
+)
