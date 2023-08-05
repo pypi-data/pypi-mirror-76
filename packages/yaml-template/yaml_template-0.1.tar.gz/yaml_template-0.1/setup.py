18f9749f88ca75ab45b3e4c39de8b2e829005c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:08 +0000

    Add package yaml-template/0.1/yaml_template-0.1.tar.gz

---
+++ packages/yaml-template/yaml_template-0.1.tar.gz/yaml_template-0.1/setup.py
@@ -0,0 +1,23 @@
+
+# Use this command for deploy.
+#   python3 setup.py sdist bdist_wheel && python3 -m twine upload --skip-existing dist/*
+
+import io
+from setuptools import find_packages, setup
+
+setup(name='yaml_template',
+      version='0.1',
+      description='쿠버네티스처럼 YAML 파일로 이루어진 템플릿을 쉽게 템플릿의 타입에 따라 다르게 처리해주는 패키지입니다.',
+      long_description="Please refer to the https://github.com/da-huin/yaml_template",
+      long_description_content_type="text/markdown",
+      url='https://github.com/da-huin/yaml_template',
+      download_url= 'https://github.com/da-huin/yaml_template/archive/master.zip',
+      author='JunYeong Park',
+      author_email='dahuin000@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=["jsonschema", "pyyaml"],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+    ]
+)
