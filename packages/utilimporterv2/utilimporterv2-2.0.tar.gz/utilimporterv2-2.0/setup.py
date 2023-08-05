883a7ee578810de6e06822a43e722b3f346b683f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:44 +0000

    Add package utilimporterv2/2.0/utilimporterv2-2.0.tar.gz

---
+++ packages/utilimporterv2/utilimporterv2-2.0.tar.gz/utilimporterv2-2.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+ name = 'utilimporterv2',
+ version = '2.0',
+ py_modules = ['utilimporterv2'],
+ author = 'Akshat Garg',
+ author_email='akshatgarg789@gmail.com',
+ description=  'Import every module you need in one function callt',
+ )
