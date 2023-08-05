d462750060e44fc73a31b95edaf2d8a6b4eb5e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:57 +0000

    Add package setux/0.4.2.1/setux-0.4.2.1.tar.gz

---
+++ packages/setux/setux-0.4.2.1.tar.gz/setux-0.4.2.1/setup.py
@@ -0,0 +1,48 @@
+from os.path import join, dirname, abspath
+
+from setuptools import setup, find_namespace_packages
+
+from setux.main import __version__
+
+curdir = abspath(dirname(__file__))
+readme = open(join(curdir, 'README.rst')).read()
+
+setup(
+    name             = 'setux',
+    version          = __version__,
+    description      = 'System deployment',
+    long_description = readme,
+    keywords         = ['utility', ],
+    url              = 'https://framagit.org/louis-riviere-xyz/setux/tree/stable',
+    author           = 'Louis RIVIERE',
+    author_email     = 'louis@riviere.xyz',
+    license          = 'MIT',
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    install_requires = [
+        'setux_core==0.1.0',
+        'setux_distros==0.1.3',
+        'setux_targets==0.1.0',
+        'setux_managers==0.1.0',
+        'setux_repl==0.2.0',
+    ],
+    packages = find_namespace_packages(
+        include=['setux.*']
+    ),
+    entry_points = dict(
+        console_scripts = (
+            'setux=setux.main.main:main',
+        ),
+    ),
+)
---
+++ packages/setux/setux-0.4.2.1.tar.gz/setux-0.4.2.1/setux/main/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '0.4.2.1'
+
+banner = f'    * * * *    setux {__version__}    * * * *'
