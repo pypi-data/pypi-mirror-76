b47cc5f9595974200e1a5e40f61931b588a6c566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:23 +0000

    Add package py-pure-client/1.7.0/py-pure-client-1.7.0.tar.gz

---
+++ packages/py-pure-client/py-pure-client-1.7.0.tar.gz/py-pure-client-1.7.0/pypureclient/__init__.py
@@ -0,0 +1,7 @@
+from . import pure1
+from . import flasharray
+from . import mordac
+
+from .exceptions import PureError
+from .properties import Property, Filter
+from .responses import ValidResponse, ErrorResponse, ApiError, ResponseHeaders
