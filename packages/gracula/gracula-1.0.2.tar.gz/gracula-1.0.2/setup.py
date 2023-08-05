59b66523c4bbc858a2f2b0830b948dbff8b175f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:55 +0000

    Add package gracula/1.0.2/gracula-1.0.2.tar.gz

---
+++ packages/gracula/gracula-1.0.2.tar.gz/gracula-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='gracula',
+    packages=['gracula'],
+    version='1.0.2',
+    license='MIT',
+    author='yutakojima',
+    author_email='info@idea-tools.space',
+    url='https://github.com/yuutakojima/gracula',
+    description='Gracula is a very simple Twitter api. This package can be done without OAuth.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='python twitter api',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
