cadbfaa149a4f4e4884970629e7eca9e312ef49f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package datetime-truncate/1.1.1/datetime_truncate-1.1.1.tar.gz

---
+++ packages/datetime-truncate/datetime_truncate-1.1.1.tar.gz/datetime_truncate-1.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+import setuptools
+from distutils.core import setup
+
+setup(
+    name='datetime_truncate',
+    version='1.1.1',
+    url='https://github.com/mediapop/datetime_truncate',
+    author='Björn Andersson / Media Pop',
+    author_email='bjorn@mediapop.co',
+    description='Truncate datetime objects to a set level of precision',
+    license='BSD',
+    long_description=open('README.rst').read(),
+    packages=['datetime_truncate'],
+    package_data={
+        '': ['README.rst']
+    },
+    include_package_data=True,
+    test_suite='nose.collector',
+    tests_require=['nose>=1.2.1'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+    ],
+)
