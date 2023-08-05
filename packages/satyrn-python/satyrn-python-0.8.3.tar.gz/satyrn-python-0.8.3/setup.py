6685323391584e38dec3cdcfdd906ebe3f4b61d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:17 +0000

    Add package satyrn-python/0.8.3/satyrn-python-0.8.3.tar.gz

---
+++ packages/satyrn-python/satyrn-python-0.8.3.tar.gz/satyrn-python-0.8.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='satyrn-python',
+    version='0.8.3',
+    entry_points={"console_scripts": ["satyrnCLI = satyrnCLI.__main__:start",
+                                      "satyrn = satyrnUI.__main__:run"]},
+    author="Charles Averill",
+    author_email="charlesaverill20@gmail.com",
+    description="A Notebook alternative that supports branching code",
+    long_description=long_description,
+    install_requires=['networkx', 'matplotlib', 'flask', 'cherrypy'],
+    long_description_content_type="text/markdown",
+    url="https://github.com/CharlesAverill/satyrn/",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
