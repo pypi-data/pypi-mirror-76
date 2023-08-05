f8c59c70708f631797c36752253f614ed8c4a99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package distributions-caduc37/0.1/distributions_caduc37-0.1.tar.gz

---
+++ packages/distributions-caduc37/distributions_caduc37-0.1.tar.gz/distributions_caduc37-0.1/README.md
@@ -0,0 +1,5 @@
+test 
+
+blablabla
+
+package UDACITY machine learning engineer
---
+++ packages/distributions-caduc37/distributions_caduc37-0.1.tar.gz/distributions_caduc37-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_caduc37',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = "Alexandre Caduc",
+      author_email = " alexandre.caduc@gmail.com",
+      zip_safe=False)
