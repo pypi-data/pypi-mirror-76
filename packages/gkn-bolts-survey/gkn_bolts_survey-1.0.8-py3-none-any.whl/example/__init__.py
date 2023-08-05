0203df52dc68fd41082b848c6b864955c7ea64e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:26 +0000

    Add package gkn-bolts-survey/1.0.8/gkn_bolts_survey-1.0.8-py3-none-any.whl

---
+++ packages/gkn-bolts-survey/gkn_bolts_survey-1.0.8-py3-none-any.whl/example/__init__.py
@@ -0,0 +1,11 @@
+
+
+from gkn_bolts_survey import SignalType
+from gkn_bolts_survey import TargetType
+from gkn_bolts_survey import read_signals
+from gkn_bolts_survey import clean_signal
+from gkn_bolts_survey import sigmoid_approximation
+from gkn_bolts_survey import plot_raw_signals
+from gkn_bolts_survey import plot_failure
+
+
