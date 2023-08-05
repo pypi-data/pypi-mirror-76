9e875528aaad861750faf3a34be12c4f69e5bae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:56 +0000

    Add package demogua/1.0/demogua-1.0.tar.gz

---
+++ packages/demogua/demogua-1.0.tar.gz/demogua-1.0/setup.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from setuptools import setup
+# or
+# from distutils.core import setup  
+
+setup(
+        name='demogua',     # 包名字
+        version='1.0',   # 包版本
+        description='This is a test of the setup',   # 简单描述
+        author='cccc',  # 作者
+        author_email='cccc@cccc.com',  # 作者邮箱
+        url='https://www.baidu.com',      # 包的主页
+        packages=['demogua'],                 # 包
+)
