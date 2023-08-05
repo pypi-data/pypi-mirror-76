d1b0b3109c8aa7f34ae4be97a2557ef9d1b69324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:47 +0000

    Add package copulae/0.7.0/copulae-0.7.0-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/copulae/copulae-0.7.0-cp38-cp38-manylinux1_x86_64.whl/copulae/__init__.py
@@ -0,0 +1,13 @@
+from copulae.archimedean import *
+from copulae.elliptical import *
+from copulae.empirical import *
+from copulae.indep import *
+from copulae.marginal import *
+from copulae.mixtures import *
+
+__version__ = "0.7.0"
+
+
+def doc():
+    import webbrowser
+    webbrowser.open('https://copulae.readthedocs.io/en/latest')
