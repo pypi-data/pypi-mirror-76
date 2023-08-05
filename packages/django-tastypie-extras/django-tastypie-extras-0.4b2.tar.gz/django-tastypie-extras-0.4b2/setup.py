77f22cf20dd17563a8bf3475a6b8734f3f95d471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:27 +0000

    Add package django-tastypie-extras/0.4b2/django-tastypie-extras-0.4b2.tar.gz

---
+++ packages/django-tastypie-extras/django-tastypie-extras-0.4b2.tar.gz/django-tastypie-extras-0.4b2/setup.py
@@ -0,0 +1,32 @@
+from codecs import open
+
+from setuptools import setup, find_packages
+
+setup(
+    name="django-tastypie-extras",
+    version='0.4b2',
+    author='Tomasz Jakub Rup',
+    author_email='tomasz.rup@gmail.com',
+    url='https://github.com/tomi77/django-tastypie-extras',
+    description='A set of Django tastypie extras (Multipart resource, smart paginator, SwaggerUI authentication)',
+    long_description=open("README.rst").read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+    ],
+    license='MIT',
+    packages=find_packages()
+)
