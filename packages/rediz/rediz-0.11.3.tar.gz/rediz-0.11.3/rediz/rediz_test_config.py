8a0192e701396104bacea0e44f12990e14db910e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:53 +0000

    Add package rediz/0.11.3/rediz-0.11.3.tar.gz

---
+++ packages/rediz/rediz-0.11.3.tar.gz/rediz-0.11.3/rediz/rediz_test_config.py
@@ -0,0 +1,19 @@
+import os
+from sys import  platform
+from getjson import getjson
+
+# Load a couple of environment variables for local testing.
+# These are secrets on github.
+try:
+    from rediz.rediz_test_config_private import NOTHING_MUCH
+except:
+    pass
+
+test_config_url = os.getenv('TEST_CONFIG_URL')
+test_config_failover_url = os.getenv('TEST_CONFIG_FAILOVER_URL')
+REDIZ_TEST_CONFIG = getjson(url=test_config_url,failover_url=test_config_failover_url)
+if REDIZ_TEST_CONFIG is None:
+    raise Exception('Could not get configuration')
+
+if platform=='darwin':
+    REDIZ_TEST_CONFIG['DUMP']=True
