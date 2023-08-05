b2caebb9b94bf4146e000914ba3d14ae8c0e342a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:56 +0000

    Add package migri/0.5.0/migri-0.5.0-py3-none-any.whl

---
+++ packages/migri/migri-0.5.0-py3-none-any.whl/migri/main.py
@@ -0,0 +1,190 @@
+import asyncio
+import logging
+import os
+import sys
+from importlib import import_module
+from typing import Optional, Type, TYPE_CHECKING, Union
+
+import click
+
+if TYPE_CHECKING:
+    from asyncpg import Connection
+
+from migri import migration
+from migri.interfaces import ConnectionBackend
+from migri.utils import deprecated, Echo
+
+DEFAULT_LOG_LEVEL = "error"
+LEGACY_FUNCTIONALITY_END_OF_LIFE = "1.1.0"
+SUPPORTED_DIALECTS = {
+    "mysql": "migri.backends.mysql::MySQL",
+    "postgresql": "migri.backends.postgresql::PostgreSQL",
+    "sqlite": "migri.backends.sqlite::SQLite",
+}
+
+logging.basicConfig(
+    format="%(asctime)s\t%(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %I:%M:%S%z",
+    level=os.getenv("LOG_LEVEL", DEFAULT_LOG_LEVEL).upper(),
+)
+logger = logging.getLogger(__name__)
+
+
+async def run_initialization(*args, **kwargs):
+    """No longer supported but left here for compatibility"""
+    message = (
+        "Command `init` and run_initialization() are no longer supported and are now "
+        "handled automatically by `migrate` and run_migrations(). See README."
+    )
+    deprecated(message, LEGACY_FUNCTIONALITY_END_OF_LIFE)
+    Echo.info(message)
+
+
+async def run_migrations(
+    migrations_dir: str,
+    conn: "Connection" = None,
+    db_user: str = None,
+    db_pass: str = None,
+    db_name: str = None,
+    db_host: str = None,
+    db_port: str = None,
+    dry_run: bool = False,
+    force_close_conn: bool = True,
+):
+    message = (
+        "run_migrations() is deprecated. Please use " "apply_migrations(). See README."
+    )
+    deprecated(message, LEGACY_FUNCTIONALITY_END_OF_LIFE)
+    Echo.info(message)
+
+    # Rig new functionality to provide backwards compatibility
+    conn_backend = _get_backend(SUPPORTED_DIALECTS["postgresql"])
+
+    if not conn:
+        conn = conn_backend(
+            db_name, db_user=db_user, db_pass=db_pass, db_host=db_host, db_port=db_port,
+        )
+    else:
+        conn = conn_backend("postgres", db=conn)
+
+    # Now call new migration interface 💪
+    await apply_migrations(migrations_dir, conn, dry_run, force_close_conn)
+
+
+async def apply_migrations(
+    migrations_dir: str,
+    conn: ConnectionBackend,
+    dry_run: bool = False,
+    force_close_conn: bool = True,  # TODO For backwards compatibility, remove in 1.1.0
+):
+    init_task = migration.Initialize(conn)
+    migrate_task = migration.Migrate(conn)
+
+    await conn.connect()
+    await init_task.run()
+    await migrate_task.run(migrations_dir, dry_run)
+
+    if force_close_conn:
+        await conn.disconnect()
+
+
+def _get_backend(module_info: str) -> Type[ConnectionBackend]:
+    module_name, module_class_prefix = module_info.split("::")
+    module = import_module(module_name)
+    return getattr(module, f"{module_class_prefix}Connection")
+
+
+def get_connection(
+    db_name: str,
+    db_user: Optional[str] = None,
+    db_pass: Optional[str] = None,
+    db_host: Optional[str] = None,
+    db_port: Optional[Union[int, str]] = None,
+    dialect: Optional[str] = None,
+) -> ConnectionBackend:
+    """Infer db dialect if not provided and initialize connection to database"""
+    # If no dialect, infer
+    if not dialect:
+        if not db_port:
+            dialect = "sqlite"
+        elif int(db_port) == 3306:
+            dialect = "mysql"
+        elif int(db_port) == 5432:
+            dialect = "postgresql"
+        else:
+            raise RuntimeError(
+                "Unable to infer database dialect, please specify dialect"
+            )
+
+    try:
+        module_info = SUPPORTED_DIALECTS[dialect]
+    except KeyError:
+        raise RuntimeError(f"The dialect '{dialect}' is not supported")
+
+    connection = _get_backend(module_info)
+
+    return connection(
+        db_name,
+        db_user=db_user,
+        db_pass=db_pass,
+        db_host=db_host,
+        db_port=int(db_port) if db_port else None,
+    )
+
+
+@click.group()
+@click.option("-n", "--db-name", required=True, default=lambda: os.getenv("DB_NAME"))
+@click.option("-u", "--db-user", default=lambda: os.getenv("DB_USER"))
+@click.option("-s", "--db-pass", default=lambda: os.getenv("DB_PASS"))
+@click.option("-h", "--db-host", default=lambda: os.getenv("DB_HOST"))
+@click.option("-p", "--db-port", default=lambda: os.getenv("DB_PORT"))
+@click.option("-d", "--dialect", default=lambda: os.getenv("DB_DIALECT"))
+@click.option(
+    "-l", "--log-level", default=lambda: os.getenv("LOG_LEVEL", DEFAULT_LOG_LEVEL)
+)
+@click.pass_context
+def cli(ctx, **kwargs) -> None:
+    log_level = kwargs.pop("log_level")
+
+    logger.setLevel(log_level.upper())
+    logger.debug("Log %s", logging.getLevelName(log_level))
+
+    # Expose db creds to commands via context
+    ctx.ensure_object(dict)
+    ctx.obj["connection"] = get_connection(
+        db_name=kwargs["db_name"],
+        db_user=kwargs["db_user"],
+        db_pass=kwargs["db_pass"],
+        db_host=kwargs["db_host"],
+        db_port=kwargs["db_port"],
+        dialect=kwargs["dialect"],
+    )
+
+
+@cli.command(short_help="Create migrations table to begin using migri [unsupported]")
+@click.pass_context
+def init(ctx) -> None:
+    asyncio.run(run_initialization())
+
+
+@cli.command(short_help="Run all unapplied migrations in lexicographical order")
+@click.option(
+    "-m",
+    "--migrations-dir",
+    required=True,
+    default=lambda: os.getenv("MIGRATIONS_DIR", "migrations"),
+)
+@click.option("--dry-run", default=False, is_flag=True)
+@click.pass_context
+def migrate(ctx, migrations_dir: str, dry_run: bool) -> None:
+    asyncio.run(apply_migrations(migrations_dir, ctx.obj["connection"], dry_run))
+
+
+def main():
+    try:
+        cli()
+    except Exception:
+        logger.exception(
+            "Caught exception in migri.main::main that caused migri to exit"
+        )
+        sys.exit(1)
---
+++ packages/migri/migri-0.5.0-py3-none-any.whl/test/asyncpg/test_apply_migrations.py
@@ -0,0 +1,374 @@
+from datetime import datetime
+
+import pytest
+from asyncpg import InterfaceError
+from freezegun import freeze_time
+
+from migri import apply_migrations, run_migrations
+from migri.elements import Query
+
+MIGRATIONS_BASE = "test/asyncpg/migrations"
+
+# OK
+MIGRATIONS_A_DIR = f"{MIGRATIONS_BASE}/migrations_a"
+MIGRATIONS_B_DIR = f"{MIGRATIONS_BASE}/migrations_b"
+
+# Empty directory
+MIGRATIONS_C_DIR = f"{MIGRATIONS_BASE}/migrations_c"
+
+# Has an empty SQL migration file
+MIGRATIONS_D_DIR = f"{MIGRATIONS_BASE}/migrations_d"
+
+# Has an empty Python migration file
+MIGRATIONS_E_DIR = f"{MIGRATIONS_BASE}/migrations_e"
+
+# Python migration file has a sync migrate() function
+MIGRATIONS_F_DIR = f"{MIGRATIONS_BASE}/migrations_f"
+
+pytestmark = pytest.mark.asyncio
+
+
+@freeze_time("2020-7-3 22:00:01")
+async def test_apply_migrations_successful(capsys, postgresql_conn_factory):
+    """Apply migrations and then check that friendly message is output if
+    migrations are up-to-date"""
+    # Apply migrations
+    conn = postgresql_conn_factory()
+    await apply_migrations(MIGRATIONS_A_DIR, conn)
+
+    # Check that record table was created
+    record_query = Query(
+        "SELECT column_name FROM information_schema.columns "
+        "WHERE table_schema = $schema AND table_name = $table_name",
+        values={"schema": "public", "table_name": "record"},
+    )
+
+    conn = postgresql_conn_factory()
+
+    async with conn:
+        record_columns = await conn.fetch_all(record_query)
+
+    assert len(record_columns) == 2
+    assert record_columns[0]["column_name"] == "id"
+    assert record_columns[1]["column_name"] == "user_id"
+
+    # Check that migrations were recorded
+    conn = postgresql_conn_factory()
+
+    async with conn:
+        migrations_query = Query("SELECT * FROM applied_migration")
+        applied_migrations = await conn.fetch_all(migrations_query)
+
+    assert len(applied_migrations) == 3
+
+    for migration in applied_migrations:
+        assert (
+            migration["date_applied"].isoformat()
+            == f"{datetime.utcnow().isoformat()}+00:00"
+        )
+
+    # Check output
+    captured = capsys.readouterr()
+    expected_output = (
+        "Applying migrations\n"
+        "0001_initial...ok\n"
+        "0002_add_initial_data...ok\n"
+        "0003_record...ok\n"
+    )
+
+    assert captured.out == expected_output
+    assert captured.err == ""
+
+    # Apply migrations again - we expect friendly output to let us know that migrations
+    # are all up to date
+    conn = postgresql_conn_factory()
+
+    await apply_migrations(MIGRATIONS_A_DIR, conn)
+
+    captured = capsys.readouterr()
+    expected_output = "All synced! No new migrations to apply! 🥳\n"
+
+    assert captured.out == expected_output
+    assert captured.err == ""
+
+
+async def test_apply_migrations_none(capsys, postgresql_conn_factory):
+    """Ensure friendly message is output when migrations directory is empty"""
+    # Apply migrations
+    conn = postgresql_conn_factory()
+    await apply_migrations(MIGRATIONS_C_DIR, conn)
+
+    # Check that there aren't any tables
+    conn = postgresql_conn_factory()
+
+    async with conn:
+        tables_query = Query(
+            "SELECT table_name FROM information_schema.tables"
+            " WHERE table_schema='public' AND table_type='BASE TABLE';"
+        )
+        tables = await conn.fetch_all(tables_query)
+
+    assert len(tables) == 1  # applied_migration expected
+    assert tables[0]["table_name"] == "applied_migration"
+
+    # Check output
+    captured = capsys.readouterr()
+    expected_output = "No migrations to apply. Migrations directory is empty.\n"
+
+    assert captured.out == expected_output
+    assert captured.err == ""
+
+
+async def test_apply_migrations_dry_run(capsys, postgresql_conn_factory):
+    # Apply migrations in dry run mode
+    conn = postgresql_conn_factory()
+    await apply_migrations(MIGRATIONS_A_DIR, conn, dry_run=True)
+
+    # Check that there aren't any tables
+    conn = postgresql_conn_factory()
+
+    async with conn:
+        tables_query = Query(
+            "SELECT table_name FROM information_schema.tables"
+            " WHERE table_schema='public' AND table_type='BASE TABLE';"
+        )
+        tables = await conn.fetch_all(tables_query)
+
+    assert len(tables) == 1  # applied_migration expected
+    assert tables[0]["table_name"] == "applied_migration"
+
+    # Check output
+    captured = capsys.readouterr()
+    expected_output = (
+        "Applying migrations\n"
+        "0001_initial...ok\n"
+        "0002_add_initial_data...ok\n"
+        "0003_record...ok\n"
+        "Successfully applied migrations in dry run mode.\n"
+    )
+
+    assert captured.out == expected_output
+    assert captured.err == ""
+
+
+async def test_apply_migrations_with_empty_statement_successful(
+    capsys, postgresql_conn_factory
+):
+    """
+    If sql file ends w/ empty line, sqlparse returns an empty string as a statement.
+    Test that migration goes through and empty statement is filtered out
+    """
+    conn = postgresql_conn_factory()
+    await apply_migrations(MIGRATIONS_B_DIR, conn)
+
+    # Query for tables
+    conn = postgresql_conn_factory()
+
+    async with conn:
+        tables_query = Query(
+            "SELECT table_name FROM information_schema.tables"
+            " WHERE table_schema='public' AND table_type='BASE TABLE';"
+        )
+        tables = await conn.fetch_all(tables_query)
+
+    assert len(tables) == 3
+
+    for table in tables:
+        assert table["table_name"] in [
+            "applied_migration",
+            "state_machine",
+            "state_history",
+        ]
+
+    # Check output
+    captured = capsys.readouterr()
+    expected_output = "Applying migrations\n0001_initial...ok\n"
+
+    assert captured.out == expected_output
+    assert captured.err == ""
+
+
+@pytest.mark.parametrize(
+    "migrations_dir,expected_tables,expected_output,successful_migration_name",
+    [
+        (
+            MIGRATIONS_D_DIR,
+            ["applied_migration", "quote"],
+            (
+                "Applying migrations\n"
+                "0001_initial...ok\n"
+                "0002_empty...fail [empty migration]\n"
+                "0003_view...fail [previous migration failed]\n"
+            ),
+            "0001_initial",
+        ),
+        (
+            MIGRATIONS_E_DIR,
+            ["applied_migration", "satellite"],
+            (
+                "Applying migrations\n"
+                "0001_initial...ok\n"
+                "0002_load_satellites...fail [module missing migrate()]\n"
+                "0003_add_manufacturer_table...fail [previous migration failed]\n"
+            ),
+            "0001_initial",
+        ),
+        (
+            MIGRATIONS_F_DIR,
+            ["applied_migration", "student"],
+            (
+                "Applying migrations\n"
+                "0001_initial...ok\n"
+                "0002_add_students..."
+                "fail [migrate() expected to be an async function]\n"
+                "0003_school...fail [previous migration failed]\n"
+            ),
+            "0001_initial",
+        ),
+    ],
+)
+async def test_apply_migrations_empty_or_invalid_migration(
+    migrations_dir,
+    expected_tables,
+    expected_output,
+    successful_migration_name,
+    capsys,
+    postgresql_conn_factory,
+):
+    # Attempt to run migrations
+    conn = postgresql_conn_factory()
+    await apply_migrations(migrations_dir, conn)
+
+    # Query for tables
+    conn = postgresql_conn_factory()
+
+    async with conn:
+        tables_query = Query(
+            "SELECT table_name FROM information_schema.tables"
+            " WHERE table_schema='public' AND table_type='BASE TABLE';"
+        )
+        tables = await conn.fetch_all(tables_query)
+
+    assert len(tables) == 2
+
+    for table in tables:
+        assert table["table_name"] in expected_tables
+
+    # Check output
+    captured = capsys.readouterr()
+
+    assert captured.out == expected_output
+    assert captured.err == ""
+
+    # Check that successful migrations were recorded
+    conn = postgresql_conn_factory()
+
+    async with conn:
+        migrations_query = Query("SELECT * FROM applied_migration")
+        applied_migrations = await conn.fetch_all(migrations_query)
+
+    assert len(applied_migrations) == 1
+    assert applied_migrations[0]["name"] == successful_migration_name
+
+
+# TODO remove in 1.1.0
+async def test_apply_migrations_no_close_conn(postgresql_conn_factory):
+    """force_close_conn is a feature for backwards compatibility. Defaults to true"""
+    conn = postgresql_conn_factory()
+    await apply_migrations(MIGRATIONS_A_DIR, conn, force_close_conn=False)
+
+    # Check that underlying connection can still be used
+    result = await conn.database.fetchrow(
+        "SELECT * FROM account WHERE name = $1", "My Account"
+    )
+
+    assert result
+
+    await conn.disconnect()
+
+
+@freeze_time("2019-10-7 19:00:01")
+async def test_deprecated_run_migrations_successful(postgresql_conn_factory, conn):
+    await run_migrations(MIGRATIONS_A_DIR, conn, force_close_conn=False)
+
+    # Check account
+    account = await conn.fetchrow("SELECT * FROM account WHERE name = $1", "My Account")
+
+    assert account["name"] == "My Account"
+
+    # Check user
+    user = await conn.fetchrow("SELECT * FROM app_user")
+
+    assert user["account_id"] == account["id"]
+    assert user["first_name"] == "M"
+    assert user["last_name"] == "Test"
+    assert user["email"] == "test@example.com"
+    assert user["password"] == "078bbb4bf0f7117fb131ec45f15b5b87"
+
+    # Check that record table was created
+    record_columns = await conn.fetch(
+        "SELECT column_name FROM information_schema.columns "
+        "WHERE table_schema = $1 AND table_name = $2",
+        "public",
+        "record",
+    )
+
+    assert len(record_columns) == 2
+    assert record_columns[0]["column_name"] == "id"
+    assert record_columns[1]["column_name"] == "user_id"
+
+    # Check that migrations were recorded
+    applied_migrations = await conn.fetch("SELECT * FROM applied_migration")
+
+    assert len(applied_migrations) == 3
+
+    for migration in applied_migrations:
+        assert (
+            migration["date_applied"].isoformat()
+            == f"{datetime.utcnow().isoformat()}+00:00"
+        )
+
+
+async def test_deprecated_run_migrations_dry_run(postgresql_conn_factory, conn):
+    """If running in dry run mode, pending migration(s) won't be committed. Useful for
+    testing that migrations are error-free"""
+    await run_migrations(MIGRATIONS_A_DIR, conn, dry_run=True, force_close_conn=False)
+
+    tables = await conn.fetch(
+        "SELECT table_name FROM information_schema.tables"
+        " WHERE table_schema='public' AND table_type='BASE TABLE';"
+    )
+
+    assert len(tables) == 1  # applied_migration expected
+    assert tables[0]["table_name"] == "applied_migration"
+
+
+async def test_deprecated_run_migrations_with_empty_statement_successful(
+    postgresql_conn_factory, conn
+):
+    """
+    If sql file ends w/ empty line, sqlparse returns an empty string as a statement.
+    Test that migration goes through and empty statement is filtered out
+    """
+    await run_migrations(MIGRATIONS_B_DIR, conn, force_close_conn=False)
+
+    tables = await conn.fetch(
+        "SELECT table_name FROM information_schema.tables WHERE table_schema='public'"
+    )
+
+    assert len(tables) == 3
+
+    for table in tables:
+        assert table["table_name"] in [
+            "applied_migration",
+            "state_machine",
+            "state_history",
+        ]
+
+
+async def test_deprecated_run_migrations_close_conn(postgresql_conn_factory, conn):
+    """By default, passed in conn should be closed"""
+    await run_migrations(MIGRATIONS_A_DIR, conn)
+
+    with pytest.raises(InterfaceError):
+        await conn.fetchrow("SELECT * FROM account WHERE name = $1", "My Account")
