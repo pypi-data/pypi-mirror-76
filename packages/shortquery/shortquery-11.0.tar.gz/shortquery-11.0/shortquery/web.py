6124af5109b5ec398e893cdf4717d956b5063977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:08 +0000

    Add package shortquery/11.0/shortquery-11.0.tar.gz

---
+++ packages/shortquery/shortquery-11.0.tar.gz/shortquery-11.0/shortquery/web.py
@@ -0,0 +1,33 @@
+import pyshorteners
+import requests
+import webbrowser
+import json
+from icrawler.builtin import GoogleImageCrawler
+
+s = pyshorteners.Shortener()
+
+def shorten(url):
+    if "https://" or "http://" or "file:///" or "ftp://" in url:
+        mini_url = s.tinyurl.short(url)
+        return mini_url
+    else:
+        raise Exception("The URL is invalid. It should start with https://, ftp://, http:// or file:///")
+
+def downloader(keyword, imgnum):
+    google_crawler = GoogleImageCrawler(storage={'root_dir': './images'})
+    google_crawler.crawl(keyword=keyword, max_num=imgnum)
+
+def responsecode(url):
+    resp = requests.get(url).status_code
+    return resp
+
+def webopen(url):
+    webbrowser.open_new_tab(url)
+
+def decodejson(jsonobj):
+    obj = json.loads(jsonobj)
+    return obj
+
+def encodejson(dictobj):
+    obj = json.dumps(dictobj)
+    return obj
