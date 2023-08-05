60e5fe43d9a82ad59562f23f19fbed278ef6efea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:04 +0000

    Add package Sree-probability/0.2/Sree-probability-0.2.tar.gz

---
+++ packages/Sree-probability/Sree-probability-0.2.tar.gz/Sree-probability-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Sree-probability',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['Sree-probability'],
+      author='Sreedhar Gunda',
+      author_email='blueskybuildtech@gmail.com',
+      zip_safe=False)
