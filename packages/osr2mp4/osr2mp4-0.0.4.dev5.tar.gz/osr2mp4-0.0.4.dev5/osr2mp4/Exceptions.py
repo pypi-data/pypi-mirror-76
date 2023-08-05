936022b667d53db096470b80589d58a14ad774ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:53 +0000

    Add package osr2mp4/0.0.4.dev5/osr2mp4-0.0.4.dev5.tar.gz

---
+++ packages/osr2mp4/osr2mp4-0.0.4.dev5.tar.gz/osr2mp4-0.0.4.dev5/osr2mp4/Exceptions.py
@@ -0,0 +1,14 @@
+class BeatmapNotFound(Exception):
+	pass
+
+
+class ReplayNotFound(Exception):
+	pass
+
+
+class GameModeNotSupported(Exception):
+	pass
+
+
+class HarumachiCloverBan(Exception):
+	pass
