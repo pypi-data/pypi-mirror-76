2266635862c430748e52fc97a870c6c4abc81183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:30 +0000

    Add package iscan/0.3.5/iscan-0.3.5.tar.gz

---
+++ packages/iscan/iscan-0.3.5.tar.gz/iscan-0.3.5/iscan/tests/test_modules/relative.py
@@ -0,0 +1,3 @@
+from ..grandparentutils import baz
+from ..parentutils import bar
+from .utils import foo
---
+++ packages/iscan/iscan-0.3.5.tar.gz/iscan-0.3.5/setup.py
@@ -0,0 +1,36 @@
+import re
+from pathlib import Path
+
+import setuptools
+
+import iscan
+
+
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+
+setuptools.setup(
+    name='iscan',
+    version=iscan.__version__,
+    author='Zhengnan Zhao',
+    description='iscan helps you identify your project\'s third-party dependencies',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ZhengnanZhao/iscan',
+    packages=setuptools.find_packages(),
+    extras_require={
+        'build': ['setuptools', 'twine', 'wheel'],
+        'dev': ['pytest']
+    },
+    entry_points={'console_scripts': ['iscan=iscan.scan:main']},
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.5'
+)
