a0835dc8c37f4169de2a325ddb3d5630428f58ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:04 +0000

    Add package devilparser/0.0.4/devilparser-0.0.4-py2.7.egg

---
+++ packages/devilparser/devilparser-0.0.4-py2.7.egg/devilparser/rcfile.py
@@ -0,0 +1,30 @@
+import yaml
+import os
+import subprocess
+from sys import platform
+
+
+class parse(object):
+    def __init__(self, rcfile, section='main'):
+        rcfile = os.path.expanduser(rcfile)
+        raw_yaml = yaml.load(open(rcfile), Loader=yaml.FullLoader)[section]
+        shell_fields = ['password']
+        self.yaml = {}
+
+        for field, value in raw_yaml.items():
+            if value[0:2] == '$ ':
+                #value = os.popen(value[2:]).readline().strip()
+                #value = os.popen("TERM=xterm %s" % value[2:], shell=True).readline().strip()
+                if platform == "win32":
+                    value = os.popen("%s" % value[2:]).readline().strip()
+                else:
+                    value = os.popen("TERM=xterm %s" %
+                                     value[2:]).readline().strip()
+
+            self.yaml[field] = value
+
+    def contents(self):
+        return self.yaml
+
+    def __getitem__(self, item_name):
+        return self.yaml[item_name]
