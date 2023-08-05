5c07d5fca2a2fc1ce8ca7752e6a5ffd61ed212cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:20 +0000

    Add package dsnd-probability-rrld/0.2/dsnd_probability_rrld-0.2.tar.gz

---
+++ packages/dsnd-probability-rrld/dsnd_probability_rrld-0.2.tar.gz/dsnd_probability_rrld-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_rrld', 
+      version='0.2', 
+      description='Gaussian and Binomial distributions', 
+      packages=['dsnd_probability_rrld'], 
+      author='Rafael Roldao Udacity Student', 
+      zip_safe=False)
