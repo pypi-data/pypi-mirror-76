1bb0f21fe7417961f6324dbe214203b440aa7230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:12:08 +0000

    Add package salabim/20.0.3/salabim-20.0.3.tar.gz

---
+++ packages/salabim/salabim-20.0.3.tar.gz/salabim-20.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="salabim",
+    packages=["salabim"],
+    version="20.0.3",
+    include_package_data=True,
+    long_description="salabim\n\nsalabim is a discrete event simulation package in Python\nwith builtin *animation*.",
+    description="discrete event simulation in Python",
+    author="Ruud van der Ham",
+    author_email="info@salabim.org",
+    url="https://github.com/salabim/salabim",
+    download_url="https://github.com/salabim/salabim",
+    keywords=["statistics", "math", "simulation", "des", "discrete event simulation"],
+    classifiers=[],
+)
