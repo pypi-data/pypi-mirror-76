809a09478a0d88c221c4cc77f4f0d49bf4e07014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:13 +0000

    Add package beanbag-docutils/1.7.1/beanbag-docutils-1.7.1.tar.gz

---
+++ packages/beanbag-docutils/beanbag-docutils-1.7.1.tar.gz/beanbag-docutils-1.7.1/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from beanbag_docutils import get_package_version
+
+
+PACKAGE_NAME = 'beanbag-docutils'
+
+
+with open('README.rst', 'r') as fp:
+    readme = fp.read()
+
+
+setup(
+    name=PACKAGE_NAME,
+    version=get_package_version(),
+    license='MIT',
+    description="Sphinx utility modules for Beanbag's documentation format.",
+    long_description=readme,
+    url='https://github.com/beanbaginc/beanbag-docutils',
+    packages=find_packages(),
+    install_requires=[
+        'six',
+        'Sphinx>=1.7.1',
+    ],
+    maintainer='Christian Hammond',
+    maintainer_email='christian@beanbaginc.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Documentation',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
