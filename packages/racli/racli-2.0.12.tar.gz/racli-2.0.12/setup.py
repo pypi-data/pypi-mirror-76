d1d1dccdfca43c104433c940bd9454d1c1d6ea8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package racli/2.0.12/racli-2.0.12.tar.gz

---
+++ packages/racli/racli-2.0.12.tar.gz/racli-2.0.12/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['racli', 'racli.ractf', 'racli.ractf.helpers', 'racli.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0']
+
+entry_points = \
+{'console_scripts': ['racli = racli.racli:cli']}
+
+setup_kwargs = {
+    'name': 'racli',
+    'version': '2.0.12',
+    'description': 'A command-line program for interacting with RACTF.',
+    'long_description': None,
+    'author': 'Ben Griffiths',
+    'author_email': 'sendbenspam@yahoo.co.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
