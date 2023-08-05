dee139c3efe86d9958f613bf804a640967aa6803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:53 +0000

    Add package smush/0.0.7/smush-0.0.7.tar.gz

---
+++ packages/smush/smush-0.0.7.tar.gz/smush-0.0.7/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import setup
+
+# Get requirements
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# Get text of the README file
+with open('README.rst') as f:
+    README = f.read()
+
+
+setup(
+  name='smush',
+  packages=['smush'],
+  version='0.0.7',
+  license='MIT',
+  description='Tool to automate merging of topic branches',
+  author='Mike Cantelon',
+  author_email='mcantelon@gmail.com',
+
+  long_description=README,
+  long_description_content_type='text/x-rst',
+
+  url='https://github.com/artefactual-labs/smush',
+
+  keywords=['git'],
+
+  install_requires=requirements,
+
+  scripts=['bin/smush'],
+)
