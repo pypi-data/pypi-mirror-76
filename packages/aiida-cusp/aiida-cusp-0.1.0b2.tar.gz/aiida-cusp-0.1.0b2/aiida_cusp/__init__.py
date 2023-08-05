cfdabbce52aff5694cc6c69e2fc785e66b61f9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:27 +0000

    Add package aiida-cusp/0.1.0b2/aiida-cusp-0.1.0b2.tar.gz

---
+++ packages/aiida-cusp/aiida-cusp-0.1.0b2.tar.gz/aiida-cusp-0.1.0b2/aiida_cusp/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Custodian based VASP Plugin for AiiDA enabling automated error correction
+for AiiDA managed VASP calculations.
+"""
+
+__license__ = 'MIT license, see LICENSE.txt file.'
+__version__ = '0.1.0b2'
