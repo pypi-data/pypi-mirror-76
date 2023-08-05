2f59b7702efac4dbd9aa80d5af60d21c1652940c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:25 +0000

    Add package setux-distros/0.1.0/setux_distros-0.1.0.tar.gz

---
+++ packages/setux-distros/setux_distros-0.1.0.tar.gz/setux_distros-0.1.0/setux/distros/fedora.py
@@ -0,0 +1,18 @@
+from setux.core.distro import Distro
+from setux.managers.system.package import Dnf
+from setux.managers.system.service import SystemD
+
+
+class Fedora(Distro):
+    Package = Dnf
+    Service = SystemD
+    pkgmap = dict(
+        pip = 'python3-pip',
+    )
+    svcmap = dict(
+        ssh = 'sshd',
+    )
+
+
+class fedora_32(Fedora):
+    pass
