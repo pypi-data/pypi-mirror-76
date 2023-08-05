1b5c3bd25a526bf8e764707625fc45908cd9b677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:20 +0000

    Add package sortd/1.0.13/sortd-1.0.13.tar.gz

---
+++ packages/sortd/sortd-1.0.13.tar.gz/sortd-1.0.13/README.md
@@ -0,0 +1,18 @@
+# Sortd
+
+Collection of `stdin` -> `stdout` sorting commands
+
+## Commands
+
+| command | object |
+| ------- | ------ |
+| slines  | lines  |
+| sjson   | json   |
+| syaml   | yaml   |
+| stoml   | toml   |
+
+## [Install](https://pypi.org/project/sortd)
+
+```sh
+pip3 install -U sortd
+```
---
+++ packages/sortd/sortd-1.0.13.tar.gz/sortd-1.0.13/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from os import chdir
+from os.path import dirname
+
+from setuptools import setup
+
+__dir__ = dirname(__file__)
+chdir(__dir__)
+
+
+def slurp(path: str) -> str:
+    with open(path) as fd:
+        return fd.read()
+
+
+setup(
+    name="sortd",
+    version="1.0.13",
+    description="sorting commands for stdin -> stdout",
+    long_description=slurp("README.md"),
+    long_description_content_type="text/markdown",
+    author="ms-jpq",
+    author_email="github@bigly.dog",
+    url="https://github.com/ms-jpq/sortd",
+    install_requires=slurp("requirements.txt").splitlines(),
+    packages=("sortd",),
+    scripts=("scfg", "slines", "sjson", "syaml", "stoml"),
+)
