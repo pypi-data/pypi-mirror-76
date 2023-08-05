64a96cd85ba3b29d0d2a1f3890844ab476a6df9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:44 +0000

    Add package thinknum/1.87/thinknum-1.87.tar.gz

---
+++ packages/thinknum/thinknum-1.87.tar.gz/thinknum-1.87/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='thinknum',
+    packages=['thinknum'],
+    version='1.87',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Thinknum',                   # Type in your name
+    author_email='customersuccess@thinknum.com',      # Type in your E-Mail
+    url='https://www.thinknum.com/demo/',
+    download_url='https://github.com/thinknum/thinknum-client',
+    keywords=['Thinknum', 'Alternative data', 'Data'],
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
