a07ee33d7ec551355a9f36ed6c6d4a88bf6198f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:34 +0000

    Add package ploomber/0.7.1/ploomber-0.7.1.tar.gz

---
+++ packages/ploomber/ploomber-0.7.1.tar.gz/ploomber-0.7.1/src/ploomber/cli/report.py
@@ -0,0 +1,13 @@
+from ploomber.cli.parsers import _custom_command, CustomParser
+
+
+def main():
+    parser = CustomParser(description='Make a pipeline report')
+    parser.add_argument(
+        '--output',
+        '-o',
+        help='Where to save the report, defaults to pipeline.html',
+        default='pipeline.html')
+    dag, args = _custom_command(parser)
+    dag.to_markup(path=args.output)
+    print('Report saved at:', args.output)
---
+++ packages/ploomber/ploomber-0.7.1.tar.gz/ploomber-0.7.1/src/ploomber/validators/__init__.py
@@ -0,0 +1,5 @@
+from ploomber.validators.validators import (data_frame_validator,
+                                            validate_schema,
+                                            validate_values)
+
+__all__ = ['data_frame_validator', 'validate_schema', 'validate_values']
