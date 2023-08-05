0a6ca9a5f7d435acdb0de9c74c80fcdad9789e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:58 +0000

    Add package slipbox/0.8.3/slipbox-0.8.3-py3-none-any.whl

---
+++ packages/slipbox/slipbox-0.8.3-py3-none-any.whl/slipbox/filters/slipbox.lua
@@ -0,0 +1,282 @@
+local utils = require "filters/utils"
+
+local SlipBox = {}
+function SlipBox:new()
+  self.__index = self
+  return setmetatable({
+    notes = {},
+    links = {},
+    aliases = {},
+    children = {},
+    tags = {},
+    citations = {},
+  }, self)
+end
+
+function SlipBox:save_citation(id, citation)
+  -- Save citation from note id (number).
+  assert(type(id) == "number")
+  local citations = self.citations[id] or {}
+  citations[citation] = true
+  self.citations[id] = citations
+end
+
+function SlipBox:save_note(id, title)
+  assert(type(id) == "number")
+  assert(type(title) == "string")
+  assert(title ~= "")
+  self.notes[id] = {title = title}
+end
+
+local function parent_sequence(s)
+  local t, count = s:gsub('^(.-)%d+$', '%1')
+  if count ~= 0 then return t end
+  t, count = s:gsub('^(.-)%a+$', '%1')
+  if count ~= 0 then return t end
+end
+
+function SlipBox:save_sequence(link)
+  -- Set aliases and children tables (for sequence/folgezettel notes).
+  assert(link ~= nil)
+  assert(link.tag == "sequence" and link.description)
+  assert(link.description~= "")
+
+  if link.dest and link.description then
+    self.aliases[link.description] = {
+      id = link.dest,
+      owner = utils.alias_root(link.description),
+    }
+    -- dest might not be in notes if it's not in the current set of input files
+    if self.notes[link.dest] then
+      local aliases = assert(self.notes[link.dest]).aliases or {}
+      table.insert(aliases, link.description)
+      self.notes[link.dest].aliases = aliases
+    end
+    local parent = parent_sequence(link.description)
+    if parent then
+      local children = self.children[parent] or {}
+      table.insert(children, link.description)
+      self.children[parent] = children
+
+      if parent == tostring(link.src) then
+        local aliases = assert(self.notes[link.src]).aliases or {}
+        table.insert(aliases, tostring(link.src))
+        self.notes[link.src].aliases = aliases
+
+        self.aliases[tostring(link.src)] = {
+          id = link.src,
+          owner = utils.alias_root(link.description),
+        }
+      end
+    end
+  end
+end
+
+function SlipBox:save_link(link)
+  if link and link.src then
+    if link.tag == "direct" or link.tag == "sequence" then
+      local links = self.links[link.src] or {}
+      table.insert(links, link)
+      self.links[link.src] = links
+    end
+    if link.tag == "sequence" then
+      self:save_sequence(link)
+    end
+  end
+end
+
+function SlipBox:save_tag(id, tag)
+  -- Insert id into self:tags[tag].
+  assert(type(id) == "number")
+  assert(type(tag) == "string")
+  assert(tag ~= "")
+
+  local tags = self.tags[tag] or {}
+  table.insert(tags, id)
+  self.tags[tag] = tags
+end
+
+local function sqlite_string(s)
+  return string.format("'%s'", s:gsub("'", "''"))
+end
+
+local function notes_to_sql(notes, filenames)
+  -- Generate sql from notes in slipbox.
+  -- Get filenames from filenames table.
+  local values = ""
+  for id, note in pairs(notes) do
+    local result = filenames[id]
+    if result then
+      local title = sqlite_string(note.title)
+      local filename = sqlite_string(result.filename)
+      local value = string.format("(%d, %s, %s)", id, title, filename)
+      if values == "" then
+        values = values .. ' ' .. value
+      else
+        values = values .. ", " .. value
+      end
+      -- result might be nil if scan.lua couldn't find its filename.
+      -- This occurs when the title in the header contains other symbols
+      -- (ex: links, references, equations, etc.).
+      -- TODO warning
+    end
+  end
+  if values ~= "" then
+    return "INSERT OR IGNORE INTO Notes (id, title, filename) VALUES "
+      .. values .. ";\n"
+  end
+  return ""
+end
+
+local function tags_to_sql(tags)
+  -- Generate sql from tags in slipbox.
+  local values = ""
+  for tag, ids in pairs(tags) do
+    for _, id in ipairs(ids) do
+      assert(id and id == tonumber(id))
+      local value = string.format("(%s, %d)", sqlite_string(tag), id)
+      if values == "" then
+        values = values .. ' ' .. value
+      else
+        values = values .. ", " .. value
+      end
+    end
+  end
+  if values ~= "" then
+    return "INSERT OR IGNORE INTO Tags (tag, id) VALUES " .. values .. ";\n"
+  end
+  return ""
+end
+
+local function links_to_sql(links)
+  -- Create sql from direct links in slipbox.
+  -- Ignores sequence links.
+  local values = ""
+  for src, dests in pairs(links) do
+    for _, dest in ipairs(dests) do
+      if dest.tag == "direct" then
+        local annotation = sqlite_string(dest.description)
+        local value = string.format("(%d, %d, %s)", src, dest.dest, annotation)
+        if values == "" then
+          values = values .. ' ' .. value
+        else
+          values = values .. ", " .. value
+        end
+      end
+    end
+  end
+  if values ~= "" then
+    return "INSERT OR IGNORE INTO Links (src, dest, annotation) VALUES "
+      ..values.. ";\n"
+  end
+  return ""
+end
+
+local function aliases_to_sql(aliases)
+  local values = ""
+  for alias, rec in pairs(aliases) do
+    local value = string.format("(%d, %s, %d)", rec.id, sqlite_string(alias),
+      rec.owner)
+    if values == "" then
+      values = values .. ' ' .. value
+    else
+      values = values .. ", " .. value
+    end
+  end
+  if values ~= "" then
+    return "INSERT OR IGNORE INTO Aliases (id, alias, owner) VALUES "
+      .. values .. ";\n"
+  end
+  return ""
+end
+
+local function sequences_to_sql(sequences)
+  local values = ""
+  for alias, children in pairs(sequences) do
+    local parent = sqlite_string(alias)
+    for _, v in ipairs(children) do
+      local child = sqlite_string(v)
+      local value = string.format("(%s, %s)", parent, child)
+      if values == "" then
+        values = values .. ' ' .. value
+      else
+        values = values .. ", " .. value
+      end
+    end
+  end
+  if values ~= "" then
+    return "INSERT OR IGNORE INTO Sequences (prev, next) VALUES "
+      .. values .. ";\n"
+  end
+  return ""
+end
+
+local function files_to_sql(filenames)
+  local values = ""
+  local unique_filenames = {}
+  for _, v in pairs(filenames) do
+    unique_filenames[v.filename] = true
+  end
+  for filename in pairs(unique_filenames) do
+    local value = string.format("(%s)", sqlite_string(filename))
+    if values == "" then
+      values = values .. ' ' .. value
+    else
+      values = values .. ", " .. value
+    end
+  end
+  if values ~= "" then
+    return "INSERT OR IGNORE INTO Files (filename) VALUES " .. values .. ";\n"
+  end
+  return ""
+end
+
+local function citations_to_sql(citations)
+  local references = {}
+  local values = ""
+  for id, cites in pairs(citations) do
+    for cite in pairs(cites) do
+      local ref = sqlite_string("ref-"..cite)
+      references[ref] = true
+      local value = string.format("(%d, %s)", id, ref)
+      if values == "" then
+        values = values .. ' ' .. value
+      else
+        values = values .. ", " .. value
+      end
+    end
+  end
+  local sql = ""
+  if values ~= "" then
+    local ref_sql = ""
+    for ref in pairs(references) do
+      if ref_sql == "" then
+        ref_sql = ref_sql .. string.format("(%s)", ref)
+      else
+        ref_sql = ref_sql .. string.format(", (%s)", ref)
+      end
+    end
+    sql = sql .. "INSERT OR IGNORE INTO Bibliography (key) VALUES " .. ref_sql ..
+      ";\n"
+    sql = sql .. "INSERT OR IGNORE INTO Citations (note, reference) VALUES " ..
+      values ..";\n"
+    return sql
+  end
+  return ""
+end
+
+function SlipBox:to_sql(filenames)
+  -- Create sql statements from slipbox contents.
+  -- filenames
+  -- : Table that maps note ids to filenames (see scan.parse_grep_output).
+  return files_to_sql(filenames) .. notes_to_sql(self.notes, filenames) ..
+    tags_to_sql(self.tags) .. links_to_sql(self.links) ..
+    aliases_to_sql(self.aliases) .. sequences_to_sql(self.children) ..
+    citations_to_sql(self.citations)
+end
+
+return {
+  SlipBox = SlipBox,
+  -- private:
+  parent_sequence = parent_sequence,
+}
---
+++ packages/slipbox/slipbox-0.8.3-py3-none-any.whl/slipbox/test_scan.py
@@ -0,0 +1,300 @@
+# type: ignore
+"""Test scan.py."""
+
+import os
+import pathlib
+import time
+
+import pytest
+
+from . import scan
+from .utils import sqlite_string, check_requirements
+
+def insert_file_script(*files: pathlib.Path):
+    """Create SQL query string to insert into the Files table."""
+    sql = "INSERT INTO Files (filename) VALUES ({})"
+    filenames = (sqlite_string(str(p)) for p in files)
+    return sql.format("), (".join(filenames))
+
+def test_is_recently_modified(tmp_path):
+    """is_recently_modified should return true iff the input file was modified
+    after the specified timestamp.
+    """
+    before = time.time()
+    path = tmp_path/"file"
+    path.touch()
+    os.utime(path, ns=(time.time_ns(), time.time_ns()))
+    after = time.time()
+
+    assert scan.is_recently_modified(before, path)
+    assert not scan.is_recently_modified(after, path)
+
+    os.utime(path, ns=(time.time_ns(), time.time_ns()))
+    assert scan.is_recently_modified(after, path)
+
+def test_is_file_in_db(mock_db, tmp_path):
+    """Quick check for is_file_in_db."""
+    conn = mock_db
+    present = tmp_path/"present"
+    absent = tmp_path/"absent"
+    conn.executescript(insert_file_script(present.name))
+    assert scan.is_file_in_db(present.name, conn)
+    assert not scan.is_file_in_db(absent.name, conn)
+
+def test_has_valid_pattern():
+    """has_valid_pattern is true only for filenames that match at least one of
+    the wildcard patterns."""
+    patterns = ("*.md", "*.rst")
+    assert scan.has_valid_pattern("a.md", patterns)
+    assert scan.has_valid_pattern("a.rst", patterns)
+    assert not scan.has_valid_pattern("a.tex", patterns)
+    assert not scan.has_valid_pattern("a.md", (".md"))
+
+def test_files_in_path(tmp_path):
+    """files_in_path should only find files in the specified directory."""
+    subdir = tmp_path/"subdir"
+    inside = subdir/"inside.md"
+    outside = tmp_path/"outside.md"
+    nested = subdir/"nested"
+    subdir.mkdir()
+    inside.touch()
+    outside.touch()
+    nested.mkdir()
+
+    files = [p.resolve() for p in scan.files_in_path(subdir)]
+    assert inside in files
+    assert outside not in files
+    assert subdir not in files
+    assert nested not in files
+
+def test_files_in_path_to_file(tmp_path):
+    """files_in_path should yield input if it's just a file."""
+    input_file = tmp_path/"input.md"
+    input_file.touch()
+
+    files = list(scan.files_in_path(input_file))
+    assert files == [input_file]
+
+def test_find_new_files(mock_db, tmp_path):
+    """find_new_files must only return existing files that aren't yet in the
+    database and match the input patterns (*.md by default).
+    """
+    conn = mock_db
+    present = tmp_path/"present.md"
+    absent = tmp_path/"absent.md"
+    directory = tmp_path/"directory"
+    txt = tmp_path/"ignore.txt"
+    present.touch()
+    absent.touch()
+    directory.mkdir()
+    txt.touch()
+
+    conn.executescript(insert_file_script(present))
+    new_files = list(scan.find_new_files(conn, [tmp_path]))
+    assert new_files == [absent]
+
+def test_group_by_file_extension():
+    """group_by_file_extension should split by file type.
+
+    Files with no extension should be considered their own type.
+    """
+
+    files = ["a.md", "b.md", ".md", "c.tex", ".tex", ""]
+    groups = list(map(list, scan.group_by_file_extension(files)))
+    assert len(groups) == 5
+    assert ["a.md", "b.md"] in groups
+    assert [".md"] in groups
+    assert ["c.tex"] in groups
+    assert [".tex"] in groups
+    assert [""] in groups
+
+def test_group_by_file_extension_on_the_same_type():
+    """group_by_file_extension should group files with the same type together."""
+    files = [f"{c}.md" for c in "abcdefg"]
+    groups = list(map(list, scan.group_by_file_extension(files)))
+    assert len(groups) == 1
+    assert files in groups
+
+def test_build_command():
+    """Sanity check for build_command."""
+    output = "output.html"
+    options = "--mathjax"
+    assert not scan.build_command("  ", output, options)
+    cmd = scan.build_command("cha1.md cha2.md", output, options)
+    assert "cha1.md cha2.md" in cmd
+    assert f"-o {output}" in cmd
+    assert options in cmd
+
+def test_remove_outdated_files_from_database(mock_db, tmp_path):
+    """remove_outdated_files_from_database must remove:
+
+    - Files that have been deleted from the file system.
+    - Files that have been modified since the most recent scan.
+    """
+    conn = mock_db
+    missing = tmp_path/"missing.md"
+    modified = tmp_path/"modified.md"
+    temp = tmp_path/"temp.md"
+    missing.touch()
+    modified.touch()
+    temp.touch()
+
+    conn.executescript(insert_file_script(missing, modified, temp))
+    assert scan.is_file_in_db(missing, conn)
+    assert scan.is_file_in_db(modified, conn)
+    assert scan.is_file_in_db(temp, conn)
+
+    timestamp = time.time()
+    missing.unlink()
+    os.utime(modified, ns=(time.time_ns(), time.time_ns()))
+
+    scan.remove_outdated_files_from_database(conn, timestamp)
+
+    assert not scan.is_file_in_db(missing, conn)
+    assert not scan.is_file_in_db(modified, conn)
+    assert scan.is_file_in_db(temp, conn)
+
+@pytest.mark.skipif(not check_requirements(), reason="requires grep and pandoc")
+def test_scan(mock_db, tmp_path):
+    """Smoke test for scan."""
+    input_file = tmp_path/"input.md"
+    input_file.write_text("# 1 Test note\n\nHello, world!\n")
+    assert not list(mock_db.execute("SELECT * FROM Html"))
+    scan.scan(mock_db, [input_file], "", False)
+    assert len(list(mock_db.execute("SELECT * FROM Html"))) == 1
+
+@pytest.mark.skipif(not check_requirements(), reason="requires grep and pandoc")
+def test_scan_empty_file(mock_db, tmp_path):
+    """Scanned files that are empty shouldn't have entries in the database."""
+    empty = tmp_path/"empty.md"
+    empty.touch()
+    scan.scan(mock_db, [empty], "", False)
+    assert not list(mock_db.execute("SELECT * FROM Files"))
+    assert not list(mock_db.execute("SELECT * FROM Notes"))
+    assert not list(mock_db.execute("SELECT * FROM Tags"))
+    assert not list(mock_db.execute("SELECT * FROM Links"))
+    assert not list(mock_db.execute("SELECT * FROM Aliases"))
+    assert not list(mock_db.execute("SELECT * FROM Sequences"))
+    assert not list(mock_db.execute("SELECT * FROM Html"))
+    assert not list(mock_db.execute("SELECT * FROM Sections"))
+    assert not list(mock_db.execute("SELECT * FROM Bibliography"))
+    assert not list(mock_db.execute("SELECT * FROM Citations"))
+
+@pytest.mark.skipif(not check_requirements(), reason="requires grep and pandoc")
+def test_scan_filenames(mock_db, tmp_path):
+    """Filenames must be grepped from input files only."""
+    markdown = tmp_path/"foo.md"
+    skip = tmp_path/"bar.md"
+    markdown.write_text("# 0 Note\n\nBody.\n")
+    skip.write_text("# 0 Note\n\nBody.\n")
+    scan.scan(mock_db, [markdown], "", False)
+
+    result = list(mock_db.execute("SELECT filename FROM Notes WHERE id = 0"))
+    assert len(result) == 1
+    assert markdown.samefile(result[0][0])
+
+@pytest.mark.skipif(not check_requirements(), reason="requires grep and pandoc")
+def test_scan_filenames0(mock_db, tmp_path):
+    """Filenames must be grepped from input files only."""
+    markdown = tmp_path/"bar.md"
+    skip = tmp_path/"foo.md"
+    markdown.write_text("# 0 Note\n\nBody.\n")
+    skip.write_text("# 0 Note\n\nBody.\n")
+    scan.scan(mock_db, [markdown], "", False)
+
+    result = list(mock_db.execute("SELECT filename FROM Notes WHERE id = 0"))
+    assert len(result) == 1
+    assert markdown.samefile(result[0][0])
+
+@pytest.mark.skipif(not check_requirements(), reason="requires grep and pandoc")
+def test_scan_external_sequence_links(mock_db, tmp_path):
+    """Alias.owner must be equal to the root of the sequence defined by the
+    alias, not to the source of the sequence link.
+    """
+    markdown = tmp_path/"test.md"
+    markdown.write_text("""# 0 Foo
+
+[Bar](#1 '0a')
+
+# 1 Bar
+
+[Bar](#1 '0b')
+""")
+    scan.scan(mock_db, [markdown], "", False)
+    result = list(mock_db.execute("""
+        SELECT id, owner, alias FROM Aliases ORDER BY alias
+    """))
+    assert len(result) == 3
+    assert result[0] == (0, 0, '0')
+    assert result[1] == (1, 0, '0a')
+    assert result[2] == (1, 0, '0b')
+
+@pytest.mark.skipif(not check_requirements(), reason="requires grep and pandoc")
+def test_scan_non_level1_headers(mock_db, tmp_path):
+    """Only level 1 headers must be considered as note headers."""
+    markdown = tmp_path/"test.md"
+    markdown.write_text("""# 0 Valid note header
+
+Foo.
+
+## 1 Invalid note header
+
+Bar.
+""")
+    scan.scan(mock_db, [markdown], "", False)
+    result = [nid for nid, in mock_db.execute("SELECT id FROM Notes")]
+    assert len(result) == 1
+    assert result == [0]
+
+def test_input_files_that_match_pattern(mock_db, tmp_path):
+    """input_files must only return files that match the input pattern."""
+    directory = tmp_path/"directory"
+    markdown = tmp_path/"input.md"
+    txt = tmp_path/"input.txt"
+    tex = tmp_path/"ignore.tex"
+    directory.mkdir()
+    markdown.touch()
+    txt.touch()
+    tex.touch()
+
+    conn = mock_db
+    timestamp = time.time()
+    patterns = ("*.md", "*.txt")
+    inputs = scan.input_files(conn, timestamp, [tmp_path], patterns)
+    assert sorted(inputs) == [markdown, txt]
+
+def test_input_files_not_in_db(mock_db, tmp_path):
+    """input_files must not include files already in the database."""
+    new = tmp_path/"new.md"
+    skip = tmp_path/"skip.md"
+    new.touch()
+    skip.touch()
+
+    conn = mock_db
+    conn.executescript(insert_file_script(skip))
+
+    timestamp = time.time()
+    inputs = scan.input_files(conn, timestamp, [tmp_path])
+    assert list(inputs) == [new]
+
+def test_input_files_recursive(mock_db, tmp_path):
+    """input_files must find files recursively."""
+    directory = tmp_path/"directory"
+    new = directory/"new.md"
+    directory.mkdir()
+    new.touch()
+
+    conn = mock_db
+    timestamp = time.time()
+    inputs = scan.input_files(conn, timestamp, [tmp_path])
+    assert sorted(inputs) == [new]
+
+def test_input_files_single_input(mock_db, tmp_path):
+    """input_files must work with a single input file."""
+    new = tmp_path/"new.md"
+    new.touch()
+
+    conn = mock_db
+    timestamp = time.time()
+    inputs = scan.input_files(conn, timestamp, [new])
+    assert sorted(inputs) == [new]
