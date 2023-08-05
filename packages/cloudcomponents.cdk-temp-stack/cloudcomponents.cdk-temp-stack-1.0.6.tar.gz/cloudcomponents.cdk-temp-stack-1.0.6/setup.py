801e943d2def34128eee857af2d50028b01e0aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:02 +0000

    Add package cloudcomponents.cdk-temp-stack/1.0.6/cloudcomponents.cdk-temp-stack-1.0.6.tar.gz

---
+++ packages/cloudcomponents.cdk-temp-stack/cloudcomponents.cdk-temp-stack-1.0.6.tar.gz/cloudcomponents.cdk-temp-stack-1.0.6/setup.py
@@ -0,0 +1,62 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cloudcomponents.cdk-temp-stack",
+    "version": "1.0.6",
+    "description": "A stack that destroys itself after a given time (ttl)",
+    "license": "MIT",
+    "url": "https://github.com/cloudcomponents/cdk-constructs",
+    "long_description_content_type": "text/markdown",
+    "author": "hupe1980",
+    "project_urls": {
+        "Source": "https://github.com/cloudcomponents/cdk-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cloudcomponents.cdk_temp_stack",
+        "cloudcomponents.cdk_temp_stack._jsii"
+    ],
+    "package_data": {
+        "cloudcomponents.cdk_temp_stack._jsii": [
+            "cdk-temp-stack@1.0.6.jsii.tgz"
+        ],
+        "cloudcomponents.cdk_temp_stack": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii>=1.10.0, <2.0.0",
+        "publication>=0.0.3",
+        "aws-cdk.aws-events>=1.57.0, <2.0.0",
+        "aws-cdk.aws-events-targets>=1.57.0, <2.0.0",
+        "aws-cdk.aws-iam>=1.57.0, <2.0.0",
+        "aws-cdk.aws-lambda>=1.57.0, <2.0.0",
+        "aws-cdk.aws-logs>=1.57.0, <2.0.0",
+        "aws-cdk.core>=1.57.0, <2.0.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ]
+}
+"""
+)
+
+with open("README.md") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
