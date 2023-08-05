8ee4412f483d43d67e398129420467a0eacf9e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:37 +0000

    Add package youtube-search-python/1.2.8/youtube-search-python-1.2.8.tar.gz

---
+++ packages/youtube-search-python/youtube-search-python-1.2.8.tar.gz/youtube-search-python-1.2.8/youtubesearchpython/__requesthandler.py
@@ -0,0 +1,30 @@
+import sys
+from urllib.request import urlopen
+from urllib.parse import urlencode
+
+
+class requesthandler:
+
+    def request(self):
+        
+        try:
+            query = urlencode({
+                "search_query": self.keyword,
+                "page": self.offset,
+                "sp": self.searchPreferences
+            })
+            request = "https://www.youtube.com/results" + "?" + query
+
+            #########Making Network Request#########
+
+            response = urlopen(request).read()
+
+            self.page = response.decode('utf_8')
+
+            #########Identifying the type of response returned.#########
+
+            if self.page[0:29] == '  <!DOCTYPE html><html lang="':
+                self.validResponse = True
+
+        except: 
+            self.networkError = True
