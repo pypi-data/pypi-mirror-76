a2567a8e97ec3e82ecedc858199114771be74e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:43 +0000

    Add package yongshi-pyeth/0.1.3/yongshi-pyeth-0.1.3.tar.gz

---
+++ packages/yongshi-pyeth/yongshi-pyeth-0.1.3.tar.gz/yongshi-pyeth-0.1.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yongshi-pyeth",
+    version="0.1.3",
+    author="Michael-Yongshi",
+    author_email="4registration@outlook.com",
+    description="A python library for web3 eth",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Michael-Yongshi/Py-Library-Eth",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
