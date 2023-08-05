7ab8fce3677e3388559a1627d7a89caf616aec36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:54 +0000

    Add package NBprocessing/0.0.9/NBprocessing-0.0.9.tar.gz

---
+++ packages/NBprocessing/NBprocessing-0.0.9.tar.gz/NBprocessing-0.0.9/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='NBprocessing',
+    version='0.0.9',
+    author="Nir Barazida",
+    author_email="nirbarazida@gmail.com",
+    description="Pre-processing database using pre-written functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nirbarazida/NBprocessing",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
+
+# install_requires=['pandas', 'numpy','matplotlib','seaborn']
+
+# name = pip install name
+# 0.0.X - unstable
+# scripts - all the py file we
+# will look for every file that has a __init__ and will load it
+#  find_packages(exclude=['docs','test*'])
+# scripts = ['check']
+
+
+# twine upload --skip-existing dist/*
