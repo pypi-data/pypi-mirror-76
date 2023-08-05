4600b1bab5b9cbbc3b3d93ecf76dd58568bbafc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:55 +0000

    Add package ashe/0.0.1/ashe-0.0.1.tar.gz

---
+++ packages/ashe/ashe-0.0.1.tar.gz/ashe-0.0.1/README.md
@@ -0,0 +1,13 @@
+# ashe
+
+✨ `ashe` is super extension of Python.
+
+## Install
+
+```bash
+pip install ashe
+```
+
+## License
+
+[![](https://award.dovolopor.com?lt=License&rt=MIT&rbc=green)](./LICENSE)
---
+++ packages/ashe/ashe-0.0.1.tar.gz/ashe-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+NAME = "ashe"
+AUTHOR = "Ailln"
+EMAIL = "kinggreenhall@gmail.com"
+URL = "https://github.com/Ailln/ashe"
+LICENSE = "MIT License"
+DESCRIPTION = "ashe is super extension of Python."
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version="0.0.1",
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=open("./requirements.txt", "r").read().splitlines(),
+        long_description=open("./README.md", "r").read(),
+        long_description_content_type='text/markdown',
+        zip_safe=True,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            f"License :: OSI Approved :: {LICENSE}",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6"
+    )
