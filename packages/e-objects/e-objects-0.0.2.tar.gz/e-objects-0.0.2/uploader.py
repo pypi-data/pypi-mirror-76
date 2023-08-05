747cb5ab113916b2dbe841f7e8d27eb25d2457d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:54 +0000

    Add package e-objects/0.0.2/e-objects-0.0.2.tar.gz

---
+++ packages/e-objects/e-objects-0.0.2.tar.gz/e-objects-0.0.2/uploader.py
@@ -0,0 +1,4 @@
+import os
+os.system('python setup.py sdist')
+os.system(r'twine upload --repository-url https://upload.pypi.org/legacy/ dist/* '
+          r'--username evolvestin --password YweyTu7hYrTvMDfj7AnX2fYLG6ZzGeRewyym8QaY')
---
+++ packages/e-objects/e-objects-0.0.2.tar.gz/e-objects-0.0.2/version.py
@@ -0,0 +1,2 @@
+version = '0.0.2'
+requirements = ['heroku3', 'aiogram', 'pyTelegramBotApi', 'requests', 'bs4', 'Unidecode']
