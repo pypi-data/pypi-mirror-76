1061f95db2bbb678dfb811f7b530416e25e30f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:21 +0000

    Add package elt-tools-aio/0.1.0/elt_tools_aio-0.1.0-py3-none-any.whl

---
+++ packages/elt-tools-aio/elt_tools_aio-0.1.0-py3-none-any.whl/elt_tools_aio/engines.py
@@ -0,0 +1,58 @@
+import logging
+from unittest.mock import MagicMock
+from sqlalchemy.engine import create_engine
+from sqlalchemy.sql import text
+from sqlalchemy_aio import ASYNCIO_STRATEGY
+
+
+OLTP_ENGINE = 'oltp_engine'
+BIGQUERY_ENGINE = 'bigquery_engine'
+REDSHIFT_ENGINE = 'redshift_engine'
+MOCK_ENGINE = 'mock_engine'
+
+
+def mock_engine(*args, **kwargs):
+    return MagicMock()
+
+
+def bigquery_engine(gcp_project=None, dataset_id=None, gcp_credentials=None, **kwargs):
+    bigquery_uri = f'bigquery://{gcp_project}/{dataset_id}'
+    engine = create_engine(bigquery_uri, credentials_path=gcp_credentials, strategy=ASYNCIO_STRATEGY, pool_size=5, max_overflow=10)
+    return engine
+
+
+def redshift_engine(sql_alchemy_conn_string=None, default_schema='public', connect_timeout=3600, **kwargs):
+    engine = create_engine(sql_alchemy_conn_string, connect_args={'connect_timeout': connect_timeout}, strategy=ASYNCIO_STRATEGY)
+    engine.execute(text('SET search_path TO %s,public;' % default_schema).execution_options(
+        autocommit=True))
+    return engine
+
+
+def oltp_engine(sql_alchemy_conn_string=None, connect_timeout=3600, **kwargs):
+    connect_args = {}
+    if connect_timeout is not None:
+        connect_args['connect_timeout'] = connect_timeout
+    if sql_alchemy_conn_string is None:
+        raise ValueError("Got a None connection string when trying to create engine.")
+    engine = create_engine(sql_alchemy_conn_string, connect_args=connect_args, strategy=ASYNCIO_STRATEGY, pool_size=5, max_overflow=10)
+    return engine
+
+
+engine_key_to_engine = {
+    OLTP_ENGINE: oltp_engine,
+    BIGQUERY_ENGINE: bigquery_engine,
+    REDSHIFT_ENGINE: redshift_engine,
+    MOCK_ENGINE: mock_engine,
+}
+
+
+def engine_from_settings(db_key, database_settings):
+    # Use settings that are passed in,
+    # otherwise read from settings module
+    config = database_settings[db_key]
+    engine_func = engine_key_to_engine[config['engine']]
+    params = config.copy()
+    del params['engine']
+    logging.info("Creating engine for %s" % db_key)
+    engine = engine_func(**params)
+    return engine
---
+++ packages/elt-tools-aio/elt_tools_aio-0.1.0-py3-none-any.whl/elt_tools_aio/tests/test_client.py
@@ -0,0 +1,71 @@
+import datetime
+from elt_tools_aio.client import _construct_where_clause_from_timerange, DataClientFactory, DataClient, ELTDBPairFactory, \
+    ELTDBPair
+
+
+def test_construct_where_clause_with_datetimes():
+    start_datetime = datetime.datetime(2020, 1, 1, 0, 0, 0)
+    end_datetime = datetime.datetime(2020, 2, 1, 0, 0, 0)
+    timestamp_fields = ['created_at', 'updated_at']
+
+    where_clause = _construct_where_clause_from_timerange(
+        start_datetime=start_datetime,
+        end_datetime=end_datetime,
+        timestamp_fields=timestamp_fields,
+        stick_to_dates=False,  # !important
+    )
+
+    answ = " WHERE (created_at >= '2020-01-01 00:00:00' AND created_at < '2020-02-01 00:00:00')" \
+           " OR (updated_at >= '2020-01-01 00:00:00' AND updated_at < '2020-02-01 00:00:00')"
+    assert where_clause == answ, where_clause
+
+
+def test_construct_where_clause_with_dates():
+    start_datetime = datetime.datetime(2020, 1, 1)
+    end_datetime = datetime.datetime(2020, 2, 1, )
+    timestamp_fields = ['created_at', 'updated_at']
+
+    where_clause = _construct_where_clause_from_timerange(
+        start_datetime=start_datetime,
+        end_datetime=end_datetime,
+        timestamp_fields=timestamp_fields,
+        stick_to_dates=True,  # !important
+    )
+
+    answ = " WHERE (created_at >= '2020-01-01' AND created_at < '2020-02-01')" \
+           " OR (updated_at >= '2020-01-01' AND updated_at < '2020-02-01')"
+    assert where_clause == answ
+
+
+def test_data_client_factory():
+    DATABASES = {
+        'db_key11': {
+            'engine': 'mock_engine',
+        },
+        'db_key12': {
+            'engine': 'mock_engine',
+        },
+    }
+    factory = DataClientFactory(DATABASES)
+    client = factory(db_key='db_key11')
+    assert type(client) == DataClient
+
+
+def test_elt_pair_factory():
+    DATABASES = {
+        'db_key11': {
+            'engine': 'mock_engine',
+        },
+        'db_key12': {
+            'engine': 'mock_engine',
+        },
+    }
+
+    ELT_PAIRS = {
+        'pair1': {
+            'source': 'db_key11', 'target': 'db_key12'
+        },
+    }
+    factory = ELTDBPairFactory(ELT_PAIRS, DATABASES)
+    elt_pair = factory(pair_key='pair1')
+    assert type(elt_pair) == ELTDBPair
