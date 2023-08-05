ecd1cb681c1576d748988a076d0f1fb963983495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:49 +0000

    Add package pywikiapi/4.3.0/pywikiapi-4.3.0-py3-none-any.whl

---
+++ packages/pywikiapi/pywikiapi-4.3.0-py3-none-any.whl/pywikiapi/__init__.py
@@ -0,0 +1,13 @@
+"""Minimalistic MediaWiki API library by the author of the MediaWiki API itself.
+See README.md"""
+
+__version__ = "4.3.0"
+
+__author__ = "Yuri Astrakhan"
+__copyright__ = "Copyright (C) 2018-2020 Yuri Astrakhan"
+__license__ = "MIT License"
+__email__ = "YuriAstrakhan@gmail.com"
+
+from .Site import Site
+from .api import wikipedia
+from .utils import ApiError, ApiPagesModifiedError, AttrDict, to_datetime, to_timestamp
