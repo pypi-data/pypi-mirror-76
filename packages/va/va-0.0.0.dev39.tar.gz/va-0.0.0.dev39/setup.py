4d7b8e83a60017274af0fdf3bd7395eb2252ea41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:04 +0000

    Add package va/0.0.0.dev39/va-0.0.0.dev39.tar.gz

---
+++ packages/va/va-0.0.0.dev39.tar.gz/va-0.0.0.dev39/setup.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# setup.py
+from setuptools import find_packages, setup
+from va import __version__
+
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(
+    name="va",
+    version=__version__,
+    packages=find_packages(),
+    exclude_package_data={
+        'va':['test/*', 'va/onedepva.py'],
+    },
+    include_package_data=True,
+    author="Zhe Wang",
+    author_email="zhe@ebi.ac.uk",
+    description="CryoEM validation toolkit",
+    long_description=long_description,
+    long_description_content_type='text/x-rst; charset=UTF-8',
+    url="https://test.pypi.org/project/va/",
+    license="Apache License",
+    keywords="EMDB,Cryoem, Validation",
+    # setup_requires=[
+    #     "setuptools",
+    #     "wheel",
+    #     "twine",
+    # ],
+    install_requires=[
+        "numpy",
+        "scipy>=0.14",
+        "scikit-learn",
+        "Pillow",
+        "memory-profiler",
+        "matplotlib",
+        "pandas",
+        "mrcfile",
+        "emda"
+        "opencv-python"
+    ],
+    classifiers=[
+        # maturity
+        'Development Status :: 2 - Pre-Alpha',
+        # environment
+        'Environment :: Console',
+        # audience
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        # license
+        'License :: OSI Approved :: Apache Software License',
+        # python version
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 2.7',
+    ],
+    entry_points={
+        'console_scripts': [
+            'va = va.mainva:main',
+        ]
+    },
+)
