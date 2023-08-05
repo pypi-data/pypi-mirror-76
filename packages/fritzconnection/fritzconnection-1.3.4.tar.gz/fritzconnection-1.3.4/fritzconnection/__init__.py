8a692312ee6357c3104dcaef5626d30da3cc16b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:54 +0000

    Add package fritzconnection/1.3.4/fritzconnection-1.3.4.tar.gz

---
+++ packages/fritzconnection/fritzconnection-1.3.4.tar.gz/fritzconnection-1.3.4/fritzconnection/__init__.py
@@ -0,0 +1,15 @@
+"""
+fritzconnection
+
+library to communicate with the AVM Fritz!Box
+via Soap and TR-064.
+
+for documentation refer:
+https://avm.de/service/schnittstellen/
+https://fritzconnection.readthedocs.io/
+"""
+
+__version__ = '1.3.4'
+
+# import shortcut
+from .core.fritzconnection import FritzConnection
