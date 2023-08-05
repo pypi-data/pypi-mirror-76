2eedb0dabf03a669491526c7d874e861603d891d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package wandb-ng/0.0.40/wandb_ng-0.0.40-py2.py3-none-any.whl

---
+++ packages/wandb-ng/wandb_ng-0.0.40-py2.py3-none-any.whl/wandb/interface/interface.py
@@ -0,0 +1,372 @@
+# -*- coding: utf-8 -*-
+"""Backend Sender - Send to internal process
+
+Manage backend sender.
+
+"""
+
+import json
+import logging
+
+import six
+from six.moves import queue
+from wandb import data_types
+from wandb.interface import constants
+from wandb.proto import wandb_internal_pb2  # type: ignore
+from wandb.util import (
+    get_h5_typename,
+    json_dumps_safer,
+    json_dumps_safer_history,
+    json_friendly,
+    maybe_compress_summary,
+    WandBJSONEncoderOld,
+)
+
+logger = logging.getLogger("wandb")
+
+
+def file_policy_to_enum(policy):
+    if policy == "now":
+        enum = wandb_internal_pb2.FilesItem.PolicyType.NOW
+    elif policy == "end":
+        enum = wandb_internal_pb2.FilesItem.PolicyType.END
+    elif policy == "live":
+        enum = wandb_internal_pb2.FilesItem.PolicyType.LIVE
+    return enum
+
+
+def file_enum_to_policy(enum):
+    if enum == wandb_internal_pb2.FilesItem.PolicyType.NOW:
+        policy = "now"
+    elif enum == wandb_internal_pb2.FilesItem.PolicyType.END:
+        policy = "end"
+    elif enum == wandb_internal_pb2.FilesItem.PolicyType.LIVE:
+        policy = "live"
+    return policy
+
+
+class BackendSender(object):
+    class ExceptionTimeout(Exception):
+        pass
+
+    def __init__(
+        self,
+        process_queue=None,
+        notify_queue=None,
+        request_queue=None,
+        response_queue=None,
+        process=None,
+    ):
+        self.process_queue = process_queue
+        self.notify_queue = notify_queue
+        self.request_queue = request_queue
+        self.response_queue = response_queue
+        self._run = None
+        self._process = process
+
+    def _hack_set_run(self, run):
+        self._run = run
+
+    def send_output(self, name, data):
+        # from vendor.protobuf import google3.protobuf.timestamp
+        # ts = timestamp.Timestamp()
+        # ts.GetCurrentTime()
+        # now = datetime.now()
+        if name == "stdout":
+            otype = wandb_internal_pb2.OutputData.OutputType.STDOUT
+        elif name == "stderr":
+            otype = wandb_internal_pb2.OutputData.OutputType.STDERR
+        else:
+            # TODO(jhr): throw error?
+            print("unknown type")
+        o = wandb_internal_pb2.OutputData(output_type=otype, line=data)
+        o.timestamp.GetCurrentTime()
+        self._send_output(o)
+
+    def _send_output(self, outdata):
+        rec = wandb_internal_pb2.Record()
+        rec.output.CopyFrom(outdata)
+        self._queue_process(rec)
+
+    def send_tbdata(self, log_dir, save):
+        tbdata = wandb_internal_pb2.TBData()
+        tbdata.log_dir = log_dir
+        tbdata.save = save
+        rec = wandb_internal_pb2.Record(tbdata=tbdata)
+        self._queue_process(rec)
+
+    def _send_history(self, history):
+        rec = self._make_record(history=history)
+        self._queue_process(rec)
+
+    def send_history(self, data, step):
+        data = data_types.history_dict_to_json(self._run, data, step)
+        history = wandb_internal_pb2.HistoryData()
+        for k, v in six.iteritems(data):
+            item = history.item.add()
+            item.key = k
+            item.value_json = json_dumps_safer_history(v)
+        self._send_history(history)
+
+    def _make_run(self, run):
+        proto_run = wandb_internal_pb2.RunData()
+        run._make_proto_run(proto_run)
+        proto_run.start_time.GetCurrentTime()
+        proto_run.host = run._settings.host
+        if run._config is not None:
+            config_dict = run._config._as_dict()
+            self._make_config(config_dict, obj=proto_run.config)
+        return proto_run
+
+    def _make_artifact(self, artifact):
+        proto_artifact = wandb_internal_pb2.ArtifactData()
+        proto_artifact.type = artifact.type
+        proto_artifact.name = artifact.name
+        proto_artifact.digest = artifact.digest
+        if proto_artifact.description:
+            proto_artifact.description = artifact.description
+        if proto_artifact.metadata:
+            proto_artifact.metadata = artifact.metadata
+        self._make_artifact_manifest(artifact.manifest, obj=proto_artifact.manifest)
+        return proto_artifact
+
+    def _make_artifact_manifest(self, artifact_manifest, obj=None):
+        proto_manifest = obj or wandb_internal_pb2.ArtifactManifest()
+        proto_manifest.version = artifact_manifest.version()
+        proto_manifest.storage_policy = artifact_manifest.storage_policy.name()
+
+        for k, v in artifact_manifest.storage_policy.config() or {}:
+            cfg = proto_manifest.storage_policy_config.add()
+            cfg.key = k
+            cfg.value_json = json.dumps(v)
+
+        for entry in sorted(artifact_manifest.entries.values(), key=lambda k: k.path):
+            proto_entry = proto_manifest.contents.add()
+            proto_entry.path = entry.path
+            proto_entry.digest = entry.digest
+            proto_entry.size = entry.size
+            if entry.ref:
+                proto_entry.ref = entry.ref
+            if entry.local_path:
+                proto_entry.local_path = entry.local_path
+            for k, v in entry.extra.items():
+                proto_extra = proto_entry.extra.add()
+                proto_extra.key = k
+                proto_extra.value_json = json.dumps(v)
+        return proto_manifest
+
+    def _make_exit(self, exit_code):
+        exit = wandb_internal_pb2.ExitData()
+        exit.exit_code = exit_code
+        return exit
+
+    def _make_config(self, config_dict, obj=None):
+        config = obj or wandb_internal_pb2.ConfigData()
+        for k, v in six.iteritems(config_dict):
+            update = config.update.add()
+            update.key = k
+            update.value_json = json_dumps_safer(json_friendly(v)[0])
+
+        return config
+
+    def _make_stats(self, stats_dict):
+        stats = wandb_internal_pb2.StatsData()
+        stats.stats_type = wandb_internal_pb2.StatsData.StatsType.SYSTEM
+        stats.timestamp.GetCurrentTime()
+        for k, v in six.iteritems(stats_dict):
+            item = stats.item.add()
+            item.key = k
+            item.value_json = json_dumps_safer(json_friendly(v)[0])
+        return stats
+
+    def _summary_encode(self, value, path_from_root):
+        """Normalize, compress, and encode sub-objects for backend storage.
+
+        value: Object to encode.
+        path_from_root: `tuple` of key strings from the top-level summary to the
+            current `value`.
+
+        Returns:
+            A new tree of dict's with large objects replaced with dictionaries
+            with "_type" entries that say which type the original data was.
+        """
+
+        # Constructs a new `dict` tree in `json_value` that discards and/or
+        # encodes objects that aren't JSON serializable.
+
+        if isinstance(value, dict):
+            json_value = {}
+            for key, value in six.iteritems(value):
+                json_value[key] = self._summary_encode(value, path_from_root + (key,))
+            return json_value
+        else:
+            path = ".".join(path_from_root)
+            friendly_value, converted = json_friendly(
+                data_types.val_to_json(self._run, path, value, namespace="summary")
+            )
+            json_value, compressed = maybe_compress_summary(
+                friendly_value, get_h5_typename(value)
+            )
+            if compressed:
+                # TODO(jhr): impleement me
+                pass
+                # self.write_h5(path_from_root, friendly_value)
+
+            return json_value
+
+    def _make_summary(self, summary_dict):
+        data = self._summary_encode(summary_dict, tuple())
+        summary = wandb_internal_pb2.SummaryData()
+        for k, v in six.iteritems(data):
+            update = summary.update.add()
+            update.key = k
+            update.value_json = json.dumps(json_friendly(v)[0], cls=WandBJSONEncoderOld)
+        return summary
+
+    def _make_files(self, files_dict):
+        files = wandb_internal_pb2.FilesData()
+        for path, policy in files_dict["files"]:
+            f = files.files.add()
+            f.path = path
+            f.policy = file_policy_to_enum(policy)
+        return files
+
+    def _make_record(
+        self,
+        run=None,
+        config=None,
+        files=None,
+        summary=None,
+        history=None,
+        stats=None,
+        exit=None,
+        artifact=None,
+    ):
+        rec = wandb_internal_pb2.Record()
+        if run:
+            rec.run.CopyFrom(run)
+        if config:
+            rec.config.CopyFrom(config)
+        if summary:
+            rec.summary.CopyFrom(summary)
+        if history:
+            rec.history.CopyFrom(history)
+        if files:
+            rec.files.CopyFrom(files)
+        if stats:
+            rec.stats.CopyFrom(stats)
+        if exit:
+            rec.exit.CopyFrom(exit)
+        if artifact:
+            rec.artifact.CopyFrom(artifact)
+        return rec
+
+    def _queue_process(self, rec):
+        if self._process and not self._process.is_alive():
+            raise Exception("problem")
+        self.process_queue.put(rec)
+        self.notify_queue.put(constants.NOTIFY_PROCESS)
+
+    def _request_flush(self):
+        # TODO: make sure request queue is cleared
+        # probably need to send a cancel message and
+        # wait for it to come back
+        pass
+
+    def _request_response(self, rec, timeout=5):
+        # TODO: make sure this is called from main process.
+        # can only be one outstanding
+        # add a cancel queue
+        rec.control.req_resp = True
+        self.request_queue.put(rec)
+        self.notify_queue.put(constants.NOTIFY_REQUEST)
+
+        try:
+            rsp = self.response_queue.get(timeout=timeout)
+        except queue.Empty:
+            self._request_flush()
+            # raise BackendSender.ExceptionTimeout("timeout")
+            return None
+
+        # returns response, err
+        return rsp
+
+    def send_run(self, run_obj):
+        run = self._make_run(run_obj)
+        rec = self._make_record(run=run)
+        self._queue_process(rec)
+
+    def send_config(self, config_dict):
+        cfg = self._make_config(config_dict)
+        self._send_config(cfg)
+
+    def _send_config(self, cfg):
+        rec = self._make_record(config=cfg)
+        self._queue_process(rec)
+
+    def send_summary(self, summary_dict):
+        summary = self._make_summary(summary_dict)
+        self._send_summary(summary)
+
+    def _send_summary(self, summary):
+        rec = self._make_record(summary=summary)
+        self._queue_process(rec)
+
+    def _send_run_sync(self, run, timeout=None):
+        """Send synchronous run object waiting for a response.
+
+        Args:
+            run: RunData object
+            timeout: number of seconds to wait
+
+        Returns:
+            RunData object
+        """
+
+        req = self._make_record(run=run)
+        resp = self._request_response(req, timeout=timeout)
+        assert resp.run_result
+        return resp.run_result
+
+    def send_run_sync(self, run_obj, timeout=None):
+        run = self._make_run(run_obj)
+        return self._send_run_sync(run, timeout=timeout)
+
+    def send_stats(self, stats_dict):
+        stats = self._make_stats(stats_dict)
+        rec = self._make_record(stats=stats)
+        self._queue_process(rec)
+
+    def send_files(self, files_dict):
+        files = self._make_files(files_dict)
+        rec = self._make_record(files=files)
+        self._queue_process(rec)
+
+    def send_artifact(
+        self, run, artifact, aliases, is_user_created=False, use_after_commit=False
+    ):
+        proto_run = self._make_run(run)
+        proto_artifact = self._make_artifact(artifact)
+        proto_artifact.run_id = proto_run.run_id
+        proto_artifact.project = proto_run.project
+        proto_artifact.entity = proto_run.entity
+        proto_artifact.user_created = is_user_created
+        proto_artifact.use_after_commit = use_after_commit
+        for alias in aliases:
+            proto_artifact.aliases.append(alias)
+        rec = self._make_record(artifact=proto_artifact)
+        self._queue_process(rec)
+
+    def send_exit(self, exit_code):
+        pass
+
+    def _send_exit_sync(self, exit_data, timeout=None):
+        req = self._make_record(exit=exit_data)
+
+        resp = self._request_response(req, timeout=timeout)
+        assert resp.exit_result
+        return resp.exit_result
+
+    def send_exit_sync(self, exit_code, timeout=None):
+        exit_data = self._make_exit(exit_code)
+        return self._send_exit_sync(exit_data, timeout=timeout)
---
+++ packages/wandb-ng/wandb_ng-0.0.40-py2.py3-none-any.whl/wandb/sdk_py27/wandb_init.py
@@ -0,0 +1,423 @@
+# -*- coding: utf-8 -*-
+"""
+init.
+"""
+
+from __future__ import print_function
+
+import datetime
+import logging
+import os
+import sys
+import time
+
+from six import raise_from, reraise
+import wandb
+from wandb.backend.backend import Backend
+from wandb.lib import console as lib_console
+from wandb.lib import filesystem, module, reporting
+from wandb.old import io_wrap
+from wandb.util import sentry_exc
+
+from .wandb_config import parse_config
+from .wandb_run import Run, RunDummy, RunManaged
+from .wandb_settings import Settings
+
+if wandb.TYPE_CHECKING:  # type: ignore
+    from typing import Optional, Union, List, Dict, Any  # noqa: F401
+
+logger = None  # logger configured during wandb.init()
+
+
+def _set_logger(log_object):
+    """Configure module logger."""
+    global logger
+    logger = log_object
+
+
+def online_status(*args, **kwargs):
+    pass
+
+
+class _WandbInit(object):
+    def __init__(self):
+        self.kwargs = None
+        self.settings = None
+        self.config = None
+        self.run = None
+        self.backend = None
+
+        self._wl = None
+        self._reporter = None
+
+    def setup(self, kwargs):
+        """Complete setup for wandb.init().
+
+        This includes parsing all arguments, applying them with settings and enabling
+        logging.
+
+        """
+        self.kwargs = kwargs
+
+        # Some settings should be persisted across multiple runs the first
+        # time setup is called.
+        # TODO: Is this the best way to do this?
+        session_settings_keys = ["anonymous"]
+        session_settings = {k: kwargs[k] for k in session_settings_keys}
+        self._wl = wandb.setup(settings=session_settings)
+        # Make sure we have a logger setup (might be an early logger)
+        _set_logger(self._wl._get_logger())
+
+        settings = self._wl.settings(
+            dict(kwargs.pop("settings", None) or tuple())
+        )
+
+        self._reporter = reporting.setup_reporter(
+            settings=settings.duplicate().freeze()
+        )
+
+        # Remove parameters that are not part of settings
+        init_config = kwargs.pop("config", None) or dict()
+        if not isinstance(init_config, dict):
+            init_config = parse_config(init_config)
+
+        # merge config with sweep (or config file)
+        self.config = self._wl._config or dict()
+        for k, v in init_config.items():
+            self.config.setdefault(k, v)
+
+        # Temporarily unsupported parameters
+        unsupported = (
+            "magic",
+            "config_exclude_keys",
+            "config_include_keys",
+            "allow_val_change",
+            "resume",
+            "force",
+        )
+        for key in unsupported:
+            val = kwargs.pop(key, None)
+            if val:
+                self._reporter.warning(
+                    "currently unsupported wandb.init() arg: %s", key
+                )
+
+        monitor_gym = kwargs.pop("monitor_gym", None)
+        if monitor_gym and len(wandb.patched["gym"]) == 0:
+            wandb.gym.monitor()
+
+        tensorboard = kwargs.pop("tensorboard", None)
+        sync_tensorboard = kwargs.pop("sync_tensorboard", None)
+        if tensorboard or sync_tensorboard and len(wandb.patched["tensorboard"]) == 0:
+            wandb.tensorboard.patch()
+
+        # prevent setting project, entity if in sweep
+        # TODO(jhr): these should be locked elements in the future or at least
+        #            moved to apply_init()
+        if settings.sweep_id:
+            for key in ("project", "entity"):
+                val = kwargs.pop(key, None)
+                if val:
+                    print("Ignored wandb.init() arg %s when running a sweep" % key)
+        settings.apply_init(kwargs)
+
+        # TODO(jhr): should this be moved? probably.
+        d = dict(_start_time=time.time(), _start_datetime=datetime.datetime.now(),)
+        settings.update(d)
+
+        if settings.jupyter:
+            self._jupyter_setup()
+
+        self._log_setup(settings)
+
+        self.settings = settings.freeze()
+
+    def _enable_logging(self, log_fname, run_id=None):
+        """Enable logging to the global debug log.  This adds a run_id to the log,
+        in case of muliple processes on the same machine.
+
+        Currently no way to disable logging after it's enabled.
+        """
+        handler = logging.FileHandler(log_fname)
+        handler.setLevel(logging.INFO)
+
+        class WBFilter(logging.Filter):
+            def filter(self, record):
+                record.run_id = run_id
+                return True
+
+        if run_id:
+            formatter = logging.Formatter(
+                "%(asctime)s %(levelname)-7s %(threadName)-10s:%(process)d "
+                "[%(run_id)s:%(filename)s:%(funcName)s():%(lineno)s] %(message)s"
+            )
+        else:
+            formatter = logging.Formatter(
+                "%(asctime)s %(levelname)-7s %(threadName)-10s:%(process)d "
+                "[%(filename)s:%(funcName)s():%(lineno)s] %(message)s"
+            )
+
+        handler.setFormatter(formatter)
+        if run_id:
+            handler.addFilter(WBFilter())
+        logger.propagate = False
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(handler)
+
+    def _safe_symlink(self, base, target, name, delete=False):
+        # TODO(jhr): do this with relpaths, but i cant figure it out on no sleep
+        if not hasattr(os, "symlink"):
+            return
+
+        pid = os.getpid()
+        tmp_name = "%s.%d" % (name, pid)
+        owd = os.getcwd()
+        os.chdir(base)
+        if delete:
+            try:
+                os.remove(name)
+            except OSError:
+                pass
+        target = os.path.relpath(target, base)
+        os.symlink(target, tmp_name)
+        os.rename(tmp_name, name)
+        os.chdir(owd)
+
+    def _jupyter_setup(self):
+        self.notebook = wandb.jupyter.Notebook()
+        ipython = self.notebook.shell
+        ipython.register_magics(wandb.jupyter.WandBMagics)
+
+        # Monkey patch ipython publish to capture displayed outputs
+        if not hasattr(ipython.display_pub, "_orig_publish"):
+            ipython.display_pub._orig_publish = ipython.display_pub.publish
+
+        def publish(data, metadata=None, **kwargs):
+            ipython.display_pub._orig_publish(data, metadata=metadata, **kwargs)
+            self.notebook.save_display(
+                ipython.execution_count, {"data": data, "metadata": metadata}
+            )
+
+        ipython.display_pub.publish = publish
+        # TODO: should we reset start or any other fancy pre or post run cell magic?
+        # ipython.events.register("pre_run_cell", reset_start)
+
+    def _log_setup(self, settings):
+        """Setup logging from settings."""
+
+        settings.log_user = settings._path_convert(
+            settings.log_dir_spec, settings.log_user_spec
+        )
+        settings.log_internal = settings._path_convert(
+            settings.log_dir_spec, settings.log_internal_spec
+        )
+        settings.sync_file = settings._path_convert(
+            settings.sync_dir_spec, settings.sync_file_spec
+        )
+        settings.files_dir = settings._path_convert(settings.files_dir_spec)
+        filesystem._safe_makedirs(os.path.dirname(settings.log_user))
+        filesystem._safe_makedirs(os.path.dirname(settings.log_internal))
+        filesystem._safe_makedirs(os.path.dirname(settings.sync_file))
+        filesystem._safe_makedirs(settings.files_dir)
+
+        log_symlink_user = settings._path_convert(settings.log_symlink_user_spec)
+        log_symlink_internal = settings._path_convert(
+            settings.log_symlink_internal_spec
+        )
+        sync_symlink_latest = settings._path_convert(settings.sync_symlink_latest_spec)
+
+        if settings.symlink:
+            self._safe_symlink(
+                os.path.dirname(sync_symlink_latest),
+                os.path.dirname(settings.sync_file),
+                os.path.basename(sync_symlink_latest),
+                delete=True,
+            )
+            self._safe_symlink(
+                os.path.dirname(log_symlink_user),
+                settings.log_user,
+                os.path.basename(log_symlink_user),
+                delete=True,
+            )
+            self._safe_symlink(
+                os.path.dirname(log_symlink_internal),
+                settings.log_internal,
+                os.path.basename(log_symlink_internal),
+                delete=True,
+            )
+
+        _set_logger(logging.getLogger("wandb"))
+        self._enable_logging(settings.log_user)
+
+        logger.info("Logging user logs to {}".format(settings.log_user))
+        logger.info("Logging internal logs to {}".format(settings.log_internal))
+
+        self._wl._early_logger_flush(logger)
+
+    def init(self):
+        s = self.settings
+        config = self.config
+
+        if s.reinit:
+            if len(self._wl._global_run_stack) > 0:
+                if len(self._wl._global_run_stack) > 1:
+                    wandb.termwarn(
+                        "If you want to track multiple runs concurrently in wandb you should use multi-processing not threads"  # noqa: E501
+                    )
+                wandb.join()
+
+        if s.mode == "noop":
+            # TODO(jhr): return dummy object
+            return None
+
+        # Make sure we are logged in
+        wandb.login()
+
+        console = s.console
+        use_redirect = True
+        stdout_master_fd, stderr_master_fd = None, None
+        stdout_slave_fd, stderr_slave_fd = None, None
+        if console == "iowrap":
+            stdout_master_fd, stdout_slave_fd = io_wrap.wandb_pty(resize=False)
+            stderr_master_fd, stderr_slave_fd = io_wrap.wandb_pty(resize=False)
+        elif console == "_win32":
+            # Not used right now
+            stdout_master_fd, stdout_slave_fd = lib_console.win32_create_pipe()
+            stderr_master_fd, stderr_slave_fd = lib_console.win32_create_pipe()
+
+        backend = Backend(mode=s.mode)
+        backend.ensure_launched(
+            settings=s,
+            stdout_fd=stdout_master_fd,
+            stderr_fd=stderr_master_fd,
+            use_redirect=use_redirect,
+        )
+        backend.server_connect()
+
+        # resuming needs access to the server, check server_status()?
+
+        run = RunManaged(config=config, settings=s)
+        run._set_console(
+            use_redirect=use_redirect,
+            stdout_slave_fd=stdout_slave_fd,
+            stderr_slave_fd=stderr_slave_fd,
+        )
+        run._set_library(self._wl)
+        run._set_backend(backend)
+        run._set_reporter(self._reporter)
+        # TODO: pass mode to backend
+        # run_synced = None
+
+        self._wl._global_run_stack.append(run)
+
+        backend._hack_set_run(run)
+
+        if s.mode == "online":
+            ret = backend.interface.send_run_sync(run, timeout=30)
+            # TODO: fail on error, check return type
+            run._set_run_obj(ret.run)
+        elif s.mode in ("offline", "dryrun"):
+            backend.interface.send_run(run)
+        elif s.mode in ("async", "run"):
+            ret = backend.interface.send_run_sync(run, timeout=10)
+            # TODO: on network error, do async run save
+            backend.interface.send_run(run)
+
+        self.run = run
+        self.backend = backend
+        module.set_global(
+            run=run,
+            config=run.config,
+            log=run.log,
+            join=run.join,
+            summary=run.summary,
+            save=run.save,
+            restore=run.restore,
+            use_artifact=run.use_artifact,
+            log_artifact=run.log_artifact,
+        )
+        self._reporter.set_context(run=run)
+        run._on_start()
+
+        return run
+
+
+def getcaller():
+    # py2 doesnt have stack_info
+    # src, line, func, stack = logger.findCaller(stack_info=True)
+    src, line, func = logger.findCaller()[:3]
+    print("Problem at:", src, line, func)
+
+
+def init(
+    job_type = None,
+    dir=None,
+    config = None,  # TODO(jhr): type is a union for argparse/absl
+    project = None,
+    entity = None,
+    reinit = None,
+    tags = None,
+    team = None,
+    group = None,
+    name = None,
+    notes = None,
+    magic = None,  # TODO(jhr): type is union
+    config_exclude_keys=None,
+    config_include_keys=None,
+    anonymous = None,
+    disable = None,
+    offline = None,
+    allow_val_change = None,
+    resume=None,
+    force=None,
+    tensorboard=None,  # alias for sync_tensorboard
+    sync_tensorboard=None,
+    monitor_gym=None,
+    id=None,
+    settings = None,
+):
+    """Initialize a wandb Run.
+
+    Args:
+        entity: alias for team.
+        team: personal user or team to use for Run.
+        project: project name for the Run.
+
+    Raises:
+        Exception: if problem.
+
+    Returns:
+        wandb Run object
+
+    """
+    assert not wandb._IS_INTERNAL_PROCESS
+    kwargs = locals()
+    try:
+        wi = _WandbInit()
+        wi.setup(kwargs)
+        try:
+            run = wi.init()
+        except (KeyboardInterrupt, Exception) as e:
+            if not isinstance(e, KeyboardInterrupt):
+                sentry_exc(e)
+            getcaller()
+            assert logger
+            logger.exception("we got issues")
+            if wi.settings.problem == "fatal":
+                raise
+            if wi.settings.problem == "warn":
+                pass
+            run = RunDummy()
+    except KeyboardInterrupt as e:
+        assert logger
+        logger.warning("interupted", exc_info=e)
+        raise_from(Exception("interrupted"), e)
+    except Exception as e:
+        assert logger
+        logger.error("error", exc_info=e)
+        # Need to build delay into this sentry capture because our exit hooks
+        # mess with sentry's ability to send out errors before the program ends.
+        sentry_exc(e, delay=True)
+        reraise(*sys.exc_info())
+        #  raise_from(Exception("problem"), e)
+    return run
