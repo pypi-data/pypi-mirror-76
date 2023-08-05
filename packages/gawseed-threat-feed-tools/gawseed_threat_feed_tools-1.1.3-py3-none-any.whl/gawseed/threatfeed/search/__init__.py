94ae9d2a3d1629b1be3c1c593b83e21a6ac5c083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:47 +0000

    Add package gawseed-threat-feed-tools/1.1.3/gawseed_threat_feed_tools-1.1.3-py3-none-any.whl

---
+++ packages/gawseed-threat-feed-tools/gawseed_threat_feed_tools-1.1.3-py3-none-any.whl/gawseed/threatfeed/search/__init__.py
@@ -0,0 +1,24 @@
+from gawseed.threatfeed.config import Config
+
+class Search(Config):
+    def __init__(self, conf, search_list, data_iterator, binary_search):
+        super().__init__(conf)
+        self._search_list = search_list
+        self._data_iterator = data_iterator
+        self._binary_search = binary_search
+
+    def initialize(self):
+        self._search_list = self._data_iterator.encode_dict(self._search_list)
+    
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        for row in self._data_iterator:
+            match = self.search(row)
+            if match:
+                row = self._data_iterator.convert_row_to_utf8(row)
+                yield (row, match)
+
+
+        
