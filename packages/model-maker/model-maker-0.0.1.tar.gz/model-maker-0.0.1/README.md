d51b1c175a77ad5b8fb3458071acc71a5c94d384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:16 +0000

    Add package model-maker/0.0.1/model-maker-0.0.1.tar.gz

---
+++ packages/model-maker/model-maker-0.0.1.tar.gz/model-maker-0.0.1/README.md
@@ -0,0 +1,36 @@
+# ModelMaker
+
+Framework and utilities for creating and validating ML models
+
+## Features
+
+- CLI tool for creation and validation of models
+- Choose from predefined templates
+- validate model
+- write custom tests for each model
+
+## Install the tool
+
+This will install the modelmaker CLI tool
+
+```bash
+git clone https://github.com/shirecoding/ModelMaker.git
+cd ModelMaker
+pip3 install ./
+```
+
+## Create a New Project
+
+```bash
+modelmaker new --name mynewproject
+```
+
+## List Templates
+
+```bash
+modelmaker templates
+```
+
+**Current Templates**
+
+- default (simple classification model)
---
+++ packages/model-maker/model-maker-0.0.1.tar.gz/model-maker-0.0.1/setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+from setuptools import find_packages
+from modelmaker import __version__
+
+setup(
+    name='model-maker',
+    version=__version__,
+    author='shirecoding',
+    author_email="shirecoding@gmail.com",
+    scripts=['bin/modelmaker'],
+    install_requires=[
+        'Jinja2',
+        'opencv-python',
+        'numpy'
+    ],
+    url='https://github.com/shirecoding/ModelMaker',
+    download_url='https://github.com/shirecoding/ModelMaker/archive/0.0.1.tar.gz',
+    long_description=open('README.md').read(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages() + [
+        'modelmaker.resources'
+    ],
+    package_data={
+        'modelmaker.resources': [
+            'templates/*',
+            'templates/**/*',
+            'templates/**/**/*',
+            'templates/**/**/**/*',
+            'templates/**/**/**/**/*',
+        ]
+    }
+)
