db1a10996a5e52a770f3b13044d39c42ae276b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:30 +0000

    Add package cdk-elasticache-monitor/0.0.11/cdk_elasticache_monitor-0.0.11-py3-none-any.whl

---
+++ packages/cdk-elasticache-monitor/cdk_elasticache_monitor-0.0.11-py3-none-any.whl/cdk-elasticache-monitor/_jsii/__init__.py
@@ -0,0 +1,30 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import jsii.compat
+import publication
+
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_cloudwatch_actions._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-elasticache-monitor",
+    "0.0.11",
+    __name__[0:-6],
+    "cdk-elasticache-monitor@0.0.11.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
