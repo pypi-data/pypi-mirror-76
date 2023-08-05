f7ccbcf4efd45be0d8f41bd5b2dc59bb5243f8e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:59 +0000

    Add package Encod/0.0.5/Encod-0.0.5.tar.gz

---
+++ packages/Encod/Encod-0.0.5.tar.gz/Encod-0.0.5/encoding/encode.py
@@ -0,0 +1,55 @@
+import base64
+import random
+
+
+def encrypter(fileToEncrypt):
+    
+
+
+    encrypted = fileToEncrypt.encode("utf-8")
+
+    encryptIn64 = base64.b64encode(encrypted)
+
+    encryptIn85 = base64.a85encode(encryptIn64)
+
+    final = encryptIn85.decode()
+
+    
+
+    asciiDictRev = {chr(i): i for i in reversed(range(128))}
+
+    returnSentence = ""
+
+    for letter in final:
+        for key, value in asciiDictRev.items():
+            if letter == key:
+                returnSentence = "".join([returnSentence, "#" + str(value)])
+
+    return returnSentence
+
+   
+
+
+def decrypter(fileToDecrypt):
+    
+
+    asciiDict = {i: chr(i) for i in range(128)}
+
+    toDecryptList = fileToDecrypt.split("#")
+
+    returnSentence = ""
+
+    for section in toDecryptList:
+        for key, value in asciiDict.items():
+            if str(key) == section:
+                returnSentence = "".join([returnSentence, value])
+
+
+    bcc = returnSentence.encode("utf-8")
+
+    bcc2 = base64.a85decode(bcc)
+
+    bcc3 = base64.b64decode(bcc2)
+
+
+    return bcc3.decode()
