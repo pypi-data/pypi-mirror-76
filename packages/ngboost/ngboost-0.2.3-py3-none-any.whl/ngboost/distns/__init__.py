40832b373b7762e3d26f3e5b0ba5a9bc8b4085d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:21 +0000

    Add package ngboost/0.2.3/ngboost-0.2.3-py3-none-any.whl

---
+++ packages/ngboost/ngboost-0.2.3-py3-none-any.whl/ngboost/distns/__init__.py
@@ -0,0 +1,6 @@
+from .distn import RegressionDistn, ClassificationDistn
+from .normal import Normal, NormalFixedVar
+from .multivariate_normal import MultivariateNormal
+from .lognormal import LogNormal
+from .exponential import Exponential
+from .categorical import k_categorical, Bernoulli
