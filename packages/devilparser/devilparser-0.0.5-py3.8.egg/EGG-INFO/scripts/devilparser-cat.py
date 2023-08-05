9ccec2ad3cdf0cbc4f8aa6b6bc647f296e46fb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:35:04 +0000

    Add package devilparser/0.0.5/devilparser-0.0.5-py3.8.egg

---
+++ packages/devilparser/devilparser-0.0.5-py3.8.egg/EGG-INFO/scripts/devilparser-cat.py
@@ -0,0 +1,15 @@
+#!/usr/local/opt/python@3.8/bin/python3.8
+import sys
+import os
+from devilparser import rcfile
+
+
+def main():
+    yaml_file = sys.argv[1]
+    print("Parsing %s" % yaml_file)
+    info = rcfile.parse(os.path.join(yaml_file)).contents()
+    print(info)
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
