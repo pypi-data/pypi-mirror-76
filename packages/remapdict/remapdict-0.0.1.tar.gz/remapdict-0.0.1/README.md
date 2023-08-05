6955a9d12236e8ef4ef69383a663d5db720cb4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:17 +0000

    Add package remapdict/0.0.1/remapdict-0.0.1.tar.gz

---
+++ packages/remapdict/remapdict-0.0.1.tar.gz/remapdict-0.0.1/README.md
@@ -0,0 +1,7 @@
+# remapdict
+
+Really simple Python function for arbitrarily remapping dictionary keys value pairs with arbitrary transformations and convenience configuration like passthrough and error handling.
+
+## Documentation
+
+TBD
---
+++ packages/remapdict/remapdict-0.0.1.tar.gz/remapdict-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="remapdict", # Replace with your own username
+    version="0.0.1",
+    author="Dylan Culfogienis",
+    author_email="dculfogienis@expr.net",
+    description="A convenience function for arbitrarily remapping dictionary keys, taking subsets, etc.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DylanCulfogienis/remapdict.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
