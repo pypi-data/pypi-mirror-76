234b03de12c0f9f8399d3ba5c51d850a87f7793c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:22 +0000

    Add package widgetastic.patternfly4/0.17.2/widgetastic.patternfly4-0.17.2.tar.gz

---
+++ packages/widgetastic.patternfly4/widgetastic.patternfly4-0.17.2.tar.gz/widgetastic.patternfly4-0.17.2/testing/conftest.py
@@ -0,0 +1,71 @@
+import os
+import subprocess
+from urllib.request import urlopen
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from wait_for import wait_for_decorator
+from widgetastic.browser import Browser
+
+
+@pytest.fixture(scope="session")
+def browser_name():
+    return os.environ["BROWSER"]
+
+
+@pytest.fixture(scope="session")
+def selenium_host(worker_id):
+    oktet = 1 if worker_id == "master" else int(worker_id.lstrip("gw")) + 1
+    host = f"127.0.0.{oktet}"
+    ps = subprocess.run(
+        [
+            "sudo",
+            "podman",
+            "run",
+            "--rm",
+            "-d",
+            "-p",
+            f"{host}:4444:4444",
+            "-p",
+            f"{host}:5999:5999",
+            "--shm-size=2g",
+            "quay.io/redhatqe/selenium-standalone:latest",
+        ],
+        stdout=subprocess.PIPE,
+    )
+    yield host
+    container_id = ps.stdout.decode("utf-8").strip()
+    subprocess.run(["sudo", "podman", "kill", container_id])
+
+
+@pytest.fixture(scope="session")
+def wait_for_selenium(selenium_host):
+    @wait_for_decorator(timeout=10, handle_exception=True)
+    def make_request():
+        urlopen(f"http://{selenium_host}:4444/wd/hub")
+
+
+@pytest.fixture(scope="module")
+def selenium(browser_name, wait_for_selenium, selenium_host):
+    command_executor = f"http://{selenium_host}:4444/wd/hub"
+    if browser_name == "firefox":
+        driver = webdriver.Remote(
+            command_executor=command_executor, desired_capabilities=DesiredCapabilities.FIREFOX,
+        )
+    elif browser_name == "chrome":
+        caps = DesiredCapabilities.CHROME.copy()
+        caps["chromeOptions"] = {"args": ["disable-dev-shm-usage", "no-sandbox"]}
+        driver = webdriver.Remote(command_executor=command_executor, desired_capabilities=caps)
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(scope="module")
+def browser(selenium, request):
+    name = request.module.__name__.split("_")[1]
+    category = getattr(request.module, "CATEGORY", "components")
+    url = f"https://patternfly-react.surge.sh/documentation/react/{category}/{name}"
+    selenium.maximize_window()
+    selenium.get(url)
+    return Browser(selenium)
