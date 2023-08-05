009a6c8e59aad9bb622df09a89b81acd6c5fba63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:34 +0000

    Add package j-basic-stock-prediction/0.0.7/j-basic-stock-prediction-0.0.7.tar.gz

---
+++ packages/j-basic-stock-prediction/j-basic-stock-prediction-0.0.7.tar.gz/j-basic-stock-prediction-0.0.7/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="j-basic-stock-prediction", # Replace with your own username
+    version="0.0.7",
+    author="dream.attempter",
+    author_email="dream.attempter@gmail.com",
+    description="Basic Stock Prediction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'tensorflow>=2.3.0',
+        'yfinance',
+        'matplotlib'
+    ],
+    package_data={
+        '': ['*.dylib']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
