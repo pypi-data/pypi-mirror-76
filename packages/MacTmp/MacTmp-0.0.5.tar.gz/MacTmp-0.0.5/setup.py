4ad38da3d279c02437466fd02d00f21efb0ff5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package MacTmp/0.0.5/MacTmp-0.0.5.tar.gz

---
+++ packages/MacTmp/MacTmp-0.0.5.tar.gz/MacTmp-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MacTmp",
+    version="0.0.5",
+    author="Kethan",
+    author_email="kethan@vegunta.com",
+    description="A package used to get temperature on Mac OS Machines. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+    python_requires='>=3.3',
+)
