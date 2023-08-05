990d4cdac9505dcf211a52653199f5bd0a407e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:17 +0000

    Add package blade-runner/1.0.4/blade_runner-1.0.4.tar.gz

---
+++ packages/blade-runner/blade_runner-1.0.4.tar.gz/blade_runner-1.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+setup(
+    name='blade_runner',
+    include_package_data=True,
+    version='1.0.4',
+    packages=find_packages(),
+    license='MIT',
+    author='Elisha Hollander',
+    classifiers=['Programming Language :: Python :: 3'],
+    install_requires=['pygame'],
+    package_data={'':['*.png','*.pxe','*.wav']}
+)
