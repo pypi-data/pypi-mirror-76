8932d46032c7793cceed2b4ef650c895d63dbc0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:44 +0000

    Add package ambiverseclient/0.3/ambiverseclient-0.3.tar.gz

---
+++ packages/ambiverseclient/ambiverseclient-0.3.tar.gz/ambiverseclient-0.3/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('requirements.txt', 'r') as r:
+    requirements = r.read().splitlines()
+
+with open('README.rst', 'r') as r:
+    readme = r.read()
+
+setup(
+    name='ambiverseclient',
+    version='0.3',
+    author="Linus Kohl",
+    author_email="linus@munichresearch.com",
+    packages=['ambiverseclient'],
+    python_requires='>=3.7',
+    license='GPLv3',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    install_requires=requirements
+)
