afbf37464286910a98d644ec09918198e2cdde98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:29 +0000

    Add package leibniz/0.0.21/leibniz-0.0.21.tar.gz

---
+++ packages/leibniz/leibniz-0.0.21.tar.gz/leibniz-0.0.21/leibniz/nn/activation.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import torch as th
+import torch.nn as nn
+
+
+class Swish(th.nn.Module):
+    def __init__(self):
+        super(Swish, self).__init__()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class Mish(th.nn.Module):
+    def __init__(self):
+        super(Mish, self).__init__()
+        self.tanh = nn.Tanh()
+        self.softplus = nn.Softplus()
+
+    def forward(self, x):
+        return x * self.tanh(self.softplus(x))
+
+
+class CappingRelu(th.nn.Module):
+    def __init__(self):
+        super(CappingRelu, self).__init__()
+        self.leaky = nn.LeakyReLU(inplace=True)
+
+    def forward(self, x):
+        return th.clamp(self.leaky(x), max=10)
+
