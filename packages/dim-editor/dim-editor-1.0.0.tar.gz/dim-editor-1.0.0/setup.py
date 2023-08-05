e78e71b08ac435470f8813043f7aa58e34be2199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:16 +0000

    Add package dim-editor/1.0.0/dim-editor-1.0.0.tar.gz

---
+++ packages/dim-editor/dim-editor-1.0.0.tar.gz/dim-editor-1.0.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import sys
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dim-editor",
+    version="1.0.0",
+    author="Leyang Zou",
+    description="A simple terminal text editor based on Vim",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BattleMage0231/dim",
+    packages=["dim"],
+    package_data={"dim": ["debug/*", "tutorial/*"]},
+    include_package_data=True,
+    install_requires = (["windows-curses"] if sys.platform.startswith("win") else []),
+    python_requires='>=3.8',
+    entry_points={
+        "console_scripts": [
+            "dim = dim.dim:launch",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
