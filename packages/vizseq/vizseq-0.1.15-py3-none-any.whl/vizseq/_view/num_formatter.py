69c02251c4f9a20bd27d0ee6873c63d594b63c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:13 +0000

    Add package vizseq/0.1.15/vizseq-0.1.15-py3-none-any.whl

---
+++ packages/vizseq/vizseq-0.1.15-py3-none-any.whl/vizseq/_view/num_formatter.py
@@ -0,0 +1,14 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+
+class VizSeqNumFormatter(object):
+    def __init__(self, precision=3):
+        self.precision = precision
+
+    def format(self):
+        pass
