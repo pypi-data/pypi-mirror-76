5d3c736a2315004c0bbdbc13ebfb6e5cad428686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:54 +0000

    Add package gridchen/0.1.2/gridchen-0.1.2-py3-none-any.whl

---
+++ packages/gridchen/gridchen-0.1.2-py3-none-any.whl/gridchen/tests/modules.js
@@ -0,0 +1,11 @@
+export const modules = [
+    'test_patch',
+    'test_date',
+    'test_GridChen',
+    'test_Clipboard',
+    "test_Converter",
+    "test_tsvToMatrix",
+    "test_DataViews",
+    "test_range",
+    "test_selection"
+];
