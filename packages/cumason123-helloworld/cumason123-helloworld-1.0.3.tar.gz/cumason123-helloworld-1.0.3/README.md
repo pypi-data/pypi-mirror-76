1c36f18c56cfb0950a400ebbeb16595445cff8f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package cumason123-helloworld/1.0.3/cumason123-helloworld-1.0.3.tar.gz

---
+++ packages/cumason123-helloworld/cumason123-helloworld-1.0.3.tar.gz/cumason123-helloworld-1.0.3/README.md
@@ -0,0 +1,5 @@
+# Automated PyPI Deployment sample
+
+For used github action see [pypi-release.yml](.github/workflows/pypi-release.yml)
+
+For automatically creating tags see [pypi_packaging.py](pypi_packaging.py)
---
+++ packages/cumason123-helloworld/cumason123-helloworld-1.0.3.tar.gz/cumason123-helloworld-1.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from pypi_packaging import _LOCAL_PYPI_VERSION
+
+
+setup(
+    name="cumason123-helloworld",
+    summary="Example automated pypi packaging",
+    long_description_content_type="text/markdown",
+    author="Curtis Mason",
+    author_email="cumason@bu.edu",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    package_dir={'':'src'},
+    packages=["helloworld"],
+    version=_LOCAL_PYPI_VERSION
+)
