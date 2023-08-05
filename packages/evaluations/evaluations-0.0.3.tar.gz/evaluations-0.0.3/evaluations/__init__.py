66f4fc279603a8d3a282b00d958fa81604b7fe80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:12 +0000

    Add package evaluations/0.0.3/evaluations-0.0.3.tar.gz

---
+++ packages/evaluations/evaluations-0.0.3.tar.gz/evaluations-0.0.3/evaluations/__init__.py
@@ -0,0 +1,9 @@
+"""Evaluations library"""
+
+__version__ = "0.0.3"
+
+__all__ = [
+    'classification',
+    'text_extraction',
+    'kaggle_2020',
+]
---
+++ packages/evaluations/evaluations-0.0.3.tar.gz/evaluations-0.0.3/setup.py
@@ -0,0 +1,28 @@
+"""setup.py"""
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="evaluations",
+    version="0.0.3",
+    author="Yaroslav Isaienkov",
+    author_email="oiuygl@gmail.com",
+    description="The library for models evaluation",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yisaienkov/evaluations",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    test_suite="tests",
+)
