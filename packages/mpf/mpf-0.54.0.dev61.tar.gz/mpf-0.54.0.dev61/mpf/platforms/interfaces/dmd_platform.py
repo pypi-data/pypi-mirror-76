a4920dbb0204aca487bacab53a0db211957b0b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:55 +0000

    Add package mpf/0.54.0.dev61/mpf-0.54.0.dev61.tar.gz

---
+++ packages/mpf/mpf-0.54.0.dev61.tar.gz/mpf-0.54.0.dev61/mpf/platforms/interfaces/dmd_platform.py
@@ -0,0 +1,23 @@
+"""Interface for monochrome and rgb platform devices."""
+import abc
+
+
+class DmdPlatformInterface(metaclass=abc.ABCMeta):
+
+    """Interface for monochrome DMDs in hardware platforms."""
+
+    __slots__ = []
+
+    @abc.abstractmethod
+    def update(self, data: bytes):
+        """Update data on the DMD.
+
+        Args:
+            data: bytes to send to DMD
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def set_brightness(self, brightness: float):
+        """Set brightness of DMD."""
+        raise NotImplementedError
---
+++ packages/mpf/mpf-0.54.0.dev61.tar.gz/mpf-0.54.0.dev61/mpf/platforms/interfaces/hardware_sound_platform_interface.py
@@ -0,0 +1,34 @@
+"""Interface for a hardware sound platform."""
+import abc
+
+
+class HardwareSoundPlatformInterface(metaclass=abc.ABCMeta):
+
+    """Interface for a hardware sound platform."""
+
+    __slots__ = []
+
+    @abc.abstractmethod
+    def play_sound(self, number: int, track: int = 1):
+        """Play a sound."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def play_sound_file(self, file: str, platform_options: dict, track: int = 1):
+        """Play a sound file."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def text_to_speech(self, text: str, platform_options: dict, track: int = 1):
+        """Text to speech output."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def set_volume(self, volume: float, track: int = 1):
+        """Set volume."""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def stop_all_sounds(self, track: int = 1):
+        """Play a sound."""
+        raise NotImplementedError
