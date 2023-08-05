4c8f8c17749fd717933cecf8fb0954989b941098
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:26 +0000

    Add package pytest-homeassistant/0.1.1/pytest_homeassistant-0.1.1-py3.7.egg

---
+++ packages/pytest-homeassistant/pytest_homeassistant-0.1.1-py3.7.egg/pytest_homeassistant.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup('homeassistant')
+    group.addoption(
+        '--foo',
+        action='store',
+        dest='dest_foo',
+        default='2020',
+        help='Set the value for the fixture "bar".'
+    )
+
+    parser.addini('HELLO', 'Dummy pytest.ini setting')
+
+
+@pytest.fixture
+def bar(request):
+    return request.config.option.dest_foo
+
+@pytest.fixture
+def foobar():
+    yield "foobar"
