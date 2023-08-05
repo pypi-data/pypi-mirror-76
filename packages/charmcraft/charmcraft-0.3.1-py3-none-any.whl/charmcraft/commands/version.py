e9747ba055075f6f99bef70c761444193b4c8a61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:11 +0000

    Add package charmcraft/0.3.1/charmcraft-0.3.1-py3-none-any.whl

---
+++ packages/charmcraft/charmcraft-0.3.1-py3-none-any.whl/charmcraft/commands/version.py
@@ -0,0 +1,33 @@
+# Copyright 2020 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For further info, check https://github.com/canonical/charmcraft
+
+
+import logging
+
+from charmcraft import __version__
+from charmcraft.cmdbase import BaseCommand
+
+logger = logging.getLogger(__name__)
+
+
+class VersionCommand(BaseCommand):
+    """Show the version."""
+    name = 'version'
+    help_msg = "show the version"
+
+    def run(self, parsed_args):
+        """Run the command."""
+        logger.info('Version: %s', __version__)
