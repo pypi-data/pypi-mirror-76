a77f59dcef12b58c53dd5744745fdb145fff6c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:16 +0000

    Add package distributions-mfp/0.1.2/distributions_mfp-0.1.2.tar.gz

---
+++ packages/distributions-mfp/distributions_mfp-0.1.2.tar.gz/distributions_mfp-0.1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_mfp',
+      version='0.1.2',
+      description='Gaussian distributions',
+      packages=['distributions_mfp'],
+      author='Wendy Navarrete',
+      zip_safe=False)
