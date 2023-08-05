6ad34048e23e3f53f421785e3130236563337d88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:21 +0000

    Add package utilimporter/1.0.0/utilimporter-1.0.0.tar.gz

---
+++ packages/utilimporter/utilimporter-1.0.0.tar.gz/utilimporter-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+ name = 'utilimporter',
+ version = '1.0.0',
+ py_modules = ['utilimporter'],
+ author = 'Akshat Garg',
+ description = 'Import every module you need in one function callt',
+ )
---
+++ packages/utilimporter/utilimporter-1.0.0.tar.gz/utilimporter-1.0.0/utilimporter.py
@@ -0,0 +1,12 @@
+import os
+def importer(name):
+    data=open(name,"w")
+    data.seek(0)
+    data.write("""import os
+import torch
+import numpy
+import math
+import turtle
+import openpyxl""")
+    os.startfile(name)
+    data.close()
