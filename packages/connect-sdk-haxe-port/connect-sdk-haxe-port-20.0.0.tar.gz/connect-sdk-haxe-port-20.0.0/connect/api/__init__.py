30015d510bd114cc829da5df110f61297804f456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:45 +0000

    Add package connect-sdk-haxe-port/20.0.0/connect-sdk-haxe-port-20.0.0.tar.gz

---
+++ packages/connect-sdk-haxe-port/connect-sdk-haxe-port-20.0.0.tar.gz/connect-sdk-haxe-port-20.0.0/connect/api/__init__.py
@@ -0,0 +1,24 @@
+from connect.autogen import connect_api_ConnectHelper as ConnectHelper
+from connect.autogen import connect_api_IApiClient as IApiClient
+from connect.autogen import connect_api_IFulfillmentApi as IFulfillmentApi
+from connect.autogen import connect_api_IGeneralApi as IGeneralApi
+from connect.autogen import connect_api_IMarketplaceApi as IMarketplaceApi
+from connect.autogen import connect_api_ITierApi as ITierApi
+from connect.autogen import connect_api_IUsageApi as IUsageApi
+from connect.autogen import connect_api_Query as Query
+from connect.autogen import connect_api_Response as Response
+from connect.autogen import connect_api_SubscriptionsApi as SubscriptionsApi
+
+
+__all__ = [
+    'ConnectHelper',
+    'IApiClient',
+    'IFulfillmentApi',
+    'IGeneralApi',
+    'IMarketplaceApi',
+    'ITierApi',
+    'IUsageApi',
+    'Query',
+    'Response',
+    'SubscriptionsApi',
+]
