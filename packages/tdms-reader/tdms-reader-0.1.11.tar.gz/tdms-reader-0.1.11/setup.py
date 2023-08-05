b2b2257074341332473ab7a8fcf73b74ea741564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:43 +0000

    Add package tdms-reader/0.1.11/tdms-reader-0.1.11.tar.gz

---
+++ packages/tdms-reader/tdms-reader-0.1.11.tar.gz/tdms-reader-0.1.11/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tdms_reader']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['bokeh>=2.1.1,<3.0.0',
+ 'nptdms>=0.27.0,<0.28.0',
+ 'pandas>=1.1.0,<2.0.0',
+ 'scipy>=1.5.2,<2.0.0',
+ 'simplelogging>=0.10.0,<0.11.0']
+
+setup_kwargs = {
+    'name': 'tdms-reader',
+    'version': '0.1.11',
+    'description': '',
+    'long_description': None,
+    'author': 'mjaquier',
+    'author_email': 'mjaquier@front.xyz',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
