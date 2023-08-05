0a0b97425a12b450f8abea2d75eb55f925852dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:46 +0000

    Add package ivolve-cloud7-logger/0.5/ivolve-cloud7-logger-0.5.tar.gz

---
+++ packages/ivolve-cloud7-logger/ivolve-cloud7-logger-0.5.tar.gz/ivolve-cloud7-logger-0.5/test/test.py
@@ -0,0 +1,14 @@
+from ivolve_cloud7_logger import Console
+data={
+        "type_": "log",
+        "message": "success",
+        "url": 'my url',
+        "messageType": {
+            "request_payload": {},
+            "response_payload": {},
+        },        
+        "project_id": 'my project id',
+        "user_id": 'my user id',
+        "domain_id": "default",
+        }
+Console.log(data)
