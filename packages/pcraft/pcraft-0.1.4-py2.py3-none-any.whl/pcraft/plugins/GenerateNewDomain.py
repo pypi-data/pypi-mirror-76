1d6972e9b87d5eccc590513bfa99e832bb129749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:20 +0000

    Add package pcraft/0.1.4/pcraft-0.1.4-py2.py3-none-any.whl

---
+++ packages/pcraft/pcraft-0.1.4-py2.py3-none-any.whl/pcraft/plugins/GenerateNewDomain.py
@@ -0,0 +1,49 @@
+from pcraft.PluginsContext import PluginsContext
+import dns.resolver
+import random
+
+class PCraftPlugin(PluginsContext):
+    name = "GenerateNewDomain"
+
+    def help(self):
+        helpstr="""
+Generate a valid non-existing domain name. It takes two words from the dictionary, seperates them with a dash and appends a '.com'.
+
+### Examples
+
+#### 1: Connect to a domain set previously from the plugin chain
+
+```
+generate:
+  _plugin: GenerateNewDomain
+  _next: dnsconnect
+```
+"""
+        return helpstr
+    
+    def __init__(self, app, session, plugins_data):
+        super().__init__(app, session, plugins_data)
+
+    def run(self, script=None):
+        self.update_vars_from_script(script)
+        new_domain_found = False
+
+        fp = open("/usr/share/dict/words")
+        words = fp.readlines()
+        while not new_domain_found:        
+            word1_idx = random.randint(1,len(words))
+            word2_idx = random.randint(1,len(words))    
+            word1 = words[word1_idx][:-1].lower()
+            word2 = words[word2_idx][:-1].lower()
+
+            domain = "%s-%s.com" % (word1, word2)
+            domain = domain.replace("'","")
+            try:
+                answers = dns.resolver.query(domain, "A")
+            except dns.resolver.NXDOMAIN:
+                new_domain_found = True
+
+        fp.close()
+        
+        self.plugins_data._set("domain", domain)
+        return script["_next"], self.plugins_data
---
+++ packages/pcraft/pcraft-0.1.4-py2.py3-none-any.whl/pcraft/plugins/Ping.py
@@ -0,0 +1,72 @@
+from IPy import IP as IP_y
+from scapy.all import *
+from . import _utils as utils
+
+from pcraft.PluginsContext import PluginsContext
+
+class PCraftPlugin(PluginsContext):
+    name = "Ping"
+
+    required = ["ip-src", "ip-dst"]
+    
+    def help(self):
+        helpstr="""
+Ping and get an ECHO REPLY between two hosts.
+
+### Examples
+
+#### 1: Ping a host
+
+```
+ping:
+  _plugin: Ping
+  _next: done
+```
+"""
+        return helpstr
+
+    def __init__(self, app, session, plugins_data):
+        super().__init__(app, session, plugins_data)
+        
+    def run(self, script=None):
+        self.update_vars_from_script(script)
+        # pprint.pprint(script)
+        ip = script["ip-dst"]
+        all_ips = IP_y(ip)
+
+        has_started = False
+        ipstart = ""
+        ipstop = ""
+        try:
+            ipstart = script["ipstart"]
+        except:
+            has_started = True
+        try:
+            ipstop = script["ipstop"]
+        except:
+            pass
+        has_stoped = False
+
+        
+        for individual_ip in all_ips:
+            if has_stoped:
+                break
+
+            if str(individual_ip).endswith(".0") or str(individual_ip).endswith(".255"):
+                continue
+            if not has_started:
+                if str(individual_ip) != ipstart:
+                    continue
+                else:
+                    has_started = True
+            
+            # PING
+            echo_request = Ether() / IP(src=script["ip-src"], dst=str(individual_ip)) / ICMP(type="echo-request")
+            self.plugins_data.pcap.append(echo_request)
+            echo_reply = Ether() / IP(src=str(individual_ip), dst=script["ip-src"]) / ICMP(type="echo-reply")
+            self.plugins_data.pcap.append(echo_reply)
+            if str(individual_ip) == ipstop:
+                has_stoped= True
+            
+            
+        return script["_next"], self.plugins_data
