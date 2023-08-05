ccfce961c53c8f747a775be3a801fbb388a98214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:02 +0000

    Add package CAMSCS-Keywords/0.0.1/CAMSCS_Keywords-0.0.1.tar.gz

---
+++ packages/CAMSCS-Keywords/CAMSCS_Keywords-0.0.1.tar.gz/CAMSCS_Keywords-0.0.1/CAMSCS_Keywords/DisposeWebDriverKW.py
@@ -0,0 +1,22 @@
+def DisposeWebDriver(self):
+        """
+        Closes the browser and shuts down the ChromeDriver executable
+        that is started when starting the ChromeDriver
+        """			
+        try:
+            self._debug('Closing all browsers')
+            
+            browser.dispose(self)
+            browser.quit(self)
+            self._cache.close_all()
+            self.empty_cache()
+            RemoteWebDriver.close(self)
+            Thread.Sleep(3000)
+            RemoteWebDriver.quit(self)
+            RemoteWebDriver.dispose(self)
+            RemoteWebDrive = null
+            WindowsUtils.KillProcessAndChildren("C:\Program Files\Python38\Scripts\chromedriver.exe") 
+        except:
+            # We don't care about the message because something probably has gone wrong
+            pass
+        
