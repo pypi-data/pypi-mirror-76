e2e1fc4cad37d54edd9594fad6e43694c1917764
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:29 +0000

    Add package awesome-panel-extensions/20200806.1/awesome-panel-extensions-20200806.1.tar.gz

---
+++ packages/awesome-panel-extensions/awesome-panel-extensions-20200806.1.tar.gz/awesome-panel-extensions-20200806.1/awesome_panel_extensions/frameworks/material/__init__.py
@@ -0,0 +1,10 @@
+"""Implementation of MWC Material Components"""
+import panel as pn
+import param
+
+from .config import MWC_ICONS
+from .material_button import Button
+from .material_extension import Extension
+from .select import MWCSelect
+from .slider import MWCSlider
+from .style.stylesheet import Stylesheet
