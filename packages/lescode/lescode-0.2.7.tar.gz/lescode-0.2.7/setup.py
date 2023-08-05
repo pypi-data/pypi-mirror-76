8e021c3918cf08c3b034991b8d4aecafa1c865f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:46 +0000

    Add package lescode/0.2.7/lescode-0.2.7.tar.gz

---
+++ packages/lescode/lescode-0.2.7.tar.gz/lescode-0.2.7/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+__VERSION__ = "0.2.7"
+
+
+setuptools.setup(
+    name="lescode",
+    packages=setuptools.find_packages(),
+    version=__VERSION__,
+    author="nghoangdat",
+    author_email="18.hoang.dat.12@gmail.com",
+    description="quiver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NgHoangDat/lescode.git",
+    download_url=f"https://github.com/NgHoangDat/lescode/archive/v{__VERSION__}.tar.gz",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'dataclasses; python_version=="3.6"',
+        'msgpack',
+        'python-dateutil',
+        'pyyaml',
+        'toolz'
+    ]
+)
