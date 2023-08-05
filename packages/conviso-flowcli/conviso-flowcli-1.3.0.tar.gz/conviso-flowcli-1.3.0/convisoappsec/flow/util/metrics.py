7084f73b9f59f8fda89221ed6cb5680fb4323c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:34:26 +0000

    Add package conviso-flowcli/1.3.0/conviso-flowcli-1.3.0.tar.gz

---
+++ packages/conviso-flowcli/conviso-flowcli-1.3.0.tar.gz/conviso-flowcli-1.3.0/convisoappsec/flow/util/metrics.py
@@ -0,0 +1,9 @@
+from convisoappsec.flow.source_code_scanner import SCC
+
+def project_metrics(source_code_dir):
+    scanner = SCC(source_code_dir)
+    scanner.scan()
+
+    return {
+        'total_lines': scanner.total_source_code_lines
+    }
