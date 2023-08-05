e92a9c8273a3f73b7282bef5b60e12fa6575743b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:37 +0000

    Add package plspm/0.5.6/plspm-0.5.6.tar.gz

---
+++ packages/plspm/plspm-0.5.6.tar.gz/plspm-0.5.6/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+#
+# Copyright (C) 2019 Google Inc.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plspm",
+    version="0.5.6",
+    author="Jez Humble",
+    author_email="humble@google.com",
+    description="A library implementing the Partial Least Squares Path Model algorithm",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/googlecloudplatform/plspm-python",
+    install_requires=[
+        "pandas",
+        "numpy",
+        "scipy",
+        "statsmodels",
+        "scikit-learn"
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
