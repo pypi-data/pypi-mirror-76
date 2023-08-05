5f8175911f8e7cd2db412ebfac8fc160dba99edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:33 +0000

    Add package AstroAtmosphere/1.2.2/AstroAtmosphere-1.2.2.tar.gz

---
+++ packages/AstroAtmosphere/AstroAtmosphere-1.2.2.tar.gz/AstroAtmosphere-1.2.2/AstroAtmosphere/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env Python3
+import os
+
+from .ciddorModel import *
+from .dispersionModels import *
+from .refractionModels import *
+from .refractionModels import *
+from .misc import *
+from .refractivityModels import *
+from .neoslalib import *
