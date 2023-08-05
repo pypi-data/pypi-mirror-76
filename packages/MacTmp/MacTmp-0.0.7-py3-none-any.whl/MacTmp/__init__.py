8488976eb394b8bbf6c50b63bb6951ee4283b8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:24 +0000

    Add package MacTmp/0.0.7/MacTmp-0.0.7-py3-none-any.whl

---
+++ packages/MacTmp/MacTmp-0.0.7-py3-none-any.whl/MacTmp/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+# ioreg -l |grep \"PerformanceStatistics\" | cut -d '{' -f 2 | tr '|' ',' | tr -d '}' | tr ',' '\n'|grep 'Temp\|Fan\|Clock'; done
+
+def CPU_Temp():
+    tmp = [each.strip() for each in (os.popen('sudo powermetrics --samplers smc -i1 -n1')).read().split('\n') if each != '']
+    return tmp[11].strip('CPU die temperature: ')
+
+def GPU_Temp():
+    return [i.strip('GPU die temperature: ' for i in os.popen('sudo powermetrics|grep -i "GPU die temperature"').readlines())]
