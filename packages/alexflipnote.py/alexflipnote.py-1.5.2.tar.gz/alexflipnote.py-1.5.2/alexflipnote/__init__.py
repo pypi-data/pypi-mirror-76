4af535bd88d9f636b84bff4486eeae0746eca676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:38 +0000

    Add package alexflipnote.py/1.5.2/alexflipnote.py-1.5.2.tar.gz

---
+++ packages/alexflipnote.py/alexflipnote.py-1.5.2.tar.gz/alexflipnote.py-1.5.2/alexflipnote/__init__.py
@@ -0,0 +1,2 @@
+from .classes import Colour, Icon, Image, Steam
+from .client import BadRequest, Client, InternalServerError, NotFound
---
+++ packages/alexflipnote.py/alexflipnote.py-1.5.2.tar.gz/alexflipnote.py-1.5.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup  # type: ignore
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name = 'alexflipnote.py',
+    description = 'An easy to use Python Wrapper for the AlexFlipnote API',
+    long_description = readme,
+    long_description_content_type = 'text/markdown',
+    version = '1.5.2',
+    packages = ['alexflipnote'],
+    url = 'https://github.com/Soheab/Alexflipnote.py',
+    download_url = 'https://github.com/Soheab/alexflipnote.py/archive/v1.5.2.tar.gz',
+    license = 'MIT',
+    author = 'Soheab_',
+    install_requires = ['aiohttp'],
+    keywords = ['alexflipnote', 'discord', 'api'],
+    project_urls = {
+        "Discord":       "https://discord.gg/DpxkY3x",
+        "Source":        "https://github.com/Soheab/alexflipnote.py",
+        "Documentation": "https://github.com/Soheab/alexflipnote.py/blob/master/docs.md",
+        "Issue tracker": "https://github.com/Soheab/alexflipnote.py/issues",
+        },
+
+    python_requires = '>=3.6',
+    )
