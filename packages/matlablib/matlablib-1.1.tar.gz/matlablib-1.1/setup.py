5e32383cd0a46667df3cd20dd8682ce2a7f0b560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:10:06 +0000

    Add package matlablib/1.1/matlablib-1.1.tar.gz

---
+++ packages/matlablib/matlablib-1.1.tar.gz/matlablib-1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+	
+setup(
+	long_description = long_description,
+	long_description_content_type="text/markdown",
+	name = 'matlablib',
+	version='1.1',
+	description = 'Matlab-tool suite', 
+	license = 'MIT',
+	author='Josh Philipson',
+	author_email = 'philipson@gmail.com',
+	url='https://github.com/OSHI7/matlablib',
+	py_modules = ["matlablib"],
+	package_dir = {'': 'src'},
+	classifiers = [
+	"Programming Language :: Python :: 3",
+	"Programming Language :: Python :: 3.6",
+	"Programming Language :: Python :: 3.7",
+	"Programming Language :: Python :: 3.8",
+	"License :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent"],
+	install_requires = ['pandas', 'matplotlib', 'ipython'],
+	)
