2b4d336443ffff55a1b56a8aec82ff6c9c9c611d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package quickstartup/0.17.0/quickstartup-0.17.0-py2.py3-none-any.whl

---
+++ packages/quickstartup/quickstartup-0.17.0-py2.py3-none-any.whl/quickstartup/qs_pages/urlresolver.py
@@ -0,0 +1,12 @@
+from django.urls import NoReverseMatch
+
+from .models import Page
+
+
+def page_reverse(slug):
+    page = Page.objects.filter(slug=slug).first()
+
+    if not page:
+        raise NoReverseMatch("Page {!r} not found.".format(slug))
+
+    return page.get_absolute_url()
