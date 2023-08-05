9d43e59eac491516226f14f3f1ca9dcb1e134d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:45 +0000

    Add package redis2/2.10.6.1/redis2-2.10.6.1.tar.gz

---
+++ packages/redis2/redis2-2.10.6.1.tar.gz/redis2-2.10.6.1/redis2/__init__.py
@@ -0,0 +1,34 @@
+from redis2.client import Redis, StrictRedis
+from redis2.connection import (
+    BlockingConnectionPool,
+    ConnectionPool,
+    Connection,
+    SSLConnection,
+    UnixDomainSocketConnection
+)
+from redis2.utils import from_url
+from redis2.exceptions import (
+    AuthenticationError,
+    BusyLoadingError,
+    ConnectionError,
+    DataError,
+    InvalidResponse,
+    PubSubError,
+    ReadOnlyError,
+    RedisError,
+    ResponseError,
+    TimeoutError,
+    WatchError
+)
+
+
+__version__ = '2.10.6.1'
+VERSION = tuple(map(int, __version__.split('.')))
+
+__all__ = [
+    'Redis', 'StrictRedis', 'ConnectionPool', 'BlockingConnectionPool',
+    'Connection', 'SSLConnection', 'UnixDomainSocketConnection', 'from_url',
+    'AuthenticationError', 'BusyLoadingError', 'ConnectionError', 'DataError',
+    'InvalidResponse', 'PubSubError', 'ReadOnlyError', 'RedisError',
+    'ResponseError', 'TimeoutError', 'WatchError'
+]
