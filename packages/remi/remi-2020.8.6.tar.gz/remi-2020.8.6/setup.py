556111739e86358e2f0161900a6894436b8dc517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:31 +0000

    Add package remi/2020.8.6/remi-2020.8.6.tar.gz

---
+++ packages/remi/remi-2020.8.6.tar.gz/remi-2020.8.6/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='remi',
+    version='2020.08.06',
+    description='Python REMote Interface library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/dddomodossola/remi',
+    download_url='https://github.com/dddomodossola/remi/archive/master.zip',
+    keywords=['gui-library','remi','platform-independent','ui','gui'],
+    author='Davide Rosa',
+    author_email='dddomodossola@gmail.com',
+    license='Apache',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+)
