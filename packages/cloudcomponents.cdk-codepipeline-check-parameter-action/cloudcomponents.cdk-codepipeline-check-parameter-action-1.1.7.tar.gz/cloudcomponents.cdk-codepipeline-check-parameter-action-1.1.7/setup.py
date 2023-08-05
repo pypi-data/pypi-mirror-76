65a0a20d5c728584321fd9aff061d6caf3f7a5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:59 +0000

    Add package cloudcomponents.cdk-codepipeline-check-parameter-action/1.1.7/cloudcomponents.cdk-codepipeline-check-parameter-action-1.1.7.tar.gz

---
+++ packages/cloudcomponents.cdk-codepipeline-check-parameter-action/cloudcomponents.cdk-codepipeline-check-parameter-action-1.1.7.tar.gz/cloudcomponents.cdk-codepipeline-check-parameter-action-1.1.7/setup.py
@@ -0,0 +1,61 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cloudcomponents.cdk-codepipeline-check-parameter-action",
+    "version": "1.1.7",
+    "description": "Cdk component that checks if system parameters are set correctly",
+    "license": "MIT",
+    "url": "https://github.com/cloudcomponents/cdk-constructs",
+    "long_description_content_type": "text/markdown",
+    "author": "hupe1980",
+    "project_urls": {
+        "Source": "https://github.com/cloudcomponents/cdk-constructs.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cloudcomponents.cdk_codepipeline_check_parameter_action",
+        "cloudcomponents.cdk_codepipeline_check_parameter_action._jsii"
+    ],
+    "package_data": {
+        "cloudcomponents.cdk_codepipeline_check_parameter_action._jsii": [
+            "cdk-codepipeline-check-parameter-action@1.1.7.jsii.tgz"
+        ],
+        "cloudcomponents.cdk_codepipeline_check_parameter_action": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "jsii>=1.10.0, <2.0.0",
+        "publication>=0.0.3",
+        "aws-cdk.aws-codepipeline>=1.57.0, <2.0.0",
+        "aws-cdk.aws-codepipeline-actions>=1.57.0, <2.0.0",
+        "aws-cdk.aws-iam>=1.57.0, <2.0.0",
+        "aws-cdk.aws-lambda>=1.57.0, <2.0.0",
+        "aws-cdk.core>=1.57.0, <2.0.0"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Typing :: Typed",
+        "License :: OSI Approved"
+    ]
+}
+"""
+)
+
+with open("README.md") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
