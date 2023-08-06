ed1f61c5d58b9fea09321b2ef42aab091cd6e47f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:15 +0000

    Add package pybigcommerce/0.1.0/pybigcommerce-0.1.0.tar.gz

---
+++ packages/pybigcommerce/pybigcommerce-0.1.0.tar.gz/pybigcommerce-0.1.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(
+    name='pybigcommerce',
+    version=version,
+
+    packages=find_packages(),
+    install_requires=['requests>=2.20.0'],
+
+    url='https://github.com/JamessenLiu/pybigcommerce',
+
+    author='Jamessen.Liu',
+    author_email='liumaosen121@gmail.com',
+
+    description='Using Python to connect Bigcommerce API',
+    license='MIT',
+
+    keywords=['bigcommerce', 'api'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
