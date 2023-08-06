1d819c3fe3948fde83deb9f60c4ee69310c539c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:41 +0000

    Add package altdg/1.0.5/altdg-1.0.5.tar.gz

---
+++ packages/altdg/altdg-1.0.5.tar.gz/altdg-1.0.5/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup, find_packages
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='altdg',
+    description='Utility libraries from Alternative Data Group',
+    url='https://github.com/altdg/bulk_mapper',
+    version='1.0.5',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['requests', 'chardet'],
+    python_requires='>=3.6',
+    license='MIT',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    entry_points={
+        'console_scripts': ['altdg=altdg.api:main'],
+    },
+    keywords='adg alternative data group',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+)
