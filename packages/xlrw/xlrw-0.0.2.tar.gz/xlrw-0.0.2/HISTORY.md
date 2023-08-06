5819f0399c8438fe5237ee27fd80aa3aabdf4269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:51 +0000

    Add package xlrw/0.0.2/xlrw-0.0.2.tar.gz

---
+++ packages/xlrw/xlrw-0.0.2.tar.gz/xlrw-0.0.2/HISTORY.md
@@ -0,0 +1,15 @@
+Release History
+===============
+
+dev
+---
+
+-   \[Short description of non-trivial change.\]
+
+
+0.0.1 (2020-08-15)
+------------------
+
+-   Frustration
+-   Conception
+
---
+++ packages/xlrw/xlrw-0.0.2.tar.gz/xlrw-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xlrw",  # Replace with your own username
+    version="0.0.2",
+    author="Liuke",
+    author_email="",
+    keywords=['python', 'xlrw'],
+    description="xlrw is a Python library to read/write Excel files.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/liukecode/xlrw.git",
+    packages=setuptools.find_packages(),
+	include_package_data=True,
+	install_requires = [
+    'xlrd',
+    'xlwt',
+	'xlutils',
+	'openpyxl',
+    ],
+    platforms=["any"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3',
+)
