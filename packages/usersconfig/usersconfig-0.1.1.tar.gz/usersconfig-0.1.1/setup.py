005d82cadacda61523bf8cf92ade4f82cd40c3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:37 +0000

    Add package usersconfig/0.1.1/usersconfig-0.1.1.tar.gz

---
+++ packages/usersconfig/usersconfig-0.1.1.tar.gz/usersconfig-0.1.1/setup.py
@@ -0,0 +1,29 @@
+"""
+Setup configuration for the package.
+"""
+from setuptools import setup, find_packages
+
+setup(name='usersconfig',
+      version='0.1.1',
+      packages=find_packages(),
+      license='GPL3',
+      # zip_safe=False,
+      #install_requires=['click', 'click_log'],
+      #include_package_data=True,
+      package_data={"": ["default_config.yaml"]},
+      # metadata to display on PyPI
+      author='Alen Šiljak',
+      author_email='usersconfig@alensiljak.eu.org',
+      description='Library for application configuration management',
+      keywords='configuration library yaml',
+      url='https://gitlab.com/alensiljak/usersconfig',
+      project_urls={
+          "Source Code": "https://gitlab.com/alensiljak/usersconfig.git"
+      },
+      # Scripts
+    #   entry_points={
+    #       "console_scripts": [
+    #           "ib = ibcli.cli:cli"
+    #       ]
+    #   }
+      )
