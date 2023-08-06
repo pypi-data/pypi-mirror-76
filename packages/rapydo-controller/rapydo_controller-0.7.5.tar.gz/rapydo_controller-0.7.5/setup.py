df90cbb8edf29d375c71f8cdd778dab8c86705cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:44 +0000

    Add package rapydo-controller/0.7.5/rapydo_controller-0.7.5.tar.gz

---
+++ packages/rapydo-controller/rapydo_controller-0.7.5.tar.gz/rapydo_controller-0.7.5/setup.py
@@ -0,0 +1,52 @@
+from setuptools import find_packages, setup
+
+current_version = "0.7.5"
+
+setup(
+    name="rapydo_controller",
+    version=current_version,
+    author="Paolo D'Onorio De Meo",
+    author_email="p.donorio.de.meo@gmail.com",
+    description="Manage and deploy projects based on RAPyDo framework",
+    url="https://github.com/rapydo",
+    license="MIT",
+    packages=find_packages(where=".", exclude=["tests*"]),
+    package_data={"controller": ["templates/*", "confs/*"]},
+    python_requires=">=3.6.0",
+    entry_points={
+        "console_scripts": [
+            "rapydo=controller.__main__:main",
+            "do=controller.__main__:main",
+        ],
+    },
+    install_requires=[
+        "docker-compose==1.26.2",
+        "docker==4.2.2",
+        "dockerfile-parse==1.0.0",
+        "python-dateutil",
+        "pytz",
+        "loguru",
+        "prettyprinter",
+        "jinja2",
+        "sultan==0.9.1",
+        "plumbum",
+        "glom",
+        "GitPython==3.1.7",
+        "PyYAML==5.3.1",
+        "pip>=10.0.0",
+        "typer[all]==0.3.0",
+    ],
+    keywords=["http", "api", "rest", "web", "backend", "rapydo"],
+    classifiers=[
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        # End-of-life: 2021-12-23
+        "Programming Language :: Python :: 3.6",
+        # End-of-life: 2023-06-27
+        "Programming Language :: Python :: 3.7",
+        # End-of-life: 2024-10
+        "Programming Language :: Python :: 3.8",
+    ],
+)
