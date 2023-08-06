218ba2604c18ff9729cc00a89f8db5f859568dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:30 +0000

    Add package CAMSCS-Keywords/0.0.6/CAMSCS_Keywords-0.0.6-py3-none-any.whl

---
+++ packages/CAMSCS-Keywords/CAMSCS_Keywords-0.0.6-py3-none-any.whl/CAMSCS_Keywords/__init__.py
@@ -0,0 +1,1 @@
+from CAMSCS_Keywords.CAMS_Keywords import check_email, contains_only_digits, Convert, getDataFromSpreadsheet, DeleteFolderContent, remove, Format_Validator, StrPos, getTestCaseDataRow, getDataRowCount, getDatabyRowIndex, getUniqueItems
