26ffbbbcf7178a7f21aef3f391cec7615416fd4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:17 +0000

    Add package yogurt/0.0.2/yogurt-0.0.2-py3-none-any.whl

---
+++ packages/yogurt/yogurt-0.0.2-py3-none-any.whl/yogurt_py/__init__.py
@@ -0,0 +1,10 @@
+'''
+@Author: Zitian(Daniel) Tong
+@Date: 2020-07-17 17:28:02
+@LastEditTime: 2020-07-31 00:41:27
+@LastEditors: Zitian(Daniel) Tong
+@Description: init files
+@FilePath: /yogurt/yogurt_py/__init__.py
+'''
+
+import yogurt_py.utils.Interaction
