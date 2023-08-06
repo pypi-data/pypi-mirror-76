7e76b8bfb27d55ca9dded2c82f93dbc316b20fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:30 +0000

    Add package wagtail-inventory/1.1.1/wagtail_inventory-1.1.1-py2.py3-none-any.whl

---
+++ packages/wagtail-inventory/wagtail_inventory-1.1.1-py2.py3-none-any.whl/wagtailinventory/management/commands/block_inventory.py
@@ -0,0 +1,22 @@
+from django.core.management import BaseCommand
+
+from tqdm import tqdm
+from wagtail.core.models import Page
+
+from wagtailinventory.helpers import (
+    create_page_inventory,
+    delete_page_inventory,
+)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        delete_page_inventory()
+
+        pages = Page.objects.all()
+
+        if options.get("verbosity"):  # pragma: no cover
+            pages = tqdm(pages)
+
+        for page in pages:
+            create_page_inventory(page)
