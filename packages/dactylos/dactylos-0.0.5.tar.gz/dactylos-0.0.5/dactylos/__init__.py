bb88aae1ea40153364cfd00862f5efce1d664332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:44 +0000

    Add package dactylos/0.0.5/dactylos-0.0.5.tar.gz

---
+++ packages/dactylos/dactylos-0.0.5.tar.gz/dactylos-0.0.5/dactylos/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.0.5'
+
+from _pyCaenN6725 import DPPVirtualProbe1, DPPVirtualProbe2
+from _pyCaenN6725 import DPPDigitalProbe1, DPPDigitalProbe2
+from .CaenN6725 import CaenN6725
+
+LOGLEVEL = 20
