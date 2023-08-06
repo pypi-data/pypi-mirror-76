23bfa7ba8e383ef4452ce60b2e594501b0c86fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:48 +0000

    Add package enough/2.0.12/enough-2.0.12.tar.gz

---
+++ packages/enough/enough-2.0.12.tar.gz/enough-2.0.12/playbooks/icinga/tests/test_icinga_checks.py
@@ -0,0 +1,22 @@
+from icinga2api.exceptions import Icinga2ApiException
+from tests.icinga_helper import IcingaHelper
+import pytest
+
+testinfra_hosts = ['ansible://icinga-host']
+
+
+class TestChecks(IcingaHelper):
+
+    def test_icinga_host(self):
+        objects = self.get_client().objects
+        r = objects.get('Host', 'bind-host')
+        assert r['attrs']['name'] == 'bind-host'
+        with pytest.raises(Icinga2ApiException):
+            objects.get('Host', 'deleted-host')
+
+    def test_disk(self):
+        assert self.is_service_ok('website-host!disk')
+        assert self.is_service_ok('packages-host!disk')
+
+    def test_icinga_ntp_time(self):
+        assert self.is_service_ok('website-host!systemd-timesyncd is working')
