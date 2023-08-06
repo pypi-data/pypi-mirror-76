d85f06c76f4d7414d55a4590d3cb6303084e1d48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:37 +0000

    Add package gbd-tools/3.1.1/gbd_tools-3.1.1.tar.gz

---
+++ packages/gbd-tools/gbd_tools-3.1.1.tar.gz/gbd_tools-3.1.1/gbd_tool/search.py
@@ -0,0 +1,118 @@
+# Global Benchmark Database (GBD)
+# Copyright (C) 2020 Markus Iser, Karlsruhe Institute of Technology (KIT)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gbd_tool.util import eprint
+from tatsu import parse, exceptions
+import pprint
+
+
+def find_hashes(database, query=None, resolve=[], collapse=False, group_by="hash", hashes=[], separator=",", join_type="INNER"):
+    statement = "SELECT {} FROM {} {} WHERE {} GROUP BY {}"
+    s_attributes = group_by + ".value"
+    s_from = group_by
+    s_tables = ""
+    s_conditions = "1=1"
+    s_group_by = group_by + ".value"
+    tables = set(resolve)
+    
+    if query is not None and query:
+        try:
+            ast = parse(GRAMMAR, query)
+        except exceptions.FailedParse as err:
+            eprint(err)
+            eprint("Exception in Query-Parser: Try to put arithmetic expression in parentheses or reorder constraints.")
+            return list() 
+        s_conditions = build_where(ast)
+        tables.update(collect_tables(ast))
+    elif len(hashes) > 0:
+        s_conditions = "hash.hash in ('{}')".format("', '".join(hashes))
+
+    s_attributes = s_group_by
+    if len(resolve):
+        if collapse:
+            s_attributes = s_attributes + ", " + ", ".join(['MIN({}.value)'.format(table) for table in resolve])
+        else:
+            s_attributes = s_attributes + ", " + ", ".join(['REPLACE(GROUP_CONCAT(DISTINCT({}.value)), ",", "{}")'.format(table, separator) for table in resolve])
+
+    s_tables = " ".join(['{} JOIN {} ON {}.hash = {}.hash'.format(join_type, table, group_by, table) for table in tables if table != group_by])
+
+    eprint(statement.format(s_attributes, s_from, s_tables, s_conditions, s_group_by))
+
+    return database.query(statement.format(s_attributes, s_from, s_tables, s_conditions, s_group_by))
+
+
+def build_where(ast):
+    result = ""
+    if ast["q"] is not None:
+        result = build_where(ast["q"])
+    elif ast["qop"] is not None:
+        result = '(' + build_where(ast["left"]) + " " + ast["qop"] + " " + build_where(ast["right"]) + ')'
+    elif ast["sop"] is not None:
+        result = ast["left"] + ".value " + ast["sop"] + " \"" + ast["right"] + "\""
+    elif ast["aop"] is not None:
+        result = build_where(ast["left"]) + " " + ast["aop"] + " " + build_where(ast["right"])
+    elif ast["bracket_term"] is not None:
+        result = '(' + build_where(ast["bracket"]) + ')'
+    elif ast["top"] is not None:
+        result = build_where(ast["left"]) + " " + ast["top"] + " " + build_where(ast["right"])
+    elif ast["value"] is not None:
+        result = "CAST(" + ast["value"] + ".value AS FLOAT)"
+    elif ast["constant"] is not None:
+        result = ast["constant"]
+    return result
+
+
+def collect_tables(ast):
+    result = set()
+    if ast["q"] is not None:
+        result.update(collect_tables(ast["q"]))
+    elif ast["qop"] is not None:
+        result.update(collect_tables(ast['left']))
+        result.update(collect_tables(ast['right']))
+    elif ast["sop"] is not None:
+        result.add(ast["left"])
+    elif ast["aop"] is not None:
+        result.update(collect_tables(ast["left"]))
+        result.update(collect_tables(ast["right"]))
+    elif ast["bracket_term"] is not None:
+        result.update(collect_tables(ast["bracket_term"]))
+    elif ast["top"] is not None:
+        result.update(collect_tables(ast['left']))
+        result.update(collect_tables(ast['right']))
+    elif ast["value"] is not None:
+        result.add(ast["value"])
+    return result
+
+
+GRAMMAR = r'''
+    @@grammar::EXP
+    @@ignorecase::True
+
+    start = q:query $ ;
+
+    query = '(' q:query ')' | left:query qop:('and' | 'or') right:query | scon | acon;
+
+    scon = left:colname sop:('=' | '!=') right:alnum | left:colname sop:('like') right:likean ;
+        
+    acon = left:term aop:('=' | '!=' | '<' | '>' | '<=' | '>=' ) right:term ;
+
+    term = value:colname | constant:num | '(' left:term top:('+'|'-'|'*'|'/') right:term ')' ;
+
+    num = /[0-9\.\-]+/ ;
+    alnum = /[a-zA-Z0-9_\.\-\/\?]+/ ;
+    likean = /[\%]?[a-zA-Z0-9_\.\-\/\?]+[\%]?/;
+    colname = /[a-zA-Z][a-zA-Z0-9_]+/ ;
+'''
