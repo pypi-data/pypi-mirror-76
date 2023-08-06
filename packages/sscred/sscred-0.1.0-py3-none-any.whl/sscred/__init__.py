341f5a238a1cefaefb9d30579f09eda1dbe846a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:40 +0000

    Add package sscred/0.1.0/sscred-0.1.0-py3-none-any.whl

---
+++ packages/sscred/sscred-0.1.0-py3-none-any.whl/sscred/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.1.0"
+__title__ = "sscred"
+__author__ = "Kasra EdalatNejad"
+__email__ = "kasra.edalat@epfl.ch"
+__url__ = "https://github.com/spring-epfl/SSCred"
+__license__ = "BSD-3-Clause"
+__description__ = "Single Show Credentials: A Python library for anonymous authentication."
+__copyright__ = "2020, Kasra Edalatnejad  (SPRING Lab, EPFL)"
+
+from sscred.commitment import *
+from sscred.acl import *
+from sscred.pack import *
+from sscred.blind_pedersen import *
+from sscred.blind_signature import *
+
