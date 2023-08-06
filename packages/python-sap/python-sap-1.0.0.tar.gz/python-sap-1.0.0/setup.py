0d0cdfa3db2f8086777c94bebeb6978f82dcd8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:37 +0000

    Add package python-sap/1.0.0/python-sap-1.0.0.tar.gz

---
+++ packages/python-sap/python-sap-1.0.0.tar.gz/python-sap-1.0.0/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+requirements = [
+    "Click>=7.0,<8",
+    "python-igraph>=0.8.0,<1",
+    "wostools>=2.0.5,<3",
+]
+
+setup_requirements = [
+    "pytest-runner",
+]
+
+test_requirements = [
+    "pytest>=3",
+]
+
+setup(
+    author="Juan David Alzate Cardona",
+    author_email="jdalzatec@unal.edu.co",
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    description="Python Tree of Science package",
+    entry_points={"console_scripts": ["sap=sap.cli:main",],},
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    keywords="python-sap",
+    name="python-sap",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    setup_requires=setup_requirements,
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/coreofscience/python-sap",
+    version="1.0.0",
+    zip_safe=False,
+)
