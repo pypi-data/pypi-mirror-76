275460a6fe27fc812353add6c11a9d03d8aebf3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:54 +0000

    Add package bbrecon/0.0.14/bbrecon-0.0.14-py3-none-any.whl

---
+++ packages/bbrecon/bbrecon-0.0.14-py3-none-any.whl/bbrecon/manager.py
@@ -0,0 +1,46 @@
+from typing import Optional, List, Generator
+from pydantic import HttpUrl
+from datetime import datetime
+
+from .client import AuthenticatedClient
+from .api.programs import get_programs, get_program
+from .models import Program
+
+
+class BugBountyRecon:
+    def __init__(
+        self, token: str, *, base_url: HttpUrl = "https://api.bugbountyrecon.com"
+    ):
+        self.client = AuthenticatedClient(base_url=base_url, token=token)
+
+    def _paginate(self, api_function, **kwargs):
+        page = 0
+        while page is not None:
+            response = api_function(client=self.client, page=page, **kwargs)
+            for target in response.data:
+                yield target
+            page = response.next_page
+
+    def program(self, slug: str) -> Program:
+        return get_program(client=self.client, slug=slug)
+
+    def programs(
+        self,
+        *,
+        name: Optional[str] = None,
+        types: Optional[List[str]] = None,
+        platforms: Optional[List[str]] = None,
+        exclude_platforms: Optional[List[str]] = None,
+        rewards: Optional[List[str]] = None,
+        created_since: Optional[datetime] = None,
+    ) -> Generator[Program, None, None]:
+        for program in self._paginate(
+            get_programs,
+            name=name,
+            types=types,
+            platforms=platforms,
+            exclude_platforms=exclude_platforms,
+            rewards=rewards,
+            created_since=created_since,
+        ):
+            yield program
