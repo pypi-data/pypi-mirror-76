3514597d3802dd3b14587444ae8671eb75a54450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:11 +0000

    Add package asyncpg/0.21.0/asyncpg-0.21.0.tar.gz

---
+++ packages/asyncpg/asyncpg-0.21.0.tar.gz/asyncpg-0.21.0/tests/test_execute.py
@@ -0,0 +1,153 @@
+# Copyright (C) 2016-present the asyncpg authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of asyncpg and is released under
+# the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
+
+
+import asyncio
+import asyncpg
+
+from asyncpg import _testbase as tb
+
+
+class TestExecuteScript(tb.ConnectedTestCase):
+
+    async def test_execute_script_1(self):
+        self.assertEqual(self.con._protocol.queries_count, 0)
+        status = await self.con.execute('''
+            SELECT 1;
+
+            SELECT true FROM pg_type WHERE false = true;
+
+            SELECT generate_series(0, 9);
+        ''')
+        self.assertEqual(self.con._protocol.queries_count, 1)
+        self.assertEqual(status, 'SELECT 10')
+
+    async def test_execute_script_2(self):
+        status = await self.con.execute('''
+            CREATE TABLE mytab (a int);
+        ''')
+        self.assertEqual(status, 'CREATE TABLE')
+
+        try:
+            status = await self.con.execute('''
+                INSERT INTO mytab (a) VALUES ($1), ($2)
+            ''', 10, 20)
+            self.assertEqual(status, 'INSERT 0 2')
+        finally:
+            await self.con.execute('DROP TABLE mytab')
+
+    async def test_execute_script_3(self):
+        with self.assertRaisesRegex(asyncpg.PostgresSyntaxError,
+                                    'cannot insert multiple commands'):
+
+            await self.con.execute('''
+                CREATE TABLE mytab (a int);
+                INSERT INTO mytab (a) VALUES ($1), ($2);
+            ''', 10, 20)
+
+    async def test_execute_script_check_transactionality(self):
+        with self.assertRaises(asyncpg.PostgresError):
+            await self.con.execute('''
+                CREATE TABLE mytab (a int);
+                SELECT * FROM mytab WHERE 1 / 0 = 1;
+            ''')
+
+        with self.assertRaisesRegex(asyncpg.PostgresError,
+                                    '"mytab" does not exist'):
+
+            await self.con.prepare('''
+                SELECT * FROM mytab
+            ''')
+
+    async def test_execute_exceptions_1(self):
+        with self.assertRaisesRegex(asyncpg.PostgresError,
+                                    'relation "__dne__" does not exist'):
+
+            await self.con.execute('select * from __dne__')
+
+    async def test_execute_script_interrupted_close(self):
+        fut = self.loop.create_task(
+            self.con.execute('''SELECT pg_sleep(10)'''))
+
+        await asyncio.sleep(0.2)
+
+        self.assertFalse(self.con.is_closed())
+        await self.con.close()
+        self.assertTrue(self.con.is_closed())
+
+        with self.assertRaises(asyncpg.QueryCanceledError):
+            await fut
+
+    async def test_execute_script_interrupted_terminate(self):
+        fut = self.loop.create_task(
+            self.con.execute('''SELECT pg_sleep(10)'''))
+
+        await asyncio.sleep(0.2)
+
+        self.assertFalse(self.con.is_closed())
+        self.con.terminate()
+        self.assertTrue(self.con.is_closed())
+
+        with self.assertRaisesRegex(asyncpg.ConnectionDoesNotExistError,
+                                    'closed in the middle'):
+            await fut
+
+        self.con.terminate()
+
+    async def test_execute_many_1(self):
+        await self.con.execute('CREATE TEMP TABLE exmany (a text, b int)')
+
+        try:
+            result = await self.con.executemany('''
+                INSERT INTO exmany VALUES($1, $2)
+            ''', [
+                ('a', 1), ('b', 2), ('c', 3), ('d', 4)
+            ])
+
+            self.assertIsNone(result)
+
+            result = await self.con.fetch('''
+                SELECT * FROM exmany
+            ''')
+
+            self.assertEqual(result, [
+                ('a', 1), ('b', 2), ('c', 3), ('d', 4)
+            ])
+
+            # Empty set
+            result = await self.con.executemany('''
+                INSERT INTO exmany VALUES($1, $2)
+            ''', ())
+
+            result = await self.con.fetch('''
+                SELECT * FROM exmany
+            ''')
+
+            self.assertEqual(result, [
+                ('a', 1), ('b', 2), ('c', 3), ('d', 4)
+            ])
+        finally:
+            await self.con.execute('DROP TABLE exmany')
+
+    async def test_execute_many_2(self):
+        await self.con.execute('CREATE TEMP TABLE exmany (b int)')
+
+        try:
+            bad_data = ([1 / 0] for v in range(10))
+
+            with self.assertRaises(ZeroDivisionError):
+                async with self.con.transaction():
+                    await self.con.executemany('''
+                        INSERT INTO exmany VALUES($1)
+                    ''', bad_data)
+
+            good_data = ([v] for v in range(10))
+            async with self.con.transaction():
+                await self.con.executemany('''
+                    INSERT INTO exmany VALUES($1)
+                ''', good_data)
+        finally:
+            await self.con.execute('DROP TABLE exmany')
---
+++ packages/asyncpg/asyncpg-0.21.0.tar.gz/asyncpg-0.21.0/tests/test_transaction.py
@@ -0,0 +1,181 @@
+# Copyright (C) 2016-present the asyncpg authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of asyncpg and is released under
+# the Apache 2.0 License: http://www.apache.org/licenses/LICENSE-2.0
+
+
+import asyncpg
+
+from asyncpg import _testbase as tb
+
+
+class TestTransaction(tb.ConnectedTestCase):
+
+    async def test_transaction_regular(self):
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+        tr = self.con.transaction()
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        with self.assertRaises(ZeroDivisionError):
+            async with tr as with_tr:
+                self.assertIs(self.con._top_xact, tr)
+                self.assertTrue(self.con.is_in_transaction())
+
+                # We don't return the transaction object from __aenter__,
+                # to make it harder for people to use '.rollback()' and
+                # '.commit()' from within an 'async with' block.
+                self.assertIsNone(with_tr)
+
+                await self.con.execute('''
+                    CREATE TABLE mytab (a int);
+                ''')
+
+                1 / 0
+
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        with self.assertRaisesRegex(asyncpg.PostgresError,
+                                    '"mytab" does not exist'):
+            await self.con.prepare('''
+                SELECT * FROM mytab
+            ''')
+
+    async def test_transaction_nested(self):
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        tr = self.con.transaction()
+
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        with self.assertRaises(ZeroDivisionError):
+            async with tr:
+                self.assertIs(self.con._top_xact, tr)
+                self.assertTrue(self.con.is_in_transaction())
+
+                await self.con.execute('''
+                    CREATE TABLE mytab (a int);
+                ''')
+
+                async with self.con.transaction():
+                    self.assertIs(self.con._top_xact, tr)
+                    self.assertTrue(self.con.is_in_transaction())
+
+                    await self.con.execute('''
+                        INSERT INTO mytab (a) VALUES (1), (2);
+                    ''')
+
+                self.assertIs(self.con._top_xact, tr)
+                self.assertTrue(self.con.is_in_transaction())
+
+                with self.assertRaises(ZeroDivisionError):
+                    in_tr = self.con.transaction()
+                    async with in_tr:
+
+                        self.assertIs(self.con._top_xact, tr)
+                        self.assertTrue(self.con.is_in_transaction())
+
+                        await self.con.execute('''
+                            INSERT INTO mytab (a) VALUES (3), (4);
+                        ''')
+
+                        1 / 0
+
+                st = await self.con.prepare('SELECT * FROM mytab;')
+
+                recs = []
+                async for rec in st.cursor():
+                    recs.append(rec)
+
+                self.assertEqual(len(recs), 2)
+                self.assertEqual(recs[0][0], 1)
+                self.assertEqual(recs[1][0], 2)
+
+                self.assertIs(self.con._top_xact, tr)
+                self.assertTrue(self.con.is_in_transaction())
+
+                1 / 0
+
+        self.assertIs(self.con._top_xact, None)
+        self.assertFalse(self.con.is_in_transaction())
+
+        with self.assertRaisesRegex(asyncpg.PostgresError,
+                                    '"mytab" does not exist'):
+            await self.con.prepare('''
+                SELECT * FROM mytab
+            ''')
+
+    async def test_transaction_interface_errors(self):
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        tr = self.con.transaction(readonly=True, isolation='serializable')
+        with self.assertRaisesRegex(asyncpg.InterfaceError,
+                                    'cannot start; .* already started'):
+            async with tr:
+                await tr.start()
+
+        self.assertTrue(repr(tr).startswith(
+            '<asyncpg.Transaction state:rolledback serializable readonly'))
+
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        with self.assertRaisesRegex(asyncpg.InterfaceError,
+                                    'cannot start; .* already rolled back'):
+            async with tr:
+                pass
+
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        tr = self.con.transaction()
+        with self.assertRaisesRegex(asyncpg.InterfaceError,
+                                    'cannot manually commit.*async with'):
+            async with tr:
+                await tr.commit()
+
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        tr = self.con.transaction()
+        with self.assertRaisesRegex(asyncpg.InterfaceError,
+                                    'cannot manually rollback.*async with'):
+            async with tr:
+                await tr.rollback()
+
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        tr = self.con.transaction()
+        with self.assertRaisesRegex(asyncpg.InterfaceError,
+                                    'cannot enter context:.*async with'):
+            async with tr:
+                async with tr:
+                    pass
+
+    async def test_transaction_within_manual_transaction(self):
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
+
+        await self.con.execute('BEGIN')
+
+        tr = self.con.transaction()
+        self.assertIsNone(self.con._top_xact)
+        self.assertTrue(self.con.is_in_transaction())
+
+        with self.assertRaisesRegex(asyncpg.InterfaceError,
+                                    'cannot use Connection.transaction'):
+            await tr.start()
+
+        with self.assertLoopErrorHandlerCalled(
+                'Resetting connection with an active transaction'):
+            await self.con.reset()
+
+        self.assertIsNone(self.con._top_xact)
+        self.assertFalse(self.con.is_in_transaction())
