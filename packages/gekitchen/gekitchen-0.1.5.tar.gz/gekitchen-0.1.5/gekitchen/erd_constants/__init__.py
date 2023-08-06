8d2300bf7ab4b6adf5fd73d750fd85549040afa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:19 +0000

    Add package gekitchen/0.1.5/gekitchen-0.1.5.tar.gz

---
+++ packages/gekitchen/gekitchen-0.1.5.tar.gz/gekitchen-0.1.5/gekitchen/erd_constants/__init__.py
@@ -0,0 +1,12 @@
+"""GE Constants"""
+
+from .erd_codes import ErdCode
+from .erd_constants import (
+    ErdApplianceType,
+    ErdAvailableCookMode,
+    ErdMeasurementUnits,
+    ErdOvenConfiguration,
+    ErdOvenCookMode,
+    ErdOvenState,
+    OVEN_COOK_MODE_MAP,
+)
