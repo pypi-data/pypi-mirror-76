10d9140a586ef1d3d8e716ccdbd2540a4691a227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:02 +0000

    Add package papersurfer/0.9.0/papersurfer-0.9.0.tar.gz

---
+++ packages/papersurfer/papersurfer-0.9.0.tar.gz/papersurfer-0.9.0/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+BASE = HERE.parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+VERSION = (BASE / "_version.txt").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="papersurfer",
+    version=VERSION,
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Johann Jacobsohn",
+    author_email="johann.jacobsohn@uni-hamburg.de",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["papersurfer"],
+    include_package_data=True,
+    install_requires=["requests", "mattermostdriver", "urwid", "configargparse"],
+    entry_points={
+        "console_scripts": [
+            "papersurfer=papersurfer.papersurfer:main",
+        ]
+    },
+)
