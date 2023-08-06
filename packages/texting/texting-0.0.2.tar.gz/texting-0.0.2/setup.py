2d4ccb6fb3ca40482dc48be260563e2e3dd08fe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:23 +0000

    Add package texting/0.0.2/texting-0.0.2.tar.gz

---
+++ packages/texting/texting-0.0.2.tar.gz/texting-0.0.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['texting',
+ 'texting.chars',
+ 'texting.has_ansi',
+ 'texting.indexing',
+ 'texting.lange',
+ 'texting.pad',
+ 'texting.str_util',
+ 'texting.str_value',
+ 'texting.tap']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'texting',
+    'version': '0.0.2',
+    'description': 'misc for string',
+    'long_description': "## texting\n##### misc for string\n\n### Usage\n```python\nfrom texting.has_ansi import has_ansi\nfrom texting.lange import lange\nwords = [\n    'peace',\n    '\\u001B[4mwar\\u001B[0m',\n    '\\u001b[38;2;255;255;85mtolstoy\\u001b[0m',\n]\n\nfor word in words:\n    print(f'[{word}] [has_ansi] ({has_ansi(word)}) [lange] ({lange(word)})')\n```",
+    'author': 'Hoyeung Wong',
+    'author_email': 'hoyeungw@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/hoyeungw/intype',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/texting/texting-0.0.2.tar.gz/texting-0.0.2/texting/__init__.py
@@ -0,0 +1,7 @@
+# from .tap import link, tag, tags
+from .chars import AEU, CO, COLF, COSP, CR, DA, DASH, DOSP, DOT, DT, ELLIP, LF, QT, RN, RT, RTSP, SP, TB, UL, VO
+from .has_ansi import has_ansi, has_astral, has_han
+from .indexing import after_nontab, index_nontab
+from .lange import lange
+from .pad import pad_centered, pad_end, pad_start
+from .str_util import StrTemp
