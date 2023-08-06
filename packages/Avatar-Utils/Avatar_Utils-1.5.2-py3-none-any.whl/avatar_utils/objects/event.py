b29abc27d2afb27b5c1b937f98a234d805f73dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:53 +0000

    Add package Avatar-Utils/1.5.2/Avatar_Utils-1.5.2-py3-none-any.whl

---
+++ packages/Avatar-Utils/Avatar_Utils-1.5.2-py3-none-any.whl/avatar_utils/objects/event.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass as python_dataclass
+from typing import Any, List
+
+from marshmallow_dataclass import dataclass
+
+from avatar_utils.objects import Image
+from avatar_utils.objects.abstracts.abstract_object import AbstractObject
+
+
+@dataclass
+@python_dataclass
+class Event(AbstractObject):
+    title: str = None
+    text: str = None
+    preview_text: str = None
+
+    date_from: str = None
+    date_to: str = None
+
+    origin: str = None
+    url: str = None
+    deadline: str = None
+    location: str = None
+    image: Image = None
+    tags: List[Any] = None
+
+    repr_type: str = None
+
+    id: int = None
+    stars: int = None
