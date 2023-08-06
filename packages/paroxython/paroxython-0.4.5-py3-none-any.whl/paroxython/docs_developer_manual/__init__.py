545e59152757ad6136d587becc09aa4aa8ef91b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:54 +0000

    Add package paroxython/0.4.5/paroxython-0.4.5-py3-none-any.whl

---
+++ packages/paroxython/paroxython-0.4.5-py3-none-any.whl/paroxython/docs_developer_manual/__init__.py
@@ -0,0 +1,11 @@
+r"""
+.. include:: ../../docs/md/bird_view.md
+
+<br>
+
+.. include:: ../../docs/md/helpers.md
+
+<br>
+
+.. include:: ../../docs/md/implementation_notes.md
+"""
---
+++ packages/paroxython/paroxython-0.4.5-py3-none-any.whl/paroxython/docs_user_manual/__init__.py
@@ -0,0 +1,19 @@
+"""
+.. include:: ../../docs/md/preparing.md
+
+<br>
+
+.. include:: ../../docs/md/pipeline_tutorial.md
+
+<br>
+
+.. include:: ../../docs/md/taxonomy.md
+
+<br>
+
+.. include:: ../../docs/md/databases.md
+
+<br>
+
+.. include:: ../../docs/md/pipeline_documentation.md
+"""
