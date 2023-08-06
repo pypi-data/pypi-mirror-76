034c82e039e13f2e703a89748f18988dcaab47b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:31 +0000

    Add package stock-pandas/0.25.3/stock-pandas-0.25.3.tar.gz

---
+++ packages/stock-pandas/stock-pandas-0.25.3.tar.gz/stock-pandas-0.25.3/setup.py
@@ -0,0 +1,59 @@
+
+import os
+from setuptools import setup
+
+from stock_pandas import __version__
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(*fname):
+    return open(os.path.join(os.path.dirname(__file__), *fname)).read()
+
+
+def read_requirements(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
+
+
+settings = dict(
+    name='stock-pandas',
+    packages=[
+        'stock_pandas',
+        'stock_pandas.commands',
+        'stock_pandas.directive',
+        'stock_pandas.math'
+    ],
+    version=__version__,
+    author='Kael Zhang',
+    author_email='i+pypi@kael.me',
+    description='The wrapper of `pandas.DataFrame` with stock statistics and indicators support.',  # noqa:E501
+    install_requires=read_requirements('requirements.txt'),
+    tests_require=read_requirements('test-requirements.txt'),
+    license='MIT',
+    keywords='pandas pandas-dataframe stock stat indicators macd',
+    url='https://github.com/kaelzhang/stock-pandas',
+    long_description=read('docs', 'README.md'),
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Natural Language :: English",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
+
+
+if __name__ == '__main__':
+    setup(**settings)
---
+++ packages/stock-pandas/stock-pandas-0.25.3.tar.gz/stock-pandas-0.25.3/stock_pandas/math/ma.py
@@ -0,0 +1,69 @@
+from stock_pandas.common import rolling_calc
+import pandas._libs.window.aggregations as window_aggregations
+import numpy as np
+
+_ewma = window_aggregations.ewma  # type: ignore
+
+
+def calc_ewma(
+    array: np.ndarray,
+    period: int
+) -> np.ndarray:
+    """Calculates Exponential Weighted Moving Average.
+
+    About `_ewma::com`
+
+    According to pandas._libs.window.aggregations.pyx,
+    the ewma alpha which follows the formula:
+        alpha = 1. / (1. + com)
+
+    And for ewma:
+        alpha = 2. / (1 + period)
+
+    So:
+        com = (period - 1.) / 2.
+    """
+
+    return _ewma(
+        # Sometimes, the series in a DataFrame is of int type
+        array.astype(float),
+        (period - 1.) / 2.,
+        # we always use adjust=True in ewma
+        1,
+        # ignore_na=False
+        0,
+
+        # For now, all calculations require `min_periods` as `period`
+        period
+    )
+
+
+def calc_smma(
+    array: np.ndarray,
+    period: int
+) -> np.ndarray:
+    """Calculates Smoothed Moving Average(Modified Moving Average)
+    https://en.wikipedia.org/wiki/Moving_average#Modified_moving_average
+
+    >  SMMA is an EWMA, with `alpha = 1 / N`
+
+    1. / period = 1. / (1. + com)
+    """
+
+    return _ewma(
+        array.astype(float),
+        period - 1.,
+        1,
+        0,
+        period
+    )
+
+
+def calc_ma(
+    array: np.ndarray,
+    period: int
+) -> np.ndarray:
+    """Calculates N-period Simple Moving Average
+    """
+
+    return rolling_calc(array, period, np.mean)
