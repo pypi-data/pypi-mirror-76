5ede2b3ba71b481294f5ce5a262c9e7a25afad25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:28 +0000

    Add package pyspace-toolkit/1.1.9.12/pyspace_toolkit-1.1.9.12-py3-none-any.whl

---
+++ packages/pyspace-toolkit/pyspace_toolkit-1.1.9.12-py3-none-any.whl/pyspace/rasa/components/log.py
@@ -0,0 +1,29 @@
+
+from typing import Any, Dict, List, Optional, Text, Union, Type
+
+from rasa.nlu.components import Component
+from rasa.nlu.training_data import Message, TrainingData
+from rasa.nlu.config import RasaNLUModelConfig
+
+from rasa.nlu.constants import (
+    CLS_TOKEN,
+    RESPONSE,
+    SPARSE_FEATURE_NAMES,
+    TEXT,
+    TOKENS_NAMES,
+    INTENT,
+    MESSAGE_ATTRIBUTES,
+    ENTITIES,
+)
+
+class Log(Component):
+
+    def train(self, training_data: TrainingData, config: Optional[RasaNLUModelConfig] = None, **kwargs: Any,):
+
+        sparse_features = training_data.training_examples[0].get(SPARSE_FEATURE_NAMES[TEXT])
+        if sparse_features:
+            print(sparse_features.shape)
+        else:
+            print(sparse_features)
+        
+
