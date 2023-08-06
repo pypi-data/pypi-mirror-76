3fe16f6eef43fb0d04ddd7a278e9f76011669ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:47 +0000

    Add package hitanshsumsquare3/1.0/hitanshsumsquare3-1.0-py3-none-any.whl

---
+++ packages/hitanshsumsquare3/hitanshsumsquare3-1.0-py3-none-any.whl/hitanshsumsq3/sumsquare3.py
@@ -0,0 +1,15 @@
+""" This function helps you to find sum of squares of three numbers."""
+
+
+class AddSq3:
+    """ This class has a function which helps you to find sum of squares of three numbers."""
+
+    def AddSq3(self, a, b, c):
+        """Asso
+
+        Parameters:
+        Send three integer parameters(a,b,c).   
+        Returns:
+        Sum of squares of three numbers(a^2 + b^2 + c^2)."""
+
+        return a ^ 2 + b ^ 2 + c ^ 2
