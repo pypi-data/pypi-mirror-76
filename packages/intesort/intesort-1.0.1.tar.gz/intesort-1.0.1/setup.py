87c3dec4002d8f03b8faf084e889ea5ab8187fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:53 +0000

    Add package intesort/1.0.1/intesort-1.0.1.tar.gz

---
+++ packages/intesort/intesort-1.0.1.tar.gz/intesort-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name = 'intesort',
+	version = '1.0.1',
+	py_modules = ['intesort'],
+	author = 'choose',
+	author_email = 'morning131@hotmail.com',
+	url = 'https://kibiwebgeek.com/a-python-module-to-sort-a-number-list/',
+	description = 'Module for any number list sorting',
+)
