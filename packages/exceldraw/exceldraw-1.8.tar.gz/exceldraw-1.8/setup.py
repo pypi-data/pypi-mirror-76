7825e7962d99fd9eadb69ebd8492c0e3f49ff18a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:02 +0000

    Add package exceldraw/1.8/exceldraw-1.8.tar.gz

---
+++ packages/exceldraw/exceldraw-1.8.tar.gz/exceldraw-1.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='exceldraw',
+      version='1.8',
+      author='Ujjayanta',
+      description='Upload any image and get image drawn on Excel',
+      packages=['exceldraw'],
+      author_email='ujjayanta.bhaumik.18@ucl.ac.uk',
+      url="https://github.com/jojo96/exceldraw",
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+     python_requires='>=3',
+      zip_safe=False)
