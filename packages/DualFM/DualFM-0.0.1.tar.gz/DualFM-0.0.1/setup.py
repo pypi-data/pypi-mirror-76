1fdba128d11ff574a2468efa2a5952356891e50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:10 +0000

    Add package DualFM/0.0.1/DualFM-0.0.1.tar.gz

---
+++ packages/DualFM/DualFM-0.0.1.tar.gz/DualFM-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DualFM", # Replace with your own username
+    version="0.0.1",
+    author="DualFM",
+    author_email="doe92899@gmail.com",
+    description="Python module for dualfm api.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cynlis/DualFM-Python/tree/master",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
