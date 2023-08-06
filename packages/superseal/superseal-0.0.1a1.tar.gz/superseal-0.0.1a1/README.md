e88e15a3885f45821a61cf1bf94a0ec43a287187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:53 +0000

    Add package superseal/0.0.1a1/superseal-0.0.1a1.tar.gz

---
+++ packages/superseal/superseal-0.0.1a1.tar.gz/superseal-0.0.1a1/README.md
@@ -0,0 +1,21 @@
+# superseal
+
+SUPERread Seed Expansion, Absorption, and Localization. A toolkit for reference-guided viral quasispecies reconstruction. **Under development**.
+
+## Install
+
+Coming to soon to PyPI and Bioconda. For now:
+
+```
+git clone https://github.com/stephenshank/superseal
+cd superseal
+pip install .
+```
+
+## Run
+
+For a list of available options:
+
+```
+superseal
+```
---
+++ packages/superseal/superseal-0.0.1a1.tar.gz/superseal-0.0.1a1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+setup(
+    name='superseal',
+    python_requires='>3.6.6',
+    version='0.0.1.alpha1',
+    url='https://github.com/stephenshank/superseal',
+    description='Reference-guided viral quasipsecies reconstruction',
+    author='Stephen D. Shank',
+    author_email='sshank314@gmail.com',
+    maintainer='Stephen D. Shank',
+    maintainer_email='sshank314@gmail.com',
+    install_requires=[
+        'biopython>=1.76',
+        'numpy>=1.18.4',
+        'pandas>=1.0.0',
+        'pysam>=0.15.4',
+        'PyVCF>=0.6.8'
+    ],
+    packages=['superseal'],
+    entry_points={
+        'console_scripts': [
+            'superseal = superseal.cli:command_line_interface'
+        ]
+    },
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+    ],
+    include_package_data=True
+)
