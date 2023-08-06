b89cd2c924e81e47e7f486d11aea1ba876e49726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:14 +0000

    Add package pyvuejs/0.5.2/pyvuejs-0.5.2-py3-none-any.whl

---
+++ packages/pyvuejs/pyvuejs-0.5.2-py3-none-any.whl/pyvuejs/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "eseunghwan"
+__email__ = "shlee0920@naver.com"
+__version__ = "0.5.2"
+
+from .core._vue import VueUtils as Vue
+
+__all__ = [
+    "Vue"
+]
