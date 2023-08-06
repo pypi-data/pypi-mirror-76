90853a13ea0bd589e3fdee654fe7b8998ea591b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:01 +0000

    Add package jaynes/0.6.0rc5/jaynes-0.6.0rc5-py3-none-any.whl

---
+++ packages/jaynes/jaynes-0.6.0rc5-py3-none-any.whl/jaynes/client.py
@@ -0,0 +1,91 @@
+import requests
+import json
+from urllib.parse import urlencode, quote
+
+
+class JaynesClient:
+    def __init__(self, server="http://localhost:8092", token=None):
+        self.server = server
+
+    def post(self, path, data, **kwargs):
+        if kwargs:
+            path += "?" + urlencode(kwargs)
+        r = requests.post(self.server + path, data=data)
+        try:
+            return r.json()
+        except:
+            return r
+
+    def post_json(self, path, data, **kwargs):
+        serialized = json.dumps(data)
+        return self.post(path, data=serialized, **kwargs)
+
+    def put(self, path, data, **kwargs):
+        if kwargs:
+            path += "?" + urlencode(kwargs)
+        r = requests.put(self.server + path, data=data)
+        if r.status_code > 200:
+            print(r.text)
+        try:
+            return r.json()
+        except:
+            return r
+
+    def gzip_local(self, dir, target):
+        pass
+
+    def upload_file(self, file, remote_path=None):
+        """used to upload files that have been changed"""
+        if remote_path is None:
+            remote_path = file
+        with open(file, 'rb') as f:
+            text = f.read()
+        r = self.put("/files/" + quote(remote_path), data=text)
+        return r
+
+    def update_file(self, file, remote_path=None, overwrite=True):
+        """used to upload files that have been changed"""
+        if remote_path is None:
+            remote_path = file
+        with open(file, 'rb') as f:
+            text = f.read()
+        r = self.post("/files/" + quote(remote_path), data=text, overwrite=overwrite)
+        return r
+
+    def unzip_remote(self, dir):
+        pass
+
+    def execute(self, cmd):
+        return self.post_json("/exec", dict(cmd=cmd))
+
+    def map(self, *cmds):
+        return self.post_json("/exec", dict(cmds=cmds))
+
+
+if __name__ == '__main__':
+    # from .server import run
+    # test this
+    client = JaynesClient()
+    # client = JaynesClient(server="http://mars.ge.ngrok.io")
+    status, stdout, stderr = client.execute('ls')
+    print(status, stdout, stderr)
+    outs = client.map('ls', "ls", "ls")
+    for status, stdout, stderr in outs:
+        print(status, stdout, stderr)
+
+    client.execute('mkdir -p ".test"')
+    client.execute('touch ".test/README.md"')
+    r = client.execute('ls ".test"')
+    print(r)
+    client.upload_file("../README.md", "$JYNMNT/.test/README.md")
+    client.execute('rm ".test"')
+    client.update_file("../README.md", ".test/README.md")
+    client.execute('rm ".test"')
+    # run(f"gzip ")
+
+# todo:
+#   1. gzip locally
+#   2. watch files locally
+#   3. upload file that has changed
+#   4. copy file to new location
+#   5. launch remotely
---
+++ packages/jaynes/jaynes-0.6.0rc5-py3-none-any.whl/jaynes/server.py
@@ -0,0 +1,127 @@
+import os
+import asyncio
+from aiofile import AIOFile, Reader, Writer
+from sanic import Sanic
+from sanic.response import json
+from sanic.response import json
+from params_proto.neo_proto import ParamsProto
+
+
+# todo use neo_proto to support better logic in
+#  init call
+class ServerConfig(ParamsProto, prefix=""):
+    server = None
+    protocol = "http"
+    host = "0.0.0.0"
+    port = 8092
+    token = None
+    file_root = os.getcwd()
+    envs = {"JYNMNT": os.getcwd() + "/jaynes-mount"}
+
+    @classmethod
+    def __init__(cls, deps=None, **kwargs):
+        cls.server = f"{cls.protocol}://{cls.host}:{cls.port}"
+        cls._update(deps, **kwargs)
+
+        if cls.envs:
+            os.environ.update(cls.envs)
+
+        print("""
+        Jaynes Server is now running!
+        """)
+
+
+app = Sanic("Jaynes Server")
+
+
+def interpolate(path: str, envs, is_path=True):
+    if path is None:
+        return path
+    sorted_envs = list(envs.items())
+    sorted_envs.sort(key=lambda kv: kv[0], reverse=True)
+    for k, v in sorted_envs:
+        path = path.replace("$" + k, v)
+    # remove occasional double slash due to $TMPDIR end in "/"
+    if is_path:
+        return os.path.realpath(path)
+    return path
+
+
+# note: let's build this using an RPC pattern, where
+#  the client controls the process. This makes scripting
+#  easy.
+
+@app.route("/files/<path:path>", methods=["PUT"], stream=True)
+async def upload(request, path):
+    content = b''
+    path = interpolate(path, os.environ)
+    dirname = os.path.dirname(path)
+    os.makedirs(dirname, exist_ok=True)
+
+    print(">>", dirname, path)
+
+    # async with AIOFile(path, 'w+') as afp:
+    while True:
+        body = await request.stream.read()
+        if body is None:
+            break
+        content += body#.decode('utf-8')
+        # await afp.write(content)
+        # await afp.fsync()
+    with open(path, "wb") as f:
+        f.write(content)
+    return json({"status": 1})
+
+
+@app.route("/files/<path:path>", methods=["POST"], stream=True)
+async def update(request, path):
+    path = interpolate(path, os.environ)
+    query_args = dict(request.query_args)
+    overwrite = query_args.get('overwrite', True)
+    content = b''
+    dirname = os.path.dirname(path)
+    os.makedirs(dirname, exist_ok=True)
+    # info: currently NFS has a c bug, causing file streaming to fail.
+    # async with AIOFile(path, 'w+' if overwrite else 'a') as afp:
+    while True:
+        body = await request.stream.read()
+        if body is None:
+            break
+        content += body#.decode('utf-8')
+        # await afp.write(content)
+        # await afp.fsync()
+    with open(path, 'wb' if overwrite else 'ab') as f:
+        f.write(content)
+    return json({"status": 1})
+
+
+async def run(cmd):
+    proc = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
+    stdout, stderr = await proc.communicate()
+    return proc.returncode, stdout.decode(), stderr.decode()
+
+
+@app.route("/exec", methods=["POST"])
+async def execute(request):
+    cmd = request.json.get('cmd', None)
+    # cmd = interpolate(cmd, os.environ)
+    if cmd is not None:
+        result = await run(cmd)
+        return json(result)
+    cmds = request.json.get('cmds', [])
+    # cmds = [interpolate(c, os.environ) for c in cmds]
+    if cmds:
+        results = await asyncio.gather(
+            *[run(c) for c in cmds]
+        )
+        return json(results)
+    return json([])
+
+
+if __name__ == "__main__":
+    ServerConfig()
+    app.run(host=ServerConfig.host, port=ServerConfig.port)
