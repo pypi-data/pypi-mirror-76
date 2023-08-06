0aa9ccfc0ef8a3020510c2cafe3ddeaba788384f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:02 +0000

    Add package vdnjobs-client/1.0.0/vdnjobs_client-1.0.0-py3-none-any.whl

---
+++ packages/vdnjobs-client/vdnjobs_client-1.0.0-py3-none-any.whl/swagger_client/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.companies_api import CompaniesApi
+from swagger_client.api.jobs_api import JobsApi
