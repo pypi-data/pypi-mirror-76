8359d7eaad3c2a335762c03eb524b3da6d301384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:46 +0000

    Add package Contentstack/1.1.0/Contentstack-1.1.0.tar.gz

---
+++ packages/Contentstack/Contentstack-1.1.0.tar.gz/Contentstack-1.1.0/setup.py
@@ -0,0 +1,54 @@
+# https://packaging.python.org/tutorials/packaging-projects/#creating-setup-py
+# setup.py is the build script for setuptools.
+# It tells setuptools about your package (such
+# as the name and version) as well as which code files to include
+
+import os
+
+import contentstack
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    long_description = readme.read()
+
+# from os import path
+# this_directory = path.abspath(path.dirname(__file__))
+# with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+setup(
+    title="contentstack-python",
+    name="Contentstack",
+    status="Active",
+    type="process",
+    created="17 Jun",
+    keywords=contentstack.__title__,
+    version=contentstack.__version__,
+    author="Contentstack",
+    author_email="shailesh.mishra@contentstack.com",
+    description="Contentstack is a headless CMS with an API-first approach.",
+    # long_description="Contentstack is a headless CMS with an API-first approach. It is a CMS that developers can use "
+    #                  "to build powerful cross-platform applications in their favorite languages. Build your "
+    #                  "application frontend, and Contentstack will take care of the rest",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/contentstack/contentstack-python",
+    packages=['contentstack'],
+    license='MIT',
+    test_suite='tests.all_tests',
+    install_requires=['requests>=1.1.0'],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    python_requires='>=3.6',
+    zip_safe=False,
+)
