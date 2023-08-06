ef319ff44347d2303eca1088338a4ef4cad9964b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:47 +0000

    Add package git-networkx/2.3.0/git_networkx-2.3.0-py3-none-any.whl

---
+++ packages/git-networkx/git_networkx-2.3.0-py3-none-any.whl/git_networkx/__init__.py
@@ -0,0 +1,7 @@
+from git_networkx.git_networkx import GitNX
+from git_networkx.git_node import *
+from git_networkx.exceptions import NotAGitCommandException
+
+__version__ = "2.3.0"
+__package__="git_networkx"
+
---
+++ packages/git-networkx/git_networkx-2.3.0-py3-none-any.whl/git_networkx/git_node.py
@@ -0,0 +1,61 @@
+
+
+class GitNode:
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return type(self).__name__+"("+str(self.data)+")"
+
+    def __str__(self):
+        return repr(self)
+
+    def __eq__(self, other):
+        if not type(other)==type(self):
+            return False
+        return self.data == other.data
+    def __hash__(self):
+        return self.data.__hash__()
+
+class Blob(GitNode):
+    pass
+
+
+class RemoteBranch(GitNode):
+    pass
+
+
+class Tree(GitNode):
+    pass
+
+
+class RemoteHead(GitNode):
+    pass
+
+
+class Commit(GitNode):
+    pass
+
+
+class RemoteServer(GitNode):
+    pass
+
+
+class LocalBranch(GitNode):
+    pass
+
+
+class AnnotatedTag(GitNode):
+    pass
+
+
+class LocalHead(GitNode):
+    pass
+
+
+class Tag(GitNode):
+    pass
+
+
+# class Upstream(GitNode):
+#     pass
