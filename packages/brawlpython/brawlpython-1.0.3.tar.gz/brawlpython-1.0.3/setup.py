4fc44b569d2f3a9ec3c4f0d913f5d38e99e95a44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:14 +0000

    Add package brawlpython/1.0.3/brawlpython-1.0.3.tar.gz

---
+++ packages/brawlpython/brawlpython-1.0.3.tar.gz/brawlpython-1.0.3/setup.py
@@ -0,0 +1,42 @@
+from brawlpython import __version__
+from setuptools import find_packages, setup
+
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+with open("requirements.txt", "r") as file:
+    requirements = [line.strip() for line in file]
+
+setup(
+    name="brawlpython",
+    version=__version__,
+    description="Easy-to-configure library to use any Brawl Stars API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="0dminnimda",
+    author_email="0dminnimda@gmail.com",
+    url="https://github.com/0dminnimda/brawlpython",
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Framework :: AsyncIO",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8",
+        "Natural Language :: English",
+        "Topic :: Games/Entertainment :: Real Time Strategy",
+        "Topic :: Internet :: WWW/HTTP :: Session",
+        "Topic :: Utilities",
+        "Typing :: Typed",
+    ],
+    license="MIT License",
+    keywords=(
+        "brawl stars, brawl stars api, brawlstars,"
+        "supercell, brawl, stars, api,"
+        "brawlpython, python, async, aiohttp, sync, requests,"
+    ),
+    install_requires=requirements,
+    python_requires=">=3.8",
+)
