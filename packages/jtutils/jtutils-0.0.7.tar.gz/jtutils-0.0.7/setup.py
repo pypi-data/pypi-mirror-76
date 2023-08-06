8c46cc68f8d0989215bb6deded301f400ef5aa1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:08 +0000

    Add package jtutils/0.0.7/jtutils-0.0.7.tar.gz

---
+++ packages/jtutils/jtutils-0.0.7.tar.gz/jtutils-0.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+setup(
+    name = 'jtutils',
+    packages = ['jtutils'],
+    version = "0.0.7",
+    description = 'Placeholder description',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/jtutils",
+    download_url = 'https://github.com/jasontrigg0/jtutils/tarball/0.0.7',
+    scripts=[],
+    install_requires=["requests",
+                      "beautifulsoup4"
+    ],
+    keywords = [],
+    classifiers = [],
+)
