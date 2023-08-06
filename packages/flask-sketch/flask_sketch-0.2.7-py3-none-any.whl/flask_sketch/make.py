231a69f7607f75a7eb4bf2b1225ecdd0fff64cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:53 +0000

    Add package flask-sketch/0.2.7/flask_sketch-0.2.7-py3-none-any.whl

---
+++ packages/flask-sketch/flask_sketch-0.2.7-py3-none-any.whl/flask_sketch/make.py
@@ -0,0 +1,42 @@
+import os
+import pathlib
+from argparse import Namespace
+
+from flask_sketch.handlers import (
+    api_framework_handler,
+    app_type_handler,
+    auth_handler,
+    config_handler,
+    database_handler,
+    handle_features,
+)
+from flask_sketch.utils import Answers, cleanup, make_commom, pjoin
+
+
+def create_project(args: Namespace, asws: dict):
+    pf = pjoin(str(pathlib.Path().absolute()), args.project_name)
+    apf = pjoin(
+        str(pathlib.Path().absolute()),
+        args.project_name,
+        args.project_name.replace("-", "_"),
+    )
+
+    answers = Answers(pf, apf, asws, args)
+
+    make_commom(answers)
+
+    answers.settings["default"]["DEBUG"] = False
+    answers.settings["development"]["DEBUG"] = True
+    answers.secrets["default"]["SECRET_KEY"] = str(os.urandom(32))
+
+    app_type_handler(answers)
+    database_handler(answers)
+    auth_handler(answers)
+    if "api" in answers.application_type:
+        api_framework_handler(answers)
+    handle_features(answers)
+    config_handler(answers)
+    cleanup(answers)
+
+    if args.e:
+        os.system(f"python -m venv {pf}/.venv")
