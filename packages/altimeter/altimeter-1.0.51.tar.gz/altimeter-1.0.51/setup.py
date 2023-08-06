710b3a3263bd534f413b023fc6bb3b712b9c6cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:19 +0000

    Add package altimeter/1.0.51/altimeter-1.0.51.tar.gz

---
+++ packages/altimeter/altimeter-1.0.51.tar.gz/altimeter-1.0.51/setup.py
@@ -0,0 +1,40 @@
+"""setup.py"""
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="altimeter",
+    version="1.0.51",
+    packages=find_packages(exclude=["tests"]),
+    author="Tableau",
+    description="Graph AWS resources in Neptune",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tableau/altimeter",
+    python_requires=">=3.7,<4",
+    install_requires=[
+        "aws-requests-auth==0.4.2",
+        "rdflib==4.2.2",
+        "structlog>=20.1.0,<21",
+        "boto3>=1.9.130",
+        "typing_extensions>=3.7.4.1,<3.8",
+        "jinja2>=2.11.1,<3",
+        "toml>=0.10.0,<1",
+    ],
+    scripts=[
+        "bin/account_scan.py",
+        "bin/altimeter",
+        "bin/aws2n.py",
+        "bin/graphpruner.py",
+        "bin/rdf2blaze",
+        "bin/runquery.py",
+        "bin/scan_resource.py",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
