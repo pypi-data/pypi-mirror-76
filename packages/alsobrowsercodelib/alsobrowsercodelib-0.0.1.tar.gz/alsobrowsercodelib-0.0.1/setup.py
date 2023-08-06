3cc6e1708afa9481febcbb25eff644ee533e1fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:06 +0000

    Add package alsobrowsercodelib/0.0.1/alsobrowsercodelib-0.0.1.tar.gz

---
+++ packages/alsobrowsercodelib/alsobrowsercodelib-0.0.1.tar.gz/alsobrowsercodelib-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="alsobrowsercodelib",
+    version="0.0.1",
+    description="学而思爱搜浏览器功能库,版权为爱搜浏览器团队",
+    author="asunc",
+    author_email="3182305655@qq.com",
+    py_modules=["alsobrowser"]
+)
