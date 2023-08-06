a879ca74ee38eb1d7a09e8b5008e1b212aa032f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:36 +0000

    Add package setux-modules/0.2.0/setux_modules-0.2.0.tar.gz

---
+++ packages/setux-modules/setux_modules-0.2.0.tar.gz/setux_modules-0.2.0/setux/modules/copy_id.py
@@ -0,0 +1,29 @@
+from .init import Module
+
+
+class Distro(Module):
+    def deploy(self, target, **kw):
+
+        usr = kw['usr']
+        pub = kw['pub']
+
+        user = target.User(usr)
+
+        path = f'/home/{usr}/.ssh'
+        name = f'authorized_keys'
+        full = f'{path}/{name}'
+
+        ok = target.Dir(
+            path, mode='700', user=usr, group=user.group
+        ).deploy()
+
+        if not ok:
+            return False
+
+        target.send(pub, full)
+
+        ok = target.File(
+            full, mod='600', usr=usr, grp=user.group
+        ).deploy()
+
+        return ok
