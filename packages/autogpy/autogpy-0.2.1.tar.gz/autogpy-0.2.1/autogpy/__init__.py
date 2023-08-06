1440b1a0c797ab8aa484bebefcff2417593c62ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:12 +0000

    Add package autogpy/0.2.1/autogpy-0.2.1.tar.gz

---
+++ packages/autogpy/autogpy-0.2.1.tar.gz/autogpy-0.2.1/autogpy/__init__.py
@@ -0,0 +1,10 @@
+"""autogpy - AutoGnuplot.py: generate gnuplot figures, with code and data, easily from python
+
+.. moduleauthor:: Alessandro Corbetta <a.corbetta@tue.nl>
+
+"""
+
+from . import plot_helpers 
+from .autognuplot import AutoGnuplotFigure
+
+AutogpyFigure = AutoGnuplotFigure
