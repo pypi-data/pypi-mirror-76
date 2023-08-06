04fcfec60311aaf7557054a4d4f96696463bafa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:56 +0000

    Add package Spinmob/3.4.11/Spinmob-3.4.11.tar.gz

---
+++ packages/Spinmob/Spinmob-3.4.11.tar.gz/Spinmob-3.4.11/egg/__init__.py
@@ -0,0 +1,16 @@
+# existing libraries (install these)
+import pyqtgraph
+import numpy as _n
+
+# home-made code
+import spinmob as _spinmob
+from . import _gui as gui
+
+dialogs           = _spinmob.dialogs
+data              = _spinmob.data
+settings          = _spinmob.settings
+gui._settings     = settings
+dialogs._settings = settings
+
+
+
