736c68af6ac30404f285a7fa4254a5619f8c54a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package load-m3u8/0.0.9/load_m3u8-0.0.9.tar.gz

---
+++ packages/load-m3u8/load_m3u8-0.0.9.tar.gz/load_m3u8-0.0.9/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" Install packages for resolve.py """
+
+from os.path import dirname, abspath, join
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+_locals = {}
+with open("load_m3u8/_version.py") as fp:
+    exec(fp.read(), None, _locals)
+version = _locals["__version__"]
+
+install_requires = [req for req in open(abspath(join(dirname(__file__), 'requirements.txt')))]
+
+setuptools.setup(
+    name="load_m3u8",
+    version=version,
+    author="sunmaolin.com",
+    author_email="xxdx@sunmaolin.com",
+    description="Download video by m3u8 file",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/16beer/load_m3u8",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    license="Apache License",
+    platforms='any',
+    test_suite='tests',
+    keywords='m3u8 m3u8-downloader m3u8-download',
+    entry_points={'console_scripts': ['load-m3u8 = load_m3u8.__main__:main']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
