8480f7c0ee8f6ae71eafaf64c31a1b0e82f6bf96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:29 +0000

    Add package cihai-cli/0.6.0/cihai-cli-0.6.0.tar.gz

---
+++ packages/cihai-cli/cihai-cli-0.6.0.tar.gz/cihai-cli-0.6.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cihai_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=3.12,<6', 'cihai>=0.10.0,<0.11.0', 'click>=7']
+
+entry_points = \
+{'console_scripts': ['cihai = cihai_cli.cli:cli']}
+
+setup_kwargs = {
+    'name': 'cihai-cli',
+    'version': '0.6.0',
+    'description': 'Command line frontend for the cihai CJK language library',
+    'long_description': None,
+    'author': 'Tony Narlock',
+    'author_email': 'tony@git-pull.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://cihai-cli.git-pull.com',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+}
+
+
+setup(**setup_kwargs)
