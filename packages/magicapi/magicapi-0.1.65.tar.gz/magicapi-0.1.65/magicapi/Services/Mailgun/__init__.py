2e7aada4a6a378987b8212ebd44f3e31aabd3381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:48 +0000

    Add package magicapi/0.1.65/magicapi-0.1.65.tar.gz

---
+++ packages/magicapi/magicapi-0.1.65.tar.gz/magicapi-0.1.65/magicapi/Services/Mailgun/__init__.py
@@ -0,0 +1,29 @@
+from typing import List
+import requests
+
+from magicapi import g
+
+
+def send_email(text: str, recipients: List[str], subject: str = ""):
+    data = {
+        "from": f"{g.settings.mailgun_sender_name} <{g.settings.mailgun_sender_email}>",
+        "to": recipients,
+        "subject": subject,
+        "text": text,
+    }
+    if "html" in data["text"]:
+        data["html"] = data["text"]
+        del data["text"]
+
+    return requests.post(
+        f"https://api.mailgun.net/v3/{g.settings.mailgun_domain_name}/messages",
+        auth=("api", g.settings.mailgun_private_api_key),
+        data=data,
+    )
+
+
+if __name__ == "__main__":
+    resp = send_email(
+        recipients=["kellycup8@aim.com"], text="jey man", subject="yessir"
+    )
+    print("resp content", resp.content)
