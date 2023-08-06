fb3a050293df5de34376b3e798ae5608aec4e42c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:31 +0000

    Add package data-slicer/0.0.3/data-slicer-0.0.3.tar.gz

---
+++ packages/data-slicer/data-slicer-0.0.3.tar.gz/data-slicer-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open('requirements.txt', 'r') as fh :
+    requirements = fh.read()
+
+with open("README.md", "r") as fh :
+    long_description = fh.read()
+
+setuptools.setup(
+    name='data-slicer',
+    version='0.0.3',
+#    version='0.0.14',
+    author='Kevin Kramer',
+    author_email='kevin.kramer@uzh.ch',
+    description='Tools for quick visualization of three dimensional datasets.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kuadrat/data_slicer.git',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    include_package_data=True,
+#    data_files=[
+#        ('example_data', ['data/testdata_100_150_200.p'])
+#    ],
+)
