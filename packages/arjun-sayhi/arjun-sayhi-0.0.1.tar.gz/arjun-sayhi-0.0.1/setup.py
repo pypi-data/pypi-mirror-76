7cac6a02d04d7d7da1ad5e9af4fecf3dd66ac0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:59 +0000

    Add package arjun-sayhi/0.0.1/arjun-sayhi-0.0.1.tar.gz

---
+++ packages/arjun-sayhi/arjun-sayhi-0.0.1.tar.gz/arjun-sayhi-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arjun-sayhi", # Replace with your own username
+    version="0.0.1",
+    author="Arjun Dandagi",
+    author_email="dandagi.arjun95@gmail.com",
+    description="A package that prints Hi n times",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arjundandagi/sayhi",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
