3e750ae7bdf4c706f0665fedee02d09db7d2799f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:03 +0000

    Add package seedrandom/2.1/seedrandom-2.1.tar.gz

---
+++ packages/seedrandom/seedrandom-2.1.tar.gz/seedrandom-2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from seedrandom import PACKAGE_NAME, __version__
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name=PACKAGE_NAME,
+    version=__version__,
+    packages=find_packages(),
+
+    # Metadata for PyPi
+    author="BananaLoaf",
+    author_email="bananaloaf@protonmail.com",
+    license="MIT",
+
+    description="Deterministic seeded RNG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=["random", "seed", "generator", "hash", "int", "float", "bool", "bytes", "rng", "deterministic"],
+
+    url=f"https://github.com/BananaLoaf/seedrandom",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"])
