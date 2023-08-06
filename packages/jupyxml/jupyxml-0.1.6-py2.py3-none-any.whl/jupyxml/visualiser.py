c5db556ad218dfffcf99059a5d435e2fd720520b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:36 +0000

    Add package jupyxml/0.1.6/jupyxml-0.1.6-py2.py3-none-any.whl

---
+++ packages/jupyxml/jupyxml-0.1.6-py2.py3-none-any.whl/jupyxml/visualiser.py
@@ -0,0 +1,42 @@
+import os
+
+from io import StringIO
+
+from lxml import etree
+try:
+    import importlib.resources as pkg_resources
+except ImportError:
+    # Try backported to PY<37 `importlib_resources`.
+    import importlib_resources as pkg_resources
+
+from typing import Union, TextIO, AnyStr
+
+from jupyxml.accordian_creator import create_recursive_accordian_html
+from . import styles
+
+
+class JupyXML:
+
+    def __init__(self, xml_file_or_path: Union[AnyStr, TextIO]):
+        self.xml_file_or_path = xml_file_or_path
+        if os.path.exists(xml_file_or_path):
+            print("Loading from file")
+            self._file = self.xml_file_or_path
+        else:
+            print("Loading from string")
+            self._file = StringIO(xml_file_or_path)
+        self.root = etree.parse(self._file)
+
+    def _repr_html_(self):
+        cards = create_recursive_accordian_html(self.root.getroot(), root_identifier="0",
+                                                max_depth=10, current_depth=0)
+        css = pkg_resources.read_text(styles, 'jupyxml.css')
+        # Concatenate css and cards. TODO: It would be nice if the css was "once-only"
+        # TODO: Ability to turn off CSS adding
+        html_representation = f"""
+            <style>
+                {css}
+            </style>
+            {cards}
+        """
+        return html_representation
