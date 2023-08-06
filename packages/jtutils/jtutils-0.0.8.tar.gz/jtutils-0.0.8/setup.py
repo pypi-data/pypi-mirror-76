f16ec22d3e9731ad7ced0f6c06100e98c7b8ef96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:09 +0000

    Add package jtutils/0.0.8/jtutils-0.0.8.tar.gz

---
+++ packages/jtutils/jtutils-0.0.8.tar.gz/jtutils-0.0.8/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name = 'jtutils',
+    packages = ['jtutils'],
+    version = "0.0.8",
+    description = 'Placeholder description',
+    author = "Jason Trigg",
+    author_email = "jasontrigg0@gmail.com",
+    url = "https://github.com/jasontrigg0/jtutils",
+    download_url = 'https://github.com/jasontrigg0/jtutils/tarball/0.0.8',
+    scripts=[],
+    install_requires=["requests",
+                      "beautifulsoup4",
+                      "selenium"
+    ],
+    keywords = [],
+    classifiers = [],
+)
