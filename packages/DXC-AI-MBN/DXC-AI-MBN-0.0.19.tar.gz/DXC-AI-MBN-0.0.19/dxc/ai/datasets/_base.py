27c22eaf391ce5cc3eb72987286cb1720ccb8a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:22 +0000

    Add package DXC-AI-MBN/0.0.19/DXC-AI-MBN-0.0.19.tar.gz

---
+++ packages/DXC-AI-MBN/DXC-AI-MBN-0.0.19.tar.gz/DXC-AI-MBN-0.0.19/dxc/ai/datasets/_base.py
@@ -0,0 +1,25 @@
+# import pandas as pd
+
+# def load_abdata():
+#     df = pd.read_csv("data/ABDATA.csv")
+#     return df
+
+from os.path import dirname, join
+
+
+def load_data(module_path, data_file_name):
+    with open(join(module_path, 'data', data_file_name)) as csv_file:
+        data_file = csv.reader(csv_file)
+    return data_file
+
+
+def load_abdata():
+    module_path = dirname(__file__)
+    df = load_data(module_path, 'ABDATA.csv')
+    return df
+    
+
+
+
+
+
