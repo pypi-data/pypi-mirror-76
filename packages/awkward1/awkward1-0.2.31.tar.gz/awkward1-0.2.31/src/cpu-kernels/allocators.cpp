fb0549d490ca27a9aeb1a40089a6e7fd6ef666c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:39 +0000

    Add package awkward1/0.2.31/awkward1-0.2.31.tar.gz

---
+++ packages/awkward1/awkward1-0.2.31.tar.gz/awkward1-0.2.31/src/cpu-kernels/allocators.cpp
@@ -0,0 +1,18 @@
+// BSD 3-Clause License; see https://github.com/scikit-hep/awkward-1.0/blob/master/LICENSE
+
+#include "awkward/kernels/allocators.h"
+
+void* awkward_malloc(int64_t bytelength) {
+  if (bytelength == 0) {
+    return nullptr;
+  }
+  else {
+    uint8_t* out = new uint8_t[bytelength];
+    return reinterpret_cast<void*>(out);
+  }
+}
+
+void awkward_free(void const *ptr) {
+  uint8_t const* in = reinterpret_cast<uint8_t const*>(ptr);
+  delete [] in;
+}
