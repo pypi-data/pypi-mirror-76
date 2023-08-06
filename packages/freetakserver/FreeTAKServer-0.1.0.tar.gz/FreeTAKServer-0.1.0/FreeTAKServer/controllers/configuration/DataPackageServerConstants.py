678b71136b6f9e28214bf50920c7d0429fe4350f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:24 +0000

    Add package freetakserver/0.1.0/FreeTAKServer-0.1.0.tar.gz

---
+++ packages/freetakserver/FreeTAKServer-0.1.0.tar.gz/FreeTAKServer-0.1.0/FreeTAKServer/controllers/configuration/DataPackageServerConstants.py
@@ -0,0 +1,21 @@
+import os
+import pathlib
+
+
+class DataPackageServerConstants:
+    def __init__(self):
+        # http server config
+        self.DATABASE = 'C:\\Users\\natha\\PycharmProjects\\FreeTAKServer\\FreeTAKServer\\controllers\\FreeTAKServerDataPackageDataBase.db'
+        self.APIPORT = '8080'
+        self.DEFAULTRETURN = 'other'
+        self.GET = 'GET'
+        self.PUT = 'PUT'
+        self.POST = 'POST'
+        self.DATAPACKAGEFOLDER = 'C:\\Users\\natha\\PycharmProjects\\FreeTAKServer\\FreeTAKServer\\controllers\\FreeTAKServerDataPackageFolder'
+        self.HTTPDEBUG = False
+        self.HTTPMETHODS = ['POST', 'GET', 'PUT']
+        self.IP = "0.0.0.0"
+        self.versionInfo = 'FreeTAKServer-1.0RC1'
+        self.NodeID = 'FTS'
+        self.VERSIONJSON = '{"version":"2","type":"ServerConfig", "data":{"version": "%s", "api": "2","hostname":"%s"}, "nodeId":"%s"}' % (
+            self.versionInfo, "0.0.0.0", self.NodeID)
