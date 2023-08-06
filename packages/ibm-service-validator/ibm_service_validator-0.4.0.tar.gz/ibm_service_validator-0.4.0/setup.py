51bd3c81a34ff4e60b241b023415e53ce337ef7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:24 +0000

    Add package ibm-service-validator/0.4.0/ibm_service_validator-0.4.0.tar.gz

---
+++ packages/ibm-service-validator/ibm_service_validator-0.4.0.tar.gz/ibm_service_validator-0.4.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.4.0"
+
+setup(
+    name="ibm_service_validator",
+    version=__version__,
+    description="Python package, Schemathesis, extended to include IBM API Handbook validation.",
+    url="https://github.ibm.com/CloudEngineering/ibm-service-validator",
+    author="Barrett Schonefeld, IBM",
+    author_email="barrett.schonefeld@ibm.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=["click", "ibm-cloud-sdk-core", "pyyaml", "schemathesis"],
+    entry_points="""
+        [console_scripts]
+        ibm-service-validator=ibm_service_validator.cli:ibm_service_validator
+    """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
---
+++ packages/ibm-service-validator/ibm_service_validator-0.4.0.tar.gz/ibm_service_validator-0.4.0/src/ibm_service_validator/handbook_rules/add_case_rules/invalid_accept_header.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from requests import Response
+
+from schemathesis.models import Case
+from schemathesis.hooks import HookContext
+
+from . import get_request_header, original_case_successful, set_request_header
+
+
+def add_invalid_accept_header(
+    context: HookContext, case: Case, response: Response
+) -> Optional[Case]:
+    if original_case_successful(response):
+        set_request_header(case, "Accept", "invalid/accept")
+        return case
+    else:
+        return None
+
+
+def invalid_accept_header(response: Response, case: Case) -> Optional[bool]:
+    accept_header = get_request_header(response.request, "Accept")
+    if accept_header == "invalid/accept":
+        assert (
+            response.status_code == 406
+        ), "Requests including Accept header with only unsupported formats MUST be rejected with a 406 status code. https://cloud.ibm.com/docs/api-handbook?topic=api-handbook-headers#negotiation-headers"
+    else:
+        # skips the check when it's not relevant
+        return True
+    return None
