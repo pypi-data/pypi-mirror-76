a961c228f80dcc2e1d95d2a09fa0d0127e95dcc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:04 +0000

    Add package distinomial/1.0/distinomial-1.0.tar.gz

---
+++ packages/distinomial/distinomial-1.0.tar.gz/distinomial-1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distinomial',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['distinomial'],
+      zip_safe=False)
