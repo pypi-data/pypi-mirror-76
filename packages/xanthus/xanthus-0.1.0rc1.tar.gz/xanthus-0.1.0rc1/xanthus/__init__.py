ce68b9910b7faf33d1c330f4c77383c40b5ccd93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:44 +0000

    Add package xanthus/0.1.0rc1/xanthus-0.1.0rc1.tar.gz

---
+++ packages/xanthus/xanthus-0.1.0rc1.tar.gz/xanthus-0.1.0rc1/xanthus/__init__.py
@@ -0,0 +1,8 @@
+"""
+The MIT License
+
+Copyright (c) 2018-2020 Mark Douthwaite
+"""
+
+__version__ = "0.1.0rc1"
+__author__ = "Mark Douthwaite"
---
+++ packages/xanthus/xanthus-0.1.0rc1.tar.gz/xanthus-0.1.0rc1/xanthus/evaluate/__init__.py
@@ -0,0 +1,30 @@
+"""
+The MIT License
+
+Copyright (c) 2018-2020 Mark Douthwaite
+"""
+
+from .metrics import (
+    score,
+    ndcg,
+    normalized_discounted_cumulative_gain,
+    precision_at_k,
+    coverage_at_k,
+    cak,
+    pak,
+)
+
+from .utils import split, create_rankings, leave_one_out
+
+__all__ = [
+    "score",
+    "ndcg",
+    "normalized_discounted_cumulative_gain",
+    "precision_at_k",
+    "coverage_at_k",
+    "cak",
+    "pak",
+    "create_rankings",
+    "leave_one_out",
+    "split",
+]
