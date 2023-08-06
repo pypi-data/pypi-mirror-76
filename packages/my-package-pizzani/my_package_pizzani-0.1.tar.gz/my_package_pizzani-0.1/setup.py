47cfe6be1d8a324bb5ba40ff55fbd86f5a62294a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:39 +0000

    Add package my-package-pizzani/0.1/my_package_pizzani-0.1.tar.gz

---
+++ packages/my-package-pizzani/my_package_pizzani-0.1.tar.gz/my_package_pizzani-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='my_package_pizzani',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['my_package_pizzani'],
+      zip_safe=False)
