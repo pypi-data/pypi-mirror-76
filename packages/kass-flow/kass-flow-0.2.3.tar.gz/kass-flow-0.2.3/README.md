8572435b0e3414f1ca29f465faca0931a60a78c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:55 +0000

    Add package kass-flow/0.2.3/kass-flow-0.2.3.tar.gz

---
+++ packages/kass-flow/kass-flow-0.2.3.tar.gz/kass-flow-0.2.3/README.md
@@ -0,0 +1,86 @@
+# Kass Flow
+
+Helper for the [KASS](https://www.kass.is/) payment gateway. Written for python 3.8.5 (probably works on versions >= 3.6).
+
+See the [KASS API docs](https://kass.github.io/api/) for more info.
+
+This module is not affiliated with KASS.
+
+## TODO
+
+- [x] Create payment
+- [ ] Retreive payment info
+- [ ] Retreive payment status
+- [ ] Cancel payment
+- [ ] Add concurrency with RQ when dispatching multiple payments
+
+## Usage
+
+Here is a short guide to get you started.
+
+This will dispatch a payment to the KASS API.
+
+```sh
+poetry add kass-flow
+# or
+pip install kass-flow
+```
+
+```python
+from kass_flow.kass import KassBilling
+from kass_flow.interfaces import KassRequestPaymentDict
+
+kass_token: str = "some-token"
+kass_url: str = "https://api.kass.is/v1/payments"
+instance = KassBilling(kass_token, kass_url)
+
+payload: KassRequestPaymentDict = {
+    "amount": 2199,
+    "description": "Kass bolur",
+    "image_url": "https://photos.kassapi.is/kass/kass-bolur.jpg",
+    "order": "ABC123",
+    "recipient": "7798217",
+    "terminal": 1,
+    "expires_in": 90,
+    "notify_url": "https://example.com/callbacks/kass",
+}
+
+result, is_valid = instance.dispatch(payload)
+```
+
+When the recipient cancels or pays the requested order KASS will send a callback to the `notify_url`, which you need to catch on your server and probably validate the signature that is included in the POST payload.
+
+At some point in time the user will pay/reject the requested order. Here is an example on how to validate the payload.
+
+```python
+from kass_flow.kass import KassBilling
+from kass_flow.interfaces import KassRequestPaymentDict
+
+def some_view_that_handles_kass_callback(request):
+  payload = request.data
+  kass_token: str = "some-token"
+  kass_url: str = "https://api.kass.is/v1/payments"
+  instance = KassBilling(kass_token, kass_url)
+
+  is_valid = instance.is_signature_valid(payload)
+  if is_valid:
+    # process the payload.
+```
+
+## Development
+
+```sh
+pip install poetry
+ # to manage envs yourself
+poetry config virtualenvs.create false
+poetry install
+pytest tests
+```
+
+If you are using VSCode for development there is a `.vscode/settings.example.json` for sensible defaults. Since mypy is used you need to install the `mypyls` language server.
+
+```
+poetry install "https://github.com/matangover/mypyls/archive/master.zip#egg=mypyls[default-mypy]"
+# or
+pip install "https://github.com/matangover/mypyls/archive/master.zip#egg=mypyls[default-mypy]"
+```
---
+++ packages/kass-flow/kass-flow-0.2.3.tar.gz/kass-flow-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kass_flow']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.24.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'kass-flow',
+    'version': '0.2.3',
+    'description': 'Library to manage payments using the KASS API',
+    'long_description': '# Kass Flow\n\nHelper for the [KASS](https://www.kass.is/) payment gateway. Written for python 3.8.5 (probably works on versions >= 3.6).\n\nSee the [KASS API docs](https://kass.github.io/api/) for more info.\n\nThis module is not affiliated with KASS.\n\n## TODO\n\n- [x] Create payment\n- [ ] Retreive payment info\n- [ ] Retreive payment status\n- [ ] Cancel payment\n- [ ] Add concurrency with RQ when dispatching multiple payments\n\n## Usage\n\nHere is a short guide to get you started.\n\nThis will dispatch a payment to the KASS API.\n\n```sh\npoetry add kass-flow\n# or\npip install kass-flow\n```\n\n```python\nfrom kass_flow.kass import KassBilling\nfrom kass_flow.interfaces import KassRequestPaymentDict\n\nkass_token: str = "some-token"\nkass_url: str = "https://api.kass.is/v1/payments"\ninstance = KassBilling(kass_token, kass_url)\n\npayload: KassRequestPaymentDict = {\n    "amount": 2199,\n    "description": "Kass bolur",\n    "image_url": "https://photos.kassapi.is/kass/kass-bolur.jpg",\n    "order": "ABC123",\n    "recipient": "7798217",\n    "terminal": 1,\n    "expires_in": 90,\n    "notify_url": "https://example.com/callbacks/kass",\n}\n\nresult, is_valid = instance.dispatch(payload)\n```\n\nWhen the recipient cancels or pays the requested order KASS will send a callback to the `notify_url`, which you need to catch on your server and probably validate the signature that is included in the POST payload.\n\nAt some point in time the user will pay/reject the requested order. Here is an example on how to validate the payload.\n\n```python\nfrom kass_flow.kass import KassBilling\nfrom kass_flow.interfaces import KassRequestPaymentDict\n\ndef some_view_that_handles_kass_callback(request):\n  payload = request.data\n  kass_token: str = "some-token"\n  kass_url: str = "https://api.kass.is/v1/payments"\n  instance = KassBilling(kass_token, kass_url)\n\n  is_valid = instance.is_signature_valid(payload)\n  if is_valid:\n    # process the payload.\n```\n\n## Development\n\n```sh\npip install poetry\n # to manage envs yourself\npoetry config virtualenvs.create false\npoetry install\npytest tests\n```\n\nIf you are using VSCode for development there is a `.vscode/settings.example.json` for sensible defaults. Since mypy is used you need to install the `mypyls` language server.\n\n```\npoetry install "https://github.com/matangover/mypyls/archive/master.zip#egg=mypyls[default-mypy]"\n# or\npip install "https://github.com/matangover/mypyls/archive/master.zip#egg=mypyls[default-mypy]"\n```\n',
+    'author': 'Jón Levy',
+    'author_email': 'nonni@nonni.cc',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/busla/kass-flow',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
