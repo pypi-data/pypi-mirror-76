0c11054e8b6c841d0dd3babdd97e837d0ae12790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:37 +0000

    Add package minorimpact/0.0.1/minorimpact-0.0.1.tar.gz

---
+++ packages/minorimpact/minorimpact-0.0.1.tar.gz/minorimpact-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='minorimpact',
+    packages=find_packages(include=['minorimpact']),
+    version='0.0.1',
+    description='Personal utility library',
+    author='Patrick Gillan',
+    license='GPLv3',
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+)
