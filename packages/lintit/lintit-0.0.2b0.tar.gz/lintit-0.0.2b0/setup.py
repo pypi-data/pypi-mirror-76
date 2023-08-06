b9627068663aebc53a7a718e6ef4fcd94a2740d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:13 +0000

    Add package lintit/0.0.2b0/lintit-0.0.2b0.tar.gz

---
+++ packages/lintit/lintit-0.0.2b0.tar.gz/lintit-0.0.2b0/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+dependencies = [
+    "click>7.1.2",
+    "flake8>3.8.3",
+    "black>19.10b0",
+    "isort>5.0.9",
+    "mypy>0.782",
+    "autoflake>1.3.1",
+]
+
+setuptools.setup(
+    name="lintit",
+    version="0.0.2b",
+    author="Everton Tomalok",
+    author_email="evertontomalok123@gmail.com",
+    description="A cli linter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EvertonTomalok/lint-it",
+    packages=setuptools.find_packages(exclude="tests"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'lintit = lint_it.linter:main',
+        ],
+    },
+)
