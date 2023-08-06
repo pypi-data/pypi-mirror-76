da8b1bf0f72d380a8a95d260d5d333c72714a929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package kde-account-oauth-client/0.9.4/kde-account-oauth-client-0.9.4.tar.gz

---
+++ packages/kde-account-oauth-client/kde-account-oauth-client-0.9.4.tar.gz/kde-account-oauth-client-0.9.4/kde_account_oauth_client/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KDEAccountOAuthConfig(AppConfig):
+    name = 'kde_account_oauth_client'
+    verbose_name = 'KDE Account OAuth'
---
+++ packages/kde-account-oauth-client/kde-account-oauth-client-0.9.4.tar.gz/kde-account-oauth-client-0.9.4/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['kde_account_oauth_client',
+ 'kde_account_oauth_client.management',
+ 'kde_account_oauth_client.management.commands',
+ 'kde_account_oauth_client.migrations']
+
+package_data = \
+{'': ['*'], 'kde_account_oauth_client': ['templates/blender_id_oauth_client/*']}
+
+install_requires = \
+['Django>=2.1,<3.1.0', 'requests-oauthlib>=1.0,<2.0']
+
+setup_kwargs = {
+    'name': 'kde-account-oauth-client',
+    'version': '0.9.4',
+    'description': 'Django app for using KDE Account as OAuth2 authentication service.',
+    'long_description': None,
+    'author': 'Sybren A. Stüvel',
+    'author_email': 'sybren@blender.studio',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://gitlab.com/blender-institute/kde-account-oauth-client',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
