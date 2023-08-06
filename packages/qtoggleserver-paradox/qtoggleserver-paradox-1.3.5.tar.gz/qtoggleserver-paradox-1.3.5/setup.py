2a326dff917360d50dbe06f2680f8906069a7e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:29 +0000

    Add package qtoggleserver-paradox/1.3.5/qtoggleserver-paradox-1.3.5.tar.gz

---
+++ packages/qtoggleserver-paradox/qtoggleserver-paradox-1.3.5.tar.gz/qtoggleserver-paradox-1.3.5/setup.py
@@ -0,0 +1,21 @@
+
+from setuptools import setup, find_namespace_packages
+
+
+setup(
+    name='qtoggleserver-paradox',
+    version='1.3.5',
+    description='Control your Paradox alarm with qToggleServer',
+    author='Calin Crisan',
+    author_email='ccrisan@gmail.com',
+    license='Apache 2.0',
+
+    packages=find_namespace_packages(),
+
+    install_requires=[
+        'paradox-alarm-interface==2.1.0',
+        'pyserial>=3.4',
+        'pyserial-asyncio>=0.4',
+        'requests'
+    ]
+)
