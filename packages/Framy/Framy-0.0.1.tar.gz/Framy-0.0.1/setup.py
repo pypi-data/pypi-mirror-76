024455080a160b2a326acf7a5695ed121d6c1e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:51 +0000

    Add package Framy/0.0.1/Framy-0.0.1.tar.gz

---
+++ packages/Framy/Framy-0.0.1.tar.gz/Framy-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Framy',
+    version='0.0.1',
+    description='web framewrok',
+    py_modules=[],
+    package_dir={'': 'Framy'},
+    install_requires=[
+        "jinja2 ~= 2.11.2",
+    ],
+    author='a276me',
+    author_email='2230061@ncpachina.org',
+    license='MIT',
+
+)
