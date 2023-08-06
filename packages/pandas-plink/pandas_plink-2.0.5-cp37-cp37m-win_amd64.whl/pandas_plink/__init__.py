db384902c95e08815b4d8dddbad5469fb4706523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:26 +0000

    Add package pandas-plink/2.0.5/pandas_plink-2.0.5-cp37-cp37m-win_amd64.whl

---
+++ packages/pandas-plink/pandas_plink-2.0.5-cp37-cp37m-win_amd64.whl/pandas_plink/__init__.py
@@ -0,0 +1,19 @@
+from ._data import example_file_prefix, get_data_folder
+from ._read import read_plink, read_plink1_bin
+from ._read_grm import read_grm
+from ._read_rel import read_rel
+from ._testit import test
+
+__version__ = "2.0.5"
+
+
+__all__ = [
+    "__version__",
+    "test",
+    "example_file_prefix",
+    "read_plink",
+    "read_plink1_bin",
+    "read_grm",
+    "read_rel",
+    "get_data_folder",
+]
