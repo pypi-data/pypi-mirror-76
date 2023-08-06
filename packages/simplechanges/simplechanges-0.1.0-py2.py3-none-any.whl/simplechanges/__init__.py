c38df618805c8b969daa301be3b793fbf80fbeb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:34 +0000

    Add package simplechanges/0.1.0/simplechanges-0.1.0-py2.py3-none-any.whl

---
+++ packages/simplechanges/simplechanges-0.1.0-py2.py3-none-any.whl/simplechanges/__init__.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+#
+# __init__.py
+# Simple Changes
+#
+# Created by Marquis Kurt on 08/08/20.
+# Copyright © 2020 Marquis Kurt. All rights reserved.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
+from .parser import SimpleChangesParser
+__version__ = '0.1.0'
