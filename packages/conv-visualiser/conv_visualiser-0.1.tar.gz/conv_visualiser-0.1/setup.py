975bc857eba2722a40587c9f48911014f7eda95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:00 +0000

    Add package conv-visualiser/0.1/conv_visualiser-0.1.tar.gz

---
+++ packages/conv-visualiser/conv_visualiser-0.1.tar.gz/conv_visualiser-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name  = 'conv_visualiser',
+      version = '0.1',
+      description= 'Functions to visualize intermediate activations and features of a CNN',
+      packages = ['cnn_visualiser'],
+      url = 'https://github.com/rshah240/conv_visualiser',
+      author = 'Rachit Shah',
+      author_email = 'rshah240@gmail.com',
+      zip_safe = False)
