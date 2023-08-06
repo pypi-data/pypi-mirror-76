81b33ac16522d77059a6df9b547ff6be09864850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:57 +0000

    Add package integrationbabel/0.5/integrationbabel-0.5.tar.gz

---
+++ packages/integrationbabel/integrationbabel-0.5.tar.gz/integrationbabel-0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="integrationbabel", 
+    packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    version="0.5",
+    license='MIT',
+    author="weis",
+    author_email="shaowei@funstory.ai",
+    description="data_integration_babel",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords=['data_integration_babel'],
+    python_requires='>=3.7',
+    install_requires=[
+        "pymysql==0.10.0",
+        "kafka-python==2.0.1"
+    ],
+)
