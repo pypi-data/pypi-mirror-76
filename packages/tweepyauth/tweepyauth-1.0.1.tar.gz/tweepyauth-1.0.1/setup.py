d5f5e0d4e5d6afd559ae7fb70a0740f8d52f7720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:38 +0000

    Add package tweepyauth/1.0.1/tweepyauth-1.0.1.tar.gz

---
+++ packages/tweepyauth/tweepyauth-1.0.1.tar.gz/tweepyauth-1.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="tweepyauth",
+    version="1.0.1",
+    author="mocchapi",
+    author_email="mocchapi@gmail.com",
+    description="A small library to ease tweepy authentication",
+    packages=setuptools.find_packages(),
+    url="https://gitlab.com/mocchapi/tweepyauth",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
