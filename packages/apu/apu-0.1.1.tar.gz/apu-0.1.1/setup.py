d3875d0570bef4eb40f203a8b6b07b19e6bfc0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:59 +0000

    Add package apu/0.1.1/apu-0.1.1.tar.gz

---
+++ packages/apu/apu-0.1.1.tar.gz/apu-0.1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""apu: Antons Python Utilities."""
+
+# Third party
+from setuptools import setup
+
+requires_datetime = ["pytz"]
+requires_setup = ["GitPython"]
+requires_all = (
+    requires_datetime
+    + requires_setup
+)
+
+setup(
+    version="0.1.1",
+    package_data={"apu": []},
+    extras_require={
+        "all": requires_all,
+        "datetime": requires_datetime,
+        "setup": requires_setup,
+    },
+)
