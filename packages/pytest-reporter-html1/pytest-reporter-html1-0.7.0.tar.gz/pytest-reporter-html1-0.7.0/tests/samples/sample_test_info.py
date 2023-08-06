04494a1034b6e5fdd272e8a8ab1006146784b96d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:57 +0000

    Add package pytest-reporter-html1/0.7.0/pytest-reporter-html1-0.7.0.tar.gz

---
+++ packages/pytest-reporter-html1/pytest-reporter-html1-0.7.0.tar.gz/pytest-reporter-html1-0.7.0/tests/samples/sample_test_info.py
@@ -0,0 +1,38 @@
+import pytest
+
+
+@pytest.mark.parametrize("parameter", ["one", "two", "three"])
+def test_parametrized(parameter):
+    assert parameter != "three"
+
+
+@pytest.mark.mark1
+@pytest.mark.mark_with_args("positional", keyword=1234)
+def test_markers():
+    pass
+
+
+def test_with_documentation():
+    """This is a documentation.
+
+    Header 1
+    --------
+
+    This is a link to `pytest`_.
+
+    Here is a list:
+
+    1. One
+    2. Two
+    3. Three
+
+    .. _`pytest`: https://github.com/pytest-dev/pytest
+    """
+    pass
+
+
+def test_user_properties(record_property):
+    record_property("string", "Some string value")
+    record_property("integer", 123)
+    record_property("xml", "<blink>Blink!</blink>")
+    record_property("url", "https://github.com/christiansandberg/pytest-reporter-html1")
