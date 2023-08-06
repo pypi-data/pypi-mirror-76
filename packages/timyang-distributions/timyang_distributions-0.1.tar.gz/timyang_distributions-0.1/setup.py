6bac2607206dea4333250139cb61abbe0fbda837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:00 +0000

    Add package timyang-distributions/0.1/timyang_distributions-0.1.tar.gz

---
+++ packages/timyang-distributions/timyang_distributions-0.1.tar.gz/timyang_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='timyang_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['timyang_distributions'],
+      author='Tim Yang',
+      author_email='timonyang@gmail.com',
+      zip_safe=False)
