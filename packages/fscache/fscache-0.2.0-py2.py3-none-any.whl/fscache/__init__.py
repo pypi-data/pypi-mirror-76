be6651edf931d2f5c1f5a87e30ffccbf284ad536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:12 +0000

    Add package fscache/0.2.0/fscache-0.2.0-py2.py3-none-any.whl

---
+++ packages/fscache/fscache-0.2.0-py2.py3-none-any.whl/fscache/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""Top-level package for fscache."""
+
+__author__ = """Ramiro Gómez"""
+__email__ = 'code@ramiro.org'
+__version__ = '0.2.0'
+
+
+# from .fscache import path, load, save, valid
