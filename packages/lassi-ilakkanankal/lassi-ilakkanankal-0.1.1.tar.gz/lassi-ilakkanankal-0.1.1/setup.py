7393dedb2b272ce868cfcf7c286a8af87bc127c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:05 +0000

    Add package lassi-ilakkanankal/0.1.1/lassi-ilakkanankal-0.1.1.tar.gz

---
+++ packages/lassi-ilakkanankal/lassi-ilakkanankal-0.1.1.tar.gz/lassi-ilakkanankal-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lassi-ilakkanankal',
+    version='0.1.1',
+    packages=find_packages(where="மூலம்"),
+    package_dir={"": "மூலம்"},
+    url='https://github.com/lassi-samaaj/lassi-ilakkanangal',
+    license='AGPL-3.0-or-later',
+    author='ம. ஜூலீஎன்',
+    author_email='julien.malard@mail.mcgill.ca',
+    description='லஸ்ஸி மென்பொருளுக்காக இலக்கணம் விவரக்குறிப்புகள்',
+    install_requires=['lark-parser'],
+    package_data={'': ['*.lark', '*.json']}
+)
