201ee90130c51b2d681f0d78b1d9c1d090c0dad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:11 +0000

    Add package verta/0.14.17/verta-0.14.17.tar.gz

---
+++ packages/verta/verta-0.14.17.tar.gz/verta-0.14.17/verta/_cli/deployment/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .create import create
+from .deployment import deployment
+from .get import get
+from .list import lst
+from .update import update
+from .predict import predict
