0d6f7bfdb96709d4a486559c46bb3550b754e93e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:41 +0000

    Add package dlpoly-py/0.1.3/dlpoly-py-0.1.3.tar.gz

---
+++ packages/dlpoly-py/dlpoly-py-0.1.3.tar.gz/dlpoly-py-0.1.3/dlpoly/__init__.py
@@ -0,0 +1,28 @@
+''' simple dlpoly utilities to play with inputs and outputs '''
+
+from distutils.version import LooseVersion
+from os.path import dirname, basename, isfile, join
+import glob
+import sys
+import numpy as np
+
+if sys.version_info[0] == 2:
+    raise ImportError('dlpoly-py requires Python3. This is Python2.')
+
+if LooseVersion(np.__version__) < '1.5':
+    raise ImportError(
+        'dlpoly-py needs NumPy-1.5.0 or later. You have: {:s}'.format(np.__version__))
+
+
+# from https://stackoverflow.com/questions/1057431
+modules = glob.glob(join(dirname(__file__), "*.py"))
+__all__ = [basename(f)[:-3] for f in modules if isfile(f)
+           and not f.endswith('__init__.py')]
+
+__version__ = '0.1.3'
+
+try:
+    from .dlpoly import DLPoly
+    print("Supported DL_POLY version {}".format(DLPoly.__version__))
+except ImportError:
+    raise ImportError('error importing dlpoly')
