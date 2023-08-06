0d0e987adc78970d4e92a4cce570d8e86cb620f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:45 +0000

    Add package limit-coverage/0.3.1/limit-coverage-0.3.1.tar.gz

---
+++ packages/limit-coverage/limit-coverage-0.3.1.tar.gz/limit-coverage-0.3.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['limit_coverage']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['limit-coverage = limit_coverage:main']}
+
+setup_kwargs = {
+    'name': 'limit-coverage',
+    'version': '0.3.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Max W Chase',
+    'author_email': 'max.chase@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
