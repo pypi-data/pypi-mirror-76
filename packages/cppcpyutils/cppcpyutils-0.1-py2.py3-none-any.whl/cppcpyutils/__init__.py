a9c55bec7da589761fd41a34806e816949a9cd83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:44 +0000

    Add package cppcpyutils/0.1/cppcpyutils-0.1-py2.py3-none-any.whl

---
+++ packages/cppcpyutils/cppcpyutils-0.1-py2.py3-none-any.whl/cppcpyutils/__init__.py
@@ -0,0 +1,5 @@
+'''utilities for common tasks in plantcv pipelines'''
+__version__ = '0.1'
+from cppcpyutils import data
+from cppcpyutils import util
+from cppcpyutils import viz
---
+++ packages/cppcpyutils/cppcpyutils-0.1-py2.py3-none-any.whl/cppcpyutils/util/__init__.py
@@ -0,0 +1,4 @@
+'''miscellaneous utilities'''
+
+from cppcpyutils.util.masked_stats import mean
+from cppcpyutils.util.masked_stats import std
