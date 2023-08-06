f6b9a8b2eac574003ffb5b52ae5dd5d6646da554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:53 +0000

    Add package django-ulogin/1.1.1/django_ulogin-1.1.1-py3-none-any.whl

---
+++ packages/django-ulogin/django_ulogin-1.1.1-py3-none-any.whl/django_ulogin/tests/templatetags.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+from django import test
+from django.template import Context, Template
+
+from django_ulogin import settings
+
+
+class UloginWidgetTest(test.TestCase):
+    """"""
+
+
+class ULoginMediaTest(test.TestCase):
+    """"""
+    TEMPLATE = Template('{% load ulogin_tags %}{% ulogin_media %}')
+
+    def test_1(self):
+        t = self.TEMPLATE.render(Context())
+        self.assertIn(settings.WIDGET_URL, t)
