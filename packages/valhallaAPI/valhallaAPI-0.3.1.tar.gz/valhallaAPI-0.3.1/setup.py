849e81772cb29be89581389d5cfdfea970bd519b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:13 +0000

    Add package valhallaAPI/0.3.1/valhallaAPI-0.3.1.tar.gz

---
+++ packages/valhallaAPI/valhallaAPI-0.3.1.tar.gz/valhallaAPI-0.3.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="valhallaAPI",
+    version="0.3.1",
+    author="Nextron",
+    author_email="florian.roth@nextron-systems.com",
+    description="Valhalla API Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nextron/valhallaAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'packaging',
+        'requests',
+        'configparser',
+    ],
+    python_requires='~=3.5',
+    scripts=[
+        'valhalla-cli',
+    ]
+)
