1a0b16fc641414396441eb7a1b374d042af21ec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:19 +0000

    Add package pyesasky/1.4.3/pyesasky-1.4.3-py2.py3-none-any.whl

---
+++ packages/pyesasky/pyesasky-1.4.3-py2.py3-none-any.whl/pyesasky-1.4.3.data/data/share/jupyter/nbextensions/pyesasky/js/config-nocache.js
@@ -0,0 +1,8 @@
+var serverProperties = {
+ contextPath: "${context.path}",
+ tapContext: "https://sky.esa.int/esasky-tap",
+ localeFilesLocation: "",
+ targetListFilesLocation: "",
+ showExtTap: "true",
+ showMissingTranslationBox: "false"
+};
