ce099b2f0887c07ad00abfff225ea8ba2d82a98e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:34 +0000

    Add package orjson/3.3.1/orjson-3.3.1.tar.gz

---
+++ packages/orjson/orjson-3.3.1.tar.gz/src/serialize/encode.rs
@@ -0,0 +1,462 @@
+// SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+use crate::exc::*;
+use crate::ffi::*;
+use crate::opt::*;
+use crate::serialize::dataclass::*;
+use crate::serialize::datetime::*;
+use crate::serialize::default::*;
+use crate::serialize::dict::*;
+use crate::serialize::numpy::*;
+use crate::serialize::tuple::*;
+use crate::serialize::uuid::*;
+use crate::serialize::writer::*;
+use crate::typeref::*;
+use crate::unicode::*;
+use serde::ser::{Serialize, SerializeMap, SerializeSeq, Serializer};
+use std::io::Write;
+use std::ptr::NonNull;
+
+// https://tools.ietf.org/html/rfc7159#section-6
+// "[-(2**53)+1, (2**53)-1]"
+const STRICT_INT_MIN: i64 = -9007199254740991;
+const STRICT_INT_MAX: i64 = 9007199254740991;
+
+pub const RECURSION_LIMIT: u8 = 255;
+
+pub fn serialize(
+    ptr: *mut pyo3::ffi::PyObject,
+    default: Option<NonNull<pyo3::ffi::PyObject>>,
+    opts: Opt,
+) -> Result<NonNull<pyo3::ffi::PyObject>, String> {
+    let mut buf = BytesWriter::new();
+    let obtype = pyobject_to_obtype(ptr, opts);
+    match obtype {
+        ObType::List | ObType::Dict | ObType::Dataclass | ObType::NumpyArray => {
+            buf.resize(1024);
+        }
+        _ => {}
+    }
+    buf.prefetch();
+    let obj = SerializePyObject::with_obtype(ptr, obtype, opts, 0, 0, default);
+    let res;
+    if likely!(opts & INDENT_2 != INDENT_2) {
+        res = serde_json::to_writer(&mut buf, &obj);
+    } else {
+        res = serde_json::to_writer_pretty(&mut buf, &obj);
+    }
+    match res {
+        Ok(_) => {
+            if opts & APPEND_NEWLINE != 0 {
+                buf.write(b"\n").unwrap();
+            }
+            Ok(buf.finish())
+        }
+        Err(err) => {
+            ffi!(_Py_Dealloc(buf.finish().as_ptr()));
+            Err(err.to_string())
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum ObType {
+    Str,
+    Int,
+    Bool,
+    None,
+    Float,
+    List,
+    Dict,
+    Datetime,
+    Date,
+    Time,
+    Tuple,
+    Uuid,
+    Dataclass,
+    NumpyScalar,
+    NumpyArray,
+    Enum,
+    StrSubclass,
+    Unknown,
+}
+
+#[inline]
+pub fn pyobject_to_obtype(obj: *mut pyo3::ffi::PyObject, opts: Opt) -> ObType {
+    unsafe {
+        let ob_type = ob_type!(obj);
+        if ob_type == STR_TYPE {
+            ObType::Str
+        } else if ob_type == INT_TYPE {
+            ObType::Int
+        } else if ob_type == BOOL_TYPE {
+            ObType::Bool
+        } else if ob_type == NONE_TYPE {
+            ObType::None
+        } else if ob_type == FLOAT_TYPE {
+            ObType::Float
+        } else if ob_type == LIST_TYPE {
+            ObType::List
+        } else if ob_type == DICT_TYPE {
+            ObType::Dict
+        } else if ob_type == DATETIME_TYPE && opts & PASSTHROUGH_DATETIME == 0 {
+            ObType::Datetime
+        } else {
+            pyobject_to_obtype_unlikely(obj, opts)
+        }
+    }
+}
+
+macro_rules! is_subclass {
+    ($ob_type:expr, $flag:ident) => {
+        unsafe { (((*$ob_type).tp_flags & pyo3::ffi::$flag) != 0) }
+    };
+}
+
+#[inline(never)]
+pub fn pyobject_to_obtype_unlikely(obj: *mut pyo3::ffi::PyObject, opts: Opt) -> ObType {
+    unsafe {
+        let ob_type = ob_type!(obj);
+        if ob_type == DATE_TYPE && opts & PASSTHROUGH_DATETIME == 0 {
+            ObType::Date
+        } else if ob_type == TIME_TYPE && opts & PASSTHROUGH_DATETIME == 0 {
+            ObType::Time
+        } else if ob_type == TUPLE_TYPE {
+            ObType::Tuple
+        } else if ob_type == UUID_TYPE {
+            ObType::Uuid
+        } else if (*(ob_type as *mut LocalPyTypeObject)).ob_type == ENUM_TYPE {
+            ObType::Enum
+        } else if is_subclass!(ob_type, Py_TPFLAGS_UNICODE_SUBCLASS)
+            && opts & PASSTHROUGH_SUBCLASS == 0
+        {
+            ObType::StrSubclass
+        } else if is_subclass!(ob_type, Py_TPFLAGS_LONG_SUBCLASS)
+            && opts & PASSTHROUGH_SUBCLASS == 0
+        {
+            ObType::Int
+        } else if is_subclass!(ob_type, Py_TPFLAGS_LIST_SUBCLASS)
+            && opts & PASSTHROUGH_SUBCLASS == 0
+        {
+            ObType::List
+        } else if is_subclass!(ob_type, Py_TPFLAGS_DICT_SUBCLASS)
+            && opts & PASSTHROUGH_SUBCLASS == 0
+        {
+            ObType::Dict
+        } else if opts & PASSTHROUGH_DATACLASS == 0
+            && ffi!(PyDict_Contains((*ob_type).tp_dict, DATACLASS_FIELDS_STR)) == 1
+        {
+            ObType::Dataclass
+        } else if opts & SERIALIZE_NUMPY != 0 && is_numpy_scalar(ob_type) {
+            ObType::NumpyScalar
+        } else if opts & SERIALIZE_NUMPY != 0 && is_numpy_array(ob_type) {
+            ObType::NumpyArray
+        } else {
+            ObType::Unknown
+        }
+    }
+}
+
+pub struct SerializePyObject {
+    ptr: *mut pyo3::ffi::PyObject,
+    obtype: ObType,
+    opts: Opt,
+    default_calls: u8,
+    recursion: u8,
+    default: Option<NonNull<pyo3::ffi::PyObject>>,
+}
+
+impl SerializePyObject {
+    #[inline]
+    pub fn new(
+        ptr: *mut pyo3::ffi::PyObject,
+        opts: Opt,
+        default_calls: u8,
+        recursion: u8,
+        default: Option<NonNull<pyo3::ffi::PyObject>>,
+    ) -> Self {
+        SerializePyObject {
+            ptr: ptr,
+            obtype: pyobject_to_obtype(ptr, opts),
+            opts: opts,
+            default_calls: default_calls,
+            recursion: recursion,
+            default: default,
+        }
+    }
+
+    #[inline]
+    pub fn with_obtype(
+        ptr: *mut pyo3::ffi::PyObject,
+        obtype: ObType,
+        opts: Opt,
+        default_calls: u8,
+        recursion: u8,
+        default: Option<NonNull<pyo3::ffi::PyObject>>,
+    ) -> Self {
+        SerializePyObject {
+            ptr: ptr,
+            obtype: obtype,
+            opts: opts,
+            default_calls: default_calls,
+            recursion: recursion,
+            default: default,
+        }
+    }
+}
+
+impl<'p> Serialize for SerializePyObject {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self.obtype {
+            ObType::Str => {
+                let mut str_size: pyo3::ffi::Py_ssize_t = 0;
+                let uni = read_utf8_from_str(self.ptr, &mut str_size);
+                if unlikely!(uni.is_null()) {
+                    err!(INVALID_STR)
+                }
+                serializer.serialize_str(str_from_slice!(uni, str_size))
+            }
+            ObType::StrSubclass => {
+                let mut str_size: pyo3::ffi::Py_ssize_t = 0;
+                let uni = ffi!(PyUnicode_AsUTF8AndSize(self.ptr, &mut str_size)) as *const u8;
+                if unlikely!(uni.is_null()) {
+                    err!(INVALID_STR)
+                }
+                serializer.serialize_str(str_from_slice!(uni, str_size))
+            }
+            ObType::Int => {
+                let val = ffi!(PyLong_AsLongLong(self.ptr));
+                if unlikely!(val == -1) && !ffi!(PyErr_Occurred()).is_null() {
+                    err!("Integer exceeds 64-bit range")
+                } else if unlikely!(self.opts & STRICT_INTEGER != 0)
+                    && (val > STRICT_INT_MAX || val < STRICT_INT_MIN)
+                {
+                    err!("Integer exceeds 53-bit range")
+                }
+                serializer.serialize_i64(val)
+            }
+            ObType::None => serializer.serialize_unit(),
+            ObType::Float => serializer.serialize_f64(ffi!(PyFloat_AS_DOUBLE(self.ptr))),
+            ObType::Bool => serializer.serialize_bool(unsafe { self.ptr == TRUE }),
+            ObType::Datetime => DateTime::new(self.ptr, self.opts).serialize(serializer),
+            ObType::Date => Date::new(self.ptr).serialize(serializer),
+            ObType::Time => match Time::new(self.ptr, self.opts) {
+                Ok(val) => val.serialize(serializer),
+                Err(TimeError::HasTimezone) => err!(TIME_HAS_TZINFO),
+            },
+            ObType::Uuid => UUID::new(self.ptr).serialize(serializer),
+            ObType::Dict => {
+                if unlikely!(self.recursion == RECURSION_LIMIT) {
+                    err!(RECURSION_LIMIT_REACHED)
+                }
+                let len = unsafe { PyDict_GET_SIZE(self.ptr) as usize };
+                if unlikely!(len == 0) {
+                    serializer.serialize_map(Some(0)).unwrap().end()
+                } else if likely!(self.opts & SORT_OR_NON_STR_KEYS == 0) {
+                    let mut map = serializer.serialize_map(None).unwrap();
+                    let mut pos = 0isize;
+                    let mut str_size: pyo3::ffi::Py_ssize_t = 0;
+                    let mut key: *mut pyo3::ffi::PyObject = std::ptr::null_mut();
+                    let mut value: *mut pyo3::ffi::PyObject = std::ptr::null_mut();
+                    for _ in 0..=len - 1 {
+                        unsafe {
+                            pyo3::ffi::_PyDict_Next(
+                                self.ptr,
+                                &mut pos,
+                                &mut key,
+                                &mut value,
+                                std::ptr::null_mut(),
+                            )
+                        };
+                        if unlikely!(ob_type!(key) != STR_TYPE) {
+                            err!(KEY_MUST_BE_STR)
+                        }
+                        {
+                            let data = read_utf8_from_str(key, &mut str_size);
+                            if unlikely!(data.is_null()) {
+                                err!(INVALID_STR)
+                            }
+                            map.serialize_key(str_from_slice!(data, str_size)).unwrap();
+                        }
+
+                        map.serialize_value(&SerializePyObject::new(
+                            value,
+                            self.opts,
+                            self.default_calls,
+                            self.recursion + 1,
+                            self.default,
+                        ))?;
+                    }
+                    map.end()
+                } else if self.opts & NON_STR_KEYS != 0 {
+                    NonStrKey::new(
+                        self.ptr,
+                        self.opts,
+                        self.default_calls,
+                        self.recursion,
+                        self.default,
+                        len,
+                    )
+                    .serialize(serializer)
+                } else {
+                    DictSortedKey::new(
+                        self.ptr,
+                        self.opts,
+                        self.default_calls,
+                        self.recursion,
+                        self.default,
+                        len,
+                    )
+                    .serialize(serializer)
+                }
+            }
+            ObType::List => {
+                if unlikely!(self.recursion == RECURSION_LIMIT) {
+                    err!(RECURSION_LIMIT_REACHED)
+                }
+                let len = ffi!(PyList_GET_SIZE(self.ptr)) as usize;
+                if len == 0 {
+                    serializer.serialize_seq(Some(0)).unwrap().end()
+                } else {
+                    let mut type_ptr = std::ptr::null_mut();
+                    let mut ob_type = ObType::Str;
+
+                    let mut seq = serializer.serialize_seq(None).unwrap();
+                    for i in 0..=len - 1 {
+                        let elem = unsafe {
+                            *(*(self.ptr as *mut pyo3::ffi::PyListObject))
+                                .ob_item
+                                .offset(i as isize)
+                        };
+                        if ob_type!(elem) != type_ptr {
+                            type_ptr = ob_type!(elem);
+                            ob_type = pyobject_to_obtype(elem, self.opts);
+                        }
+                        seq.serialize_element(&SerializePyObject::with_obtype(
+                            elem,
+                            ob_type,
+                            self.opts,
+                            self.default_calls,
+                            self.recursion + 1,
+                            self.default,
+                        ))?;
+                    }
+                    seq.end()
+                }
+            }
+            ObType::Tuple => {
+                let mut seq = serializer.serialize_seq(None).unwrap();
+                for elem in PyTupleIterator::new(self.ptr) {
+                    seq.serialize_element(&SerializePyObject::new(
+                        elem.as_ptr(),
+                        self.opts,
+                        self.default_calls,
+                        self.recursion + 1,
+                        self.default,
+                    ))?
+                }
+                seq.end()
+            }
+            ObType::Dataclass => {
+                if unlikely!(self.recursion == RECURSION_LIMIT) {
+                    err!(RECURSION_LIMIT_REACHED)
+                }
+                let dict = ffi!(PyObject_GetAttr(self.ptr, DICT_STR));
+                if !dict.is_null() {
+                    ffi!(Py_DECREF(dict));
+                    let len = unsafe { PyDict_GET_SIZE(dict) as usize };
+                    if unlikely!(len == 0) {
+                        return serializer.serialize_map(Some(0)).unwrap().end();
+                    }
+                    let mut map = serializer.serialize_map(None).unwrap();
+                    let mut pos = 0isize;
+                    let mut str_size: pyo3::ffi::Py_ssize_t = 0;
+                    let mut key: *mut pyo3::ffi::PyObject = std::ptr::null_mut();
+                    let mut value: *mut pyo3::ffi::PyObject = std::ptr::null_mut();
+                    for _ in 0..=len - 1 {
+                        unsafe {
+                            pyo3::ffi::_PyDict_Next(
+                                dict,
+                                &mut pos,
+                                &mut key,
+                                &mut value,
+                                std::ptr::null_mut(),
+                            )
+                        };
+                        if unlikely!(ob_type!(key) != STR_TYPE) {
+                            err!(KEY_MUST_BE_STR)
+                        }
+                        {
+                            let data = read_utf8_from_str(key, &mut str_size);
+                            if unlikely!(data.is_null()) {
+                                err!(INVALID_STR)
+                            }
+                            let key_as_str = str_from_slice!(data, str_size);
+                            if unlikely!(key_as_str.as_bytes()[0] == b'_') {
+                                continue;
+                            }
+                            map.serialize_key(key_as_str).unwrap();
+                        }
+
+                        map.serialize_value(&SerializePyObject::new(
+                            value,
+                            self.opts,
+                            self.default_calls,
+                            self.recursion + 1,
+                            self.default,
+                        ))?;
+                    }
+                    map.end()
+                } else {
+                    unsafe { pyo3::ffi::PyErr_Clear() };
+                    DataclassSerializer::new(
+                        self.ptr,
+                        self.opts,
+                        self.default_calls,
+                        self.recursion,
+                        self.default,
+                    )
+                    .serialize(serializer)
+                }
+            }
+            ObType::Enum => {
+                let value = ffi!(PyObject_GetAttr(self.ptr, VALUE_STR));
+                ffi!(Py_DECREF(value));
+                SerializePyObject::new(
+                    value,
+                    self.opts,
+                    self.default_calls,
+                    self.recursion,
+                    self.default,
+                )
+                .serialize(serializer)
+            }
+            ObType::NumpyArray => match NumpyArray::new(self.ptr) {
+                Ok(val) => val.serialize(serializer),
+                Err(PyArrayError::Malformed) => err!("numpy array is malformed"),
+                Err(PyArrayError::NotContiguous) | Err(PyArrayError::UnsupportedDataType) => {
+                    DefaultSerializer::new(
+                        self.ptr,
+                        self.opts,
+                        self.default_calls,
+                        self.recursion,
+                        self.default,
+                    )
+                    .serialize(serializer)
+                }
+            },
+            ObType::NumpyScalar => NumpyScalar::new(self.ptr).serialize(serializer),
+            ObType::Unknown => DefaultSerializer::new(
+                self.ptr,
+                self.opts,
+                self.default_calls,
+                self.recursion,
+                self.default,
+            )
+            .serialize(serializer),
+        }
+    }
+}
---
+++ packages/orjson/orjson-3.3.1.tar.gz/test/test_numpy.py
@@ -0,0 +1,308 @@
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import unittest
+
+import pytest
+
+import orjson
+
+try:
+    import numpy
+except ImportError:
+    numpy = None
+
+
+def numpy_default(obj):
+    return obj.tolist()
+
+
+@pytest.mark.skipif(numpy is None, reason="numpy is not installed")
+class NumpyTests(unittest.TestCase):
+    def test_numpy_array_d1_uintp(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([0, 18446744073709551615], numpy.uintp),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[0,18446744073709551615]",
+        )
+
+    def test_numpy_array_d1_intp(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([-9223372036854775807, 9223372036854775807], numpy.intp),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[-9223372036854775807,9223372036854775807]",
+        )
+
+    def test_numpy_array_d1_i64(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([-9223372036854775807, 9223372036854775807], numpy.int64),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[-9223372036854775807,9223372036854775807]",
+        )
+
+    def test_numpy_array_d1_u64(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([0, 18446744073709551615], numpy.uint64),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[0,18446744073709551615]",
+        )
+
+    def test_numpy_array_d1_i32(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([-2147483647, 2147483647], numpy.int32),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[-2147483647,2147483647]",
+        )
+
+    def test_numpy_array_d1_u32(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([0, 4294967295], numpy.uint32),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[0,4294967295]",
+        )
+
+    def test_numpy_array_d1_f32(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([1.0, 3.4028235e38], numpy.float32),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[1.0,3.4028235e38]",
+        )
+
+    def test_numpy_array_d1_f64(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([1.0, 1.7976931348623157e308], numpy.float64),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[1.0,1.7976931348623157e308]",
+        )
+
+    def test_numpy_array_d1_bool(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([True, False, False, True]),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[true,false,false,true]",
+        )
+
+    def test_numpy_array_d2_i64(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([[1, 2, 3], [4, 5, 6]], numpy.int64),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[[1,2,3],[4,5,6]]",
+        )
+
+    def test_numpy_array_d2_f64(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], numpy.float64),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[[1.0,2.0,3.0],[4.0,5.0,6.0]]",
+        )
+
+    def test_numpy_array_d3_i32(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]], numpy.int32),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[[[1,2],[3,4]],[[5,6],[7,8]]]",
+        )
+
+    def test_numpy_array_d3_i64(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array([[[1, 2], [3, 4], [5, 6], [7, 8]]], numpy.int64),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[[[1,2],[3,4],[5,6],[7,8]]]",
+        )
+
+    def test_numpy_array_d3_f64(self):
+        self.assertEqual(
+            orjson.dumps(
+                numpy.array(
+                    [[[1.0, 2.0], [3.0, 4.0]], [[5.0, 6.0], [7.0, 8.0]]], numpy.float64
+                ),
+                option=orjson.OPT_SERIALIZE_NUMPY,
+            ),
+            b"[[[1.0,2.0],[3.0,4.0]],[[5.0,6.0],[7.0,8.0]]]",
+        )
+
+    def test_numpy_array_fotran(self):
+        array = numpy.array([[1, 2], [3, 4]], order="F")
+        assert array.flags["F_CONTIGUOUS"] == True
+        with self.assertRaises(orjson.JSONEncodeError):
+            orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY)
+        self.assertEqual(
+            orjson.dumps(
+                array, default=numpy_default, option=orjson.OPT_SERIALIZE_NUMPY
+            ),
+            orjson.dumps(array.tolist()),
+        )
+
+    def test_numpy_array_unsupported_dtype(self):
+        array = numpy.array([[1, 2], [3, 4]], numpy.int8)
+        with self.assertRaises(orjson.JSONEncodeError):
+            orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY)
+        self.assertEqual(
+            orjson.dumps(
+                array, default=numpy_default, option=orjson.OPT_SERIALIZE_NUMPY
+            ),
+            orjson.dumps(array.tolist()),
+        )
+
+    def test_numpy_array_d1(self):
+        array = numpy.array([1])
+        self.assertEqual(
+            orjson.loads(orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY,)),
+            array.tolist(),
+        )
+
+    def test_numpy_array_d2(self):
+        array = numpy.array([[1]])
+        self.assertEqual(
+            orjson.loads(orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY,)),
+            array.tolist(),
+        )
+
+    def test_numpy_array_d3(self):
+        array = numpy.array([[[1]]])
+        self.assertEqual(
+            orjson.loads(orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY,)),
+            array.tolist(),
+        )
+
+    def test_numpy_array_d4(self):
+        array = numpy.array([[[[1]]]])
+        self.assertEqual(
+            orjson.loads(orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY,)),
+            array.tolist(),
+        )
+
+    def test_numpy_array_4_stride(self):
+        array = numpy.random.rand(4, 4, 4, 4)
+        self.assertEqual(
+            orjson.loads(orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY,)),
+            array.tolist(),
+        )
+
+    def test_numpy_array_dimension_zero(self):
+        array = numpy.array(0)
+        assert array.ndim == 0
+        with self.assertRaises(orjson.JSONEncodeError):
+            orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY)
+
+    def test_numpy_array_dimension_max(self):
+        array = numpy.random.rand(
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        )
+        assert array.ndim == 32
+        self.assertEqual(
+            orjson.loads(orjson.dumps(array, option=orjson.OPT_SERIALIZE_NUMPY,)),
+            array.tolist(),
+        )
+
+    def test_numpy_primitives(self):
+        # int32
+        self.assertEqual(
+            orjson.dumps(numpy.int32(1), option=orjson.OPT_SERIALIZE_NUMPY), b"1"
+        )
+        self.assertEqual(
+            orjson.dumps(numpy.int32(2147483647), option=orjson.OPT_SERIALIZE_NUMPY),
+            b"2147483647",
+        )
+        self.assertEqual(
+            orjson.dumps(numpy.int32(-2147483648), option=orjson.OPT_SERIALIZE_NUMPY),
+            b"-2147483648",
+        )
+        # int 64
+        self.assertEqual(
+            orjson.dumps(
+                numpy.int64(-9223372036854775808), option=orjson.OPT_SERIALIZE_NUMPY
+            ),
+            b"-9223372036854775808",
+        )
+        self.assertEqual(
+            orjson.dumps(
+                numpy.int64(9223372036854775807), option=orjson.OPT_SERIALIZE_NUMPY
+            ),
+            b"9223372036854775807",
+        )
+        # uint32
+        self.assertEqual(
+            orjson.dumps(numpy.uint32(0), option=orjson.OPT_SERIALIZE_NUMPY), b"0"
+        )
+        self.assertEqual(
+            orjson.dumps(numpy.uint32(4294967295), option=orjson.OPT_SERIALIZE_NUMPY),
+            b"4294967295",
+        )
+        # uint64
+        self.assertEqual(
+            orjson.dumps(numpy.uint64(0), option=orjson.OPT_SERIALIZE_NUMPY), b"0"
+        )
+        self.assertEqual(
+            orjson.dumps(
+                numpy.uint64(18446744073709551615), option=orjson.OPT_SERIALIZE_NUMPY
+            ),
+            b"18446744073709551615",
+        )
+
+        # float32
+        self.assertEqual(
+            orjson.dumps(numpy.float32(1.0), option=orjson.OPT_SERIALIZE_NUMPY), b"1.0"
+        )
+
+        # float64
+        self.assertEqual(
+            orjson.dumps(numpy.float64(123.123), option=orjson.OPT_SERIALIZE_NUMPY),
+            b"123.123",
+        )
