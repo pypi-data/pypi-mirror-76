c4f71cdb9a1f4be3b169646b205f73df3ed9e517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:16 +0000

    Add package esgcet/5.0.0a0/esgcet-5.0.0a0-py3-none-any.whl

---
+++ packages/esgcet/esgcet-5.0.0a0-py3-none-any.whl/esgcet/pub_test.py
@@ -0,0 +1,40 @@
+from pub_client import publisherClient
+
+import list2json, sys, json
+
+from settings import INDEX_NODE, CERT_FN, DEBUG
+
+#hostname = INDEX_NODE
+cert_fn = CERT_FN
+
+ARGS = 1
+
+def main(args):
+
+    if len(args) < (ARGS):
+        print("Missing required arguments")
+        exit(0)
+
+
+    #	hostname = args[1]
+    #	cert_fn = args[3]
+
+    pubCli = publisherClient(cert_fn, hostname)
+
+    d = json.load(open(args[0]))
+
+    try:
+        hostname = d[-1]['index_node']
+    except Exception as e:
+        if DEBUG:
+            print(e)
+            traceback.print_exc()
+        print('Index node value missing from record!')
+        exit(-1)
+    for rec in d:
+
+        new_xml = list2json.gen_xml(rec)
+        pubCli.publish(new_xml)
+#        print(new_xml)
+if __name__ == '__main__':
+    main(sys.argv[1:])
