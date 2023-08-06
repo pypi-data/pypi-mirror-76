059fa9ee1909195682e802225940b80d84227b51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:08 +0000

    Add package django-loci/0.3.4/django-loci-0.3.4.tar.gz

---
+++ packages/django-loci/django-loci-0.3.4.tar.gz/django-loci-0.3.4/django_loci/apps.py
@@ -0,0 +1,27 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+from .channels.receivers import load_location_receivers
+
+
+class LociConfig(AppConfig):
+    name = 'django_loci'
+    verbose_name = _('django-loci')
+
+    def __setmodels__(self):
+        """
+        this method can be overridden in 3rd party apps
+        """
+        from .models import Location
+
+        self.location_model = Location
+
+    def ready(self):
+        import leaflet
+
+        leaflet.app_settings['NO_GLOBALS'] = False
+        self.__setmodels__()
+        self._load_receivers()
+
+    def _load_receivers(self):
+        load_location_receivers(sender=self.location_model)
