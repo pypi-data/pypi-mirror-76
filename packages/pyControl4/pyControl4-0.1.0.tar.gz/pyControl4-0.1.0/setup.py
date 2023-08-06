7b0f8cddff05c769c210af9ea8c13cdf07d669d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:59 +0000

    Add package pyControl4/0.1.0/pyControl4-0.1.0.tar.gz

---
+++ packages/pyControl4/pyControl4-0.1.0.tar.gz/pyControl4-0.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyControl4",  # Replace with your own username
+    version="0.1.0",
+    author="lawtancool",
+    author_email="contact@lawrencetan.ml",
+    description="Python 3 asyncio package for interacting with Control4 systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lawtancool/pyControl4",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
