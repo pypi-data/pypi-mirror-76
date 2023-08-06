b72f9c4a377376fe988566baf9d08c4276594a6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:09 +0000

    Add package kmerVC/1.0/kmerVC-1.0.tar.gz

---
+++ packages/kmerVC/kmerVC-1.0.tar.gz/kmerVC-1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'kmerVC',
+    packages = ['kmerVC'],
+    version = '1.0',
+    description = 'Statistical variant calling software that utilizes genomic kmers.',
+    author = 'Ji Research Group',
+    author_email = 'genomics_ji@stanford.edu',
+    url = 'https://github.com/ashuaibi7/kmerVC_pypi',
+    download_url = 'https://github.com/ashuaibi7/kmerVC_pypi/archive/v1.0.tar.gz',
+    keywords = ['v1.0'],
+    classifiers = [],
+)
