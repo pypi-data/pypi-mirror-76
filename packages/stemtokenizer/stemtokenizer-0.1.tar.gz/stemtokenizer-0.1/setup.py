0e78839bb8dee14cbab431e006a81cbc5e3441eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:29 +0000

    Add package stemtokenizer/0.1/stemtokenizer-0.1.tar.gz

---
+++ packages/stemtokenizer/stemtokenizer-0.1.tar.gz/stemtokenizer-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='stemtokenizer',
+      version='0.1',
+      description='Adding stemming functionality to Sklearn text vectorizers.',
+      #url='http://github.com/storborg/funniest',
+      author='Michael Cortes',
+      author_email='michaelgcortes@gmail.com',
+      license='MIT',
+      packages=['stemtokenizer'],
+      zip_safe=False)
