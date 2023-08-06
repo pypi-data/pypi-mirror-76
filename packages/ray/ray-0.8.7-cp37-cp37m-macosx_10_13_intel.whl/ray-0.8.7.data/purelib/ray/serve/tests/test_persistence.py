c847339968f8f75a3aee3bb74b588dc4d73aa3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:36 +0000

    Add package ray/0.8.7/ray-0.8.7-cp37-cp37m-macosx_10_13_intel.whl

---
+++ packages/ray/ray-0.8.7-cp37-cp37m-macosx_10_13_intel.whl/ray-0.8.7.data/purelib/ray/serve/tests/test_persistence.py
@@ -0,0 +1,29 @@
+import ray
+import ray.test_utils
+from ray import serve
+
+
+def test_new_driver(serve_instance):
+    script = """
+import ray
+ray.init(address="{}")
+
+from ray import serve
+serve.init()
+
+def driver(flask_request):
+    return "OK!"
+
+serve.create_backend("driver", driver)
+serve.create_endpoint("driver", backend="driver", route="/driver")
+""".format(ray.worker._global_node._redis_address)
+    ray.test_utils.run_string_as_driver(script)
+
+    handle = serve.get_handle("driver")
+    assert ray.get(handle.remote()) == "OK!"
+
+
+if __name__ == "__main__":
+    import sys
+    import pytest
+    sys.exit(pytest.main(["-v", "-s", __file__]))
---
+++ packages/ray/ray-0.8.7-cp37-cp37m-macosx_10_13_intel.whl/ray-0.8.7.data/purelib/ray/tune/utils/__init__.py
@@ -0,0 +1,15 @@
+from ray.tune.utils.util import deep_update, flatten_dict, get_pinned_object, \
+    merge_dicts, pin_in_object_store, unflattened_lookup, UtilMonitor, \
+    validate_save_restore, warn_if_slow
+
+__all__ = [
+    "deep_update",
+    "flatten_dict",
+    "get_pinned_object",
+    "merge_dicts",
+    "pin_in_object_store",
+    "unflattened_lookup",
+    "UtilMonitor",
+    "validate_save_restore",
+    "warn_if_slow",
+]
