e3e0e0bfb7b2dfb16b7c52f7c21b6a6d7219b542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:50 +0000

    Add package kapoor-dist/0.1/kapoor_dist-0.1.tar.gz

---
+++ packages/kapoor-dist/kapoor_dist-0.1.tar.gz/kapoor_dist-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='kapoor_dist',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['kapoor_dist'],
+      author = 'Siddharth Kapoor', 
+      author_email = 'Siddhartgautam347@gmail.com',  
+      zip_safe=False)
