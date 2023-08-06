baa7f1608351b7c0b4038cee22e3f4f3141c5c3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:44 +0000

    Add package LazyIter-teshnizi/0.5.1/LazyIter-teshnizi-0.5.1.tar.gz

---
+++ packages/LazyIter-teshnizi/LazyIter-teshnizi-0.5.1.tar.gz/LazyIter-teshnizi-0.5.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+
+setuptools.setup(
+	name="LazyIter-teshnizi",
+	version="0.5.1",
+	author="Ali AhmadiTeshnizi",
+	author_email="aliahmaditeshnizi@gmail.com",
+	description="LazyIter ICML2020",
+	long_description="Package for the ICML paper  \"LazyIter: A Fast Algorithm for Counting Markov Equivalent DAGs and Designing Experiments\"",
+	long_description_content_type="text/markdown",
+    url="https://github.com/teshnizi/LazyIter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
