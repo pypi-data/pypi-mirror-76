cb30a2a439bef444b463caca622056656197df2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:20 +0000

    Add package touchbar-lyric/0.4.1/touchbar-lyric-0.4.1.tar.gz

---
+++ packages/touchbar-lyric/touchbar-lyric-0.4.1.tar.gz/touchbar-lyric-0.4.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="touchbar-lyric",
+    version="0.4.1",
+    author="Chenghao",
+    python_requires=">3.6.0",
+    author_email="mouchenghao@gmail.com",
+    description="Show time-synced lyric with BTT!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ChenghaoMou/touchbar-lyric",
+    packages=setuptools.find_packages(),
+    install_requires=["requests", "osascript", "cachier", "hanziconv", "pinyin", "regex", "loguru", "pycrypto"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
