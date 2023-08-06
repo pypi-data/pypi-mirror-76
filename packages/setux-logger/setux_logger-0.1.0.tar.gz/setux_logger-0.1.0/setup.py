00e992b2bc2c9787cb34f7421694c1a7a5fb8dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:16 +0000

    Add package setux-logger/0.1.0/setux_logger-0.1.0.tar.gz

---
+++ packages/setux-logger/setux_logger-0.1.0.tar.gz/setux_logger-0.1.0/setup.py
@@ -0,0 +1,36 @@
+from os.path import join, dirname, abspath
+
+from setuptools import setup, find_namespace_packages
+
+from setux.logger import __version__
+
+curdir = abspath(dirname(__file__))
+readme = open(join(curdir, 'README.rst')).read()
+
+setup(
+    name             = 'setux_logger',
+    version          = __version__,
+    description      = 'System deployment',
+    long_description = readme,
+    keywords         = ['utility', ],
+    url              = 'https://gitlab.com/dugres/setux_logger',
+    author           = 'Louis RIVIERE',
+    author_email     = 'louis@riviere.xyz',
+    license          = 'MIT',
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    packages = find_namespace_packages(
+        include=['setux.*']
+    ),
+)
