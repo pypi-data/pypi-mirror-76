931e384c9a70e4a03730d9d045bbbcaa0042f0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:35 +0000

    Add package exceldraw/1.4/exceldraw-1.4.tar.gz

---
+++ packages/exceldraw/exceldraw-1.4.tar.gz/exceldraw-1.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='exceldraw',
+      version='1.4',
+      description='Draw your Excel image',
+      packages=['exceldraw'],
+      author_email='ujjayanta.bhaumik.18@ucl.ac.uk',
+      zip_safe=False)
