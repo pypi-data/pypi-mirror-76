3f410443b5915e8ef6cdf74a5cb3a30adde4cd75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:32 +0000

    Add package heliocats/1.1/heliocats-1.1.tar.gz

---
+++ packages/heliocats/heliocats-1.1.tar.gz/heliocats-1.1/heliocats/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+#from .helcats import ICMECAT_EVENT  # noqa: F401
+#from .satellites import BEPI, DSCOVR, MES, PSP, SOIO, STA, STB, VEX, WIND  # noqa: F401
+#from .spice import SpiceObject as _SpiceObject
+#from .util import get_heliosat_paths
+
+
+__author__ = "Christian Moestl"
+__copyright__ = "Copyright (C) 2020 Christian Moestl"
+__license__ = "MIT"
+__version__ = "1.1"
+
