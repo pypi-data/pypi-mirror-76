44c130896b4db1f81a4bad3d0c7b0aad3e68a6e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:22 +0000

    Add package dcutils/0.2/dcutils-0.2.tar.gz

---
+++ packages/dcutils/dcutils-0.2.tar.gz/dcutils-0.2/dcutils/move.py
@@ -0,0 +1,54 @@
+from google.cloud import storage
+from retry import retry
+import os
+import requests
+import sys
+
+log = open("../move.log", "a")
+sys.stdout = log
+
+@retry(tries=-1)
+def mv_blob(
+    STORAGE_CLIENT,
+    blob_name,
+    new_blob_name,
+    bucket_name,
+    new_bucket_name
+    ):
+    """
+    Function for moving files between directories or buckets. it will use GCP's copy
+    function then delete the blob from the old location.
+
+    inputs
+    -----
+    bucket_name: name of bucket
+    blob_name: str, name of file
+    ex. 'data/some_location/file_name'
+    new_bucket_name: name of bucket (can be same as original if we're just moving around directories)
+    new_blob_name: str, name of file in new directory in target bucket
+    ex. 'data/destination/file_name'
+    """
+    source_bucket = STORAGE_CLIENT.get_bucket(bucket_name)
+    source_blob = source_bucket.get_blob(blob_name)
+    destination_bucket = STORAGE_CLIENT.get_bucket(new_bucket_name)
+    
+    # get size of blob
+    blob_size = source_blob.size
+
+    # rewrite of blob greater than 15mb
+    
+    if (blob_size > 15000000):
+        url = """https://storage.googleapis.com/storage/v1/b/
+                {}/o/{}/rewriteTo/b/
+                {}/o/{}""".format(
+                    source_bucket, 
+                    source_blob,
+                    destination_bucket,
+                    new_blob_name)
+
+        requests.post(url)
+        print('rewrote {} to {}\n'.format(source_blob.name, new_blob_name))
+    else:
+        #copy to new destination
+        new_blob = source_bucket.copy_blob(source_blob, destination_bucket, new_blob_name)
+        print('blob {} has been moved to {}\n'.format(source_blob.name, new_blob.name))
