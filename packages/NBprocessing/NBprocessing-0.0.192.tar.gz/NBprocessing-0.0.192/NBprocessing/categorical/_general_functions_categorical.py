d2373cab51abe18ec1c58457f95180c67b99fa72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:22 +0000

    Add package NBprocessing/0.0.192/NBprocessing-0.0.192.tar.gz

---
+++ packages/NBprocessing/NBprocessing-0.0.192.tar.gz/NBprocessing-0.0.192/NBprocessing/categorical/_general_functions_categorical.py
@@ -0,0 +1,13 @@
+import re
+from NBprocessing.src import constance_object
+
+def color_imbalanced(raw_input):
+    """
+    Takes a scalar and returns a string with
+    the css property `'color: red'` for negative
+    strings, black otherwise.
+    """
+    pattern = re.compile(r'(\d+)')
+    val = int(pattern.search(raw_input)[0])
+    color = constance_object.BLACK if 5 < val < 90 else constance_object.RED
+    return constance_object.OUTPUT.format(color)
