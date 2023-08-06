4a45cf6753e404408eed25b65d4279600191f1b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:17 +0000

    Add package frpy/0.1.4/frpy-0.1.4.tar.gz

---
+++ packages/frpy/frpy-0.1.4.tar.gz/frpy-0.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import *
+
+with open("README.md", "r") as r:
+    long_description = r.read()
+
+setup(
+    name="frpy",
+    version="0.1.4",
+    author="Calculus",
+    author_email="calculus@secret.fyi",
+    description="An api for FRHD",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/calculus-dev/frpy/",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    install_requires=[
+        'requests'
+    ],
+    python_requires='>=3.8'
+)
+
+# python3 setup.py sdist bdist_wheel
+# python3 -m twine upload --repository pypi dist/*
