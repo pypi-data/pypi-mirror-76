14b3574df765cf22eb97bb48365399522811e15f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:39 +0000

    Add package googling/0.0.5/googling-0.0.5-py3-none-any.whl

---
+++ packages/googling/googling-0.0.5-py3-none-any.whl/googling/calibration.py
@@ -0,0 +1,6 @@
+def real_calibration(a,b,c):
+    print("you are calling the calibration function,this is a encrypted module.")
+    print("parameter a is :",a)
+    print("parameter b is :",b)
+    print("parameter c is :",c)
+    return a+b+c
