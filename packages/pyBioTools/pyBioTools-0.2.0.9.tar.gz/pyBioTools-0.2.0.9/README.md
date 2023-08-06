9e055b3eb71f56be40cadbe41ff46aa2e14ba50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:49 +0000

    Add package pyBioTools/0.2.0.9/pyBioTools-0.2.0.9.tar.gz

---
+++ packages/pyBioTools/pyBioTools-0.2.0.9.tar.gz/pyBioTools-0.2.0.9/README.md
@@ -0,0 +1,24 @@
+# pyBioTools
+
+[![GitHub license](https://img.shields.io/github/license/a-slide/pyBioTools.svg)](https://github.com/a-slide/pyBioTools/blob/master/LICENSE)
+[![Language](https://img.shields.io/badge/Language-Python3-yellow.svg)](https://www.python.org/)
+[![DOI](https://zenodo.org/badge/111435767.svg)](https://zenodo.org/badge/latestdoi/111435767)
+
+[![PyPI version](https://badge.fury.io/py/pyBioTools.svg)](https://badge.fury.io/py/pyBioTools)
+[![PyPI downloads](https://pepy.tech/badge/pyBioTools)](https://pepy.tech/project/pyBioTools)
+[![Anaconda version](https://anaconda.org/aleg/pybiotools/badges/version.svg)](https://anaconda.org/aleg/pybiotools)
+[![Anaconda downloads](https://anaconda.org/aleg/pybiotools/badges/downloads.svg)](https://anaconda.org/aleg/pybiotools)
+
+**pyBioTools is a Python3 package allowing to manipulate standard files containing biological sequence information**
+
+Full documentation is available at https://a-slide.github.io/pyBioTools/
+
+Please be aware that pyBioTools is a research package that is still under development.
+
+It was tested under Linux Ubuntu 16.04 and in an HPC environment running under Red Hat Enterprise 7.1.
+
+Thank you
+
+### Authors
+
+* Adrien Leger - aleg {at} ebi.ac.uk
---
+++ packages/pyBioTools/pyBioTools-0.2.0.9.tar.gz/pyBioTools-0.2.0.9/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+# Define package info
+name = "pyBioTools"
+version = '0.2.0.9'
+description = "pyBioTools is a collection of tools to manipulate biological sequences"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# Collect info in a dictionnary for setup.py
+setup(
+    name = name,
+    description = description,
+    version = version,
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/a-slide/pyBioTools",
+    author = 'Adrien Leger',
+    author_email = 'aleg@ebi.ac.uk',
+    license = 'GPLv3',
+    python_requires ='>=3.5',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'],
+    install_requires = [
+        'tqdm>=4.32.1',
+        'numpy>=1.16.4',
+        'pysam>=0.15.2',
+        'pandas>=0.24.2'],
+    packages = [name],
+    entry_points = {'console_scripts': ['pyBioTools=pyBioTools.__main__:main']})
