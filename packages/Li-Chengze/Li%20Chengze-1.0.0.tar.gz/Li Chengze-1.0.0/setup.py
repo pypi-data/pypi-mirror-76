147326622629911ed613533385f49ff4ca138fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:22 +0000

    Add package Li-Chengze/1.0.0/Li%20Chengze-1.0.0.tar.gz

---
+++ packages/Li-Chengze/Li%20Chengze-1.0.0.tar.gz/Li Chengze-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name="Li Chengze",
+    version="1.0.0",
+    description="李承泽库",
+    author="asunc",
+    author_email="3182305655@qq.com",
+    py_modules=['李承泽库']
+)
