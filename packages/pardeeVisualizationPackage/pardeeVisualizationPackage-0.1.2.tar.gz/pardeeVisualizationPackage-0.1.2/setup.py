d9f89706f8a01c2d00af2eee2d28a5c39b880d27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:51 +0000

    Add package pardeeVisualizationPackage/0.1.2/pardeeVisualizationPackage-0.1.2.tar.gz

---
+++ packages/pardeeVisualizationPackage/pardeeVisualizationPackage-0.1.2.tar.gz/pardeeVisualizationPackage-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pardeeVisualizationPackage",
+    version="0.1.2",
+    author="Cory Vandenberg",
+    author_email="cory.vandenberg@du.edu",
+    description="Package for converting seaborn visualizations into Pardee Center Friendly Packages",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Coryv221/Pardee_Theme",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
