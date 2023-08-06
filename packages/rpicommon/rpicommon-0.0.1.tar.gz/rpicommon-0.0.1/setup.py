4f6b7c38c67b1183ab636ca9a112132ce0f27ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:58 +0000

    Add package rpicommon/0.0.1/rpicommon-0.0.1.tar.gz

---
+++ packages/rpicommon/rpicommon-0.0.1.tar.gz/rpicommon-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+
+#from distutils.core import setup
+from setuptools import setup,find_packages
+# pypi-AgENdGVzdC5weXBpLm9yZwIkMDEzZGJjNjMtMTY2OC00Y2I5LWE1NWUtMGM5NWYyMWZlZTdkAAIleyJwZXJtaXNzaW9ucyI6ICJ1c2VyIiwgInZlcnNpb24iOiAxfQAABiBIXMl5tc33BCGyvHj5LoV1aVP4sq_V66OZzirI90qnqw
+setup (
+    name = "rpicommon",
+    version = "0.0.1",
+    author = "ZQK",
+    # url='https://github.com/pypa/sampleproject',
+    description = "共享仓电子秤称公共组件",
+    packages = ["common"]
+    # packages = find_packages()
+)
