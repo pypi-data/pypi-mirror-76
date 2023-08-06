73fd12ffc8573a53f29c5593483cf860e547dc8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:07 +0000

    Add package postcard/0.1.9/postcard-0.1.9-py3-none-any.whl

---
+++ packages/postcard/postcard-0.1.9-py3-none-any.whl/postcard/setting.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+
+"""postcard"""
+
+MAIL_ADDR = {
+    "tom": "pop.tom.com",
+    "gmail": "pop.gmail.com",
+    "yahoo": "pop.mail.yahoo.com",
+    "outlook": "pop-mail.outlook.com",
+    "china": "pop.china.com",
+    "21cn": "pop.21cn.com",
+    "163": "pop.163.com",
+    "126": "pop.126.com",
+    "qq": "pop.qq.com",
+    "sina": "pop3.sina.com.cn",
+}
