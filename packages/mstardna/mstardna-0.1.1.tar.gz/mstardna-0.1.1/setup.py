a81ba61704f298b9d47647f10ce62af952482733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package mstardna/0.1.1/mstardna-0.1.1.tar.gz

---
+++ packages/mstardna/mstardna-0.1.1.tar.gz/mstardna-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['mstardna']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.14.42,<2.0.0']
+
+setup_kwargs = {
+    'name': 'mstardna',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Jinyoung Kim',
+    'author_email': 'jinyoung.kim@morningstar.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
