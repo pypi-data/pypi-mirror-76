da45e748ad9b0e0da96a22ca1d00e96b4aa7cb7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:28 +0000

    Add package quantiphy-eval/0.3.0/quantiphy_eval-0.3.0.tar.gz

---
+++ packages/quantiphy-eval/quantiphy_eval-0.3.0.tar.gz/quantiphy_eval-0.3.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+from codecs import open
+
+with open('README.rst', encoding="UTF-8") as f:
+    readme = f.read()
+
+
+setup(
+    name = 'quantiphy_eval',
+    version = '0.3.0',
+    description = 'calculations with physical quantities',
+    long_description = readme,
+    long_description_content_type = 'text/x-rst',
+    author = "Ken Kundert",
+    author_email = 'quantiphy@nurdletech.com',
+    url = 'https://github.com/kenkundert/quantiphy_eval',
+    download_url = 'https://github.com/kenkundert/quantiphy_eval/tarball/master',
+    license = 'GPLv3+',
+    packages = 'quantiphy_eval'.split(),
+    install_requires = 'inform quantiphy'.split(),
+    tests_require = 'pytest'.split(),
+    python_requires = '>=3.6',
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering',
+    ],
+)
