c0cfeec624ad5dcc868e52f49a36d4e6cd1bec3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:47 +0000

    Add package margo/0.0.4/margo-0.0.4.tar.gz

---
+++ packages/margo/margo-0.0.4.tar.gz/margo-0.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="margo",
+    version="0.0.4",
+    author="Jinyu Hou",
+    author_email="jhou@lunenfeld.ca",
+    description="A tool that generates yaml cell type marker which maps cell types to gene expression.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/camlab-bioml/margo",
+    packages=["margo"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+    ],
+    license="GPLv2",
+    install_requires=[
+        "pandas",
+        "pyyaml",
+        "argparse",
+        "rootpath"
+        # "sphinx",
+        # "rinohtype"
+    ],
+    include_package_data=True,
+    scripts=["bin/margo"],
+)
