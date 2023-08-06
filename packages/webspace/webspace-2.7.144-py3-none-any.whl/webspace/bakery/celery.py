a6fcd01b727f5f6405944a1dbe68aa8a690cef4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:16 +0000

    Add package webspace/2.7.144/webspace-2.7.144-py3-none-any.whl

---
+++ packages/webspace/webspace-2.7.144-py3-none-any.whl/webspace/bakery/celery.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, unicode_literals
+from datetime import datetime
+from django.core import management
+from celery import Celery
+import os
+from django.conf import settings
+from django.utils.text import slugify
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+app = Celery(slugify(settings.SITE_NAME))
+app.config_from_object('webspace.bakery.celeryconfig', namespace='CELERY')
+app.autodiscover_tasks()
+app.control.purge()
+
+
+@app.task(bind=True)
+def bakery_build(self):
+    from .models import TaskBuild
+    task_obj = TaskBuild.objects.create()
+    task_obj.task_id = self.request.id
+    task_obj.save()
+    management.call_command('build')
+    task_obj.date_end = datetime.now()
+    task_obj.ready = True
+    task_obj.save()
