834bdea38c24126f79ffca250ea11652f4cb3d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:29 +0000

    Add package fake-bpy-module-2.79/20200812/fake-bpy-module-2.79-20200812.tar.gz

---
+++ packages/fake-bpy-module-2.79/fake-bpy-module-2.79-20200812.tar.gz/fake-bpy-module-2.79-20200812/bl_i18n_utils/__init__.py
@@ -0,0 +1,8 @@
+import sys
+import typing
+from . import bl_extract_messages
+from . import utils_rtl
+from . import utils
+from . import utils_languages_menu
+from . import settings
+from . import merge_po
---
+++ packages/fake-bpy-module-2.79/fake-bpy-module-2.79-20200812.tar.gz/fake-bpy-module-2.79-20200812/bl_operators/__init__.py
@@ -0,0 +1,43 @@
+import sys
+import typing
+from . import screen_play_rendered_anim
+from . import mask
+from . import anim
+from . import view3d
+from . import mesh
+from . import clip
+from . import freestyle
+from . import object_align
+from . import add_mesh_torus
+from . import object
+from . import object_randomize_transform
+from . import rigidbody
+from . import bmesh
+from . import vertexpaint_dirt
+from . import console
+from . import uvcalc_lightmap
+from . import presets
+from . import node
+from . import uvcalc_follow_active
+from . import file
+from . import wm
+from . import sequencer
+from . import object_quick_effects
+from . import uvcalc_smart_project
+from . import image
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
