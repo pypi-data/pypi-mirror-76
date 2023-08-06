1ab6e2ececa6d57903d819964d51a9eb8a27ddc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:34 +0000

    Add package minisignxml/20.8b0/minisignxml-20.8b0-py3-none-any.whl

---
+++ packages/minisignxml/minisignxml-20.8b0-py3-none-any.whl/minisignxml/errors.py
@@ -0,0 +1,30 @@
+class MiniSignXMLError(Exception):
+    pass
+
+
+class VerificationFailed(MiniSignXMLError):
+    pass
+
+
+class CertificateMismatch(MiniSignXMLError):
+    pass
+
+
+class UnsupportedHasher(MiniSignXMLError):
+    pass
+
+
+class UnsupportedAlgorithm(MiniSignXMLError):
+    pass
+
+
+class ElementNotFound(MiniSignXMLError):
+    pass
+
+
+class MultipleElementsFound(MiniSignXMLError):
+    pass
+
+
+class NoIDAttribute(MiniSignXMLError):
+    pass
