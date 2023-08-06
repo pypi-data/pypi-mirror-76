534e5f30d13a5456cb7eb0a17cf5f9ffe51e6b77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:16 +0000

    Add package stbpy/0.0.4/stbpy-0.0.4.zip

---
+++ packages/stbpy/stbpy-0.0.4.zip/stbpy-0.0.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import sys
+from Cython.Build import cythonize
+from setuptools.extension import Extension
+from sys import platform
+
+ext = [Extension('stb.image', sources=['stb/image.pyx'])]
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="stbpy",
+    version="0.0.4",
+    author="Alex Forrence",
+    author_email="alex.forrence@gmail.com",
+    description="Cython wrapper for nothings/stb",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aforren1/stbpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    ext_modules=cythonize(ext,
+                          compiler_directives={'language_level': 3},
+                          annotate=True)
+)
