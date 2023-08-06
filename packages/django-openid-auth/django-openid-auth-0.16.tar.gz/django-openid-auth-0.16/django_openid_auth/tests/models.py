d26bc31c56d8664d93f2ce63856bc03858c96c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:57 +0000

    Add package django-openid-auth/0.16/django-openid-auth-0.16.tar.gz

---
+++ packages/django-openid-auth/django-openid-auth-0.16.tar.gz/django-openid-auth-0.16/django_openid_auth/tests/models.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.contrib.auth.models import Group
+from django.db import models
+
+
+class UserGroup(models.Model):
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
