302cc3340ddfcd4e515560ca359972bf29f844b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:28 +0000

    Add package xlcompose/0.2.4/xlcompose-0.2.4-py3-none-any.whl

---
+++ packages/xlcompose/xlcompose-0.2.4-py3-none-any.whl/xlcompose/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.2.4'
+from xlcompose.core import (
+    Tabs, Sheet, Row, Column,
+    DataFrame, Series, CSpacer, RSpacer, Title, Image, VSpacer, HSpacer)
+from xlcompose.templates import load_json, load_yaml, EvalExtension
