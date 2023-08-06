a3d896252ddf8e9b5a2f317992c64af9d14474e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:08 +0000

    Add package pytracking2/0.4.0/pytracking2-0.4.0.tar.gz

---
+++ packages/pytracking2/pytracking2-0.4.0.tar.gz/pytracking2-0.4.0/setup.py
@@ -0,0 +1,54 @@
+from itertools import chain
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+EXTRA_REQUIRES = {
+    "test": ["tox", "pytest", "pytest-django"],
+    "webhook": ["requests>=2.10.0"],
+    "html": ["lxml>=4.4.0"],
+    "crypto": ["cryptography>=2.0.0"],
+    "django": ["django-ipware>=2.0.0", "django>=1.11"],
+}
+
+ALL_REQUIRE = list(chain(*EXTRA_REQUIRES.values()))
+
+EXTRA_REQUIRES["all"] = ALL_REQUIRE
+
+setup(
+    name="pytracking2",
+    version="0.4.0",
+    description="Email open and click tracking",
+    long_description=long_description,
+    url="https://github.com/mikicz/pytracking",
+    author="Mikuláš Poul",
+    author_email="mikulaspoul@gmail.com",
+    license="New BSD",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Communications :: Email",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    keywords="email open click tracking",
+    packages=find_packages(".", include=("pytracking", "pytracking.*")),
+    extras_require=EXTRA_REQUIRES,
+    python_requires='>=3.5',
+)
