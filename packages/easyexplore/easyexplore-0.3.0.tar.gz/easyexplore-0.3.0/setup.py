3c3d49d36fba0863d856b4e8f1bd12e75eed3b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:51 +0000

    Add package easyexplore/0.3.0/easyexplore-0.3.0.tar.gz

---
+++ packages/easyexplore/easyexplore-0.3.0.tar.gz/easyexplore-0.3.0/setup.py
@@ -0,0 +1,68 @@
+import setuptools
+import subprocess
+
+from sys import platform
+from easyexplore.text_miner import LANG_MODELS
+
+if platform.find('win') >= 0:
+    _suffix: str = ''
+else:
+    _suffix: str = '3'
+
+# Install jupyter notebook extensions for using EasyExplore_examples.ipynb more conveniently:
+subprocess.run(['python{} -m pip install jupyter_contrib_nbextensions && jupyter contrib nbextension install'.format(_suffix)], shell=True)
+
+# Install spacy language models:
+subprocess.run('python{} -m pip install spacy'.format(_suffix), shell=True)
+for lang in LANG_MODELS.keys():
+    for model in LANG_MODELS[lang]['model']['spacy'].keys():
+        subprocess.run('python{} -m spacy download {}'.format(_suffix, LANG_MODELS[lang]['model']['spacy'][model]), shell=True)
+
+with open('README.md', 'r') as _read_me:
+    long_description = _read_me.read()
+
+with open('requirements.txt', 'r') as _requirements:
+    requires = _requirements.read()
+
+requires = [r.strip() for r in requires.split('\n') if ((r.strip()[0] != "#") and (len(r.strip()) > 3) and "-e git://" not in r)]
+
+setuptools.setup(
+    name='easyexplore',
+    version='0.3.0',
+    author='Gianni Francesco Balistreri',
+    author_email='gbalistreri@gmx.de',
+    description='Toolbox for easy and effective data exploration',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='data exploration interactive visualization machine learning',
+    license='GNU',
+    url='https://github.com/GianniBalistreri/easyexplore',
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    package_data={'easyexplore': ['LICENSE',
+                                  'README.md',
+                                  'requirements.txt',
+                                  'setup.py',
+                                  'EasyExplore_examples.ipynb'
+                                  ]
+                  },
+    data_file=[('test', ['test/amazon_musical_instruments_reviews.csv',
+                         'test/test_anomaly_detector.py',
+                         'test/test_data.csv',
+                         'test/test_data_explorer.py',
+                         'test/test_data_import_export.py',
+                         'test/test_data_visualizer.py',
+                         'test/test_interactive_visualizer.py',
+                         'test/test_text_miner.py',
+                         'test/test_utils.py'
+                         ]
+                )],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=requires
+)
