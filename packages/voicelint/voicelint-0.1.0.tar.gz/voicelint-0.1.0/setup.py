5619ce40b79992e6f851adf852864ba9576445ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:43 +0000

    Add package voicelint/0.1.0/voicelint-0.1.0.tar.gz

---
+++ packages/voicelint/voicelint-0.1.0.tar.gz/voicelint-0.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="voicelint", # Replace with your own username
+    version="0.1.0",
+    author="mpourmpoulis",
+    # author_email="author@example.com",
+    description="pylint plug-in for users of the programing by voice toolkit Caster",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mpourmpoulis/voicelint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
---
+++ packages/voicelint/voicelint-0.1.0.tar.gz/voicelint-0.1.0/voicelint/checkers/__init__.py
@@ -0,0 +1,9 @@
+from voicelint.checkers.mimic_checker import MimicChecker
+from voicelint.checkers.conditional_import_checker import ConditionalImportChecker
+from voicelint.checkers.rule_details_checker import RuleDetailsChecker
+from voicelint.checkers.pause_checker import PauseChecker
+from voicelint.checkers.dyn_checker import  DynStrChecker
+
+
+
+
