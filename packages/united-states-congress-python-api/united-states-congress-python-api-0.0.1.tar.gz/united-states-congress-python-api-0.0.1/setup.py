16f5250ddcde17b79c2a8a716b57098237a431c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:16 +0000

    Add package united-states-congress-python-api/0.0.1/united-states-congress-python-api-0.0.1.tar.gz

---
+++ packages/united-states-congress-python-api/united-states-congress-python-api-0.0.1.tar.gz/united-states-congress-python-api-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from setuptools import find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='united-states-congress-python-api',
+    author='Alex Reed',
+    author_email='coding.sigma@gmail.com',
+    version='0.0.1',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/areed1192/united-states-congress-python-api',
+    install_requires=[
+        'requests'
+    ],
+    packages=find_namespace_packages(include=['congress']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: News/Diary',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Legal Industry'
+    ],
+    python_requires='>3.7'
+)
