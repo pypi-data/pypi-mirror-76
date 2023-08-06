0be085dabde86b555ca6ecf0f817587cbfa682f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:13 +0000

    Add package pycudacov/0.0.2/pycudacov-0.0.2.tar.gz

---
+++ packages/pycudacov/pycudacov-0.0.2.tar.gz/pycudacov-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycudacov", # Replace with your own username
+    version="0.0.2",
+    author="Ivan Reyes",
+    author_email="ivanrs297@gmail.com",
+    description="A PyCuda Covariance Matrix Implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ivanrs297/pycuda-covariance-matrix",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
