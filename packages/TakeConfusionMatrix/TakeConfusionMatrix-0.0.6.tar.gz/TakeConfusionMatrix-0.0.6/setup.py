2bd0a7b240954402c3474be97549dd6fa066c80e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:43 +0000

    Add package TakeConfusionMatrix/0.0.6/TakeConfusionMatrix-0.0.6.tar.gz

---
+++ packages/TakeConfusionMatrix/TakeConfusionMatrix-0.0.6.tar.gz/TakeConfusionMatrix-0.0.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fr:
+    requirements = fr.read().splitlines()
+
+setuptools.setup(
+    name="TakeConfusionMatrix",
+    version="0.0.6",
+    author="Cecília Assis",
+    author_email="cecilia.assis@take.net",
+    description="TakeConfusionMatrix is a tool for batched metrics calculations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://curupira.visualstudio.com/DefaultCollection/Data%20Analytics%20%28DA%29/_git/TakeConfusionMatrix",
+    maintainer="D&A Team",
+    maintainer_email="analytics.ped@take.net",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
