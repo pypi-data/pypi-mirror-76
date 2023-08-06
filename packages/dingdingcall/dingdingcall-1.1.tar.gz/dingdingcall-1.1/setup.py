5d97b8a19380e09b0d4c669467512fff79014964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:37 +0000

    Add package dingdingcall/1.1/dingdingcall-1.1.tar.gz

---
+++ packages/dingdingcall/dingdingcall-1.1.tar.gz/dingdingcall-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+	name='dingdingcall',
+	author='liuzihao',
+	version='1.1',
+	py_modules=['dingdingcall'],
+	install_requires=['requests','time','hmac','hashlib','base64']
+)
