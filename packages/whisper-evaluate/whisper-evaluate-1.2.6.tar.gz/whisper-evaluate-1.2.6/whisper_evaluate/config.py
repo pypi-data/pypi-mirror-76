0bb5710dcfa143c754ea75f1ccdc69306caf2abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:32 +0000

    Add package whisper-evaluate/1.2.6/whisper-evaluate-1.2.6.tar.gz

---
+++ packages/whisper-evaluate/whisper-evaluate-1.2.6.tar.gz/whisper-evaluate-1.2.6/whisper_evaluate/config.py
@@ -0,0 +1,15 @@
+en_zh = {
+    "Politics": "政治",
+    "Heresy": "邪教",
+    "Terrorism": "恐怖主义",
+    "Crime": "犯罪",
+    "Porn": "色情",
+    "Gamble": "赌博",
+    "Contraband": "违禁品",
+    "SensitiveEvent": "敏感事件",
+    "Abuse": "辱骂",
+    "IllegalWebsite": "违规网站",
+    "Adv": "广告",
+    "Normal": "其他"
+}
+
