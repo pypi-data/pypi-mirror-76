407785ab2a0058843a43819b3debbb2d08517966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:27 +0000

    Add package nlp-primitives/1.0.0/nlp_primitives-1.0.0.tar.gz

---
+++ packages/nlp-primitives/nlp_primitives-1.0.0.tar.gz/nlp_primitives-1.0.0/nlp_primitives/__init__.py
@@ -0,0 +1,12 @@
+# flake8: noqa
+__version__ = '1.0.0'
+from .diversity_score import DiversityScore
+from .lsa import LSA
+from .mean_characters_per_word import MeanCharactersPerWord
+from .part_of_speech_count import PartOfSpeechCount
+from .polarity_score import PolarityScore
+from .punctuation_count import PunctuationCount
+from .stopword_count import StopwordCount
+from .title_word_count import TitleWordCount
+from .universal_sentence_encoder import UniversalSentenceEncoder
+from .upper_case_count import UpperCaseCount
