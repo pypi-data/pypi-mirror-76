7299616a1c80a6c77858cfe28f2c8141c6c1a9d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:54 +0000

    Add package django-cfdi/1.7.3/django_cfdi-1.7.3-py3-none-any.whl

---
+++ packages/django-cfdi/django_cfdi-1.7.3-py3-none-any.whl/cfdi/constants.py
@@ -0,0 +1,13 @@
+PACS = {
+    "PRUEBA": 0,
+    "PRODIGIA": 1,
+    "FINKOK": 2,
+    "STOCONSULTING": 3,
+    "DFACTURE": 4,
+}
+
+CHOICES_PACS = []
+for nombre,valor in PACS.items():
+	CHOICES_PACS.append((valor, nombre))
+
+CLAVES_COMBUSTIBLE = ["15101506", "15101505", "15101509", "15101514", "15101515"]
