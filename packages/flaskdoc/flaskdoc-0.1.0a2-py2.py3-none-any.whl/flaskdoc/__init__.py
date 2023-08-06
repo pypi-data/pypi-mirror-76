0397ccc9f26bbf8df803127143c113bc0852adaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:46 +0000

    Add package flaskdoc/0.1.0a2/flaskdoc-0.1.0a2-py2.py3-none-any.whl

---
+++ packages/flaskdoc/flaskdoc-0.1.0a2-py2.py3-none-any.whl/flaskdoc/__init__.py
@@ -0,0 +1,7 @@
+""" Developer friendly tool for crafting OpenAPI specs
+
+    Provides OpenAPI models, decorators and swagger ui
+"""
+from flaskdoc.pallets import Blueprint, Flask, register_openapi
+
+__version__ = "0.0.1a1"
