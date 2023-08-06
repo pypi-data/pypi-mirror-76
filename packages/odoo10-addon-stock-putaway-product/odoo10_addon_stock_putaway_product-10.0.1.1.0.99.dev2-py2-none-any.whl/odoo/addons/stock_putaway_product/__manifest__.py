6b1f0acf2f90e525c807c2cf18577929c4d24cd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:41 +0000

    Add package odoo10-addon-stock-putaway-product/10.0.1.1.0.99.dev2/odoo10_addon_stock_putaway_product-10.0.1.1.0.99.dev2-py2-none-any.whl

---
+++ packages/odoo10-addon-stock-putaway-product/odoo10_addon_stock_putaway_product-10.0.1.1.0.99.dev2-py2-none-any.whl/odoo/addons/stock_putaway_product/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Jos De Graeve - Apertoso N.V. <Jos.DeGraeve@apertoso.be>
+# © 2016 Carlos Dauden - Tecnativa <carlos.dauden@tecnativa.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Putaway strategy per product',
+    'summary': 'Set a product location and put-away strategy per product',
+    'version': '10.0.1.1.0',
+    'category': 'Inventory',
+    'website': 'http://www.apertoso.be',
+    'author': 'Apertoso N.V., '
+              'Tecnativa, '
+              'Akretion, '
+              'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'depends': [
+        'product',
+        'stock'
+    ],
+    'data': [
+        'views/product.xml',
+        'views/product_putaway.xml',
+        'wizard/quick_add_location.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+        'demo/product_putaway.xml',
+    ]
+}
