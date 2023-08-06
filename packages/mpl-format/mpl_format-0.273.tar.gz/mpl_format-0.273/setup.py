e752ea83748f3a3956b174f294706e9a066c2ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:15 +0000

    Add package mpl-format/0.273/mpl_format-0.273.tar.gz

---
+++ packages/mpl-format/mpl_format-0.273.tar.gz/mpl_format-0.273/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='mpl_format',
+    packages=find_packages(),
+    version='0.273',
+    license='MIT',
+    description='Library for easier formatting of matplotlib plots written in '
+                'a functional style.',
+    author='Vahndi Minah',
+    url='https://github.com/vahndi/mpl-format',
+    download_url='https://github.com/vahndi/mpl-format/archive/v_0.269.tar.gz',
+    keywords=['matplotlib'],
+    install_requires=[
+        'compound-types',
+        'matplotlib',
+        'numpy',
+        'seaborn',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
