f3c9c004d0f6717c4fdb080803065b764c920a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:21 +0000

    Add package apex-stats/0.1.4/apex_stats-0.1.4.tar.gz

---
+++ packages/apex-stats/apex_stats-0.1.4.tar.gz/apex_stats-0.1.4/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+  name = 'apex_stats',        
+  packages = ['apex_stats'],  
+  version = '0.1.4',     
+  license='MIT',
+  long_description='https://apex-stats.rtfd.io/',
+  description = 'API wrapper for https://apex.tracker.gg',   
+  author_email = 'yamozha16@protonmail.ch',  
+  url = 'https://github.com/yamozha/apex_stats',  
+)
