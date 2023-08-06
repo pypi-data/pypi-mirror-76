81e727ef838b799e2f687ab7e8bb27984c01751f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:52 +0000

    Add package peanutbutterdatatime/0.0.18/peanutbutterdatatime-0.0.18-py3-none-any.whl

---
+++ packages/peanutbutterdatatime/peanutbutterdatatime-0.0.18-py3-none-any.whl/nlu/components/dependency_untypeds/unlabeled_dependency_parser/unlabeled_dependency_parser.py
@@ -0,0 +1,24 @@
+from sparknlp.annotator import *
+
+
+class UnlabeledDependencyParser:
+    @staticmethod
+    def get_default_model():
+        return DependencyParserModel.pretrained() \
+            .setInputCols(["sentence", "pos", "token"]) \
+            .setOutputCol("dependency")
+
+    @staticmethod
+    def get_pretrained_model(name, language):
+        return DependencyParserModel.pretrained(name,language) \
+            .setInputCols(["sentence", "pos", "token"]) \
+            .setOutputCol("dependency")
+
+
+    @staticmethod
+    def get_default_trainable_model():
+        return DependencyParserApproach() \
+            .setInputCols(["sentence", "pos", "token"]) \
+            .setOutputCol("dependency") \
+            .setDependencyTreeBank("file://parser/dependency_treebank") \
+            .setNumberOfIterations(10)
