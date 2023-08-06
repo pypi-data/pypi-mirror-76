2733911d4fefa639d8c8edc3468f8ef2639d77d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:58 +0000

    Add package molecular-builder/0.2.0/molecular_builder-0.2.0.tar.gz

---
+++ packages/molecular-builder/molecular_builder-0.2.0.tar.gz/molecular_builder-0.2.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="molecular_builder",
+    version="0.2.0",
+    author="Henrik Andersen Sveinsson",
+    author_email="henriasv@fys.uio.no",
+    description="Package for building moleular systems",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/henriasv/molecular-builder",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["ase", "requests", "requests-cache", "clint", "werkzeug","cython", "recommonmark", "ovito"],
+    python_requires='>=3.7',
+)
