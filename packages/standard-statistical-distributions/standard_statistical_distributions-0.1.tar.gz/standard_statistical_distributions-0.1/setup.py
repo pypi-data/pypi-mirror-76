b55812ddf1e469bacec1ea37bf511628233509d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:25 +0000

    Add package standard-statistical-distributions/0.1/standard_statistical_distributions-0.1.tar.gz

---
+++ packages/standard-statistical-distributions/standard_statistical_distributions-0.1.tar.gz/standard_statistical_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='standard_statistical_distributions',
+      version='0.1',
+      author = 'Chi Bui',
+      author_email = 'chibui191@gmail.com',
+      description='A simple package for Gaussian and Binomial Distributions',
+      packages=['standard_statistical_distributions'],
+      zip_safe=False)
