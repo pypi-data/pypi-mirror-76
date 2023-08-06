25e0a160c0588c6f7cde5bedaf0468442f63005b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:27 +0000

    Add package palett/0.0.4/palett-0.0.4.tar.gz

---
+++ packages/palett/palett-0.0.4.tar.gz/palett-0.0.4/setup.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['palett',
+ 'palett.card',
+ 'palett.cards',
+ 'palett.convert',
+ 'palett.convert.utils',
+ 'palett.convert.utils.hex',
+ 'palett.convert.utils.hsl',
+ 'palett.convert.utils.rgb',
+ 'palett.dyer',
+ 'palett.dyer.hex',
+ 'palett.dyer.rgb',
+ 'palett.dyer.utils',
+ 'palett.enum',
+ 'palett.enum.font_effects',
+ 'palett.fluo_entries',
+ 'palett.fluo_matrix',
+ 'palett.fluo_vector',
+ 'palett.preset',
+ 'palett.presets',
+ 'palett.projector',
+ 'palett.projector.utils',
+ 'palett.utils',
+ 'palett.utils.ansi']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aryth>=0.0.5', 'intype>=0.0.2', 'ject>=0.0.1', 'veho>=0.0.4']
+
+setup_kwargs = {
+    'name': 'palett',
+    'version': '0.0.4',
+    'description': 'pretty text for command line',
+    'long_description': "## palett\n##### pretty text for command line\n\n### Usage\n```python\n\nfrom palett.fluo_vector import fluo_vector\nfrom palett.presets import FRESH, PLANET\n\nvectorCollection = [\n    [],\n    ['Xx', 'Yy', 'Zz', 'e', 'd', 'c', '-', '1', 2, 3],\n    [1, 1, 2, 3, 5, []],\n    ['a', 'b', 'c', 'd', 'e'],\n    ['beijing', 'shanghai', 'wuhan', 'xiamen', 'changsha']\n]\n\n\ndef test():\n    COMMA_SPACE = ', '\n    for vec in vectorCollection:\n        fluoed = fluo_vector(vec, (FRESH, PLANET))\n        print(f'[{COMMA_SPACE.join(fluoed)}]')\n\n\ntest()\n```",
+    'author': 'Hoyeung Wong',
+    'author_email': 'hoyeungw@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/pydget/texting.git',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
