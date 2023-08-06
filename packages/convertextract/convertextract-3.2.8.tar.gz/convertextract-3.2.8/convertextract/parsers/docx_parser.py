90a8beefd02e92afe93b4fd084d0345214098f30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:59 +0000

    Add package convertextract/3.2.8/convertextract-3.2.8.tar.gz

---
+++ packages/convertextract/convertextract-3.2.8.tar.gz/convertextract-3.2.8/convertextract/parsers/docx_parser.py
@@ -0,0 +1,25 @@
+import docx
+
+from convertextract.parsers.utils import BaseParser
+
+class Parser(BaseParser):
+    """Extract text from docx file using python-docx.
+    """
+
+    def extract(self, filename, **kwargs):
+        if 'mapping' in kwargs and kwargs['mapping']:
+            transducer = self.create_transducer(kwargs['mapping'])
+        else:
+            transducer = self.get_transducer(kwargs.get('input_language', ''), kwargs.get('output_language', ''))
+        converted_filename = filename[:-5] + '_converted.docx'
+        document = docx.Document(filename)
+        text_runs = []
+        for paragraph in document.paragraphs:
+            for run in paragraph.runs:
+                # this line prevents images from being erased
+                if run.text != "" and run.text != " ":
+                    run.text = transducer(run.text).output_string
+                    text_runs.append(run.text)
+        if "no_write" not in kwargs or not kwargs['no_write']:
+            document.save(converted_filename)
+        return '\n\n'.join(text_runs)
