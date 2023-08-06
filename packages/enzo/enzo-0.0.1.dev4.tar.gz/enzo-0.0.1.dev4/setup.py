dfcf362cab5e278df16c28b06e6705f18bb48d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:26 +0000

    Add package enzo/0.0.1.dev4/enzo-0.0.1.dev4.tar.gz

---
+++ packages/enzo/enzo-0.0.1.dev4.tar.gz/enzo-0.0.1.dev4/setup.py
@@ -0,0 +1,11 @@
+import re
+from setuptools import setup
+
+with open("src/enzo/__init__.py", encoding="utf8") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+
+# Metadata in setup.cfg
+setup(
+    install_requires=["numpy==1.19.0",], version=version,
+)
