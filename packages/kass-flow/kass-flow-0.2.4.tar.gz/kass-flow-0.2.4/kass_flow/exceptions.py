2444f8d6cf7e1c06d6b1b4230a892e50b25a68ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:55 +0000

    Add package kass-flow/0.2.4/kass-flow-0.2.4.tar.gz

---
+++ packages/kass-flow/kass-flow-0.2.4.tar.gz/kass-flow-0.2.4/kass_flow/exceptions.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+
+class Error(Exception):
+    """General error exception to be extended
+
+    Attributes:
+        message -- explanation of the error
+        value -- value to be printed
+    """
+
+    def __init__(self, value: Any, message: str) -> None:
+        self.message = message
+        self.value = value
+
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return f"{self.value} -> {self.message}"
+
+
+class KassResponseTimeoutError(Error):
+    pass
+
+
+class KassResponseDataError(Error):
+    pass
+
