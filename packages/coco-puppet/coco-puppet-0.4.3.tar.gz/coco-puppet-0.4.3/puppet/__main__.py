e84652e8a3a36ae283b98b9317a0783387f298e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:37 +0000

    Add package coco-puppet/0.4.3/coco-puppet-0.4.3.tar.gz

---
+++ packages/coco-puppet/coco-puppet-0.4.3.tar.gz/coco-puppet-0.4.3/puppet/__main__.py
@@ -0,0 +1,13 @@
+import sys
+import importlib
+
+from .shell import bot_runner
+
+_, entry_path, *args = sys.argv
+
+entry_package_name, entry_name = entry_path.rsplit(".", 1)
+
+entry_package = importlib.import_module(entry_package_name)
+entry = getattr(entry_package, entry_name)
+
+bot_runner(entry, *args)
