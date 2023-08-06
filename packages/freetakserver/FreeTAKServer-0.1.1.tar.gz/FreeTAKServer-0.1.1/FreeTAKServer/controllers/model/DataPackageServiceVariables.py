cf1b086e36ecc8c132d18201fd3bb1ec7a90b964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:15 +0000

    Add package freetakserver/0.1.1/FreeTAKServer-0.1.1.tar.gz

---
+++ packages/freetakserver/FreeTAKServer-0.1.1.tar.gz/FreeTAKServer-0.1.1/FreeTAKServer/controllers/model/DataPackageServiceVariables.py
@@ -0,0 +1,5 @@
+class DataPackageServiceVariables:
+    def __init__(self):
+        self.DataPackageServiceIP = "142.93.239.220"
+        self.DataPackageServicePort = 8080
+        self.DataPackageServiceStatus = ""
