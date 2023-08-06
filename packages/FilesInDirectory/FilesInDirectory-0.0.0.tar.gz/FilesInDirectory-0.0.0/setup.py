b5dbd5322b6f305bde4cd5ee1409a034a9952c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:12 +0000

    Add package FilesInDirectory/0.0.0/FilesInDirectory-0.0.0.tar.gz

---
+++ packages/FilesInDirectory/FilesInDirectory-0.0.0.tar.gz/FilesInDirectory-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = "FilesInDirectory",
+      url='https://github.com/gonjubazz/FilesInPackage',
+      author='Vurdalack',
+      license="AGPL 3.0",
+      packages=["FilesInDirectory"],
+      )
+
