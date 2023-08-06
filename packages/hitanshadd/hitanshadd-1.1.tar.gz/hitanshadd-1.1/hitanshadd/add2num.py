420819c31ce33e841936924f16305d4280232aed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:23 +0000

    Add package hitanshadd/1.1/hitanshadd-1.1.tar.gz

---
+++ packages/hitanshadd/hitanshadd-1.1.tar.gz/hitanshadd-1.1/hitanshadd/add2num.py
@@ -0,0 +1,15 @@
+""" This function helps you to add two integers."""
+
+
+class Addition:
+    """ This class has a function which helps you to add two numbers."""
+
+    def adding2num(self, a, b):
+        """Addition of two numbers
+
+        Parameters:
+        two(int): Send two integer parameters
+        Returns:
+        int: As an output."""
+
+        return a+b
