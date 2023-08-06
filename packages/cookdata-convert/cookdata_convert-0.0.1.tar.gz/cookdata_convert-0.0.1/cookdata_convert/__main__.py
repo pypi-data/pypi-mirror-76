0efe3867e8604be83e4308d680bd424f4eeda084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:20 +0000

    Add package cookdata-convert/0.0.1/cookdata_convert-0.0.1.tar.gz

---
+++ packages/cookdata-convert/cookdata_convert-0.0.1.tar.gz/cookdata_convert-0.0.1/cookdata_convert/__main__.py
@@ -0,0 +1,24 @@
+import argparse
+
+from .cookDataToYml import sbyml_to_yml
+from .cookDataToSbyml import yml_to_sbyml
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Tool for converting CookData in LoZ:BotW")
+
+    subparsers = parser.add_subparsers(dest="command", help="Command")
+    subparsers.required = True
+
+    y_parser = subparsers.add_parser(
+        "yml", description="Search for flags in Bootup.pack", aliases=["y"]
+    )
+    y_parser.set_defaults(func=sbyml_to_yml())
+
+    s_parser = subparsers.add_parser(
+        "sbyml", description="Converts CookData.yml to CookData.sbyml", aliases=["s"]
+    )
+    s_parser.add_argument(
+        "-b", "--bigendian", help="Use big endian mode (for Wii U)", action="store_true",
+    )
+    s_parser.set_defaults(func=lambda a: yml_to_sbyml(a))
---
+++ packages/cookdata-convert/cookdata_convert-0.0.1.tar.gz/cookdata_convert-0.0.1/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cookdata_convert",
+    version="0.0.1",
+    author="Ginger",
+    author_email="chodness@gmail.com",
+    description="CookData converter for LoZ:BotW",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/GingerAvalanche/",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "cookdata_to_yml = cookdata_convert.cookDataToYml:main",
+            "cookdata_to_sbyml = cookdata_convert.cookDataToSbyml:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+    ],
+    python_requires=">=3.7.4",
+    install_requires=["oead>=0.11.2",],
+)
