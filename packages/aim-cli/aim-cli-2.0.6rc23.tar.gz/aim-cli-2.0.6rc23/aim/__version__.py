9e66a49036b56503059da22ae3589efcd09328c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:28 +0000

    Add package aim-cli/2.0.6rc23/aim-cli-2.0.6rc23.tar.gz

---
+++ packages/aim-cli/aim-cli-2.0.6rc23.tar.gz/aim-cli-2.0.6rc23/aim/__version__.py
@@ -0,0 +1,3 @@
+VERSION = (2, 0, '6rc23')
+
+__version__ = '.'.join(map(str, VERSION))
---
+++ packages/aim-cli/aim-cli-2.0.6rc23.tar.gz/aim-cli-2.0.6rc23/aim/sdk/__init__.py
@@ -0,0 +1,16 @@
+from aim.sdk.track import track
+from aim.sdk.init import init
+from aim.sdk.select import select
+
+
+# Serializable classes references
+metric = 'Metric'
+hyperparams = 'HyperParameters'
+dataset = 'Dataset'
+map = 'Map'
+nested_map = 'NestedMap'
+checkpoint = 'Checkpoint'
+
+# Serializable aliases
+params = hyperparams
+dictionary = nested_map
