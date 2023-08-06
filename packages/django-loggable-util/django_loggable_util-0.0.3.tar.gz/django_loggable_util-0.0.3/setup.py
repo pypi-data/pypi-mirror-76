38c635eb766bd72484945c4b6e38541540deaf61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:20 +0000

    Add package django-loggable-util/0.0.3/django_loggable_util-0.0.3.tar.gz

---
+++ packages/django-loggable-util/django_loggable_util-0.0.3.tar.gz/django_loggable_util-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setup(
+  name='django_loggable_util',
+  packages=['django_loggable_util'],
+  version='0.0.3',
+  license='MIT',
+  description='Separate logging from class based view business code',
+  author='Ruhshan Ahmed Abir',
+  author_email='ruhshan.ahmed@gmail.com',
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url='https://github.com/Ruhshan/django-loggable-util',
+  keywords=['Django', 'Loggable', 'Wrapper','Generic view','Class based view'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries',
+    'License :: OSI Approved :: MIT License',
+    'Framework :: Django',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
