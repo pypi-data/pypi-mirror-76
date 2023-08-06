e1202e1d0ef1fda473150b40c47fb94351ddeb77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:18 +0000

    Add package constants-and-utils/0.0.3/constants_and_utils-0.0.3-py3-none-any.whl

---
+++ packages/constants-and-utils/constants_and_utils-0.0.3-py3-none-any.whl/tests/utils/test_use_cases.py
@@ -0,0 +1,34 @@
+from unittest import TestCase
+from constants_and_utils.utils.use_cases import (
+    Response,
+    ExportResponse
+)
+
+
+class TestResponse(TestCase):
+
+    def test_ok(self):
+        expected = {'errors': None, 'http_code': 200, 'message': 'OK'}
+        result = Response(
+            http_code=200,
+            message='OK'
+            )
+
+        self.assertEqual(result.__json__(), expected)
+
+
+class TestExportResponse(TestCase):
+
+    def test_ok(self):
+        expected = {
+            'errors': None, 'http_code': 200, 'message': 'OK',
+            'file_name': 'file.csv'
+            }
+
+        result = ExportResponse(
+            http_code=200,
+            message='OK',
+            file_name='file.csv'
+            )
+
+        self.assertEqual(result.__json__(), expected)
