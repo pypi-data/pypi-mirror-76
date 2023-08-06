308e11cfb133eeb140e2014e5f21079f7dfd5c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:34 +0000

    Add package setux-distros/0.2.0/setux_distros-0.2.0-py3-none-any.whl

---
+++ packages/setux-distros/setux_distros-0.2.0-py3-none-any.whl/setux/distros/init.py
@@ -0,0 +1,3 @@
+__version__ = '0.2.0'
+
+from setux.logger import debug, info, error, exception
---
+++ packages/setux-distros/setux_distros-0.2.0-py3-none-any.whl/setux/distros/mx.py
@@ -0,0 +1,14 @@
+from setux.distros.debian import debian_10
+from setux.managers.system.service import SystemV
+
+
+class MX_19(debian_10):
+    Service = SystemV
+    svcmap = dict(
+    )
+
+    @classmethod
+    def release_name(cls, infos):
+        did = infos['DISTRIB_ID']
+        ver = infos['DISTRIB_RELEASE'].split('.')[0]
+        return f'{did}_{ver}'
