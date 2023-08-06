4c432548f2b4be7d4fbeb8cc0455c1daa26d0ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:57 +0000

    Add package akm-distributions/0.1/akm_distributions-0.1.tar.gz

---
+++ packages/akm-distributions/akm_distributions-0.1.tar.gz/akm_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='akm_distributions',
+      version='0.1',
+      description='Probability distributions',
+      packages=['akm_distributions'],
+      author='Akhil Kumar Mengani',
+      zip_safe=False)
