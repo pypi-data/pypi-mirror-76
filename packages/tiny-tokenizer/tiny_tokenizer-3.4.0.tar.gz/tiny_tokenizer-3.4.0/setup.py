917f21cf2880ec7214c096489f4590ac4d3c4701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:02 +0000

    Add package tiny-tokenizer/3.4.0/tiny_tokenizer-3.4.0.tar.gz

---
+++ packages/tiny-tokenizer/tiny_tokenizer-3.4.0.tar.gz/tiny_tokenizer-3.4.0/setup.py
@@ -0,0 +1,26 @@
+import sys
+import webbrowser
+from distutils.core import setup
+
+message = 'You tried to install "tiny_tokenizer". The name of tiny_tokenizer renamed to "konoha"'
+
+argv = lambda x: x in sys.argv
+
+if (argv('install') or  # pip install ..
+        (argv('--dist-dir') and argv('bdist_egg'))):  # easy_install
+    raise Exception(message)
+
+
+if argv('bdist_wheel'):  # modern pip install
+    raise Exception(message)
+
+
+setup(
+    name='tiny_tokenizer',
+    url='https://github.com/himkt/konoha',
+    version='3.4.0',
+    maintainer='Makoto Hiramatsu',
+    maintainer_email='himkt@klis.tsukuba.ac.jp',
+    long_description=message,
+)
+
