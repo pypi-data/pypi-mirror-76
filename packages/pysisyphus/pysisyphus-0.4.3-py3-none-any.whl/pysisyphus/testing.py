12a1695635c6c0d5a5223fcb7388403be7234d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:17 +0000

    Add package pysisyphus/0.4.3/pysisyphus-0.4.3-py3-none-any.whl

---
+++ packages/pysisyphus/pysisyphus-0.4.3-py3-none-any.whl/pysisyphus/testing.py
@@ -0,0 +1,79 @@
+import pytest
+import shutil
+
+from pysisyphus.config import Config
+
+
+"""Inspired by
+https://github.com/MolSSI/QCEngine/blob/master/qcengine/testing.py
+
+Maybe implement something like this to make test access easier
+    https://stackoverflow.com/a/36601576/12486216
+"""
+
+
+_reason = "Calculator {} is not available."
+_using_cache = dict()
+
+
+def using(calculator):
+    calculator = calculator.lower()
+
+    if calculator not in _using_cache:
+        available = False
+
+        try:
+            # Look for dscf availability
+            if calculator == "turbomole":
+                cmd = "dscf"
+            else:
+                # Try to read from .pysisyphusrc
+                cmd = Config[calculator]["cmd"]
+
+
+            available = bool(shutil.which(cmd))
+        except KeyError:
+            pass
+
+        if calculator == "pyscf":
+            try:
+                import pyscf
+                available = True
+            except ImportError:
+                pass
+
+        if calculator == "qcengine":
+            try:
+                import qcengine
+                available = True
+            except ImportError:
+                pass
+
+        if calculator == "openmm":
+            try:
+                import simtk.openmm
+                available = True
+            except ImportError:
+                pass
+
+        if calculator == "thermoanalysis":
+            try:
+                import thermoanalysis
+                available = True
+            except ModuleNotFoundError:
+                pass
+
+        reason = _reason.format(calculator)
+        _using_cache[calculator] = pytest.mark.skipif(not available, reason=reason)
+    return _using_cache[calculator]
+
+
+def available(calculator):
+    # True when skipif is False
+    return not using(calculator).args[0]
+
+
+using_pyscf = using("pyscf")
+using_gaussian16 = using("gaussian16")
+using_turbomole = using("turbomole")
+using_qcengine = using("qcengine")
