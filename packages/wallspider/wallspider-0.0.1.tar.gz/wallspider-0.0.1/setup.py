f4c2e1b92801fb471b303d50009793f463f57641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:00 +0000

    Add package wallspider/0.0.1/wallspider-0.0.1.tar.gz

---
+++ packages/wallspider/wallspider-0.0.1.tar.gz/wallspider-0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import os
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+my_file = os.path.join(THIS_FOLDER, 'readme.md')
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wallspider",  # Replace with your own username
+    version="0.0.1",
+    author="Manoj",
+    author_email="manojap@outlook.com",
+    description="Wallpaper downloader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/manojap/wallspyder",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
