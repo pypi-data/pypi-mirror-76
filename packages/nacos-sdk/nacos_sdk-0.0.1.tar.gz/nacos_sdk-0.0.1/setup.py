74998785f66c39de0eaa164aef7d7ca2c8c32e54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:56 +0000

    Add package nacos-sdk/0.0.1/nacos_sdk-0.0.1.tar.gz

---
+++ packages/nacos-sdk/nacos_sdk-0.0.1.tar.gz/nacos_sdk-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='nacos_sdk',
+    version='0.0.1',
+    author='zhanghaoran',
+    author_email='haoranzeus@gmail.com',
+    url='https://www.zhanghaoran.cc',
+    install_requires=[
+        'requests',
+    ])
