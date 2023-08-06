810777f1c822e72e6d71311eaa347da2ea32cec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:04 +0000

    Add package saman-distributions/0.1/saman_distributions-0.1.tar.gz

---
+++ packages/saman-distributions/saman_distributions-0.1.tar.gz/saman_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='saman_distributions',
+      version='0.1',
+      description='Gaussian distributions class',
+      packages=['saman_distributions'],
+      zip_safe=False,
+      author='Saman'
+     )
