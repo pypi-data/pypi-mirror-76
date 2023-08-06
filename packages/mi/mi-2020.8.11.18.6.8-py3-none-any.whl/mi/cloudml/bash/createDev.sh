532e57b92fff786c14835ba9867f8af266653fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:33 +0000

    Add package mi/2020.8.11.18.6.8/mi-2020.8.11.18.6.8-py3-none-any.whl

---
+++ packages/mi/mi-2020.8.11.18.6.8-py3-none-any.whl/mi/cloudml/bash/createDev.sh
@@ -0,0 +1,16 @@
+# http://docs.api.xiaomi.net/cloud-ml/usage/request_quota_v2.html
+
+#!/usr/bin/env bash
+if [ -n "$1" ]
+then name=$1
+else name=tql2dev
+fi;
+
+password=xiaomi
+cloudml dev create -n $name -p $password \
+-d cr.d.xiaomi.net/cloud-ml/tensorflow-gpu:33103tql2dev \
+--priority_class guaranteed \
+-c 32 -M 32G \
+-g 1 \
+-cv aaa \
+-cm rw
