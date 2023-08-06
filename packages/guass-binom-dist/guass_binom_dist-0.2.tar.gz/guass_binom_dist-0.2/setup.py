106e5bbd56203e45957d51119fba02c346c2d2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:02 +0000

    Add package guass-binom-dist/0.2/guass_binom_dist-0.2.tar.gz

---
+++ packages/guass-binom-dist/guass_binom_dist-0.2.tar.gz/guass_binom_dist-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='guass_binom_dist',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['guass_binom_dist'],
+      zip_safe=False)
