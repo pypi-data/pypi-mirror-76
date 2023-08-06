bcd3aafcfa35e6b2e4492fd9492c0c513d5706f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:44 +0000

    Add package botnoinlp/0.0.4/botnoinlp-0.0.4.tar.gz

---
+++ packages/botnoinlp/botnoinlp-0.0.4.tar.gz/botnoinlp-0.0.4/botnoinlp/botnoi.py
@@ -0,0 +1,13 @@
+import requests
+class BotNoi:
+    def __init__(self,message,token):
+        self.token = token
+    def chitchat(self,message,styleid,botname):
+        url = "https://openapi.botnoi.ai/service-api/botnoichitchat?keyword=test&styleid=test&botname=test"
+        headers = {
+            'Authorization': 'Bearer %s'self.token
+        }
+        response = requests.request("GET", url, headers=headers).json()
+        return response
+
+
