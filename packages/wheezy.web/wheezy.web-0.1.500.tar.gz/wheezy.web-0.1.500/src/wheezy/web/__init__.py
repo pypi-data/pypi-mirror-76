4d423854e3e9c3c45aa64e0028b6b3360d66d6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:24 +0000

    Add package wheezy.web/0.1.500/wheezy.web-0.1.500.tar.gz

---
+++ packages/wheezy.web/wheezy.web-0.1.500.tar.gz/wheezy.web-0.1.500/src/wheezy/web/__init__.py
@@ -0,0 +1,13 @@
+
+"""
+"""
+
+# flake8: noqa
+
+from wheezy.web.authorization import authorize
+from wheezy.web.authorization import secure
+from wheezy.web.caching import handler_cache
+
+
+__all__ = ("authorize", "secure", "handler_cache")
+__version__ = "0.1.500"
