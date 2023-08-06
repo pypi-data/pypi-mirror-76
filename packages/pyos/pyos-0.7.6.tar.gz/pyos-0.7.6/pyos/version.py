6ea92a0095afa92c8cf872decf45809dd1197de6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:02 +0000

    Add package pyos/0.7.6/pyos-0.7.6.tar.gz

---
+++ packages/pyos/pyos-0.7.6.tar.gz/pyos-0.7.6/pyos/version.py
@@ -0,0 +1,17 @@
+author_info = (('Martin Uhrin', 'martin.uhrin.10@ucl.ac.uk'),)
+version_info = (0, 7, 6)
+
+__author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
+__version__ = ".".join(map(str, version_info))
+
+__all__ = ('__version__',)  # pylint: disable=undefined-all-variable
+
+BANNER = """
+                  ____  ____ 
+                 / __ \/ __ /
+    ____  __  __/ / / / /_   
+   / __ \/ / / / / / /\__ \  
+  / /_/ / /_/ / /_/ /___/ /  
+ / .___/\__, /\____//____/   
+/_/    /____/                 v{}                 
+""".format(__version__)
