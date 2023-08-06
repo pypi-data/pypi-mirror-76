c9ab69c3bb8ebda8c63db98c8d5f88e7c599ed97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:06 +0000

    Add package sphinxawesome-theme/1.10.5/sphinxawesome-theme-1.10.5.tar.gz

---
+++ packages/sphinxawesome-theme/sphinxawesome-theme-1.10.5.tar.gz/sphinxawesome-theme-1.10.5/src/sphinxawesome_theme/admonitions_ids.py
@@ -0,0 +1,39 @@
+"""Add ids to all admonition nodes.
+
+So that admonitions also can have permalinks.
+
+:copyright: Copyright 2020, Kai Welke.
+:license: MIT, see LICENSE.
+"""
+
+from typing import Any
+
+from docutils import nodes
+from sphinx.transforms.post_transforms import SphinxPostTransform
+from sphinx.util import logging
+
+logger = logging.getLogger(__name__)
+
+
+class AdmonitionId(SphinxPostTransform):
+    """Traverse all admonition nodes and add IDs."""
+
+    default_priority = 10
+    # this postransform should only be applied for the HTML builders
+    builders = ("html", "dirhtml")
+
+    def run(self, **kwargs: Any) -> None:
+        """Run the AdmonitionID posttransform."""
+        note_id = 1
+        for node in self.document.traverse():
+            if isinstance(node, nodes.section):
+                if node["names"]:
+                    title = nodes.make_id(node["names"][0])
+                elif self.document.nameids:
+                    title = nodes.make_id(list(self.document.nameids)[0])
+                else:
+                    title = "unknown"
+
+            if isinstance(node, nodes.Admonition):
+                node["ids"] = [f"{title}-note-{note_id}"]
+                note_id += 1
