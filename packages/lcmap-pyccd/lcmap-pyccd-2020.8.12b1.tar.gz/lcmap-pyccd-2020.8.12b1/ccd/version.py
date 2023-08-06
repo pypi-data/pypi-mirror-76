4793c9c901c618d72fed11aac3d5dbaaf8fc641c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:24 +0000

    Add package lcmap-pyccd/2020.8.12b1/lcmap-pyccd-2020.8.12b1.tar.gz

---
+++ packages/lcmap-pyccd/lcmap-pyccd-2020.8.12b1.tar.gz/lcmap-pyccd-2020.8.12b1/ccd/version.py
@@ -0,0 +1,8 @@
+""" holds algorithm name & version information.  Information is needed in setup.py 
+as well as ccd/__init__.py.  If vals were all defined in __init__.py install would fail.
+"""
+
+__name = 'lcmap-pyccd'
+__version = '2020.08.12-b1'
+
+
