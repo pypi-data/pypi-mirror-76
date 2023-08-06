d906c63b2454bd507b1d387723fac190915ffd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:12 +0000

    Add package fwiffos/0.0.18/fwiffos-0.0.18-py3-none-any.whl

---
+++ packages/fwiffos/fwiffos-0.0.18-py3-none-any.whl/fwiffos/cli/shell.py
@@ -0,0 +1,40 @@
+from fwiffos.core import version
+from fwiffos.cli.sudo import LetUs
+from fwiffos.read.twitter import Twitter
+
+class Calculator(object):
+
+    def add(self, x, y):
+      return x + y
+
+    def multiply(self, x, y):
+      return x * y
+
+class FS(object):
+
+    def __init__(self):
+        self.filesystem = {'/': ['README']}
+
+    def ls(self, cwd='/'):
+        print(self.filesystem[cwd])
+
+class Commands(object):
+    def __init__(self):
+        fs = FS()
+        calc = Calculator()
+        sudo = LetUs()
+        twitter = Twitter()
+        self._version = version
+        self.__commands = {
+            'ls': fs.ls,
+            'add': calc.add,
+            'mult': calc.multiply,
+            'sudo': sudo.letus,
+            'letus': sudo.letus,
+            'read': twitter.tweet,
+        }
+
+    def exec(self):
+        return self.__commands
+
+commands = Commands()
