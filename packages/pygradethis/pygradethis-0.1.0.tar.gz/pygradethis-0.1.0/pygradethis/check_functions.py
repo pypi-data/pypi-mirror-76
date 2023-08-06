ababe41bf2051946b3680060c48ef575f0f24005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package pygradethis/0.1.0/pygradethis-0.1.0.tar.gz

---
+++ packages/pygradethis/pygradethis-0.1.0.tar.gz/pygradethis-0.1.0/pygradethis/check_functions.py
@@ -0,0 +1,106 @@
+"""
+This module contains functions to check function calls, standardize arguments,
+and return standardized call.
+"""
+import ast
+import asttokens
+import inspect
+import builtins
+
+from .formatters import formatted
+from math import sqrt, log
+
+from itertools import zip_longest
+
+def standardize_arguments(call: ast.Call, source_code: str) -> ast.Call:
+    """This will standardize the function calls for a function Call and return
+    the modified Call.
+    
+    Returns None if there are no issues with the function call.
+    
+    Otherwise, an error message is returned if the user code can't be parsed, 
+    if there is a problem not finding variables in environment, or if arguments 
+    cannot be standardized.
+
+    Parameters
+    ----------
+    call : ast.Call
+        the call to standardize arguments
+    source_code : str
+        the source code in which the call belongs to
+
+    Returns
+    -------
+    ast.Call
+        the standardized call
+
+    Raises
+    ------
+    TypeError
+        when call itself is invalid (e.g. wrong argument name)
+    NameError
+        when for e.g. the function name doesn't exist in environment
+    Exception
+        when other Exceptions occur, silently ignore for now
+    """
+    try:
+        # 1) introduce function call in environment
+        # first pass: is it legal Python code?
+        # for e.g., positional args should always be before keywords args
+        # exec will catch these issues
+        exec(source_code)
+        # 2) construct a environment containing encompassing both global and locals, 
+        # overwriting the globals with locals, and builtins.
+        envir = dict(globals(), **locals(), **builtins.__dict__)
+        # 3) grab the live function from the environment
+        # if we're calling a function on an object, the function info has
+        # to be extracted from an ast.Attribute
+        if isinstance(call.func, ast.Attribute):
+            func_name = getattr(call.func, "attr") 
+            # the object's name 
+            obj = call.func.value.id
+            # the live function from the environment associated with object
+            # e.g. the `df` in df.head() is a pd.DataFrame so must get the
+            # `head` associated with that class.
+            live_func = getattr(envir[obj], func_name)
+        else:
+            # if we're calling a function not associated with an object
+            # just grab function from environment using its name
+            func_name = call.func.id
+            live_func = envir[func_name]
+
+        # 4) collect the arguments passed
+        # construct keyword args mapping
+        kwargs = {a.arg:a.value for a in call.keywords}
+        
+        # 5) get the formal arguments for function
+        try:
+            # Note: this will raise ValueError if inspect cannot retrieve signature
+            # which can happen for some builtins like `print`, where underlying C
+            # code does not provide any metadata about its signature.
+            sig = inspect.signature(live_func)
+        except ValueError: 
+            # if we can't get a signature just return call for normal Call
+            # checking flow
+            return call
+
+        # 6) unpack args and kwargs and attempt to standardize argument calls
+        # returns: https://docs.python.org/3.6/library/inspect.html#inspect.BoundArguments
+        partial_args = sig.bind(*call.args, **kwargs)
+        partial_args.apply_defaults()
+
+        # 7) return a modified AST representing the standardized call
+        # we do this by updating Call.keywords and reset the args
+        # TODO handle functions where you don't have keywords: import math; math.sqrt(math.log(1))
+        new_keywords = []
+        for k, v in partial_args.arguments.items():
+            new_keywords.append(ast.keyword(arg=k, value=v))
+        call.args = []
+        call.keywords = new_keywords
+        return call
+    except TypeError as e:
+        raise AssertionError(str(e).capitalize())
+    except NameError as e:
+        raise AssertionError(str(e).capitalize())
+    except Exception as e:
+        pass
---
+++ packages/pygradethis/pygradethis-0.1.0.tar.gz/pygradethis-0.1.0/pygradethis/formatters.py
@@ -0,0 +1,197 @@
+
+"""
+Module contains a generic AST node formatter and allows for custom formatters
+for whichever data type you need.
+
+To define a node formatter simply define another `_` function, register its 
+type for the generic `formatted` function, and define the string to return:
+
+```
+@formatted.register(<your type>)
+def _(node):
+    <process your node>
+    return "<your string>"
+```
+
+Alternatively, you can also call the `format_node` function to register your
+function using one of two ways:
+
+1. `format_node(int, <"a string">)`
+
+2. `format_node(int, <a function or lambda that formats and returns string>)`
+
+The basic data types and ast types have been supported but can be changed in
+the respective register function definition, or overriden with `format_node`.
+"""
+
+import ast
+from functools import singledispatch
+
+@singledispatch
+def formatted(node):
+    """Generic function to return a formatted representation of an AST node"""
+    return "{}".format(type(node).__name__.lower())
+
+def format_node(node_type, string_format):
+    """Registers a format function for a give node type
+    
+    The node_type must be a valid data type in Python.
+    The string_format can be either a str or a function / lambda.
+    """
+    @formatted.register(node_type)
+    def _(node):
+        if callable(string_format):
+            return string_format(node)
+        else:
+            return string_format
+
+# binary operators
+format_node(ast.Add, "+")
+format_node(ast.Sub, "-")
+format_node(ast.Mult, "*")
+format_node(ast.MatMult, "@")
+format_node(ast.Div, "/")
+format_node(ast.FloorDiv, "//")
+format_node(ast.Mod, "%")
+format_node(ast.Pow, "**")
+format_node(ast.LShift, "<<")
+format_node(ast.RShift, ">>")
+format_node(ast.BitOr, "|")
+format_node(ast.BitXor, "^")
+format_node(ast.BitAnd, "&")
+# unary operators
+format_node(ast.UAdd, "+")
+format_node(ast.USub, "-")
+format_node(ast.Invert, "~")
+# bool operators
+format_node(ast.And, "and")
+format_node(ast.Or, "or")
+format_node(ast.Not, "not")
+# compare operators
+format_node(ast.Eq, "==")
+format_node(ast.NotEq, "!=")
+format_node(ast.Lt, "<")
+format_node(ast.LtE, "<=")
+format_node(ast.Gt, ">")
+format_node(ast.GtE, ">=")
+format_node(ast.Is, "is")
+format_node(ast.IsNot, "is not")
+format_node(ast.In, "in")
+format_node(ast.NotIn, "not in")
+
+# core types
+
+@formatted.register(str)
+def _(el):
+    """Formatting function for an str"""
+    return "\"{}\"".format(el)
+
+@formatted.register(int)
+def _(el):
+    """Formatting function for an int"""
+    return el
+
+@formatted.register(ast.List)
+def _(node):
+    """Formatting function for an ast.List"""
+    return [formatted(n) for n in node.elts]
+
+@formatted.register(ast.Tuple)
+def _(node):
+    """Formatting function for an ast.Tuple"""
+    return tuple([formatted(n) for n in node.elts])
+
+@formatted.register(ast.Set)
+def _(node):
+    """Formatting function for an ast.Set"""
+    return {formatted(n) for n in node.elts}
+
+@formatted.register(ast.Dict)
+def _(node):
+    """Formatting function for an ast.Dict"""
+    adict = {}
+    for k, v in zip(node.keys, node.values):
+        if isinstance(k, ast.Str):
+            adict[k.s] = formatted(v)
+        else:
+            adict[formatted(k)] = formatted(v)
+    return adict
+
+@formatted.register(ast.Expr)
+def _(node):
+    """Formatting function for an ast.Expr"""
+    return "an expression"
+
+@formatted.register(ast.arg)
+def _(node):
+    return node.arg
+
+@formatted.register(ast.arguments)
+def _(node):
+    defaults = [formatted(d) for d in node.defaults]
+    len_defaults = len(defaults)
+    args = [formatted(a) for a in node.args[:len_defaults-1]]
+    rem_args = [formatted(a) for a in node.args[len_defaults-1:]]
+    map_args_defaults = ["{}={}".format(a, d) for a, d in zip(rem_args, defaults)]
+    return  ", ".join(args + map_args_defaults)
+
+@formatted.register(ast.Lambda)
+def _(node):
+    args = formatted(getattr(node, 'args'))
+    return "lambda {}: {}".format(args, formatted(node.body))
+
+@formatted.register(ast.Call)
+def _(node):
+    """Formatting function for an ast.Call"""
+    if isinstance(node.func, ast.Attribute):
+        func_name = getattr(node.func, "attr") 
+    else:
+        func_name = node.func.id
+    # grab args and keywords
+    args = [str(formatted(a)) for a in node.args]
+    keywords = ["{}={}".format(k.arg, formatted(k.value)) for k in node.keywords]
+    # return call like it would appear
+    return "{}({})".format(func_name, ", ".join(args + keywords))
+
+@formatted.register(ast.keyword)
+def _(node):
+    """Formatting function for an ast.keyword"""
+    return getattr(node, "arg")
+
+@formatted.register(ast.BinOp)
+def _(node):
+    """Formatting function for an ast.BinOp"""
+    return "{} {} {}".format(formatted(node.left), formatted(node.op), formatted(node.right))
+
+@formatted.register(ast.UnaryOp)
+def _(node):
+    """Formatting function for an ast.UnaryOp"""
+    return "{}{}".format(formatted(node.op), formatted(node.operand))
+
+@formatted.register(ast.Attribute)
+def _(node):
+    """Formatting function for an ast.Attribute"""
+    return "\"{}\" on {}".format(getattr(node, "attr"), formatted(getattr(node, "value")))
+
+@formatted.register(ast.Name)
+def _(node):
+    """Formatting function for an ast.Name"""
+    return getattr(node, "id")
+
+@formatted.register(ast.Num)
+def _(node):
+    """Formatting function for an ast.Num"""
+    return getattr(node, "n")
+
+@formatted.register(ast.Str)
+def _(node):
+    """Formatting function for an ast.Str"""
+    return "\"{}\"".format(getattr(node, "s"))
+
+@formatted.register(ast.NameConstant)
+def _(node):
+    """Formatting function for True/False"""
+    return getattr(node, "value")
+
+def test_format(node):
+    print(formatted(node))
