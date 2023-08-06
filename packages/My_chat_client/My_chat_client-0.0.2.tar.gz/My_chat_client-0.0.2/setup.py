d5e9855609049ed3e8ce72bc41e90a7661dcc9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:36 +0000

    Add package My_chat_client/0.0.2/My_chat_client-0.0.2.tar.gz

---
+++ packages/My_chat_client/My_chat_client-0.0.2.tar.gz/My_chat_client-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name="My_chat_client",
+      version="0.0.2",
+      description="My_chat_client",
+      author="Sergey Sergeev",
+      author_email="ss2576@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex']
+      )
