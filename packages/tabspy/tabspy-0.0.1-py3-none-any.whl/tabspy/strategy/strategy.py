5b83668e12725486681a2fc102f0bd75da8aad2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:06 +0000

    Add package tabspy/0.0.1/tabspy-0.0.1-py3-none-any.whl

---
+++ packages/tabspy/tabspy-0.0.1-py3-none-any.whl/tabspy/strategy/strategy.py
@@ -0,0 +1,7 @@
+class Strategy:
+    def __init__(self, name=None):
+        self._name = name
+
+    def welcome(self):
+        print(f'Welcome {self._name} : tabspy >> ta >> strategy')
+        return
---
+++ packages/tabspy/tabspy-0.0.1-py3-none-any.whl/tabspy/utils.py
@@ -0,0 +1,4 @@
+
+def welcome(name='TABSPY'):
+    print(f'Welcome {name} : tabspy >> ta >> utils')
+    return
