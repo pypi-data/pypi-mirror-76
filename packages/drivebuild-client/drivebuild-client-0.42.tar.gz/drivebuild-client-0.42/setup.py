ad376bc6cdb6fc2b67b7b2682f3d8f66a14e0a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:21 +0000

    Add package drivebuild-client/0.42/drivebuild-client-0.42.tar.gz

---
+++ packages/drivebuild-client/drivebuild-client-0.42.tar.gz/drivebuild-client-0.42/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="drivebuild-client",
+    version="0.42",
+    author="Stefan Huber",
+    author_email="huberst@fim.uni-passau.de",
+    description="A client for accessing a DriveBuild MainApp.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=setuptools.find_packages(
+        include=("drivebuildclient",)
+    ),
+    python_requires=">=3.6",
+    install_requires=[
+        "dill",
+        "flask",
+        "protobuf"
+    ],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    project_urls={
+        "Source": "https://github.com/TrackerSB/DriveBuild"
+    }
+)
