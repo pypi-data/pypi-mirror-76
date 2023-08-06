c7625f07ac9cc7c0db4c86e28194d8cf85845d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:38 +0000

    Add package aiogram-mvc/0.1.1597097653/aiogram_mvc-0.1.1597097653.tar.gz

---
+++ packages/aiogram-mvc/aiogram_mvc-0.1.1597097653.tar.gz/aiogram_mvc-0.1.1597097653/aiogram_mvc/view/base.py
@@ -0,0 +1,76 @@
+import functools
+import typing
+from abc import ABCMeta, abstractmethod
+
+from aiogram_mvc.middleware.middleware import MiddlewareList
+from aiogram_mvc.view import exception
+
+
+class BaseMetaView(ABCMeta):
+    register_func: callable
+    handler_kwargs: dict
+    middleware: MiddlewareList
+    id: int
+
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not cls.__module__.startswith("aiogram_mvc"):
+            if getattr(cls, "handler_kwargs", None) is None:
+                raise Exception(f"handler_kwargs is required in {cls}")
+            cls.register_func(cls._validator(cls.main),
+                              *getattr(cls, "custom_filters", []),
+                              **cls.handler_kwargs)
+
+    @abstractmethod
+    def _validator(cls, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def main(cls, *args, **kwargs):
+        pass
+
+
+class BaseView(metaclass=BaseMetaView):
+    handler_kwargs: dict
+    custom_filters: list
+    middleware = MiddlewareList()
+    __global_id_counter__ = 0
+
+    def __init__(self):
+        super().__init__()
+        self.id = None
+
+    @staticmethod
+    @abstractmethod
+    async def _get_state(update_object):
+        pass
+
+    @classmethod
+    def _validator(cls, func):
+        @functools.wraps(func)
+        @cls.middleware
+        async def wrap(update_obj, *args, **kwargs):
+            state = cls._get_state(update_obj)
+            try:
+                validated_data = await cls.validate(update_obj, state=state, **kwargs)
+            except exception.NotValidated:
+                return
+            kwargs["validated_data"] = validated_data if validated_data else {}
+            return await func(update_obj, *args, state=state, **kwargs)
+
+        return wrap
+
+    @classmethod
+    async def validate(cls, update_obj, call_data: typing.Optional, **kwargs):
+        pass
+
+    @classmethod
+    @abstractmethod
+    async def main(cls, *args, **kwargs):
+        pass
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        BaseView.__global_id_counter__ += 1
+        cls.id = BaseView.__global_id_counter__
+        return cls
