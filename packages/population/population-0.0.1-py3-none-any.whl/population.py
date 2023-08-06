7219176f4b9332ac6ecf13cf667d78d7fc24363a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:34 +0000

    Add package population/0.0.1/population-0.0.1-py3-none-any.whl

---
+++ packages/population/population-0.0.1-py3-none-any.whl/population.py
@@ -0,0 +1,15 @@
+
+
+def population(item=None):
+
+        if item is None:
+                print("Enter a valid entry")
+
+        elif item == "india":
+                print("1,352,642,280")
+
+        else:
+                print("we are under dev")
+
+
+
