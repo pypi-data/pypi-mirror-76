a323c0bffc1fd7b3d994752f365800f4b2260168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:22 +0000

    Add package cihai/0.10.0/cihai-0.10.0-py2.py3-none-any.whl

---
+++ packages/cihai/cihai-0.10.0-py2.py3-none-any.whl/cihai/data/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Datasets for cihai.
+
+cihai.datasets
+~~~~~~~~~~~~~~
+
+"""
+
+from __future__ import absolute_import, print_function, unicode_literals
