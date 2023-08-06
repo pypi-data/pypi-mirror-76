4ed561b803480903956ac836eed3efb98084c663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:55 +0000

    Add package prettyqt/0.113.0/PrettyQt-0.113.0-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.113.0-py3-none-any.whl/prettyqt/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for PrettyQt."""
+
+__author__ = """Philipp Temminghoff"""
+__email__ = "phil65@kodi.tv"
+__version__ = "0.113.0"
+
+
+__all__ = [
+    "core",
+    "gui",
+    "widgets",
+    "custom_widgets",
+    "custom_models",
+    "syntaxhighlighters",
+    "charts",
+    "webenginewidgets",
+]
