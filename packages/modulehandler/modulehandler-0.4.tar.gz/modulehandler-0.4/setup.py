6c852b29cfe5e78e195119de1f61178793e20b4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:18 +0000

    Add package modulehandler/0.4/modulehandler-0.4.tar.gz

---
+++ packages/modulehandler/modulehandler-0.4.tar.gz/modulehandler-0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+setuptools.setup(
+    name="modulehandler",
+    version="0.4",
+    author="Gallay David",
+    author_email="davidtennis96@hotmail.com",
+    description="A dynamic module handler, with standalone tools",
+    setup_requires=['setuptools-markdown'],
+    long_description_content_type="text/markdown",
+    long_description_markdown_filename='README.md',
+    url="https://github.com/divad1196/ModuleHandler",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "markdown",
+        "pathlib",
+        "pydantic",
+        "typing",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
