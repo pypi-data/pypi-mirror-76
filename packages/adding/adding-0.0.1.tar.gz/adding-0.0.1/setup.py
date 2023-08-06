121db694a52a47c4a4251c697a8cf834d2915cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:12 +0000

    Add package adding/0.0.1/adding-0.0.1.tar.gz

---
+++ packages/adding/adding-0.0.1.tar.gz/adding-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='adding',
+  version='0.0.1',
+  description='A very basic adding library',
+  long_description=open('readme.txt').read(),# + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Joshua Lowe',
+  author_email='balajibetadur@gmail.com',
+  license='MIT', 
+#   classifiers=classifiers,
+  keywords='adding', 
+  packages=['packagebb'], # find_packages(),
+  install_requires=[''] 
+)
