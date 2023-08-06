230a8e138ccd7af5b87aa31b7376f312d33f3802
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:40 +0000

    Add package adr.ca/1.0/adr.ca-1.0-py3-none-any.whl

---
+++ packages/adr.ca/adr.ca-1.0-py3-none-any.whl/ADR/Components/Points/CG.py
@@ -0,0 +1,6 @@
+from ADR.Components.Component import Component
+
+
+class CG(Component):
+    def __init__(self, data):
+        super().__init__(data)
---
+++ packages/adr.ca/adr.ca-1.0-py3-none-any.whl/ADR/__init__.py
@@ -0,0 +1,8 @@
+""" 
+ADR
+"""
+from .analyzer import analyzer
+
+name = "ADR"
+version = "0.1"
+print("ADR suscefully imported")
