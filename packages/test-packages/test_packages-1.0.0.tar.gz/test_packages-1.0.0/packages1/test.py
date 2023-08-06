738a937244cbb52af89059c9630745383088a5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:31 +0000

    Add package test-packages/1.0.0/test_packages-1.0.0.tar.gz

---
+++ packages/test-packages/test_packages-1.0.0.tar.gz/test_packages-1.0.0/packages1/test.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+
+def print_sys_path():
+    print(os.path)
+    print(sys.path)
+
---
+++ packages/test-packages/test_packages-1.0.0.tar.gz/test_packages-1.0.0/setup.py
@@ -0,0 +1,41 @@
+import codecs
+import os
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+NAME = "test_packages"
+PACKAGES = ['packages1']
+DESCRIPTION = "this is a test for package by myself upload to pypi"
+LONG_DESCRIPTION = "this is a test for package by myself upload to pypi"
+KEYWORDS = "keyword"
+AUTHOR = "poison"
+AUTHOR_EMAIL = "948815501@qq.com"
+URL = "https://github.com/lichanghong/wenyali.git"
+VERSION = "1.0.0"
+LICENSE = "MIT"
+setup(
+    name=NAME, version=VERSION,
+    description=DESCRIPTION, long_description=LONG_DESCRIPTION,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords=KEYWORDS, author=AUTHOR,
+    author_email=AUTHOR_EMAIL, url=URL,
+    packages=PACKAGES, include_package_data=True, zip_safe=True,
+    entry_points={
+      "console_scripts": [
+                          "test_console = package1.test:print_sys_path",
+                          ]
+      },
+
+)
