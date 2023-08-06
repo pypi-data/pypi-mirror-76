84ce60fcad6dba07c15e500e66c6f448f1401392
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:47 +0000

    Add package aligi/1.1.0/aligi-1.1.0.tar.gz

---
+++ packages/aligi/aligi-1.1.0.tar.gz/aligi-1.1.0/README.md
@@ -0,0 +1,51 @@
+# aligi
+
+Aliyun Gateway Interface
+
+为无服务函数计算作为阿里云 API 后端运行提供转析，并且允许同一个无服务函数既支持 HTTP 触发器，也支持作为 API 网关的后端服务。
+
+## 为什么会有这个库？
+
+使用 Flask，Django 等支持标准 WSGI 协议的 Web 框架创建无服务函数时，使用HTTP触发器才能让它们正常运行。
+
+但如果想使用无服务函数作为阿里云 API 网关的后端时，则无法直接使用这些函数，只能通过网络调用，这显然是不够高效、并且费钱的。
+
+## 如何安装
+
+在项目根目录下执行
+
+```
+pip install -t . aligi
+```
+
+## 如何使用
+
+以下为一个最小的 Flask 样例
+
+```python
+from flask import Flask
+from aligi import WSGI
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+# 阿里云无服务函数的入口
+handler = WSGI(app)
+```
+
+其中`app`可以是任意一个标准 WSGI Application。
+
+在 Django 项目中，它一般在项目的`wsgi.py`里。
+
+## 参考文档
+
+阿里云文档:
+
+- [HTTP 触发器](https://help.aliyun.com/document_detail/74756.html?spm=a2c4g.11186623.2.29.4a4b6482rl6rYZ#h2--http-2)
+
+- [API 网关触发器](https://help.aliyun.com/document_detail/74800.html?spm=a2c4g.11186623.6.696.24bd2ebcuaTlC2)
---
+++ packages/aligi/aligi-1.1.0.tar.gz/aligi-1.1.0/setup.py
@@ -0,0 +1,97 @@
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+from aligi import __version__ as aligi_version
+
+# Package meta-data.
+NAME = "aligi"
+DESCRIPTION = "为阿里无服务函数提供API网关的协议解析"
+URL = "https://github.com/abersheeran/aligi"
+EMAIL = "abersheeran@qq.com"
+AUTHOR = "AberSheeran"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = ".".join([str(_) for _ in aligi_version])
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(VERSION))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+# Where the magic happens:
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=["aligi"],
+    license="MIT",
+    keywords=["aliyun", "api", "serverless", "python"],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    # $ setup.py publish support.
+    cmdclass={"upload": UploadCommand},
+)
