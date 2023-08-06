fae40a300769e218f31b72d419cbec7bf5b25ece
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:58 +0000

    Add package distinctipy/1.1.5/distinctipy-1.1.5.tar.gz

---
+++ packages/distinctipy/distinctipy-1.1.5.tar.gz/distinctipy-1.1.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="distinctipy",
+    version="1.1.5",
+    author="Jack Roberts",
+    author_email="jroberts@turing.ac.uk",
+    description="A lightweight package for generating visually distinct colours.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alan-turing-institute/distinctipy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Utilities"
+    ],
+    keywords="color colour palette colormap colorblind colourblind",
+    packages=setuptools.find_packages(),
+    install_requires=["matplotlib", "numpy", "pandas"]
+)
+
