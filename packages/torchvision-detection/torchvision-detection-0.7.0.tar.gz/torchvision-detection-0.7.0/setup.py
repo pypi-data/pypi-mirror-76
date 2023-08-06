19717ba88601018c87fbc4d25b2dc843c0bfee23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:53 +0000

    Add package torchvision-detection/0.7.0/torchvision-detection-0.7.0.tar.gz

---
+++ packages/torchvision-detection/torchvision-detection-0.7.0.tar.gz/torchvision-detection-0.7.0/setup.py
@@ -0,0 +1,27 @@
+import platform
+from setuptools import setup, find_packages
+
+PLATFORM = platform.system()
+
+REQUIREMENTS = [
+  'numpy',
+  'pillow',
+  'pycocotools-windows' if 'Windows' in PLATFORM else 'wf-pycocotools',
+  'torch',
+  'torchvision'
+]
+
+setup(
+  name = 'torchvision-detection',
+  version = '0.7.0',
+  description = 'Object detection reference training scripts.',
+  author = 'PyTorch Team',
+  maintainer = 'Danilo Peixoto',
+  maintainer_email = 'danilopeixoto@outlook.com',
+  license = 'BSD',
+  url = 'https://github.com/danilopeixoto/torchvision-detection',
+  download_url = 'https://github.com/danilopeixoto/torchvision-detection/archive/v0.7.0.tar.gz',
+  packages = find_packages(),
+  python_requires = '>=3.6',
+  install_requires = REQUIREMENTS,
+  zip_safe = False)
