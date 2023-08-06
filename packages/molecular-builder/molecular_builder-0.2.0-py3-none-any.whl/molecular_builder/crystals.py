0b0196adfa86f91170e6d1236ce72c00fecba1cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:58 +0000

    Add package molecular-builder/0.2.0/molecular_builder-0.2.0-py3-none-any.whl

---
+++ packages/molecular-builder/molecular_builder-0.2.0-py3-none-any.whl/molecular_builder/crystals.py
@@ -0,0 +1,103 @@
+crystals = {
+    "alpha_tridymite" : 
+    {
+    "a" : 8.7549, 
+    "b" : 5.0335,
+    "c" : 8.2118,
+    "alpha" : 90,
+    "beta" : 90,
+    "gamma" : 90,
+    "spacegroup" : 20,
+    "elements" : ["Si", "O", "O", "O"],
+    "positions" : [ (.1672, .5459, .1877), 
+                    (0.3315, 0.0,0.5), 
+                    (0.0,  0.5587,     0.25), 
+                    (0.2522,  0.3139,  0.2729)],
+    "citation" : "Lee S, Xu H. Acta Crystallographica B75 (2019) https://doi.org/10.1107/S2052520619000933 Using powder XRD and pair distribution function to determine anisotropic atomic displacement parameters of orthorhombic tridymite and tetragonal cristobalite"
+    },
+    "alpha_quartz" :
+    {
+        "a" : 5.02778179,
+        "b" : 5.02778179,
+        "c" : 5.51891759,
+        "alpha" : 90,
+        "beta" : 90,
+        "gamma" : 120,
+        "spacegroup" : "P3221",
+        "elements" : ["Si", "O"],
+        "positions" : [(0.000000,  0.477285,  0.833333), (0.160738,  0.745706,  0.037343)],
+        "citation" : "N/A"
+    },
+    "beta_quartz" :
+    {
+        	
+        "a" : 5.013,
+        "b" : 5.013, 
+        "c" : 5.47,
+        "alpha" : 90,
+        "beta" : 90,
+        "gamma" : 120,
+        "spacegroup" : "P6222",
+        "elements" : ["Si", "O"],
+        "positions" : [(0.5,0.5,1.0/3), (0.197, -0.197, .8333)],
+        "citation" : "Wyckoff R, Zeitschrift fur Kristallographie 63 (1926) 507-537"
+    },
+    "beta_cristobalite" :
+    {
+        "a" : 7.12,
+        "b" : 7.12,
+        "c" : 7.12,
+        "alpha" : 90,
+        "beta" : 90,
+        "gamma" : 90,
+        "spacegroup" : 227,
+        "elements" : ["Si", "O"],
+        "positions" : [(0.0,  0.0,  0.0), (0.125,  0.125,  0.125)],
+        "citation" : "Wyckoff R, Zeitschrift fur Kristallographie 62 (1925) 189-200 Die Kristallstruktur von $-beta-Crystobalit Si O2 (bei hohen Temperaturen stabile Form)."
+    },
+    "brucite" : 
+    { 
+        "a" : 3.142, 
+        "b" : 3.142,
+        "c" : 4.766,
+        "alpha" : 90,
+        "beta" : 90,
+        "gamma" : 90,
+        "spacegroup" : 164,
+        "elements" : ["Mg", "O", "H"],
+        "positions" : [(0.0, 0.0, 0.0), (1./3, 2./3, 0.2216), (1./3, 2./3, 0.4303)],
+        "citation" : "Zigan F, Rothbauer R, Neues Jahrbuch fur Mineralogie, Monatshefte 1967 (1967) 137-143, Neutronenbeugungsmessungen am Brucit"
+    },
+    "coesite" :
+    {
+        "a" : 7.173,
+        "b" : 12.328,
+        "c" : 7.175,
+        "alpha" : 90, 
+        "beta" : 120,
+        "gamma" : 90,
+        "spacegroup" : 15,
+        "elements" : ["Si", "Si", "O", "O", "O", "O", "O"],
+        "positions" :   [   (0.35900, 0.39160, 0.42740),
+                            (0.49340, 0.15770, 0.45960),
+                            (0.00000, 0.00000, 0.00000),
+                            (0.50000, 0.11720, 0.25000),
+                            (0.26700, 0.12380, 0.44180),
+                            (0.18910, 0.39640, 0.17550),
+                            (0.48070, 0.28820, 0.02120)],
+        "citation" :  "Araki T, Zoltai T, Zeitschrift fur Kristallographie 129 (1969) 381-387. Refinement of a coesite structure"
+    },
+    "diamond" : 
+    {
+        "a" : 3.57,
+        "b" : 3.57,
+        "c" : 3.57,
+        "alpha" : 90,
+        "beta" : 90,
+        "gamma" : 90,
+        "spacegroup" : 227,
+        "elements" : ["C"],
+        "positions" : [(0,0,0)],
+        "citation" : "N/A"
+    }
+}
