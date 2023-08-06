ad6ba549e1b7eeaa78ba01f46f5fd522c3fc3c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:07 +0000

    Add package prettyqt/0.116.0/PrettyQt-0.116.0-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.116.0-py3-none-any.whl/prettyqt/gui/pixmap.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+import pathlib
+from typing import Union
+
+from qtpy import QtGui
+
+from prettyqt import gui
+
+
+QtGui.QPixmap.__bases__ = (gui.PaintDevice,)
+
+
+class Pixmap(QtGui.QPixmap):
+    def __bool__(self):
+        return not self.isNull()
+
+    @classmethod
+    def from_file(cls, path: Union[pathlib.Path, str]):
+        if isinstance(path, str):
+            path = pathlib.Path(path)
+        with path.open(mode="rb") as f:
+            data = f.read()
+        # Create widget
+        pixmap = cls()
+        pixmap.loadFromData(data)
+        return pixmap
+
+
+if __name__ == "__main__":
+    from prettyqt import widgets
+
+    app = widgets.app()
+    p = Pixmap()
+    print(bool(p))
