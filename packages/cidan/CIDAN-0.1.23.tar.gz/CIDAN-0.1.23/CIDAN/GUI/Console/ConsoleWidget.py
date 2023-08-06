0704c31a73fd82b9d85b5fa6c15c90d3c0be46d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:27 +0000

    Add package cidan/0.1.23/CIDAN-0.1.23.tar.gz

---
+++ packages/cidan/CIDAN-0.1.23.tar.gz/CIDAN-0.1.23/CIDAN/GUI/Console/ConsoleWidget.py
@@ -0,0 +1,55 @@
+import sys
+
+from qtpy import QtCore
+from qtpy import QtGui
+from qtpy.QtWidgets import *
+
+
+class Stream(QtCore.QObject):
+    newText = QtCore.Signal(str)
+
+    def __init__(self):
+        super().__init__()
+
+        # print(type(self.newText))
+        # self.connect(self.newText)
+
+    def write(self, text):
+        self.newText.emit(str(text))
+
+    def flush(self):
+        pass
+
+
+class ConsoleWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel()
+        self.label.setStyleSheet("font-size: 15pt")
+        self.label.setText("")
+
+        self.layout.addWidget(self.label, alignment=QtCore.Qt.AlignRight)
+        # self.setMinimumHeight(100)
+        # self.process = QTextBrowser()
+        # self.process.moveCursor(QtGui.QTextCursor.Start)
+        # self.process.ensureCursorVisible()
+        # self.process.setLineWrapColumnOrWidth(500)
+        # self.process.setLineWrapMode(QTextEdit.FixedPixelWidth)
+        #
+        # self.layout.addWidget(self.process)
+        self.setLayout(self.layout)
+        # sys.stdout = Stream()
+        # sys.stdout.newText.connect(self.onUpdateText)
+        self.update=True
+
+    def updateText(self, text):
+        self.label.setText(text)
+        # if self.update:
+        #     self.update =False
+        #     print(text)
+        #     self.update = True
+
+    def __del__(self):
+        sys.stdout = sys.__stdout__
