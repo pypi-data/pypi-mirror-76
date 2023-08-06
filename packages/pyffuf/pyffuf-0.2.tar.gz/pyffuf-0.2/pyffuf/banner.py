15e607e0cd1fc6ab46995f7274d034f15f254f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:35 +0000

    Add package pyffuf/0.2/pyffuf-0.2.tar.gz

---
+++ packages/pyffuf/pyffuf-0.2.tar.gz/pyffuf-0.2/pyffuf/banner.py
@@ -0,0 +1,17 @@
+from clint.textui import puts, colored, indent
+
+class Banner:
+    def banner(self):
+        with indent(4, quote='>>>'):
+            puts(colored.red(str(self.bannerdata())))
+
+
+    def bannerdata(self):
+        data = r"""
+    _____  ________     ___
+    / _ \ \/ / _/ _/_ __/ _/
+    / ___/\  / _/ _/ // / _/
+    /_/    /_/_//_/ \_,_/_/
+                    version 0.2 (beta)
+        """
+        return data
