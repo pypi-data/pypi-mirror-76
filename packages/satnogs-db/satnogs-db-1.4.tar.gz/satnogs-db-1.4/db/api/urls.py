4e9215d3261994394bb82a4e5ecaa644d9b5b5c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:51 +0000

    Add package satnogs-db/1.4/satnogs-db-1.4.tar.gz

---
+++ packages/satnogs-db/satnogs-db-1.4.tar.gz/satnogs-db-1.4/db/api/urls.py
@@ -0,0 +1,14 @@
+"""SatNOGS DB django rest framework API url routings"""
+from rest_framework import routers
+
+from db.api import views
+
+ROUTER = routers.DefaultRouter()
+
+ROUTER.register(r'artifacts', views.ArtifactView)
+ROUTER.register(r'modes', views.ModeView)
+ROUTER.register(r'satellites', views.SatelliteView)
+ROUTER.register(r'transmitters', views.TransmitterView)
+ROUTER.register(r'telemetry', views.TelemetryView)
+
+API_URLPATTERNS = ROUTER.urls
