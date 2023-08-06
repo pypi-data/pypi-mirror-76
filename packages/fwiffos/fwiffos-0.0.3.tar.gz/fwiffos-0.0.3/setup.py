de60d80b29d8f3dd0e7dad5e4a2bdaa6db073ab0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:40 +0000

    Add package fwiffos/0.0.3/fwiffos-0.0.3.tar.gz

---
+++ packages/fwiffos/fwiffos-0.0.3.tar.gz/fwiffos-0.0.3/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os import path
+
+
+with open('README.fwiffos.rst', encoding='utf-8') as f:
+    readme = f.read()
+
+packages = [
+    'fwiffos'
+]
+
+requires = [
+]
+
+setup(
+    name='fwiffos',
+    version='0.0.3',
+    description='FwiffOS',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='SCUPPER™ Foundation',
+    author_email='info@scupper.org',
+    url='https://github.com/SCUPPERfoundation/fwiffo',
+    license='MIT',
+    packages=packages,
+    package_data = {'': ['LICENSE', 'README.fwiffos.rst']},
+    package_dir = {'fwiffos': 'fwiffos'},
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
+
