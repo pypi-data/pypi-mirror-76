2754ca2e4c4c5bd3f1723aa859ae4099482ce6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:09 +0000

    Add package git-remote-codecommit/1.15.1/git-remote-codecommit-1.15.1.tar.gz

---
+++ packages/git-remote-codecommit/git-remote-codecommit-1.15.1.tar.gz/git-remote-codecommit-1.15.1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+
+__version__ = '1.15.1'
+
+
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setuptools.setup(
+    name = 'git-remote-codecommit',
+    packages = ['git_remote_codecommit'],
+    version = __version__,
+    description = 'Git remote prefix to simplify pushing to and pulling from CodeCommit.',
+    long_description = read('README.rst'),
+    author = 'Amazon Web Services',
+    url = 'https://github.com/awslabs/git-remote-codecommit',
+    license = 'Apache License 2.0',
+    install_requires = ['botocore >= 1.17.0'],
+    entry_points = {
+        'console_scripts': [
+            'git-remote-codecommit = git_remote_codecommit:main',
+        ],
+    },
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
