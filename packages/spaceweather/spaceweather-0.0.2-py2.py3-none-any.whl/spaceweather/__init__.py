37c4ca2749a1367f47dbb57c7087ef78bc322012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:12 +0000

    Add package spaceweather/0.0.2/spaceweather-0.0.2-py2.py3-none-any.whl

---
+++ packages/spaceweather/spaceweather-0.0.2-py2.py3-none-any.whl/spaceweather/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2020 Stefan Bender
+#
+# This module is part of pyspaceweather.
+# pyspaceweather is free software: you can redistribute it or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, version 2.
+# See accompanying COPYING.GPLv2 file or http://www.gnu.org/licenses/gpl-2.0.html.
+"""Python interface for space weather indices
+
+"""
+__version__ = "0.0.2"
+
+from .core import *
