2afc3f9a458640985419a74d4d6b0d970647f1d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:14 +0000

    Add package featuretools/0.18.1/featuretools-0.18.1.tar.gz

---
+++ packages/featuretools/featuretools-0.18.1.tar.gz/featuretools-0.18.1/featuretools/tests/entityset_tests/test_plotting.py
@@ -0,0 +1,63 @@
+import os
+import re
+
+import graphviz
+import pandas as pd
+import pytest
+
+import featuretools as ft
+
+
+@pytest.fixture
+def pd_simple():
+    es = ft.EntitySet("test")
+    df = pd.DataFrame({"foo": [1]})
+    es.entity_from_dataframe("test", df)
+    return es
+
+
+def test_returns_digraph_object(es):
+    graph = es.plot()
+
+    assert isinstance(graph, graphviz.Digraph)
+
+
+def test_saving_png_file(es, tmpdir):
+    output_path = str(tmpdir.join("test1.png"))
+
+    es.plot(to_file=output_path)
+
+    assert os.path.isfile(output_path)
+    os.remove(output_path)
+
+
+def test_missing_file_extension(es):
+    output_path = "test1"
+
+    with pytest.raises(ValueError) as excinfo:
+        es.plot(to_file=output_path)
+
+    assert str(excinfo.value).startswith("Please use a file extension")
+
+
+def test_invalid_format(es):
+    output_path = "test1.xzy"
+
+    with pytest.raises(ValueError) as excinfo:
+        es.plot(to_file=output_path)
+
+    assert str(excinfo.value).startswith("Unknown format")
+
+
+def test_multiple_rows(pd_es):
+    plot_ = pd_es.plot()
+    result = re.findall(r"\((\d+\srows?)\)", plot_.source)
+    expected = ["{} rows".format(str(i.shape[0])) for i in pd_es.entities]
+    assert result == expected
+
+
+def test_single_row(pd_simple):
+    plot_ = pd_simple.plot()
+    result = re.findall(r"\((\d+\srows?)\)", plot_.source)
+    expected = ["1 row"]
+    assert result == expected
