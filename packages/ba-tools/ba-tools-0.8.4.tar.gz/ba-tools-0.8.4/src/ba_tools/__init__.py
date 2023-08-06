d0034e6ad6ba76b2588a819ab0d02ef303858ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:22 +0000

    Add package ba-tools/0.8.4/ba-tools-0.8.4.tar.gz

---
+++ packages/ba-tools/ba-tools-0.8.4.tar.gz/ba-tools-0.8.4/src/ba_tools/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'ba_tools'
+__version__ = '0.8.4'
+__author__ = 'Esri'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2019 by Esri'
+
+from . import analysis
+from ._data import data
+from . import enrich
+from . import proximity
+from . import utils
