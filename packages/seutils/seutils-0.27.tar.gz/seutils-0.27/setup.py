b9c22cc0ca7ea08ea141daaf95751890a2f82758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:49 +0000

    Add package seutils/0.27/seutils-0.27.tar.gz

---
+++ packages/seutils/seutils-0.27.tar.gz/seutils-0.27/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name          = 'seutils',
+    version       = '0.27',
+    license       = 'BSD 3-Clause License',
+    description   = 'Description text',
+    url           = 'https://github.com/tklijnsma/seutils.git',
+    author        = 'Thomas Klijnsma',
+    author_email  = 'tklijnsm@gmail.com',
+    packages      = ['seutils'],
+    zip_safe      = False,
+    scripts       = [
+        'bin/seu-format', 'bin/seu-ls', 'bin/seu-hadd', 'bin/seu-du',
+        'bin/seu-nentries', 'bin/seu-takechunkroot'
+        ],
+    )
