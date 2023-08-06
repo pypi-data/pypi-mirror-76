abed02142a944bd0a5c1fddbb7b6604e7c6bfa7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:57 +0000

    Add package fwl-ddd-seedwork/0.2/fwl-ddd-seedwork-0.2.tar.gz

---
+++ packages/fwl-ddd-seedwork/fwl-ddd-seedwork-0.2.tar.gz/fwl-ddd-seedwork-0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='fwl-ddd-seedwork',
+    packages=["fwl_ddd_seedwork"],
+    version='0.2',
+    license='MIT',
+    description='simple ddd seedwork',
+    author='Alexandre Herculano',
+    author_email='aherculano0109@gmail.com',
+    url='https://github.com/aherculano/fwl-ddd-seedwork',
+    python_requires='>=3.6'
+)
