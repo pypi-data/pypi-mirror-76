3da5fd1fb234d09e6ec8cf413ad0c3c8bbeab4c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:24 +0000

    Add package datasette-saved-queries/0.2.1/datasette_saved_queries-0.2.1-py3-none-any.whl

---
+++ packages/datasette-saved-queries/datasette_saved_queries-0.2.1-py3-none-any.whl/datasette_saved_queries/__init__.py
@@ -0,0 +1,56 @@
+from datasette import hookimpl
+import sqlite_utils
+
+
+def create_tables(conn):
+    db = sqlite_utils.Database(conn)
+    if not db["saved_queries"].exists():
+        db["saved_queries"].create(
+            {"name": str, "sql": str, "author_id": str,}, pk="name"
+        )
+
+
+def actorornull(key, request):
+    if request.actor is None:
+        return None
+    return request.actor.get(key)
+
+
+@hookimpl
+def register_magic_parameters():
+    return [
+        ("actorornull", actorornull),
+    ]
+
+
+@hookimpl
+def startup(datasette):
+    async def inner():
+        db = datasette.get_database()
+        if db.is_mutable:
+            await db.execute_write_fn(create_tables, block=True)
+
+    return inner
+
+
+@hookimpl
+def canned_queries(datasette, database):
+    async def inner():
+        db = datasette.get_database(database)
+        if await db.table_exists("saved_queries"):
+            queries = {
+                row["name"]: {"sql": row["sql"]}
+                for row in await db.execute("select name, sql from saved_queries")
+            }
+            queries.update(
+                {
+                    "save_query": {
+                        "sql": "insert into saved_queries (name, sql, author_id) values (:name, :sql, :_actorornull_id)",
+                        "write": True,
+                        "on_success_redirect": "/{}#queries".format(database),
+                    }
+                }
+            )
+            return queries
+
+    return inner
