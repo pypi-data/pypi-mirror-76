49a31127535224921884b0d3b24307873bf36d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:20 +0000

    Add package jamesbond/0.0.15/jamesbond-0.0.15.tar.gz

---
+++ packages/jamesbond/jamesbond-0.0.15.tar.gz/jamesbond-0.0.15/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('/Users/andrewblock/Documents/github/jamesbond/README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('/Users/andrewblock/Documents/github/jamesbond/requirements.txt') as fp:
+    requirements = fp.read().splitlines()
+
+setup(
+    name='jamesbond',
+    version='0.0.15',
+    description='James Bond tabular data package in Pandas dataframe',
+    long_description_content_type="text/markdown",
+    long_description=readme,
+    install_requires=requirements,
+    license='MIT',
+    packages=find_packages(exclude=("tests",)),
+    author='Andrew Block',
+    author_email='andrewblock87@gmail.com',
+    keywords=['Bond', 'JamesBond', 'James Bond', '007'],
+    url='https://github.com/andrew-block/jamesbond',
+    download_url='https://test.pypi.org/project/jamesbond/'
+)
