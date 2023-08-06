59e8de13882eb46c512d02c0723b9f81fc3d8bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:20 +0000

    Add package riftwrapper/1.2/riftwrapper-1.2.tar.gz

---
+++ packages/riftwrapper/riftwrapper-1.2.tar.gz/riftwrapper-1.2/setup.py
@@ -0,0 +1,30 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+  name = 'riftwrapper',
+  packages = find_packages(),
+  version = 'v1.2',
+  license='gpl-3.0',
+  description = 'Python Riot API wrapper',
+  author = 'verd1c',
+  author_email = 'kwstasfortmanik@gmail.com',
+  url = 'https://github.com/verd1c/Rift-Wrapper',
+  download_url = 'https://github.com/verd1c/Rift-Wrapper/archive/v1.2.tar.gz',
+  keywords = ['RiotAPI', 'Riot API wrapper', 'Python API'],
+  install_requires=[
+          'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
