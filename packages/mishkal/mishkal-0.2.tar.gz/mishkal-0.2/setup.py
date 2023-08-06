a69b5f07e8cc73e2d50b40b5ca9c63ed2b66e3b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:28 +0000

    Add package mishkal/0.2/mishkal-0.2.tar.gz

---
+++ packages/mishkal/mishkal-0.2.tar.gz/mishkal-0.2/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+# -*- coding=utf-8 -*-
+from setuptools import setup
+
+# to install type:
+# python setup.py install --root=/
+from io import open
+def readme():
+    with open('README.rst', encoding="utf8") as f:
+        return f.read()
+
+setup (name='mishkal', version='0.2',
+      description="Mishkal: Arabic text diacritization library for Python",
+      long_description = readme(),      
+
+      author='Taha Zerrouki',
+      author_email='taha.zerrouki@gmail.com',
+      url='http://mishkal.sourceforge.net/',
+      license='GPL',
+      package_dir={'mishkal': 'mishkal'},
+      packages=['mishkal'],
+      install_requires=[    "CodernityDB>=0.4.2",
+            "CodernityDB3>=0.6",
+            "libqutrub>=1.0",
+            "naftawayh>=0.2",
+            "pyarabic>=0.6.2",
+            "tashaphyne>=0.3.1",
+            "arramooz-pysqlite>=0.1",
+            "qalsadi>=0.2",
+            "mysam-tagmanager>=0.1",
+            "alyahmor>=0.1",
+            "asmai>=0.1",
+            "sylajone>=0.1",
+      ],         
+      include_package_data=True,
+      package_data = {
+        'mishkal': ['doc/*.*','doc/html/*', 'data/*.sqlite', 'data/*.sql'],
+        },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: End Users/Desktop',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ],
+    );
+
