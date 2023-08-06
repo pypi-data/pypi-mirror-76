8d46ca3ae49eaa133083dd06f2dec07f5e00ff29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:45 +0000

    Add package universal-parser-tool/1.4.1/universal-parser-tool-1.4.1.tar.gz

---
+++ packages/universal-parser-tool/universal-parser-tool-1.4.1.tar.gz/universal-parser-tool-1.4.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open("VERSION") as version_file:
+    version = version_file.read()
+
+setup(
+    name='universal-parser-tool',
+    version=version,
+    description='Useful tool to speedup testing codes in cp-programming',
+    license='GPL',
+    author='Parsa Alizadeh',
+    author_email="parsa.alizadeh1+upt@gmail.com",
+    url='https://github.com/ParsaAlizadeh/universal-parser-tool',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n',
+    packages=find_packages(include=['upt', 'upt.*']),
+    install_requires=[
+        'selenium',
+        'requests'
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': ['upt=upt:main']
+    }
+)
