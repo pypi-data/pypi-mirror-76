f34d88c45d56697141fd42af987c0cb193af5997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:51 +0000

    Add package jsonway/0.0.5/jsonway-0.0.5.tar.gz

---
+++ packages/jsonway/jsonway-0.0.5.tar.gz/jsonway-0.0.5/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jsonway",
+    version="0.0.5",
+    author="Vishwanath",
+    author_email="justvishu91@gmail.com",
+    description="complete tool for the JSON",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/umeshdhakar/sample-python-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
