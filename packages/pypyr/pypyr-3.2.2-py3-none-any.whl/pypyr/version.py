11936b8b454b1dfb9909e41b2b44eff558ab0a35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:30 +0000

    Add package pypyr/3.2.2/pypyr-3.2.2-py3-none-any.whl

---
+++ packages/pypyr/pypyr-3.2.2-py3-none-any.whl/pypyr/version.py
@@ -0,0 +1,10 @@
+"""Version information."""
+
+import platform
+
+__version__ = '3.2.2'
+
+
+def get_version():
+    return (f'pypyr {__version__} '
+            f'python {platform.python_version()}')
