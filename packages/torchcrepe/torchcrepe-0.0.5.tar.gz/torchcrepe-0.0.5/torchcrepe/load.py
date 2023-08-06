dc44c219f598793c11e074b24ad3437c26944fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:58 +0000

    Add package torchcrepe/0.0.5/torchcrepe-0.0.5.tar.gz

---
+++ packages/torchcrepe/torchcrepe-0.0.5.tar.gz/torchcrepe-0.0.5/torchcrepe/load.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+from scipy.io import wavfile
+
+
+def audio(filename):
+    """Load audio from disk"""
+    sample_rate, audio = wavfile.read(filename)
+
+    # Convert to float32
+    if audio.dtype == np.int16:
+        audio = audio.astype(np.float32) / np.iinfo(np.int16).max
+
+    # PyTorch is not compatible with non-writeable arrays, so we make a copy
+    return torch.tensor(np.copy(audio))[None], sample_rate
