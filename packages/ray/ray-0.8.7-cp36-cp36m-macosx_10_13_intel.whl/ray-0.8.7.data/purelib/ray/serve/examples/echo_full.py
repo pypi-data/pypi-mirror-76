069294bf5b64bea588a87447ed428759612389f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:26 +0000

    Add package ray/0.8.7/ray-0.8.7-cp36-cp36m-macosx_10_13_intel.whl

---
+++ packages/ray/ray-0.8.7-cp36-cp36m-macosx_10_13_intel.whl/ray-0.8.7.data/purelib/ray/serve/examples/echo_full.py
@@ -0,0 +1,56 @@
+import time
+
+import requests
+
+import ray
+import ray.serve as serve
+from ray.serve.metric import PrometheusExporter
+
+# initialize ray serve system.
+serve.init(metric_exporter=PrometheusExporter)
+
+
+# a backend can be a function or class.
+# it can be made to be invoked from web as well as python.
+def echo_v1(flask_request, response="hello from python!"):
+    if serve.context.web:
+        response = flask_request.url
+    return response
+
+
+serve.create_backend("echo:v1", echo_v1)
+
+# An endpoint is associated with an HTTP path and traffic to the endpoint
+# will be serviced by the echo:v1 backend.
+serve.create_endpoint("my_endpoint", backend="echo:v1", route="/echo")
+
+print(requests.get("http://127.0.0.1:8000/echo", timeout=0.5).text)
+# The service will be reachable from http
+
+print(ray.get(serve.get_handle("my_endpoint").remote(response="hello")))
+
+# as well as within the ray system.
+
+
+# We can also add a new backend and split the traffic.
+def echo_v2(flask_request):
+    # magic, only from web.
+    return "something new"
+
+
+serve.create_backend("echo:v2", echo_v2)
+
+# The two backend will now split the traffic 50%-50%.
+serve.set_traffic("my_endpoint", {"echo:v1": 0.5, "echo:v2": 0.5})
+
+# Observe requests are now split between two backends.
+for _ in range(10):
+    print(requests.get("http://127.0.0.1:8000/echo").text)
+    time.sleep(0.5)
+
+# You can also change number of replicas for each backend independently.
+serve.update_backend_config("echo:v1", {"num_replicas": 2})
+serve.update_backend_config("echo:v2", {"num_replicas": 2})
+
+# As well as retrieving relevant system metrics
+print(serve.stat().decode())
