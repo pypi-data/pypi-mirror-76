1ff886d06b4d5854bc674b7fc7f2702774ea1250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:48 +0000

    Add package azhurbfunniest/0.1/azhurbfunniest-0.1.tar.gz

---
+++ packages/azhurbfunniest/azhurbfunniest-0.1.tar.gz/azhurbfunniest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='azhurbfunniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/azhurb/funniest',
+      author='Alex Zhurbytskyi',
+      author_email='zhurbitsky@example.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
