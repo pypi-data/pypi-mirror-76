9fd812d6de3e30796761f6dd76db95a8f5b1765e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:30 +0000

    Add package gitbotpy/0.0.1/gitbotpy-0.0.1.tar.gz

---
+++ packages/gitbotpy/gitbotpy-0.0.1.tar.gz/gitbotpy-0.0.1/gitbotpy/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+class git:
+    def list_dir(path):
+        l = os.listdir(path)
+        l.remove('.git')
+        return l
+    def push_all_files(disk,path,message):
+        os.system(f'cmd /k "{disk}: && cd {path} && git pull && git add . && git commit -m "{message}" && git push origin master"')
+    def push_one_file(disk,path,filename,message):
+        os.system(f'cmd /k "{disk}: && cd {path} && git pull && git add {filename} && git commit -m "{message}" && git push origin master"')
+    def clone(disk,path,gitlink):
+        os.system(f'cmd /k "{disk}: && cd {path} && git clone {gitlink}"')
+    def remove_file(disk,path,filename,message):
+        os.system(f'cmd /k "{disk}: && cd {path} && git rm {filename} && git commit -m "{message}" && git push origin master"')
+    def remove_repo(disk,path,foldername,message):
+        os.system(f'cmd /k "{disk}: && cd {path} && git rm -r {foldername} && git commit -m "{message}" && git push origin master"')
+    def pull(disk,path):
+        os.system(f'cmd /k "{disk}: && cd {path} && git pull"')
