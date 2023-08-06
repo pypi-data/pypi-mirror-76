9ee160833924cedf9a9342ce1c41306249702757
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:20 +0000

    Add package play-checkers/1.0.2/play_checkers-1.0.2.tar.gz

---
+++ packages/play-checkers/play_checkers-1.0.2.tar.gz/play_checkers-1.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['play_checkers']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['baduk = play_checkers.__main__:main']}
+
+setup_kwargs = {
+    'name': 'play-checkers',
+    'version': '1.0.2',
+    'description': 'Python Checkers Game',
+    'long_description': None,
+    'author': 'Sarcoma',
+    'author_email': 'sarcoma@live.co.uk',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
