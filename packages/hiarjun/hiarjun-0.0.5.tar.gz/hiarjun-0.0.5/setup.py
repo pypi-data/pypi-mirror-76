32bbdf512680a6310e7d97cf8b9fed1826325453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:03 +0000

    Add package hiarjun/0.0.5/hiarjun-0.0.5.tar.gz

---
+++ packages/hiarjun/hiarjun-0.0.5.tar.gz/hiarjun-0.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hiarjun", # Replace with your own username
+    version="0.0.5",
+    author="Arjun Dandagi",
+    author_email="dandagi.arjun95@gmail.com",
+    description="A package that prints Hi n times",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arjundandagi/hiarjun",
+    packages=setuptools.find_packages(),
+    scripts = ["hiarjun/hiarjun.py"],
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
