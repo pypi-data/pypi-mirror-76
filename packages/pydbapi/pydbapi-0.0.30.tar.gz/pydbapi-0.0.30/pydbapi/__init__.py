a92c71724817843b56f2004c9fa6a6bd007edb99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:39 +0000

    Add package pydbapi/0.0.30/pydbapi-0.0.30.tar.gz

---
+++ packages/pydbapi/pydbapi-0.0.30.tar.gz/pydbapi-0.0.30/pydbapi/__init__.py
@@ -0,0 +1,12 @@
+# @Author: chunyang.xu
+# @Email:  398745129@qq.com
+# @Date:   2020-06-04 10:58:54
+# @Last Modified time: 2020-06-17 21:08:15
+# @github: https://github.com/longfengpili
+
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+from pydbapi.api import SqliteDB, RedshiftDB, MysqlDB
+
+__all__ = ['SqliteDB', 'RedshiftDB', 'MysqlDB']
