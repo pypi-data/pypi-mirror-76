db7cd82303b90d6d55db4e9e6651fa70b210be58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:19 +0000

    Add package hl7-transform/0.1.5/hl7_transform-0.1.5.tar.gz

---
+++ packages/hl7-transform/hl7_transform-0.1.5.tar.gz/hl7_transform-0.1.5/hl7_transform/__main__.py
@@ -0,0 +1,32 @@
+"""
+This package allows transformation of HL7 messages using field mapping.
+
+\author Pavlo Dyban (Doctolib GmbH)
+\date   26-May-2020
+"""
+
+import argparse
+from .cli import main_cli
+
+
+def main():
+    parser = argparse.ArgumentParser(
+            description="""Transform HL7 messages using a mapping scheme.""")
+    parser.add_argument('mappingfile',
+            help='path to file containing field mapping, e.g. mapping.json',
+            type=str)
+    parser.add_argument('-m', '--message',
+            help="path to the HL7 message file, e.g. siu_s12_in.hl7")
+    parser.add_argument('-o', '--out',
+            help="path to the output HL7 message file, e.g. siu_s12_out.hl7")
+    parser.add_argument('--type',
+            help='mapping file type, can be json (default) or csv',
+            default='json',
+            type=str)
+
+    args = parser.parse_args()
+    main_cli(args)
+
+
+if __name__ == '__main__':
+    main()
