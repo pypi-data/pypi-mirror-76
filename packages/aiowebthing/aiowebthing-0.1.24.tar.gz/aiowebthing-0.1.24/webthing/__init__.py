7df7dc32caf566ac2f7700ae331f8a08ff8bc5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:36 +0000

    Add package aiowebthing/0.1.24/aiowebthing-0.1.24.tar.gz

---
+++ packages/aiowebthing/aiowebthing-0.1.24.tar.gz/aiowebthing-0.1.24/webthing/__init__.py
@@ -0,0 +1,13 @@
+"""This module provides a high-level interface for creating a Web Thing."""
+
+# flake8: noqa
+from .action import Action
+from .event import Event
+from .property import Property
+from .server import WebThingServer
+from .containers import SingleThing, MultipleThings
+from .thing import Thing
+from .value import Value
+from .utils import background_thread_loop
+from .auth import requires
+from .middlewares import JWTAuthenticationBackend, JWTWebSocketAuthenticationBackend
---
+++ packages/aiowebthing/aiowebthing-0.1.24.tar.gz/aiowebthing-0.1.24/webthing/containers.py
@@ -0,0 +1,70 @@
+from .event import ThingPairedEvent, ThingRemovedEvent
+
+
+class SingleThing:
+    """A container for a single thing."""
+
+    def __init__(self, thing):
+        """
+        Initialize the container.
+        thing -- the thing to store
+        """
+        self.thing = thing
+
+    async def get_thing(self, _=None):
+        """Get the thing at the given index."""
+        return self.thing
+
+    async def get_things(self):
+        """Get the list of things."""
+        return [self.thing]
+
+    async def get_name(self):
+        """Get the mDNS server name."""
+        return self.thing.title
+
+
+class MultipleThings:
+    """A container for multiple things."""
+
+    def __init__(self, things, name):
+        """
+        Initialize the container.
+        things -- the things to store
+        name -- the mDNS server name
+        """
+        self.things = things
+        self.name = name
+
+    async def get_thing(self, idx):
+        """
+        Get the thing at the given index.
+        idx -- the index
+        """
+        return self.things.get(idx, None)
+
+    async def get_things(self):
+        """Get the list of things."""
+        return self.things.items()
+
+    async def get_name(self):
+        """Get the mDNS server name."""
+        return self.name
+
+    async def add_thing(self, thing):
+        self.things.update({thing.id: thing})
+        server = self.things.get('urn:webthing:server')
+
+        await server.add_event(ThingPairedEvent({
+            '@type': list(thing._type),
+            'id': thing.id,
+            'title': thing.title
+        }))
+
+    async def remove_thing(self, thing_id):
+        del self.things[thing_id]
+        server = self.things.get('urn:webthing:server')
+
+        await server.add_event(ThingRemovedEvent({
+            'id': thing_id,
+        }))
