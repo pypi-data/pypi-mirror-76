061a5e8b048946359f37e5fbd40a51ef90f7c156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:25 +0000

    Add package probability/0.0.140/probability-0.0.140.tar.gz

---
+++ packages/probability/probability-0.0.140.tar.gz/probability-0.0.140/probability/custom_types.py
@@ -0,0 +1,18 @@
+from numpy import ndarray
+from typing import Union, Iterable, Sized
+
+from pandas import Series
+
+from probability.distributions.mixins.rv_continuous_1d_mixin import RVContinuous1dMixin
+from probability.distributions.mixins.rv_discrete_1d_mixin import RVDiscrete1dMixin
+
+
+RVMixin = Union[RVContinuous1dMixin, RVDiscrete1dMixin]
+
+Array1d = Union[Iterable[int], Iterable[float], ndarray]
+FloatArray1d = Union[Iterable[float], ndarray, Series, Sized]
+FloatOrFloatArray1d = Union[float, Iterable[float], ndarray]
+
+Array2d = Union[Iterable[Iterable[int]], Iterable[Iterable[float]], ndarray]
+FloatArray2d = Union[Iterable[Iterable[float]], ndarray]
+FloatOrFloatArray2d = Union[float, Iterable[Iterable[float]], ndarray]
