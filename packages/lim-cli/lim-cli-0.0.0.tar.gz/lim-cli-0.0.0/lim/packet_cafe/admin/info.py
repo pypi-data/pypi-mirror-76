9c8bc18f8d34ebb76a0f16735fffc18c94de3ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:16 +0000

    Add package lim-cli/0.0.0/lim-cli-0.0.0.tar.gz

---
+++ packages/lim-cli/lim-cli-0.0.0.tar.gz/lim-cli-0.0.0/lim/packet_cafe/admin/info.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import textwrap
+
+from cliff.show import ShowOne
+from lim.packet_cafe import add_packet_cafe_global_options
+from lim.packet_cafe import get_packet_cafe
+
+logger = logging.getLogger(__name__)
+
+
+class AdminInfo(ShowOne):
+    """Return basic information about the packet-cafe service."""
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.formatter_class = argparse.RawDescriptionHelpFormatter
+        parser.epilog = textwrap.dedent("""
+            Return basic information about the packet-cafe service.
+
+            Use this command to determine the last session ID and last
+            request ID, if available.
+
+            .. code-block:: console
+
+                $ lim cafe admin info
+                +--------------+--------------------------------------+
+                | Field        | Value                                |
+                +--------------+--------------------------------------+
+                | url          | http://127.0.0.1:5001/v1/info        |
+                | last_session | 9a949fe6-6520-437f-89ec-e7af6925b1e0 |
+                | last_request | 81778bb8a9b946ba82659732baacdb44     |
+                | version      | v0.1.0                               |
+                | hostname     | 5df1f9a14bff                         |
+                +--------------+--------------------------------------+
+
+            ..
+
+            To programmatically obtain the last session ID for use in other
+            scripts, do the following:
+
+            .. code-block:: console
+
+                $ lim cafe admin info -f shell
+                url="http://127.0.0.1:5001/v1/info"
+                last_session="9a949fe6-6520-437f-89ec-e7af6925b1e0"
+                last_request="81778bb8a9b946ba82659732baacdb44"
+                version="v0.1.0"
+                hostname="5df1f9a14bff"
+
+            ..
+
+            See https://iqtlabs.gitbook.io/packet-cafe/design/api#v-1-info
+            """)
+        return add_packet_cafe_global_options(parser)
+
+    def take_action(self, parsed_args):
+        logger.debug('[+] showing info (admin)')
+        packet_cafe = get_packet_cafe(self.app, parsed_args)
+        response = packet_cafe.get_admin_info()
+        columns = ['url', 'last_session', 'last_request']
+        data = [
+            packet_cafe.get_admin_url(),
+            packet_cafe.get_last_session_id(),
+            packet_cafe.get_last_request_id()
+        ]
+        for k, v in response.items():
+            columns.append(k)
+            data.append((v))
+        return (columns, data)
+
+
+# vim: set ts=4 sw=4 tw=0 et :
---
+++ packages/lim-cli/lim-cli-0.0.0.tar.gz/lim-cli-0.0.0/lim/packet_cafe/api/info.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import logging
+import textwrap
+
+from cliff.show import ShowOne
+from lim.packet_cafe import add_packet_cafe_global_options
+from lim.packet_cafe import get_packet_cafe
+
+logger = logging.getLogger(__name__)
+
+
+class ApiInfo(ShowOne):
+    """Return basic information about the packet-cafe service."""
+
+    def get_parser(self, prog_name):
+        parser = super().get_parser(prog_name)
+        parser.formatter_class = argparse.RawDescriptionHelpFormatter
+        parser.epilog = textwrap.dedent("""
+            Return basic information about the packet-cafe service.
+
+            .. code-block:: console
+
+                $ lim cafe info
+                +----------+---------------------------------+
+                | Field    | Value                           |
+                +----------+---------------------------------+
+                | url      | http://127.0.0.1:80/api/v1/info |
+                | version  | v0.1.0                          |
+                | hostname | bf1456253115                    |
+                +----------+---------------------------------+
+
+            ..
+
+            Note that the last session ID and last request ID are found in the output
+            of ``lim cafe admin info`` (not ``lim cafe info``).
+
+            See https://iqtlabs.gitbook.io/packet-cafe/design/api#api-v-1-info
+            """)  # noqa
+        return add_packet_cafe_global_options(parser)
+
+    def take_action(self, parsed_args):
+        logger.debug('[+] showing info (api)')
+        packet_cafe = get_packet_cafe(self.app, parsed_args)
+        response = packet_cafe.get_api_info()
+        columns = ['url']
+        data = [(packet_cafe.get_api_url())]
+        for k, v in response.items():
+            columns.append(k)
+            data.append((v))
+        return (columns, data)
+
+
+# vim: set ts=4 sw=4 tw=0 et :
