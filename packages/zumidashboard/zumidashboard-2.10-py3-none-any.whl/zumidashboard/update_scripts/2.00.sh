665afb5a7a2bee01cab4059e17efc934a68a41fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:04 +0000

    Add package zumidashboard/2.10/zumidashboard-2.10-py3-none-any.whl

---
+++ packages/zumidashboard/zumidashboard-2.10-py3-none-any.whl/zumidashboard/update_scripts/2.00.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo rm /usr/local/lib/python3.5/dist-packages/zumidashboard/dashboard/hostname.json
+sudo ln -s /etc/hostname /usr/local/lib/python3.5/dist-packages/zumidashboard/dashboard/hostname.json
+done
