6aa47b4ddd769c823b5635d3b47a261aa1dfdd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:30 +0000

    Add package fake-bpy-module-2.82/20200812/fake-bpy-module-2.82-20200812.tar.gz

---
+++ packages/fake-bpy-module-2.82/fake-bpy-module-2.82-20200812.tar.gz/fake-bpy-module-2.82-20200812/bl_keymap_utils/__init__.py
@@ -0,0 +1,6 @@
+import sys
+import typing
+from . import io
+from . import platform_helpers
+from . import keymap_from_toolbar
+from . import keymap_hierarchy
---
+++ packages/fake-bpy-module-2.82/fake-bpy-module-2.82-20200812.tar.gz/fake-bpy-module-2.82-20200812/bl_operators/__init__.py
@@ -0,0 +1,44 @@
+import sys
+import typing
+from . import freestyle
+from . import mesh
+from . import anim
+from . import console
+from . import object
+from . import object_align
+from . import constraint
+from . import userpref
+from . import rigidbody
+from . import wm
+from . import uvcalc_lightmap
+from . import uvcalc_smart_project
+from . import node
+from . import clip
+from . import add_mesh_torus
+from . import image
+from . import bmesh
+from . import presets
+from . import object_quick_effects
+from . import vertexpaint_dirt
+from . import sequencer
+from . import file
+from . import screen_play_rendered_anim
+from . import uvcalc_follow_active
+from . import object_randomize_transform
+from . import view3d
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
