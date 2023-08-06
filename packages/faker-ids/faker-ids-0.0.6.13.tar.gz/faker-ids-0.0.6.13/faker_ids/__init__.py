cb2435bbad15f54376c77db7a0e1a2e6151a2a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:28 +0000

    Add package faker-ids/0.0.6.13/faker-ids-0.0.6.13.tar.gz

---
+++ packages/faker-ids/faker-ids-0.0.6.13.tar.gz/faker-ids-0.0.6.13/faker_ids/__init__.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+
+
+import os
+this_dir, this_filename = os.path.split(__file__)
+DATA_PATH = os.path.join(this_dir,"static","id.csv")
+csv = pd.read_csv(DATA_PATH)
+
+# csv = pd.read_csv('id.csv')
+csv = csv.values.tolist()
+
+
+def get_ids(n):
+    id = []
+    for i in range(n):
+        temp = {}
+        temp['firstName'] = csv[i][0]
+        temp['lastName'] = csv[i][1]
+        temp['emailId'] = csv[i][2]
+        temp['mobileNumber'] = csv[i][3]
+        id.append(temp)
+    return json.dumps(id, indent=4, sort_keys=True)
