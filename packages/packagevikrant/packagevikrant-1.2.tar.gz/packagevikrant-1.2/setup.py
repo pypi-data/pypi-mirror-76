5604773f9a187b41cacc935f6a4a62855848e0e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:20:02 +0000

    Add package packagevikrant/1.2/packagevikrant-1.2.tar.gz

---
+++ packages/packagevikrant/packagevikrant-1.2.tar.gz/packagevikrant-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('readme.md') as f:
+        readme = f.read()
+    return readme
+
+
+setup( name="packagevikrant",
+     version="1.2",
+     description="This code is written by Vikrant Singh.",
+     long_desription=readme(),
+     long_description_content_type="text/markdown",
+     author="Vikrant Singh",
+     packages=['packagevikrant'],
+     install_requires=['pyttsx3'],
+     license="MIT",
+     include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "packagevikrant=packagevikrant.init:speak",
+        ]
+    },
+)
