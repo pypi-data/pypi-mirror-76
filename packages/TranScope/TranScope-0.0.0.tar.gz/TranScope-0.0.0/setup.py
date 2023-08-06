2b435b5c8a47238197e528bf8dc4c741fd773ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:37 +0000

    Add package TranScope/0.0.0/TranScope-0.0.0.tar.gz

---
+++ packages/TranScope/TranScope-0.0.0.tar.gz/TranScope-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='TranScope',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/pycroscopy',
+    author='Suhas Somnath',
+    author_email='pycroscopy@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
