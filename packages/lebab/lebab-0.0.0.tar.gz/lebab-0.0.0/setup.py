5efa6995e534628cb6b9a7dd351fc344694e46b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:35 +0000

    Add package lebab/0.0.0/lebab-0.0.0.tar.gz

---
+++ packages/lebab/lebab-0.0.0.tar.gz/lebab-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lebab',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/pycroscopy',
+    author='me',
+    author_email='pycroscopy@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
