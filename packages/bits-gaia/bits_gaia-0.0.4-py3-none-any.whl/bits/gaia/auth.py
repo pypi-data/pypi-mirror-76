dd26e5904e280189066e657c3a1e3314d1f71e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package bits-gaia/0.0.4/bits_gaia-0.0.4-py3-none-any.whl

---
+++ packages/bits-gaia/bits_gaia-0.0.4-py3-none-any.whl/bits/gaia/auth.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Gaia Auth class file."""
+
+
+class Auth:
+    """Gaia Auth class."""
+
+    def __init__(self, gaia):
+        """Initialize a class instance."""
+        self.gaia = gaia
+
+    def ad_ldap(self):
+        """Return an authorized AD instance."""
+        from bits.ldap import AD
+        settings = self.gaia.get_settings("ad_ldap")
+        return AD(
+            uri=settings.get("uri"),
+            bind_dn=settings.get("bind_dn"),
+            bind_pw=self.gaia.get_secret(settings.get("bind_pw_name")),
+            base_dn=settings.get("base_dn"),
+            server_type="ad",
+            verbose=self.gaia.verbose,
+        )
+
+    def bitsdb_mongo(self):
+        """Return an authorized Mongo instances for BITSdb."""
+        from bits.mongo import Mongo
+        settings = self.gaia.get_settings("bitsdb_mongo")
+        db = settings.get("db")
+        host = settings.get("host")
+        username = settings.get("username")
+        password = self.gaia.get_secret(settings.get("password_name"))
+        return Mongo(
+            uri=f"mongodb://{username}:{password}@{host}/{db}",
+            db=db,
+        )
