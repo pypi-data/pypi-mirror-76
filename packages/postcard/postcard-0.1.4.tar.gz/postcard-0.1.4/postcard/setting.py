d73bdff8fdc5e6959cbe251f24502f2410371114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:53 +0000

    Add package postcard/0.1.4/postcard-0.1.4.tar.gz

---
+++ packages/postcard/postcard-0.1.4.tar.gz/postcard-0.1.4/postcard/setting.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# @author: leesoar
+
+"""postcard"""
+
+MAIL_ADDR = {
+    "tom": "pop.tom.com",
+    "gmail": "pop.gmail.com",
+    "yahoo": "pop.mail.yahoo.com",
+    "china": "pop.china.com",
+    "21cn": "pop.21cn.com",
+    "163": "pop.163.com",
+    "126": "pop.126.com",
+    "qq": "pop.qq.com",
+    "sina": "pop3.sina.com.cn",
+}
