a2c592e8e8aca7f063488abbf0455d5005d08fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:40 +0000

    Add package api-bridge/0.0.2/api_bridge-0.0.2.tar.gz

---
+++ packages/api-bridge/api_bridge-0.0.2.tar.gz/api_bridge-0.0.2/api_bridge/container.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Robin 'r0w' Weiland"
+__date__ = "2020-08-15"
+__version__ = "0.0.2"
+
+__all__ = ('Container',)
+
+from typing import Optional, Dict, Any
+
+
+class Container:
+    value: Optional[Dict[str, Any]]
+
+    def __init__(self): self.value = None
+
+    def __call__(self, value) -> None:
+        self.value = value
+
+    def __str__(self) -> str: return str(self.value)
+
+    def __repr__(self) -> str: return str(self)
+
+
+if __name__ == '__main__': pass
