ba00b078d38c84c06434682e02be7bd98e528824
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:20 +0000

    Add package NFSyndication/0.2.8/NFSyndication-0.2.8.tar.gz

---
+++ packages/NFSyndication/NFSyndication-0.2.8.tar.gz/NFSyndication-0.2.8/NFSyndication/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python 
+import os
+import sys
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def init():
+  #os.popen(f'{dir_path}/main.py')
+  #return os.popen(f'{dir_path}/main.py').read()
+  run = lambda filename : exec(open(filename).read(), globals())
+  return run(f'{dir_path}/main.py')
