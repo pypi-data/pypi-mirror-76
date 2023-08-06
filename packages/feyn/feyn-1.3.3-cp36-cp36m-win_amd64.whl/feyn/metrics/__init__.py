22eae11d49afd328809473d5ab0f2513bd6b33ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:18 +0000

    Add package feyn/1.3.3/feyn-1.3.3-cp36-cp36m-win_amd64.whl

---
+++ packages/feyn/feyn-1.3.3-cp36-cp36m-win_amd64.whl/feyn/metrics/__init__.py
@@ -0,0 +1,13 @@
+"""
+This module contains functions to help evaluate and compare feyn graphs and other models.
+"""
+
+from ._metrics import accuracy_score, r2_score, confusion_matrix, segmented_loss
+
+__all__ = [
+    'accuracy_score',
+    'r2_score',
+    'confusion_matrix',
+    'segmented_loss',
+]
+
