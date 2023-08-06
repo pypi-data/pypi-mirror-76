9b6454a21efdc6350f766c8bd97a400ca4847a03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:23 +0000

    Add package fefu-admission/1.2/fefu_admission-1.2.tar.gz

---
+++ packages/fefu-admission/fefu_admission-1.2.tar.gz/fefu_admission-1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='fefu_admission',
+    version='1.2',
+    packages=find_packages(),
+    long_description_content_type="text/markdown",
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    entry_points={
+        'console_scripts':
+            ['fefu_admission = fefu_admission.cli:cli']
+    },
+    install_requires=[
+        "click~=7.1.2",
+        "requests~=2.24.0",
+        "beautifulsoup4~=4.9.1",
+        "colorama~=0.4.3",
+        "tabulate~=0.8.7",
+        "setuptools~=49.2.0",
+    ],
+)
