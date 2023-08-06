a98787f54df0b06692798a21bbe86b77c6fa6ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:36 +0000

    Add package randopass/0.1.2/randopass-0.1.2-py2.py3-none-any.whl

---
+++ packages/randopass/randopass-0.1.2-py2.py3-none-any.whl/randopass/__init__.py
@@ -0,0 +1,8 @@
+'''
+randopass: strong random English-word passphrases
+'''
+
+from randopass.dictionaries import * 
+from randopass.randopass import getPass, getVersion
+
+__version__ = getVersion()
