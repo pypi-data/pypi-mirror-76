6409e67a36730cb80db22c48c1f35c5a008a208a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:47 +0000

    Add package vscale-distribution/0.1/vscale_distribution-0.1.tar.gz

---
+++ packages/vscale-distribution/vscale_distribution-0.1.tar.gz/vscale_distribution-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='vscale_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['vscale_distribution'],
+      zip_safe=False)
