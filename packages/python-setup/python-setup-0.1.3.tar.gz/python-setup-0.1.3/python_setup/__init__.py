d50a6e291adcc8634efdb9c3fcadc70bda479610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:49 +0000

    Add package python-setup/0.1.3/python-setup-0.1.3.tar.gz

---
+++ packages/python-setup/python-setup-0.1.3.tar.gz/python-setup-0.1.3/python_setup/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2020/7/25 下午7:33
+@file: __init__.py.py
+@author: zj
+@description: 
+"""
+
+# This line will be programatically read/write by setup.py.
+# Leave them at the bottom of this file and don't touch them.
+__version__ = "0.1.3"
