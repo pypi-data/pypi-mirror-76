1a4f97ca5d5585edd5dac714f4029c87d67c6f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:19 +0000

    Add package dc-stat-think/1.1.1/dc_stat_think-1.1.1-py2.py3-none-any.whl

---
+++ packages/dc-stat-think/dc_stat_think-1.1.1-py2.py3-none-any.whl/dc_stat_think/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Data Camp Statistical Thinking utilities."""
+
+__author__ = """Justin Bois"""
+__email__ = "bois@caltech.edu"
+__version__ = "1.1.1"
+
+
+from .dc_stat_think import *
