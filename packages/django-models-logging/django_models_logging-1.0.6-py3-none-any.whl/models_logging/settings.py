46c384d29e834320f4465b8f64bc6be3e62c1194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:24 +0000

    Add package django-models-logging/1.0.6/django_models_logging-1.0.6-py3-none-any.whl

---
+++ packages/django-models-logging/django_models_logging-1.0.6-py3-none-any.whl/models_logging/settings.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.utils.module_loading import import_string
+
+
+LOGGING_USER_MODEL = getattr(settings, 'LOGGING_USER_MODEL', None) or getattr(settings, 'AUTH_USER_MODEL', None)
+
+MODELS_FOR_LOGGING = getattr(settings, 'LOGGING_MODELS', None)
+MODELS_FOR_EXCLUDE = getattr(settings, 'LOGGING_EXCLUDE', [])
+
+
+REVERT_IS_ALLOWED = getattr(settings, 'LOGGING_REVERT_IS_ALLOWED', True)
+CAN_DELETE_REVISION = getattr(settings, 'LOGGING_CAN_DELETE_REVISION', False)
+CAN_DELETE_CHANGES = getattr(settings, 'LOGGING_CAN_DELETE_CHANGES', False)
+CAN_CHANGE_CHANGES = getattr(settings, 'LOGGING_CAN_CHANGE_CHANGES', False)
+CHANGES_REVISION_LIMIT = getattr(settings, 'LOGGING_CHANGES_REVISION_LIMIT', 100)
+MERGE_CHANGES = getattr(settings, 'LOGGING_MERGE_CHANGES', True)
+
+ADDED = 'added'
+CHANGED = 'changed'
+DELETED = 'deleted'
+
+MIDDLEWARES = settings.MIDDLEWARE
+
+# TODO: Is not completed feature, do not use it!
+#  It will prevent error in database if User is not in the same database (because of ForeignKey)
+LOGGING_DATABASE = getattr(settings, 'LOGGING_DATABASE', 'default')
+
+USE_POSTGRES = False
+if 'postgresql' in settings.DATABASES[LOGGING_DATABASE]['ENGINE']:
+    USE_POSTGRES = True
+
+JSON_ENCODER_PATH = getattr(settings, 'LOGGING_JSON_ENCODER', 'models_logging.utils.ExtendedEncoder')
+JSON_ENCODER = import_string(JSON_ENCODER_PATH)
---
+++ packages/django-models-logging/django_models_logging-1.0.6-py3-none-any.whl/models_logging/signals.py
@@ -0,0 +1,52 @@
+import json
+
+from django.utils.encoding import force_text
+from django.contrib.contenttypes.models import ContentType
+
+from . import _local
+from .utils import get_changed_data, model_to_dict
+from .settings import ADDED, CHANGED, DELETED, MERGE_CHANGES, MIDDLEWARES, LOGGING_DATABASE
+from .models import Change
+from .settings import USE_POSTGRES, JSON_ENCODER
+
+
+def init_model_attrs(sender, instance, **kwargs):
+    if not _local.ignore(sender, instance):
+        model_dict = model_to_dict(instance)
+        # for rest_framework
+        if not instance.id:
+            model_dict = {k: None for k, v in model_dict.items()}
+
+        instance.__attrs = model_dict
+
+
+def save_model(sender, instance, using, **kwargs):
+    if not _local.ignore(sender, instance):
+        diffs = get_changed_data(instance)
+        if diffs:
+            action = ADDED if kwargs.get('created') else CHANGED
+            _create_changes(instance, using, action)
+
+
+def delete_model(sender, instance, using, **kwargs):
+    if not _local.ignore(sender, instance):
+        _create_changes(instance, using, DELETED)
+
+
+def _create_changes(object, using, action):
+    changed_data = get_changed_data(object, action)
+    if not USE_POSTGRES:
+        changed_data = json.dumps(changed_data, cls=JSON_ENCODER)
+
+    user_id = _local.user.pk if _local.user and _local.user.is_authenticated else None
+    content_type_id = ContentType.objects.get_for_model(object._meta.model).pk
+    data = {'db': using, 'object_repr': force_text(object), 'action': action, 'user_id': user_id,
+            'changed_data': changed_data, 'object_id': object.pk, 'content_type_id': content_type_id}
+    if MERGE_CHANGES and 'models_logging.middleware.LoggingStackMiddleware' in MIDDLEWARES:
+        key = (object.pk, content_type_id)
+        old_action = _local.stack_changes.get(key, {}).get('action')
+        if old_action == ADDED:
+            data['action'] = ADDED
+        _local.stack_changes[key] = data
+    else:
+        Change.objects.using(LOGGING_DATABASE).create(**data)
