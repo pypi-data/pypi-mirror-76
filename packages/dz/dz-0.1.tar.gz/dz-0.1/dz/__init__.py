3578cc1a415e51fec3a71caee6ad9212f791d6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:52 +0000

    Add package dz/0.1/dz-0.1.tar.gz

---
+++ packages/dz/dz-0.1.tar.gz/dz-0.1/dz/__init__.py
@@ -0,0 +1,1 @@
+from .dz import DeployZone, main
---
+++ packages/dz/dz-0.1.tar.gz/dz-0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name             = 'dz',
+    version          = '0.1',
+    description      = 'deploy zone',
+    author           = 'Sebastian Wiedenroth',
+    author_email     = 'sw@core.io',
+    url              = 'https://github.com/wiedi/dz',
+    packages=['dz'],
+    entry_points={
+        'console_scripts': [
+            'dz = dz.__main__:main'
+        ]
+    },
+    
+    install_requires = [
+        "argcomplete  == 1.12.0",
+        "humanize     == 2.5.0",
+        "tabulate     == 0.8.7",
+        "tqdm         == 4.48.2",
+    ],
+)
