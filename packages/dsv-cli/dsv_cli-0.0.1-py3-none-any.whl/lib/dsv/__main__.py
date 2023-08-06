270dd943716321d55c82d1bc5919e9919e6d8b63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:21 +0000

    Add package dsv-cli/0.0.1/dsv_cli-0.0.1-py3-none-any.whl

---
+++ packages/dsv-cli/dsv_cli-0.0.1-py3-none-any.whl/lib/dsv/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import fire
+
+from lib.dsv.commands.dsv_command import DSVCommand
+
+if __name__ == '__main__':
+    fire.Fire(DSVCommand)
---
+++ packages/dsv-cli/dsv_cli-0.0.1-py3-none-any.whl/lib/dsv/dsv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import fire
+
+from commands.dsv_command import DSVCommand
+
+if __name__ == '__main__':
+    fire.Fire(DSVCommand)
