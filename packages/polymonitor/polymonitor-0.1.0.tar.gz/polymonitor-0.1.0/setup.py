e75dbfcd248b68d03da75a8b8143cb782e2e1bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:16 +0000

    Add package polymonitor/0.1.0/polymonitor-0.1.0.tar.gz

---
+++ packages/polymonitor/polymonitor-0.1.0.tar.gz/polymonitor-0.1.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['polymonitor']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['argparse>=1.4.0,<2.0.0',
+ 'requests>=2.24.0,<3.0.0',
+ 'termcolor>=1.1.0,<2.0.0',
+ 'validators>=0.17.1,<0.18.0']
+
+setup_kwargs = {
+    'name': 'polymonitor',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Bee',
+    'author_email': 'bellis8099@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
