7800fb9f3a418abe230fed538c14d8abc8987dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:12 +0000

    Add package tejask/0.0.1/tejask-0.0.1-py3-none-any.whl

---
+++ packages/tejask/tejask-0.0.1-py3-none-any.whl/tejask/__init__.py
@@ -0,0 +1,31 @@
+def ascend(a):
+    a.sort()
+    return a
+
+
+def add(a, b):
+    c = a + b
+    return c
+
+
+def sub(a, b):
+    c = a - b
+    return c
+
+
+def mult(a, b):
+    c = a * b
+    return c
+
+
+def div(a, b):
+    c = a / b
+    return c
+
+
+def descend(a):
+    a.sort(reverse=True)
+    return a
+
+def rev(a):
+    return a[::-1]
