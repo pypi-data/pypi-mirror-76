4754db8217eb6f8cd859ee1f939c1ca6f58391b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:30 +0000

    Add package testwizard.commands-camera/3.4.2/testwizard.commands-camera-3.4.2.tar.gz

---
+++ packages/testwizard.commands-camera/testwizard.commands-camera-3.4.2.tar.gz/testwizard.commands-camera-3.4.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="testwizard.commands-camera",
+    version="3.4.2",
+    author="Eurofins Digital Testing - Belgium",
+    author_email="support-be@eurofins.com",
+    description="Testwizard camera commands",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['testwizard.commands_camera'],
+    install_requires=[
+        'testwizard.commands-core==3.4.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.3",
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+
+
+
