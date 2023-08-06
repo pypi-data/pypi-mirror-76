159f4af66365615323bcfc175b979c6cf09aa36d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:17 +0000

    Add package polymonitor/0.1.1/polymonitor-0.1.1.tar.gz

---
+++ packages/polymonitor/polymonitor-0.1.1.tar.gz/polymonitor-0.1.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['polymonitor']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['argparse>=1.4.0,<2.0.0',
+ 'requests>=2.24.0,<3.0.0',
+ 'termcolor>=1.1.0,<2.0.0',
+ 'validators>=0.17.1,<0.18.0']
+
+entry_points = \
+{'console_scripts': ['polymonitor = polymonitor.polymonitor:main']}
+
+setup_kwargs = {
+    'name': 'polymonitor',
+    'version': '0.1.1',
+    'description': 'Polymonitor can be used as a stand alone CLI tool to quickly check the status of a URL or as a plugin for polybar.',
+    'long_description': '# Polymonitor\n\nPolymonitor can be used as a stand alone CLI tool to quickly check the status of a URL or as a plugin for [polybar](https://github.com/polybar/polybar).\n\n## Installation\n\nUse the package manager [pip](https://pip.pypa.io/en/stable/) to install polymonitor.\n\n```bash\npip install polymonitor\n```\n\n## Usage\n```bash\npolymonitor --help \nusage: Displays site status for polybar. [-h] [-s] [-c] [-u URLS [URLS ...]]\n\noptional arguments:\n  -h, --help            show this help message and exit\n  -s, --symbolic        Displays the results as symbols\n  -c, --compact         Reduces the results into a more compact package\n  -u URLS [URLS ...], --urls URLS [URLS ...]\n                        Pass in URLs to monitor\n```\n\n## Contributing\nPull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.\n\nPlease make sure to update tests as appropriate.\n\n## License\n[MIT](https://choosealicense.com/licenses/mit/)\n',
+    'author': 'Bee',
+    'author_email': 'bellis8099@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/hegelocampus/polymonitor',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
