23092580a55e22d904a0b86c000d0e9959dd6e33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:57 +0000

    Add package asif-alam-distributions/0.1/asif_alam_distributions-0.1.tar.gz

---
+++ packages/asif-alam-distributions/asif_alam_distributions-0.1.tar.gz/asif_alam_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='asif_alam_distributions',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      author = 'Asif Alam',
+      packages=['asif_alam_distributions'],
+      zip_safe=False)
