c475c4b830fb519de3399060500948dd7681ead4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:24 +0000

    Add package quickstartup/0.19.0/quickstartup-0.19.0-py2.py3-none-any.whl

---
+++ packages/quickstartup/quickstartup-0.19.0-py2.py3-none-any.whl/quickstartup/context_processors.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+
+from quickstartup.settings_utils import get_configuration
+
+
+# noinspection PyUnusedLocal
+def project_infos(request):
+    return {
+        "PROJECT_NAME": get_configuration("QS_PROJECT_NAME"),
+        "PROJECT_DOMAIN": get_configuration("QS_PROJECT_DOMAIN"),
+        "PROJECT_CONTACT": get_configuration("QS_PROJECT_CONTACT"),
+    }
+
+
+# noinspection PyUnusedLocal
+def project_settings(request):
+    return {"PROJECT_SETTINGS": settings}
