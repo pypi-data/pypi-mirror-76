24b8051e48778cc0d2591025ec4c8d12725b6265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:10 +0000

    Add package django-evolution/2.0/django_evolution-2.0-py3.6.egg

---
+++ packages/django-evolution/django_evolution-2.0-py3.6.egg/django_evolution/compat/patches/__init__.py
@@ -0,0 +1,25 @@
+"""Compatibility patchess for Python and Django versions."""
+
+from __future__ import unicode_literals
+
+from django_evolution.compat.patches import (
+    django2_0_quote_unique_index_name,
+    sqlite_legacy_alter_table)
+
+
+#: List of patches that can be applied.
+patches = [
+    django2_0_quote_unique_index_name,
+    sqlite_legacy_alter_table,
+]
+
+
+def apply_patches():
+    """Apply any necessary patches.
+
+    This will check which patches are required, applying them to the
+    runtime environment.
+    """
+    for patch in patches:
+        if patch.needs_patch():
+            patch.apply_patch()
---
+++ packages/django-evolution/django_evolution-2.0-py3.6.egg/django_evolution/tests/evolutions_app/evolutions/__init__.py
@@ -0,0 +1,3 @@
+SEQUENCE = [
+    'first_evolution',
+]
