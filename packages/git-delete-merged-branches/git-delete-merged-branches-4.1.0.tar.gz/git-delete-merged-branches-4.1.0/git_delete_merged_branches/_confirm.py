259108d7c06fb1eafe5f42dbe3126a4bd7c38d45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:21 +0000

    Add package git-delete-merged-branches/4.1.0/git-delete-merged-branches-4.1.0.tar.gz

---
+++ packages/git-delete-merged-branches/git-delete-merged-branches-4.1.0.tar.gz/git-delete-merged-branches-4.1.0/git_delete_merged_branches/_confirm.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2020 Sebastian Pipping <sebastian@pipping.org>
+# Licensed under GPL v3 or later
+
+class Confirmation:
+    _CONFIRM_GOOD = ('y', 'Y')
+    _CONFIRM_BAD = ('', 'n', 'N')
+    _CONFIRM_KNOWN = _CONFIRM_GOOD + _CONFIRM_BAD
+
+    def __init__(self, messenger, ask):
+        self._messenger = messenger
+        self._ask = ask
+
+    def confirmed(self, question):
+        if not self._ask:
+            return True
+
+        question = self._messenger.format_question(question)
+
+        while True:
+            reply = input(f'{question} [y/N] ')
+            if reply in self._CONFIRM_KNOWN:
+                break
+        return reply in self._CONFIRM_GOOD
