6bb1298190c6876152f737efd97f3816f5d633ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:48 +0000

    Add package alsobrowsercodelib/0.3.5/alsobrowsercodelib-0.3.5.tar.gz

---
+++ packages/alsobrowsercodelib/alsobrowsercodelib-0.3.5.tar.gz/alsobrowsercodelib-0.3.5/alsobrowsercodelib/alsoapi.py
@@ -0,0 +1,31 @@
+from urllib import parse
+import urllib
+import urllib.request
+import requests,json
+ak='PNngqzs09GAwTs0MCQqGIBvLtOe342ZU'
+def get_ipaddress(ip):
+     if ip.count(".")!=3:
+         return -1
+     else:
+         url = 'http://api.map.baidu.com/location/ip?ak='+ak+'&ip='+urllib.parse.quote(ip)+'&coor=bd09ll'
+         address=urllib.request.urlopen(url).read().decode("utf-8")
+         if 0:
+             return -1
+         else:
+             if "status" in address:
+                address = json.loads(address)
+                if address['status']==0:
+                    print(address)
+                    content=address['content']
+                    return content['address']
+                else:
+                    return -1
+def get_traffic(road,city):
+    url='http://api.map.baidu.com/traffic/v1/road?road_name='+urllib.parse.quote(road)+'&city='+urllib.parse.quote(city)+'&ak='+ak
+    address = urllib.request.urlopen(url).read().decode("utf-8")
+    address = json.loads(address)
+    if address['status']==0:
+        return address['description']
+    else:
+        return -1
+
