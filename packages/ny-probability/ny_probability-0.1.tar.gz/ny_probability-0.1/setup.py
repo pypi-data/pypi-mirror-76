9aac3b41ecb67a8cd221cbab10e3e6b19c44c4b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:34 +0000

    Add package ny-probability/0.1/ny_probability-0.1.tar.gz

---
+++ packages/ny-probability/ny_probability-0.1.tar.gz/ny_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ny_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ny_probability'],
+      author = 'Nagarjuna Yegavinti',
+      author_email = 'yegavintin@gmail.com',
+      zip_safe=False)
