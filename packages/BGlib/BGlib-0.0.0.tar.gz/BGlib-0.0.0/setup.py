ce3c4b5c554e8a3fe4a8212f45f3d83c6655eea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:36 +0000

    Add package BGlib/0.0.0/BGlib-0.0.0.tar.gz

---
+++ packages/BGlib/BGlib-0.0.0.tar.gz/BGlib-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='BGlib',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/pycroscopy',
+    author='Suhas Somnath',
+    author_email='pycroscopy@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
