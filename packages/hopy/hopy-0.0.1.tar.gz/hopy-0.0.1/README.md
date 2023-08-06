a583380aecdd4e554dfcf3ea1c7fc921f1ff70c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:54 +0000

    Add package hopy/0.0.1/hopy-0.0.1.tar.gz

---
+++ packages/hopy/hopy-0.0.1.tar.gz/hopy-0.0.1/README.md
@@ -0,0 +1,20 @@
+# hopy
+A high level language for predicate evaluation on JSON using JsonPath expressions
+
+## Getting started
+
+### Prerequisites
+- python3.7+
+
+### Installation
+- Install the package using pip.
+    ```bash
+    pip install hopy
+    ```
+
+## License
+This project is licensed under the Apache License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgements
+- Inspired from [santanusinha/hope](https://github.com/santanusinha/hope)
+- Default JsonPath implementation [jsonpath-ng](https://pypi.org/project/jsonpath-ng/)
---
+++ packages/hopy/hopy-0.0.1.tar.gz/hopy-0.0.1/hopy/evaluator.py
@@ -0,0 +1,33 @@
+import traceback
+
+from hopy.const import TokenType
+from hopy.lexer import Tokenizer
+from hopy.parser import Parser, ActionTreeGenerator
+
+
+class Evaluator:
+    def __init__(self, rules, tokenizer=Tokenizer, parser=Parser, action_tree_generator=ActionTreeGenerator):
+        tokens = tokenizer(rules).tokenize()
+        ast = parser(tokens).parse()
+        self._action_tree = action_tree_generator(ast).generate_actions()
+
+    def _inorder_evaluate(self, act_node, rule_input):
+        if act_node.ast_node.token.ttype == TokenType.LITERAL:
+            return act_node.ast_node.token.value
+        elif act_node.ast_node.token.ttype == TokenType.EXTERNAL:
+            return act_node.action(act_node.ast_node.token.value, rule_input)
+        else:
+            return act_node.action(*[self._inorder_evaluate(child, rule_input) for child in act_node.children])
+
+    def evaluate(self, rule_input):
+        return self._inorder_evaluate(self._action_tree, rule_input)
+
+
+if __name__ == '__main__':
+    while True:
+        inp = input("> Enter an expression to lex:\n> ")
+        try:
+            evaluator = Evaluator(inp)
+            print(evaluator.evaluate({"a": "1", "b": 2}))
+        except Exception as e:
+            traceback.print_exc()
