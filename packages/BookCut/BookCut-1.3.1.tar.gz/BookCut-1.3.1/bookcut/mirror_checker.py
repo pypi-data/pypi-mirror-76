42b3f9b5e3c585098e8e204fa12b34d21020d296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:52 +0000

    Add package BookCut/1.3.1/BookCut-1.3.1.tar.gz

---
+++ packages/BookCut/BookCut-1.3.1.tar.gz/BookCut-1.3.1/bookcut/mirror_checker.py
@@ -0,0 +1,30 @@
+import requests
+import configparser
+import os
+
+
+def main():
+    '''Check which LibGen mirror is available'''
+
+    config = configparser.ConfigParser()
+    module_path = os.path.dirname(os.path.realpath(__file__))
+    settings_ini = os.path.join(module_path, 'Settings.ini')
+    config.read(settings_ini)
+    mirrors = config.get("LibGen", "mirrors")
+    mirrors = mirrors.split(',')
+
+    for url in mirrors:
+        try:
+            r = requests.head(url)
+            status = r.status_code == 200
+            if status is True:
+                return url
+                break
+            else:
+                print("No mirrors available or no Internet Connection!")
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    main()
