7084ed7ef27b4731e8904a0733b90f027f641c00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:55 +0000

    Add package restful-dj/1.0.0/restful-dj-1.0.0.tar.gz

---
+++ packages/restful-dj/restful-dj-1.0.0.tar.gz/restful-dj-1.0.0/restful_dj/__init__.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.urls import path
+
+from .decorator import route
+from .meta import RouteMeta
+from .router import set_before_dispatch_handler
+from .util import collector
+from .util.dot_dict import DotDict
+from .util.logger import set_logger
+
+urls = (
+    [
+        path('', router.render_list),
+        path('<str:entry>', router.dispatch),
+        path('<str:entry>/<str:name>', router.dispatch)
+    ],
+    router.NAME,
+    router.NAME
+)
+
+__all__ = [
+    'collector',
+    'DotDict',
+    'route',
+    'RouteMeta',
+    'RouteMeta',
+    'set_before_dispatch_handler',
+    'set_logger',
+    'urls'
+]
+
+if settings.DEBUG:
+    settings.INSTALLED_APPS.append('restful_dj')
