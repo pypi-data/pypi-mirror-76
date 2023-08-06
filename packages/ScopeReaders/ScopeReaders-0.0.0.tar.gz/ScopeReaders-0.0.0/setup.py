3a9e5e95e7849e514d1f3b27017ecc7769925d83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:36 +0000

    Add package ScopeReaders/0.0.0/ScopeReaders-0.0.0.tar.gz

---
+++ packages/ScopeReaders/ScopeReaders-0.0.0.tar.gz/ScopeReaders-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ScopeReaders',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/pycroscopy',
+    author='me',
+    author_email='pycroscopy@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
