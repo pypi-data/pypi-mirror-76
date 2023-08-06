761d41e5a88fea976f1e8b8553a373666b2e9c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:52 +0000

    Add package sencrypt/0.0.4/sencrypt-0.0.4.tar.gz

---
+++ packages/sencrypt/sencrypt-0.0.4.tar.gz/sencrypt-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name = 'sencrypt',
+    version = '0.0.4',
+    author='Vipin Kasarla',
+    author_email='vipink70@gmail.com',
+    description= 'tool to test and encrypt s3 buckets',
+    license = 'GPLv3+',
+    url='',
+    packages=['sencrypt'],
+    install_requires=[
+        'click',
+        'boto3'
+    ],
+    entry_points='''
+        [console_scripts]
+        sencrypt=sencrypt.sencrypt:cli
+    '''
+
+)
