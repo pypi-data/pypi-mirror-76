44b9308d656062272b558475858f87bafb23efbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:07 +0000

    Add package DXC-AI-MBN/0.0.8/DXC-AI-MBN-0.0.8.tar.gz

---
+++ packages/DXC-AI-MBN/DXC-AI-MBN-0.0.8.tar.gz/DXC-AI-MBN-0.0.8/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from os import path
+
+def readme():
+    with open('README.md') as f:
+        README = f.read()
+    return README
+
+here = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(here, 'dxc/ai/datasets/data/ABDATA.csv'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="DXC-AI-MBN",
+    version="0.0.8",
+    description="Python library which is extensively used for all AI projects",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dxc-technology/DXC-Industrialized-AI-Starter",
+    #download_url = "https://github.com/dxc-technology/DXC-Industrialized-AI-Starter/releases/tag/v1.0.tar.gz",
+    author="DXC",
+    license="Apache License 2.0",
+    platforms='any',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+    ],
+    #packages=["dxc", "dxc.ai"],
+    packages=find_packages(exclude=['data', 'ABDATA.*']),
+    include_package_data=True,
+    install_requires=["JIRA","scikit-learn==0.22.2.post1","auto_ml","Algorithmia","gitpython","flatten_json==0.1.7","pyjanitor","ftfy","arrow",
+                      "scrubadub","yellowbrick==1.1","datacleaner","missingno","pymongo","IPython","dnspython","pmdarima","pyaf","interpret-community","flask_cors","gevent"],
+    entry_points={
+        "console_scripts": [
+            "dxc=dxc.ai:main",
+        ]
+    },
+    package_data={'datasets': ['datasets/data/ABDATA.csv']},
+)
