b8c601cebc502dec7d20b708525e085f141d93c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:06 +0000

    Add package ij-distributions/0.1/ij_distributions-0.1.tar.gz

---
+++ packages/ij-distributions/ij_distributions-0.1.tar.gz/ij_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ij_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ij_distributions'],
+      author = 'Ijeoma Ndu',
+      author_email = 'ijeendu@gmail.com',
+      zip_safe=False)
