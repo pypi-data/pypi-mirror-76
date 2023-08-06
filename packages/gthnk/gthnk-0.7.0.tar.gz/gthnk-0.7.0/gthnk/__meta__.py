e91c766c804bd1d1430f26fa51a80ddaa97f8562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:43 +0000

    Add package gthnk/0.7.0/gthnk-0.7.0.tar.gz

---
+++ packages/gthnk/gthnk-0.7.0.tar.gz/gthnk-0.7.0/gthnk/__meta__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# gthnk (c) Ian Dennis Miller
+
+__version__ = '0.7.0'
+__author__ = 'Ian Dennis Miller'
+__email__ = 'ian@iandennismiller.com'
+__url__ = 'http://www.gthnk.com'
+__copyright__ = '(c) Ian Dennis Miller'
---
+++ packages/gthnk/gthnk-0.7.0.tar.gz/gthnk-0.7.0/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# gthnk (c) Ian Dennis Miller
+
+from setuptools import setup, find_packages
+import os
+import re
+
+
+def fpath(name):
+    return os.path.join(os.path.dirname(__file__), name)
+
+
+def read(fname):
+    return open(fpath(fname)).read()
+
+
+file_text = read(fpath('gthnk/__meta__.py'))
+
+
+def grep(attrname):
+    pattern = r"{0}\W*=\W*'([^']+)'".format(attrname)
+    strval, = re.findall(pattern, file_text)
+    return strval
+
+
+setup(
+    version=grep('__version__'),
+    name='gthnk',
+    description="gthnk is a personal knowledge management system",
+    packages=find_packages(),
+    scripts=[
+        "scripts/gthnk",
+        "scripts/gthnk-config-init.py",
+    ],
+    long_description=read('Readme.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Flask",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Database :: Front-Ends",
+    ],
+    include_package_data=True,
+    keywords='',
+    author=grep('__author__'),
+    author_email=grep('__email__'),
+    url=grep('__url__'),
+    install_requires=read('requirements.txt'),
+    license='MIT',
+    zip_safe=False,
+)
