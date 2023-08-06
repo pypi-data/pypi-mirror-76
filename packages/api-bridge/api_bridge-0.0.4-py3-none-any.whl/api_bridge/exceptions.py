5ce1782ed2aae188d4a1827ef5f72b14983f9576
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:56 +0000

    Add package api-bridge/0.0.4/api_bridge-0.0.4-py3-none-any.whl

---
+++ packages/api-bridge/api_bridge-0.0.4-py3-none-any.whl/api_bridge/exceptions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Robin 'r0w' Weiland"
+__date__ = "2020-08-15"
+__version__ = "0.0.4"
+
+__all__ = ('APIException', 'ValidationException',)
+
+
+class APIException(Exception): pass
+
+
+class ValidationException(APIException): pass
+
+
+if __name__ == '__main__': pass
