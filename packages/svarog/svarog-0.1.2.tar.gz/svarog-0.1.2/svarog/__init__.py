84a325e14c19181bdc995274dcdf203f2342024c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:02 +0000

    Add package svarog/0.1.2/svarog-0.1.2.tar.gz

---
+++ packages/svarog/svarog-0.1.2.tar.gz/svarog-0.1.2/svarog/__init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for Svarog."""
+
+__author__ = """Damian Świstowski"""
+__email__ = "damian@swistowski.org"
+__version__ = "0.1.2"
+
+from .svarog import Svarog
+
+default_builder = Svarog()
+
+forge = default_builder.forge
+register_forge = default_builder.register_forge
+
+__all__ = "forge"
