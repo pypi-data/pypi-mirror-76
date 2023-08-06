9f89ba5307c807e92943717d04c7644f10e971dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:22 +0000

    Add package roulette-russe/0.0.38/roulette_russe-0.0.38.tar.gz

---
+++ packages/roulette-russe/roulette_russe-0.0.38.tar.gz/roulette_russe-0.0.38/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="roulette_russe",
+    version="0.0.38",
+    author="Mohamed Amjad LASRI",
+    author_email="amjadepot@gmail.com",
+    description="Library to write your scrapers hassle free.",
+    url="https://github.com/Homestr/roulette_russe",
+    keywords=['SCRAPING', 'ROULETTE_RUSSE', 'CRAWLING'],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'vaex',
+        'dnspython',
+        'boto3',
+        'beautifulsoup4'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
