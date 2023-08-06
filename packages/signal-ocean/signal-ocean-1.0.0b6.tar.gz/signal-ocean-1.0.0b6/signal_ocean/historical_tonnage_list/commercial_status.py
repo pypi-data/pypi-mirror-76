4e7cabf82fed3f716f20569aa7f9e3c205a72d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:58 +0000

    Add package signal-ocean/1.0.0b6/signal-ocean-1.0.0b6.tar.gz

---
+++ packages/signal-ocean/signal-ocean-1.0.0b6.tar.gz/signal-ocean-1.0.0b6/signal_ocean/historical_tonnage_list/commercial_status.py
@@ -0,0 +1,11 @@
+# noqa: D100
+
+from .._internals import IterableConstants
+
+
+class CommercialStatus(metaclass=IterableConstants):
+    """Contains constants for available commercial statuses."""
+    ON_SUBS = 'On Subs'
+    FAILED = 'Failed'
+    CANCELLED = 'Cancelled'
+    AVAILABLE = 'Available'
