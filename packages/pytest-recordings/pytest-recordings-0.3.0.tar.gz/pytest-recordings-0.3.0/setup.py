bbb1c7da6d40e01573dce66770b690f3af21c3f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:55 +0000

    Add package pytest-recordings/0.3.0/pytest-recordings-0.3.0.tar.gz

---
+++ packages/pytest-recordings/pytest-recordings-0.3.0.tar.gz/pytest-recordings-0.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pytest_recordings', 'pytest_recordings.decorators']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['vcrpy==4.0.2']
+
+entry_points = \
+{'pytest11': ['pytest_recorder = pytest_recorder.hooks']}
+
+setup_kwargs = {
+    'name': 'pytest-recordings',
+    'version': '0.3.0',
+    'description': 'Provides pytest plugins for reporting request/response traffic, screenshots, and more to ReportPortal',
+    'long_description': None,
+    'author': 'Jonathan Castillo',
+    'author_email': 'jonathan.castillo@cloudcheckr.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7',
+}
+
+
+setup(**setup_kwargs)
