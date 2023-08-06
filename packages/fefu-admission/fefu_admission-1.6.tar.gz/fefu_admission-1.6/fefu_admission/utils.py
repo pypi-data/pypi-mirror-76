2679641430f02d7982a218ee16142ee59d72b0d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:58 +0000

    Add package fefu-admission/1.6/fefu_admission-1.6.tar.gz

---
+++ packages/fefu-admission/fefu_admission-1.6.tar.gz/fefu_admission-1.6/fefu_admission/utils.py
@@ -0,0 +1,16 @@
+import logging
+import time
+
+
+class Utils:
+
+    @staticmethod
+    def log_time_of_function(function):
+        def wrapped(*args):
+            start_time = time.time()
+            res = function(*args)
+            arguments_str = ", ".join([str(item) for item in args])
+            logging.info("function {func}, args: ({args_str}),secs: {delta_time:.3f}"
+                         .format(func=function.__name__, args_str=arguments_str, delta_time=time.time() - start_time))
+            return res
+        return wrapped
