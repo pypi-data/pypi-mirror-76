ad733512f0146b8f8d6a36ae6bb307d6199265bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:18 +0000

    Add package pywaclient/0.1.3/pywaclient-0.1.3-py3-none-any.whl

---
+++ packages/pywaclient/pywaclient-0.1.3-py3-none-any.whl/pywaclient/endpoints/image.py
@@ -0,0 +1,42 @@
+#    Copyright 2020 Jonas Waeber
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from typing import Dict, Any
+
+import requests
+from pywaclient.endpoints import Endpoint
+
+
+class Image(Endpoint):
+
+    def __init__(self, client: 'AragornApiClient'):
+        super().__init__(client)
+        self.path = 'image/{0}'
+
+    def get(self, identifier: str) -> Dict[str, Any]:
+        """Get the metadata for a specific image.
+
+        :param identifier: Identifier of the image.
+        :return: Image metadata.
+        """
+        return self._request(self.path.format(identifier))
+
+    def get_binary(self, identifier: str) -> bytes:
+        """Get the image binary by identifier
+
+        :param identifier: Identifier of the image.
+        :return: Image binary data.
+        """
+        metadata = self._request(self.path.format(identifier))
+        return requests.get(metadata['url']).content
