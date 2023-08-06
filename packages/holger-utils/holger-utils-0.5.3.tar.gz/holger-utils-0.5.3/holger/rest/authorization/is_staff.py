f97be5788edb580a9eef3b7184bdc00824017c91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:26 +0000

    Add package holger-utils/0.5.3/holger-utils-0.5.3.tar.gz

---
+++ packages/holger-utils/holger-utils-0.5.3.tar.gz/holger-utils-0.5.3/holger/rest/authorization/is_staff.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+
+class IsStaff(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        is_not_anonymous = str(request.user) != 'AnonymousUser'
+        is_active = request.user.is_active
+        is_staff = request.user.is_staff
+        return is_not_anonymous and is_active and is_staff
