e28fd6dad38fd865340064572dcc0aed87d5f85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:03 +0000

    Add package sievepy/0.0.3/Sievepy-0.0.3.tar.gz

---
+++ packages/sievepy/Sievepy-0.0.3.tar.gz/Sievepy-0.0.3/Driver/__init__.py
@@ -0,0 +1,11 @@
+class Sieve:
+
+    def sieve(n):
+        prime = [True]*(n+1)
+        p = 2
+        while(p*p <= n):
+            if prime[p] == True:
+                for i in range(p*p,n+1,p):
+                    prime[i] = False
+            p += 1
+        return prime
