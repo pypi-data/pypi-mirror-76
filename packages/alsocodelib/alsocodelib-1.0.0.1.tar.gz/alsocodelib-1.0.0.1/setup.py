51eb7a8cf6a6b4c098be786ef49c9d8f04f0ea70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:44 +0000

    Add package alsocodelib/1.0.0.1/alsocodelib-1.0.0.1.tar.gz

---
+++ packages/alsocodelib/alsocodelib-1.0.0.1.tar.gz/alsocodelib-1.0.0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+setuptools.setup(
+    name="alsocodelib",
+    version="1.0.0.1",
+    author="爱搜浏览器团队",
+    author_email="3182305655@qq.com",
+    description="爱搜浏览器功能库，由爱搜浏览器团队制作，爱搜浏览器团队版权所有。",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License",
+                 "Operating System :: OS Independent",
+                 ],
+)
