3849244718251105c1eccb15fa8b3ab0aacfd3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:54 +0000

    Add package kantex/0.2.0/kantex-0.2.0-py3-none-any.whl

---
+++ packages/kantex/kantex-0.2.0-py3-none-any.whl/kantex/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.2.0'
+
+from .md import (KanTeXDocument,
+                 Section, SubSection, SubSubSection,
+                 Bold, Italic, Strikethrough, Code, Pre, Link,
+                 Mention, KeyValueItem, Item)
