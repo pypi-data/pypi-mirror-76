0b3d1b2f9da698efd5948ccdda566042420100bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:17 +0000

    Add package pagsanjan/0.0.1.1/pagsanjan-0.0.1.1.tar.gz

---
+++ packages/pagsanjan/pagsanjan-0.0.1.1.tar.gz/pagsanjan-0.0.1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pagsanjan',
+    version = '0.0.1.1',
+    author = 'Gianna Pineda',
+    author_email = 'gcppineda@gmail.com',
+    packages = find_packages(),
+    description = "simple pandas logger",
+    long_description = "pipe-able pandas logging that converts your logs to dataframe na",
+    install_requires=[
+          'markdown',
+    ],
+)
