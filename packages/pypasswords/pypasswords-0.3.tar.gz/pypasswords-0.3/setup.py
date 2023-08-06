93ab94c3f4328fd1f736a3dc8465fe39de16c52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:45 +0000

    Add package pypasswords/0.3/pypasswords-0.3.tar.gz

---
+++ packages/pypasswords/pypasswords-0.3.tar.gz/pypasswords-0.3/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'pypasswords', 
+  packages = ['pypasswords'],
+  version = '0.3', 
+  license='MIT', 
+  description = 'Working with passwords made simple', 
+  author = 'CosmoStar',                   
+  author_email = 'JustCosmos@yandex.ru',
+  url = 'https://github.com/CosmoSt4r/pypasswords',
+  download_url = 'https://github.com/CosmoSt4r/pypasswords/archive/v_03.tar.gz',
+  keywords = ['password', 'passwords', 'check', 'hash', 'simple'],
+  install_requires=[
+          'zxcvbn',
+      ],
+  classifiers=[
+      
+    'Development Status :: 4 - Beta',
+
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    'License :: OSI Approved :: MIT License',
+
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
