80e7498a72c23f56b6abe624adcd6cdc1ff45cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:20 +0000

    Add package celery-director/0.1.0/celery-director-0.1.0.tar.gz

---
+++ packages/celery-director/celery-director-0.1.0.tar.gz/celery-director-0.1.0/director/extensions.py
@@ -0,0 +1,116 @@
+import imp
+import json
+from pathlib import Path
+from json.decoder import JSONDecodeError
+
+import yaml
+from celery import Celery
+from flask_sqlalchemy import SQLAlchemy
+from flask_json_schema import JsonSchema, JsonValidationError
+from flask_migrate import Migrate
+from pluginbase import PluginBase
+from sqlalchemy.schema import MetaData
+
+from director.exceptions import SchemaNotFound, SchemaNotValid, WorkflowNotFound
+
+
+class CeleryWorkflow:
+    def __init__(self):
+        self.app = None
+        self.workflows = None
+
+    def init_app(self, app):
+        self.app = app
+        self.path = Path(self.app.config["DIRECTOR_HOME"]).resolve() / "workflows.yml"
+        with open(self.path) as f:
+            self.workflows = yaml.load(f, Loader=yaml.SafeLoader)
+
+        self.import_user_tasks()
+        self.read_schemas()
+
+    def get_by_name(self, name):
+        workflow = self.workflows.get(name)
+        if not workflow:
+            raise WorkflowNotFound(f"Workflow {name} not found")
+        return workflow
+
+    def get_tasks(self, name):
+        return self.get_by_name(name)["tasks"]
+
+    def get_queue(self, name):
+        try:
+            return self.get_by_name(name)["queue"]
+        except KeyError:
+            return "celery"
+
+    def import_user_tasks(self):
+        self.plugin_base = PluginBase(package="director.foobar")
+
+        folder = Path(self.app.config["DIRECTOR_HOME"]).resolve()
+        self.plugin_source = self.plugin_base.make_plugin_source(
+            searchpath=[str(folder)]
+        )
+
+        tasks = Path(folder / "tasks").glob("**/*.py")
+        with self.plugin_source:
+            for task in tasks:
+                if task.stem == "__init__":
+                    continue
+
+                name = str(task.relative_to(folder))[:-3].replace("/", ".")
+                __import__(
+                    self.plugin_source.base.package + "." + name,
+                    globals(),
+                    {},
+                    ["__name__"],
+                )
+
+    def read_schemas(self):
+        folder = Path(self.app.config["DIRECTOR_HOME"]).resolve()
+
+        for name, conf in self.workflows.items():
+            if "schema" in conf:
+                path = Path(folder / "schemas" / f"{conf['schema']}.json")
+
+                try:
+                    schema = json.loads(open(path).read())
+                except FileNotFoundError:
+                    raise SchemaNotFound(
+                        f"Schema '{conf['schema']}' not found ({path})"
+                    )
+                except JSONDecodeError as e:
+                    raise SchemaNotValid(f"Schema '{conf['schema']}' not valid ({e})")
+
+                self.workflows[name]["schema"] = schema
+
+
+# Celery Extension
+class FlaskCelery(Celery):
+    def __init__(self, *args, **kwargs):
+        kwargs["include"] = ["director.tasks"]
+        super(FlaskCelery, self).__init__(*args, **kwargs)
+
+        if "app" in kwargs:
+            self.init_app(kwargs["app"])
+
+    def init_app(self, app):
+        self.app = app
+        self.conf.update(app.config.get("CELERY_CONF", {}))
+
+
+# List of extensions
+db = SQLAlchemy(
+    metadata=MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(column_0_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
+)
+migrate = Migrate()
+schema = JsonSchema()
+cel = FlaskCelery("director")
+cel_workflows = CeleryWorkflow()
