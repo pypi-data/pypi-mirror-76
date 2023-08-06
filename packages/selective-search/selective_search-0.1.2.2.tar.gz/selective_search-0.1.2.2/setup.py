1b5f82e93cd79045d28472da9ba053507cbfe596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:06 +0000

    Add package selective-search/0.1.2.2/selective_search-0.1.2.2.tar.gz

---
+++ packages/selective-search/selective_search-0.1.2.2.tar.gz/selective_search-0.1.2.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding="utf8") as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    reqs = f.read()
+
+setup(
+    name="selective_search",
+    version="0.1.2.2",
+    url="https://github.com/ChenjieXu/selective_search",
+    description="Selective Search in Python",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Chenjie Xu",
+    author_email="cxuscience@gmail.com",
+    keywords='rcnn',
+    packages=find_packages(),
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+    ],
+    install_requires=reqs.strip().split('\n'),
+)
