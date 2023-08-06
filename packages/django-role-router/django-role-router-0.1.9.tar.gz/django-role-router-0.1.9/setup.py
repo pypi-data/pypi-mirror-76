966e957482c8bdb0835c0e9573f67e27254ec5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:48 +0000

    Add package django-role-router/0.1.9/django-role-router-0.1.9.tar.gz

---
+++ packages/django-role-router/django-role-router-0.1.9.tar.gz/django-role-router-0.1.9/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: jhvar
+# Mail: jhvar@outlook.com
+# Created Time:  2020-8-11 15:42:43
+#############################################
+
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name = "django-role-router",
+    version = "0.1.9",
+    keywords = ("pip", "django", "role", "util"),
+    description = "Make django router with role permission",
+    long_description = long_description,
+    license = "MIT Licence",
+
+    url = "https://github.com/jhvar/django-utils",
+    author = "jhvar",
+    author_email = "jhvar@outlook.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'django>=2.2.5'
+    ],
+    python_requires='>=3.6',
+)
