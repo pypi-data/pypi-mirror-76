1aba17227ef79cb7e91a82cb82cf5633b44b95dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:56 +0000

    Add package aug-img/0.1.1/aug_img-0.1.1.tar.gz

---
+++ packages/aug-img/aug_img-0.1.1.tar.gz/aug_img-0.1.1/README.md
@@ -0,0 +1,19 @@
+img processor for augmentation and resizing, to expand image variety before start DL phase.
+
+<!-- ``` -->
+<!-- #setup.py can be found => to make this package available in local env  -->
+<!-- pip install -e . -->
+<!-- ``` -->
+<!--  -->
+<!-- ```Python -->
+<!-- # from aug_img.imgaug import Processor -->
+<!-- # x= Processor("img/img.jpg") -->
+<!-- # x.resize(1000, "roi.jpg") -->
+<!-- ``` -->
+<!--  -->
+<!-- ``` -->
+<!-- #upload to PYPI -->
+<!--  -->
+<!--  -->
+<!-- ``` -->
+<!--  -->
---
+++ packages/aug-img/aug_img-0.1.1.tar.gz/aug_img-0.1.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages 
+import pathlib 
+import os 
+
+version = "0.1.1" 
+
+with open (os.path.join(pathlib.Path(__file__).parent, "README.md")) as f:
+    long_description = f.read()
+
+INSTALL_REQUIRES = ["numpy>=1.15",
+                    "Pillow",
+                    "pandas",
+                    "matplotlib",
+                    "scikit-image>=0.14.2",
+                    "opencv-python-headless"]
+
+setup (
+    name = "aug_img",
+    version = version, 
+    description = "package test use a simple image precessing example",
+    long_description = long_description,
+    author = "chloejay",
+    author_email = "ji.jie@edhec.com",
+    url = "",
+    license = "MIT", 
+    # classifiers= [],
+    # required
+    packages = find_packages(include= ["aug_img", "aug_img.*"]),
+    include_package_data=True,
+    install_requires = INSTALL_REQUIRES,
+    package_data = {
+        "": ["README.md", "requirements.txt"],
+    },
+    zip_safe = False
+)
