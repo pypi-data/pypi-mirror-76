6ec12cc99402454ab780e12219783f668981f9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:22 +0000

    Add package sunnyside/0.0.3/sunnyside-0.0.3.tar.gz

---
+++ packages/sunnyside/sunnyside-0.0.3.tar.gz/sunnyside-0.0.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sunnyside",
+    version="0.0.3",
+    author="Jun Qi Li",
+    author_email="JunQi.Li63@myhunter.cuny.com",
+    description="Python wrapper for OpenWeather API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
