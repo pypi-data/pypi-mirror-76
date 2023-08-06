5d84ba6bacf803a80228f4ca037ddfa4c160004f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:14 +0000

    Add package plottr/0.2.0a2/plottr-0.2.0a2.tar.gz

---
+++ packages/plottr/plottr-0.2.0a2.tar.gz/plottr-0.2.0a2/plottr/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
+Signal = QtCore.pyqtSignal
+Slot = QtCore.pyqtSlot
+
+from pyqtgraph.flowchart import Flowchart as pgFlowchart, Node as pgNode
+Flowchart = pgFlowchart
+NodeBase = pgNode
+
+plottrPath = os.path.split(os.path.abspath(__file__))[0]
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
