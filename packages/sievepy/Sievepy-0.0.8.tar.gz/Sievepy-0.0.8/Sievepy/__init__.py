1f5f02ae0bb75c0b7529150ea9a52f753cb83060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:56 +0000

    Add package sievepy/0.0.8/Sievepy-0.0.8.tar.gz

---
+++ packages/sievepy/Sievepy-0.0.8.tar.gz/Sievepy-0.0.8/Sievepy/__init__.py
@@ -0,0 +1,13 @@
+from sieve import Sieve
+
+def primes_till_n(n):
+        result = []
+        prime = Sieve.sieve(n)
+        result = [p for p in range(2,n+1) if prime[p]]
+        return result
+
+def isprime(n):
+        prime = Sieve.sieve(n)
+        if prime[-1] == True:
+            return "Prime"
+        return "Not a Prime"
