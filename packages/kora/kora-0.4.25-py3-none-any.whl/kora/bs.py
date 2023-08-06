579b8df533c9b056d1e697763c4583ff26a0af40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:31 +0000

    Add package kora/0.4.25/kora-0.4.25-py3-none-any.whl

---
+++ packages/kora/kora-0.4.25-py3-none-any.whl/kora/bs.py
@@ -0,0 +1,23 @@
+""" What bs4 should have been """
+
+from bs4 import BeautifulSoup
+import requests 
+from os.path import splitext
+from pathlib import Path
+
+
+def Soup(s, features='lxml', **kw):
+    """ A fake class, of what BeautifulSoup should have been 
+    
+    It accepts a url or a file, in addition to html/xml as usual
+    """
+    if isinstance(s, Path):
+        s = str(s)
+    if s.startswith('http'):
+        src = requests.get(s).text
+        return BeautifulSoup(src, features, **kw)
+    if splitext(s)[1] in ('.html', '.xml'):
+        src = open(s).read()
+        return BeautifulSoup(src, features, **kw)
+    return BeautifulSoup(s, features, **kw)
+    
