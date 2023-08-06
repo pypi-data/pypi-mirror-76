28c363e7f971c4f767337999fd5c8a416ff4a4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:03 +0000

    Add package ovretl/3.4.2/ovretl-3.4.2.tar.gz

---
+++ packages/ovretl/ovretl-3.4.2.tar.gz/ovretl-3.4.2/ovretl/loads_utils/calculate_entity_loads.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from ovretl.loads_utils.calculate_single_load_total_quantities import (
+    calculate_single_load_total_quantities,
+    calculate_taxable_weight,
+    calculate_weight_measurable,
+)
+
+
+def calculate_entity_loads(loads_df: pd.DataFrame, key: str):
+    entity_loads = loads_df[~loads_df[key].isnull()]
+    entity_loads = (
+        entity_loads.groupby(key)
+        .agg(
+            total_number=("total_number", "sum"),
+            total_volume=("total_volume", "sum"),
+            total_weight=("total_weight", "sum"),
+            hazardous=("hazardous", lambda load_hazardous: load_hazardous.any()),
+            lithium=("lithium", lambda load_lithium: load_lithium.any()),
+            refrigerated=("refrigerated", lambda load_refrigerated: load_refrigerated.any()),
+            magnetic=("magnetic", lambda load_magnetic: load_magnetic.any()),
+            non_stackable=("non_stackable", lambda load_non_stackable: load_non_stackable.any()),
+        )
+        .reset_index()
+        .drop_duplicates(subset=[key])
+    )
+    entity_loads["weight_measurable"] = entity_loads.apply(
+        lambda load: calculate_weight_measurable(load["total_weight"], load["total_volume"]), axis=1
+    )
+    entity_loads["taxable_weight"] = entity_loads.apply(
+        lambda load: calculate_taxable_weight(load["total_weight"], load["total_volume"]), axis=1
+    )
+    return entity_loads
+
+
+def calculate_shipments_propositions_loads(loads_df: pd.DataFrame):
+    loads_df = loads_df.apply(calculate_single_load_total_quantities, axis=1)
+    loads_df = loads_df.dropna(subset=["total_volume", "total_weight"])
+    propositions_loads = calculate_entity_loads(loads_df=loads_df, key="proposition_id")
+    shipments_loads = calculate_entity_loads(loads_df=loads_df, key="shipment_id")
+    return pd.concat([propositions_loads, shipments_loads], sort=False)
