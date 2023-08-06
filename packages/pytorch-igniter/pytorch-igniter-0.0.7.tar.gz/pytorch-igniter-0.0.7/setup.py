e508c2631580cb087e3eb2b0d86b44d13e2b0b0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:34 +0000

    Add package pytorch-igniter/0.0.7/pytorch-igniter-0.0.7.tar.gz

---
+++ packages/pytorch-igniter/pytorch-igniter-0.0.7.tar.gz/pytorch-igniter-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(name='pytorch-igniter',
+      version='0.0.7',
+      author='Ben Striner',
+      url='https://github.com/bstriner/pytorch-igniter',
+      install_requires=[
+          'pytorch-ignite',
+          'pyyaml',
+          'mlflow'
+      ],
+      packages=find_packages())
