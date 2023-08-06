d34eb5d5ff06562d8c0023e56d92ea83af8da323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:58 +0000

    Add package Wack/0.1.3/Wack-0.1.3.tar.gz

---
+++ packages/Wack/Wack-0.1.3.tar.gz/Wack-0.1.3/wack/__init__.py
@@ -0,0 +1,5 @@
+from wack.builders import PackageBuilt
+from wack.builders import PipInstallable
+from wack.core import command, echo
+
+__all__ = ["PackageBuilt", "PipInstallable", "command", "echo"]
