1e7e5b6e3df25eb7e575da077480aac7b073af7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:03 +0000

    Add package ynab-sdk/0.2.1/ynab-sdk-0.2.1.tar.gz

---
+++ packages/ynab-sdk/ynab-sdk-0.2.1.tar.gz/ynab-sdk-0.2.1/setup.py
@@ -0,0 +1,27 @@
+import os
+
+from setuptools import setup, find_packages
+
+NAME = 'ynab-sdk'
+VERSION = '0.2.1'
+REQUIRES = ['requests', 'python-dateutil', 'redis']
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description='YNAB API Endpoints',
+    author_email='a.roggeri.c@gmail.com',
+    url='https://github.com/andreroggeri/ynab-sdk-python',
+    keywords=["YNAB", "YNAB API Endpoints", ''],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown'
+)
