2db02073f0b3491c237310cad03c4d3337a87798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:00 +0000

    Add package gino-admin/0.1.1a1/gino-admin-0.1.1a1.tar.gz

---
+++ packages/gino-admin/gino-admin-0.1.1a1.tar.gz/gino-admin-0.1.1a1/gino_admin/routes/crud.py
@@ -0,0 +1,154 @@
+from typing import List, Text, Tuple, Union
+
+import asyncpg
+from sanic.request import Request
+
+from gino_admin import auth, utils
+from gino_admin.core import admin, cfg
+from gino_admin.routes.logic import render_add_or_edit_form, render_model_view
+
+
+@admin.route("/<model_id>", methods=["GET"])
+@auth.token_validation()
+async def model_view_table(
+    request: Request, model_id: Text, flash_messages: Union[List[Tuple], Tuple] = None
+):
+    if flash_messages and not isinstance(flash_messages[0], tuple):
+        request["flash"](*flash_messages)
+    elif flash_messages:
+        for flash in flash_messages:
+            request["flash"](*flash)
+    return await render_model_view(request, model_id)
+
+
+@admin.route("/<model_id>/edit", methods=["GET"])
+@auth.token_validation()
+async def model_edit_view(request, model_id):
+    _id = dict(request.query_args)["id"]
+    return await render_add_or_edit_form(request, model_id, _id)
+
+
+@admin.route("/<model_id>/edit", methods=["POST"])
+@auth.token_validation()
+async def model_edit_post(request, model_id):
+    obj_id = dict(request.query_args)["id"]
+    model_data = cfg.models[model_id]
+    model = model_data["model"]
+    obj = await model.get(model_data["columns_data"][model_data["key"]]["type"](obj_id))
+    old_obj = obj.to_dict()
+    request_params = {
+        key: request.form[key][0] if request.form[key][0] != "None" else None
+        for key in request.form
+    }
+    if model_data["hashed_indexes"]:
+        request_params = utils.reverse_hash_names(model_id, request_params)
+    request_params = utils.correct_types(request_params, model_data["columns_data"])
+    try:
+        await obj.update(**request_params).apply()
+        changes = utils.get_changes(old_obj, obj.to_dict())
+        message = f'Object with id {obj_id} was updated. Changes: from {changes["from"]} to {changes["to"]}'
+        request["flash"](message, "success")
+        request["history_action"]["log_message"] = message
+        request["history_action"]["object_id"] = obj_id
+    except asyncpg.exceptions.ForeignKeyViolationError:
+        request["flash"](
+            f"ForeignKey error. "
+            f"Impossible to edit {model_data['key']} field for row {obj_id}, "
+            f"because exists objects that depend on it. ",
+            "error",
+        )
+        request_params[model_data["key"]] = obj_id
+    except asyncpg.exceptions.UniqueViolationError:
+        request["flash"](
+            f"{model_id.capitalize()} with such id already exists", "error"
+        )
+    except asyncpg.exceptions.NotNullViolationError as e:
+        column = e.args[0].split("column")[1].split("violates")[0]
+        request["flash"](f"Field {column} cannot be null", "error")
+    return await render_add_or_edit_form(
+        request, model_id, request_params[model_data["key"]]
+    )
+
+
+@admin.route("/<model_id>/add", methods=["GET"])
+@auth.token_validation()
+async def model_add_view(request, model_id):
+    return await render_add_or_edit_form(request, model_id)
+
+
+@admin.route("/<model_id>/add", methods=["POST"])
+@auth.token_validation()
+async def model_add(request, model_id):
+    model_data = cfg.models[model_id]
+    request_params = {key: request.form[key][0] for key in request.form}
+
+    not_filled = [x for x in model_data["required_columns"] if x not in request_params]
+    if not_filled:
+        request["flash"](f"Fields {not_filled} required. Please fill it", "error")
+    else:
+        if model_data["hashed_indexes"]:
+            request_params = utils.reverse_hash_names(model_id, request_params)
+        try:
+            request_params = utils.correct_types(
+                request_params, model_data["columns_data"]
+            )
+            obj = await model_data["model"].create(**request_params)
+            message = f'Object with {model_data["key"]} {obj.to_dict()[model_data["key"]]} was added.'
+            request["flash"](message, "success")
+            request["history_action"]["log_message"] = message
+            request["history_action"]["object_id"] = obj.to_dict()[model_data["key"]]
+        except (
+            asyncpg.exceptions.StringDataRightTruncationError,
+            ValueError,
+            asyncpg.exceptions.ForeignKeyViolationError,
+        ) as e:
+            request["flash"](e.args, "error")
+        except asyncpg.exceptions.UniqueViolationError:
+            request["flash"](
+                f"{model_id.capitalize()} with such id already exists", "error"
+            )
+        except asyncpg.exceptions.NotNullViolationError as e:
+            column = e.args[0].split("column")[1].split("violates")[0]
+            request["flash"](f"Field {column} cannot be null", "error")
+
+    return await render_add_or_edit_form(request, model_id)
+
+
+@admin.route("/<model_id>/delete", methods=["POST"])
+@auth.token_validation()
+async def model_delete(request, model_id):
+    """ route for delete item per row """
+    model_data = cfg.models[model_id]
+    request_params = {key: request.form[key][0] for key in request.form}
+    # unique_column_name
+    unique_cn = model_data["key"]
+    request_params[unique_cn] = model_data["columns_data"][unique_cn]["type"](
+        request_params[unique_cn]
+    )
+
+    try:
+        await model_data["model"].delete.where(
+            getattr(model_data["model"], unique_cn) == request_params[unique_cn]
+        ).gino.status()
+        message = f"Object with {unique_cn} {request_params[unique_cn]} was deleted"
+        flash_message = (message, "success")
+        request["history_action"]["log_message"] = message
+        request["history_action"]["object_id"] = request_params[unique_cn]
+    except asyncpg.exceptions.ForeignKeyViolationError as e:
+        flash_message = (str(e.args), "error")
+
+    return await model_view_table(request, model_id, flash_message)
+
+
+@admin.route("/<model_id>/delete_all", methods=["POST"])
+@auth.token_validation()
+async def model_delete_all(request, model_id):
+    try:
+        await cfg.models[model_id]["model"].delete.where(True).gino.status()
+        message = f"All objects in {model_id} was deleted"
+        flash_message = (message, "success")
+        request["history_action"]["log_message"] = message
+        request["history_action"]["object_id"] = model_id
+    except asyncpg.exceptions.ForeignKeyViolationError as e:
+        flash_message = (e.args, "error")
+    return await model_view_table(request, model_id, flash_message)
---
+++ packages/gino-admin/gino-admin-0.1.1a1.tar.gz/gino-admin-0.1.1a1/gino_admin/utils.py
@@ -0,0 +1,257 @@
+import datetime
+import logging
+import os
+import re
+import time
+import uuid
+from random import randint
+from typing import Any, Dict, List, Text, Union
+from unicodedata import normalize
+from sqlalchemy.types import BigInteger
+
+import aiofiles
+import yaml
+from passlib.hash import pbkdf2_sha256
+
+from gino_admin import config
+
+cfg = config.cfg
+logger = logging.getLogger("Gino Admin")
+
+salt = uuid.uuid4().hex
+
+
+_windows_device_files = (
+    "CON",
+    "AUX",
+    "COM1",
+    "COM2",
+    "COM3",
+    "COM4",
+    "LPT1",
+    "LPT2",
+    "LPT3",
+    "PRN",
+    "NUL",
+)
+
+types_map = {
+    "INTEGER": int,
+    "BIGINT": int,
+    "VARCHAR": str,
+    "FLOAT": float,
+    "DECIMAL": float,
+    "NUMERIC": float,
+    "DATETIME": datetime.datetime,
+    "DATE": datetime.date,
+    "BOOLEAN": bool,
+}
+
+
+class GinoAdminError(Exception):
+    pass
+
+
+def serialize_obj(obj: Any) -> Any:
+    """ method to serialise datetime obj """
+    if isinstance(obj, (datetime.date, datetime.datetime)):
+        return obj.isoformat()
+    else:
+        return obj
+
+
+def serialize_dict(container: Dict) -> Dict:
+    """ method to serialise datetime in dict """
+    for key, value in container.items():
+        container[key] = serialize_obj(value)
+    return container
+
+
+def reverse_hash_names(model_id: Text, request_params: Dict):
+    model_data = cfg.models[model_id]
+    for hashed_index in model_data["hashed_indexes"]:
+        if model_data["columns_names"][hashed_index] in request_params:
+            request_params[
+                model_data["columns_names"][hashed_index] + "_hash"
+            ] = cfg.hash_method(
+                request_params[model_data["columns_names"][hashed_index]]
+            )
+            del request_params[model_data["columns_names"][hashed_index]]
+    return request_params
+
+
+def secure_filename(filename: Text):
+    filename = normalize("NFKD", filename).encode("ascii", "ignore")
+    filename = filename.decode("ascii")
+    for sep in os.path.sep, os.path.altsep:
+        if sep:
+            filename = filename.replace(sep, " ")
+    _filename_ascii_strip_re = re.compile(r"[^A-Za-z0-9_.-]")
+    filename = str(_filename_ascii_strip_re.sub("", "_".join(filename.split()))).strip(
+        "._"
+    )
+    if (
+        os.name == "nt"
+        and filename
+        and filename.split(".")[0].upper() in _windows_device_files
+    ):
+        filename = "_" + filename
+
+    return filename
+
+
+def valid_file_type(file_name: Text, file_type: Text, allowed_file_types: List):
+    file_name_type = file_name.split(".")[-1]
+    for allowed_type in allowed_file_types:
+        if file_name_type == allowed_type and (
+            file_type == f"application/{allowed_type}"
+            or file_type == f"text/{allowed_type}"
+        ):
+            return True
+    else:
+        return False
+
+
+def valid_file_size(file_body: bytes, max_size: int):
+    if len(file_body) < max_size:
+        return True
+    return False
+
+
+async def write_file(path, body):
+    async with aiofiles.open(path, "wb") as f:
+        await f.write(body)
+        f.close()
+
+
+class CompositeType:
+    pass
+
+
+def correct_types(params: Dict, columns_data: Dict):
+    to_del = []
+    for param in params:
+        if not params[param]:
+            # mean None
+            to_del.append(param)
+            continue
+        if isinstance(param, CompositeType):
+            continue
+        if "_hash" not in param and not isinstance(
+            params[param], columns_data[param]["type"]
+        ):
+            if columns_data[param]["type"] not in [datetime.datetime, datetime.date]:
+                params[param] = columns_data[param]["type"](params[param])
+            else:
+                params[param] = extract_datetime(params[param])
+    for param in to_del:
+        del params[param]
+    for column in columns_data:
+        if columns_data[column]["type"] == bool and column not in params:
+            params[column] = False
+    return params
+
+
+def extract_date(date_str: Text):
+    date_object = datetime.datetime.strptime(date_str, "%m-%d-%y")
+    return date_object
+
+
+def extract_datetime(datetime_str: Text):
+    for str_format in cfg.datetime_str_formats:
+        try:
+            datetime_object = datetime.datetime.strptime(datetime_str, str_format)
+            return datetime_object
+        except ValueError:
+            continue
+
+
+def generate_token(ip: Text):
+    """ generate session token based on user name and salt """
+    return pbkdf2_sha256.encrypt(salt + ip)
+
+
+def read_yaml(preset_file):
+    with open(preset_file, "r") as preset_file:
+        return yaml.safe_load(preset_file)
+
+
+def get_presets():
+    """ return previous loaded, or re-read from disk """
+    if not cfg.presets or (
+        cfg.presets.get("loaded_at")
+        and cfg.presets["loaded_at"] < os.path.getmtime(cfg.presets_folder)
+    ):
+        cfg.presets = {"presets": load_presets(), "loaded_at": time.time()}
+
+    return cfg.presets
+
+
+def get_preset_by_id(preset_id: Text):
+    """ get preset by id """
+    presets = get_presets()["presets"]
+    for preset in presets:
+        if preset_id == preset["id"]:
+            return preset
+
+
+def load_presets():
+    """ get presets data from yml configs from presets folder"""
+    presets = []
+    if not os.path.isdir(cfg.presets_folder):
+        if os.path.isfile(cfg.presets_folder):
+            logger.error(f"Presets folder is file. Must be a path to folder")
+        logger.info(f"Presets folder not found. Create new folder.")
+        os.makedirs(cfg.presets_folder)
+    else:
+        for file_name in os.listdir(cfg.presets_folder):
+            if file_name.endswith(".yml"):
+                file_path = os.path.join(cfg.presets_folder, file_name)
+                preset_definition = read_yaml(file_path)
+                presets.append(preset_definition)
+    return presets
+
+
+def get_settings():
+    """ gino admin settings '"""
+    settings = {}
+    for setting in cfg.displayable_setting:
+        settings[setting] = getattr(cfg, setting)
+    return settings
+
+
+def generate_new_id(base_key: Text, model_data: Dict) -> Union[Text, int]:
+    key = model_data["key"]
+    if isinstance(base_key, str):
+        new_obj_key = (
+            base_key
+            + f"{'_' if not base_key.endswith('_') else ''}"
+            + uuid.uuid1().hex[5:10]
+        )
+        len_ = model_data["columns_data"][key]["len"]
+        if len_:
+            if new_obj_key[:len_] == base_key:
+                # if we spend all id previous
+                new_obj_key = new_obj_key[
+                    len_ : len_ + len_  # noqa E203
+                ]  # auto format from black
+            else:
+                new_obj_key = new_obj_key[:len_]
+    else:
+        if isinstance(model_data["columns_data"][key]["db_type"], BigInteger):
+            new_obj_key = randint(0, 2 ** 63)
+        else:
+            new_obj_key = randint(0, 2 ** 31)
+    return new_obj_key
+
+
+def get_changes(old_obj: Dict, new_obj: Dict):
+    from_ = {}
+    to_ = {}
+    for key, value in new_obj.items():
+        if key not in old_obj:
+            to_[key] = value
+        elif old_obj[key] != value:
+            from_[key] = old_obj[key]
+            to_[key] = value
+    return {"from": from_, "to": to_}
