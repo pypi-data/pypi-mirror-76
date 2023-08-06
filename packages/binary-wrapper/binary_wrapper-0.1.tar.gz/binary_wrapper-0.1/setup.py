0cb080b923479204b8a4100e272dacfaa95a64f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:09 +0000

    Add package binary-wrapper/0.1/binary_wrapper-0.1.tar.gz

---
+++ packages/binary-wrapper/binary_wrapper-0.1.tar.gz/binary_wrapper-0.1/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='binary_wrapper',
+      version='0.1',
+      description='A generic binary wrapper',
+      url='https://github.com/rmonico/binary_wrapper',
+      author='Rafael Monico',
+      author_email='rmonico1@gmail.com',
+      license='GPL3',
+      include_package_data=True,
+      zip_safe=False)
