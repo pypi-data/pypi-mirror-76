bb680a9ec59a038dd28a27f747d22406155e6df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:45 +0000

    Add package pykailp/0.0.1/pykailp-0.0.1.tar.gz

---
+++ packages/pykailp/pykailp-0.0.1.tar.gz/pykailp-0.0.1/pykailp/NER/helloner.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/8/12-23:39
+# @Author  : 贾志凯
+# @File    : helloner.py
+# @Software: win10  python3.6 PyCharm
+class sum():
+    def __init__(self):
+        pass
+
+    def sum(self):
+        print("hello ner")
