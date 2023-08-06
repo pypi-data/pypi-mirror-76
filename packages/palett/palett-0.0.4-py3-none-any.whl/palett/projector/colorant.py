ebada3f0c857aa5f1f016f3c8ecb3173f25f0681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:27 +0000

    Add package palett/0.0.4/palett-0.0.4-py3-none-any.whl

---
+++ packages/palett/palett-0.0.4-py3-none-any.whl/palett/projector/colorant.py
@@ -0,0 +1,12 @@
+from palett.presets import PLANET
+from palett.projector.projector import to_projector
+from palett.projector.utils.bound_to_leap import bound_to_leap
+from palett.projector.utils.preset_to_flat import preset_to_flat
+from intype import is_numeric
+
+
+def colorant(bound, preset=PLANET, effects=[]):
+    vleap = bound_to_leap(bound)
+    prime = preset_to_flat(preset)
+    projector = to_projector(vleap, preset, effects)
+    return lambda x: projector(x) if is_numeric(x) else prime
