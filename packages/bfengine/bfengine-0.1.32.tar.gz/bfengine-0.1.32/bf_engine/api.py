f7b59a17fbe3936a8d77be407e74f733ebdbd91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:54 +0000

    Add package bfengine/0.1.32/bfengine-0.1.32.tar.gz

---
+++ packages/bfengine/bfengine-0.1.32.tar.gz/bfengine-0.1.32/bf_engine/api.py
@@ -0,0 +1,28 @@
+from . import util
+from .config import Config
+from .core.bot import Bot
+from .utils.local_cache import LocalCache
+
+
+def init(app_id: str = None, local=False, url=None)->Bot:
+    """
+    创建bot，并返回实例
+    """
+    if not app_id:
+        # app_id = util.random_uui
+        cache = LocalCache("var.bot")
+        if cache:
+            app_id = cache["app_id"];
+        if not app_id:
+            app_id = util.random_uuid()
+            cache["app_id"]=app_id
+    if local:
+        Config.base_url = 'http://172.17.0.1'
+    elif url:
+        Config.base_url = url
+    else:
+        Config.base_url = Config.remote_url
+
+    bot = Bot(app_id)
+    bot.dm.load()
+    return bot
