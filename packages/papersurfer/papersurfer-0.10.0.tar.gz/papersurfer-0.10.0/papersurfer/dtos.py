27405ab669cabf4ddbb53468f7348bc28f597a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:06 +0000

    Add package papersurfer/0.10.0/papersurfer-0.10.0.tar.gz

---
+++ packages/papersurfer/papersurfer-0.10.0.tar.gz/papersurfer-0.10.0/papersurfer/dtos.py
@@ -0,0 +1,27 @@
+"""Data transfer objects."""
+from dataclasses import dataclass
+
+
+@dataclass
+class PostDTO:
+    """Encapsulate Mattermost Posts."""
+    id: str
+    message: str
+    reporter: str
+    doi: str
+
+    def __str__(self):
+        return self.message
+
+
+@dataclass
+class PaperDTO:
+    """Encapsulate Paper meta data."""
+    author: str
+    authors: str
+    title: str
+    journal: str
+    year: int
+    abstract: str
+    doi: str
+    slug: str
