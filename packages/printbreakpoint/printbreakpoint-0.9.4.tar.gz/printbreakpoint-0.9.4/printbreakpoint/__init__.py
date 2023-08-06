9f32f1668d95c4129debe5694d5ae2a8af0ea704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:42 +0000

    Add package printbreakpoint/0.9.4/printbreakpoint-0.9.4.tar.gz

---
+++ packages/printbreakpoint/printbreakpoint-0.9.4.tar.gz/printbreakpoint-0.9.4/printbreakpoint/__init__.py
@@ -0,0 +1,15 @@
+import inspect
+import sys
+
+
+def pb(*args):
+    """ Print breakpoint """
+    callerframerecord = inspect.stack()[1]
+    frame = callerframerecord[0]
+    info = inspect.getframeinfo(frame)
+    filename = info.filename.rsplit('/', 1)[-1]
+    line = info.lineno
+    fn = info.function
+    divider = '|' if args else ''
+    meta = f'\033[92mpb\033[0m | {filename}:{line} in {fn} {divider}'
+    print(meta, *args, file=sys.stderr)
