39068bd5a0cf5c6720f35107b64d62835b04373d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:04 +0000

    Add package tamarin-utils/0.5.5/tamarin-utils-0.5.5.tar.gz

---
+++ packages/tamarin-utils/tamarin-utils-0.5.5.tar.gz/tamarin-utils-0.5.5/tamarin/rest/authorization/__init__.py
@@ -0,0 +1,6 @@
+from .authentication_backend import TamarinTokenAuthentication
+from .is_authenticated import IsAuthenticated
+from .is_staff import IsStaff
+from .has_permission import HasPermission
+from .is_member import IsMember
+from .token import TamarinTokenSerializer
