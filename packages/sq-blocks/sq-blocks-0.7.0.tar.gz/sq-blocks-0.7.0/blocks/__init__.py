ff14a6e378ed7fc832eda1dbbc96174f86095a8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:43 +0000

    Add package sq-blocks/0.7.0/sq-blocks-0.7.0.tar.gz

---
+++ packages/sq-blocks/sq-blocks-0.7.0.tar.gz/sq-blocks-0.7.0/blocks/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """Bradley Axen"""
+__email__ = "baxen@squareup.com"
+__version__ = "0.7.0"
+
+
+from blocks.core import (
+    assemble,
+    iterate,
+    partitioned,
+    divide,
+    place,
+)  # NOQA
+from blocks.dfio import read_df, write_df  # NOQA
