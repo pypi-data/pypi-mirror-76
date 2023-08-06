4829023d1849f63267460d1acbd14f17fd086a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:32 +0000

    Add package opsep/3.0.4/opsep-3.0.4-py3-none-any.whl

---
+++ packages/opsep/opsep-3.0.4-py3-none-any.whl/opsep/__init__.py
@@ -0,0 +1,2 @@
+from .main import opsep_hybrid_decrypt, opsep_hybrid_encrypt, opsep_hybrid_encrypt_with_auditlog
+from .opsep import RateLimitError, BadRequestError, OPSEP_URL
---
+++ packages/opsep/opsep-3.0.4-py3-none-any.whl/tests/utils.py
@@ -0,0 +1,18 @@
+import os
+import requests
+
+from pathlib import Path
+
+from opsep import OPSEP_URL
+
+# TODO: move test to own directory?
+key_dir = Path(__file__).absolute().parent.parent
+
+with open(os.path.join(key_dir, "insecureprivkey.pem"), "r") as f:
+    PRIVKEY_STR = f.read()
+with open(os.path.join(key_dir, "insecurepubkey.crt"), "r") as f:
+    PUBKEY_STR = f.read()
+
+
+def _fetch_testing_pubkey(opsep_url=OPSEP_URL):
+    return requests.get(opsep_url).json()['rsaPubKey']
