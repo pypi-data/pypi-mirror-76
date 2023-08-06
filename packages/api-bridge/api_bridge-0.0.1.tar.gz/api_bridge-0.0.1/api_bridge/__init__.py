01f8d9839500b6a7a102477781dbe1ed6027f1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:40 +0000

    Add package api-bridge/0.0.1/api_bridge-0.0.1.tar.gz

---
+++ packages/api-bridge/api_bridge-0.0.1.tar.gz/api_bridge-0.0.1/api_bridge/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = "Robin 'r0w' Weiland"
+__date__ = "2020-06-05"
+__version__ = "0.0.1"
+
+__all__ = ('API',)
+
+from api_bridge.api import API
+from api_bridge.filter import Filter
+from api_bridge.methods import Method
+
+
+if __name__ == '__main__': pass
