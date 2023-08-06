83ee108d225aa52ae52171aba6457d5654405b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:50 +0000

    Add package botnoi/0.1.3/botnoi-0.1.3-py3-none-any.whl

---
+++ packages/botnoi/botnoi-0.1.3-py3-none-any.whl/botnoi/__init__.py
@@ -0,0 +1,13 @@
+from . import openapi
+from . import scrape
+from . import mobilenet
+from . import resnet50
+from . import cv
+
+__all__ = [
+    'openapi',
+    'scrape',
+    'mobilenet',
+    'resnet50',
+    'cv'
+]
