ee3f269db410366b5fa172a6e0917ec04b2c4cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:07 +0000

    Add package prettyqt/0.116.0/PrettyQt-0.116.0.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.116.0.tar.gz/PrettyQt-0.116.0/prettyqt/core/file.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import contextlib
+
+from qtpy import QtCore
+
+from prettyqt import core
+
+
+OPEN_MODES = core.iodevice.OPEN_MODES  # type: ignore
+
+QtCore.QFile.__bases__ = (core.FileDevice,)
+
+
+class File(QtCore.QFile):
+    @contextlib.contextmanager
+    def open_file(self, flag):
+        if flag in OPEN_MODES:
+            flag = OPEN_MODES[flag]
+        self.open(flag)
+        yield None
+        self.close()
---
+++ packages/prettyqt/PrettyQt-0.116.0.tar.gz/PrettyQt-0.116.0/prettyqt/widgets/styleoptionslider.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+# The MIT License (MIT)
+
+# Copyright (c) 2011-2014 Marvin Killing
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from qtpy import QtCore, QtWidgets
+
+
+class StyleOptionSlider(QtWidgets.QStyleOptionSlider):
+    def is_horizontal(self) -> bool:
+        """Check if silder is horizontal.
+
+        Returns:
+            True if horizontal, else False
+        """
+        return self.orientation == QtCore.Qt.Horizontal
+
+    def is_vertical(self) -> bool:
+        """Check if silder is vertical.
+
+        Returns:
+            True if vertical, else False
+        """
+        return self.orientation == QtCore.Qt.Vertical
+
+    def set_horizontal(self) -> None:
+        """Set slider orientation to horizontal."""
+        self.orientation = QtCore.Qt.Horizontal
+
+    def set_vertical(self) -> None:
+        """Set slider orientation to vertical."""
+        self.orientation = QtCore.Qt.Vertical
