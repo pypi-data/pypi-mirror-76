5ddebb8490c5b80e8a54417af1dba6b7f9596554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:34 +0000

    Add package tamcolors/1.0.4/tamcolors-1.0.4-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/tamcolors/tamcolors-1.0.4-cp37-cp37m-macosx_10_9_x86_64.whl/tamcolors/examples/__init__.py
@@ -0,0 +1,22 @@
+from . import alpha
+from . import icon
+from . import rgb_color
+from . import basic
+from . import tabletennis
+from . import tam_loop
+from . import tam_print
+from . import tam_text_box
+from . import tam_list_buffer
+from . import tam_key_manager
+
+
+__all__ = ("alpha",
+           "icon",
+           "rgb_color",
+           "basic",
+           "tabletennis",
+           "tam_loop",
+           "tam_print",
+           "tam_text_box",
+           "tam_list_buffer",
+           "tam_key_manager")
