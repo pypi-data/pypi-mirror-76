79aba485a9cbfa344334c458a594c2466287a7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package flask-sketch/0.2.3a7/flask_sketch-0.2.3a7-py3-none-any.whl

---
+++ packages/flask-sketch/flask_sketch-0.2.3a7-py3-none-any.whl/flask_sketch/make.py
@@ -0,0 +1,38 @@
+import pathlib
+from argparse import Namespace
+import toml
+
+from flask_sketch.handlers import (
+    api_framework_handler,
+    app_type_handler,
+    auth_handler,
+    config_handler,
+    database_handler,
+)
+from flask_sketch.utils import (
+    Answers,
+    make_commom_folders,
+    pjoin,
+)
+
+
+def create_project(args: Namespace, asws: dict):
+    pf = pjoin(str(pathlib.Path().absolute()), args.project_name)
+    apf = pjoin(
+        str(pathlib.Path().absolute()), args.project_name, args.project_name
+    )
+
+    answers = Answers(pf, apf, asws, args)
+
+    make_commom_folders(answers)
+
+    answers.settings["default"]["debug"] = False
+    answers.settings["default"]["EXTENSIONS"] = []
+
+    app_type_handler(answers)
+    database_handler(answers)
+    auth_handler(answers)
+    if "api" in answers.application_type:
+        api_framework_handler(answers)
+    config_handler(answers)
+
