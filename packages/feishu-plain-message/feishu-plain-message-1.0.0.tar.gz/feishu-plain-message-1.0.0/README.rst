038ec355f538d58e5c2f129d41d5545db0b3f01f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:09 +0000

    Add package feishu-plain-message/1.0.0/feishu-plain-message-1.0.0.tar.gz

---
+++ packages/feishu-plain-message/feishu-plain-message-1.0.0.tar.gz/feishu-plain-message-1.0.0/README.rst
@@ -0,0 +1,30 @@
+（一）feishu-message 模块使用方法
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    **``(一)：安装``**
+
+-  从 PYPI 安装
+
+::
+
+    pip install -U  feishu-plain-message
+
+-  从 Github 安装
+
+::
+
+    pip install git+https://github.com/yinhuanyi/feishu-plain-message.git
+
+    **``(二)：使用方法``**
+
+::
+
+    from feishu_message import FeishuPlainMessage
+
+    if __name__ == '__main__':
+        feishu = FeishuPlainMessage('13970236750', 'cli_9f9b87ccdsc970d00b', 'SVWv3GtMxkVlPgo0feOsdsUhyA728025qnf')
+
+        feishu.send_message('工单类型+工单ID', '工单基本内容')
+
+欢迎提交PR
+==========
---
+++ packages/feishu-plain-message/feishu-plain-message-1.0.0.tar.gz/feishu-plain-message-1.0.0/setup.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+"""
+@Author: Robby
+@Module name: setup.py
+@Create date: 2020-06-06
+@Function: 
+"""
+
+import os
+from setuptools import setup
+
+def package_data(pkg, roots=tuple()):
+    data = []
+    for root in roots:
+        for dirname, _, files in os.walk(os.path.join(pkg, root)):
+            for fname in files:
+                print(os.path.relpath(os.path.join(dirname, fname), pkg))
+                data.append(os.path.relpath(os.path.join(dirname, fname), pkg))
+
+    return {pkg: data}
+
+with open("README.rst", "r") as f:
+  long_description = f.read()
+
+setup(
+    name = 'feishu-plain-message',
+    author = 'Robby',
+    author_email = 'yinhuanyicn@gmail.com',
+    url = 'https://github.com/yinhuanyi/feishu-plain-message',
+    license = "MIT",
+    version = '1.0.0',
+    description = 'Send Plain Message To Feishu',
+    long_description = long_description,
+    packages = [
+        'feishu_plain_message',
+    ],
+    install_requires = [
+        'requests',
+    ],
+    dependency_links = [],
+    package_data = package_data("feishu_plain_message",),
+)
