ac511eec1cdb463572bc78626b11b58fa5a1f0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:49 +0000

    Add package tri-distributions/0.1/tri_distributions-0.1.tar.gz

---
+++ packages/tri-distributions/tri_distributions-0.1.tar.gz/tri_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tri_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tri_distributions'],
+      author= 'Andrew Doan',
+      author_email= 'tri.sidoan@gmail.com',
+      zip_safe=False)
