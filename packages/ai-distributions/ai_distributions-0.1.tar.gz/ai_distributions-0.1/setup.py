1ac5e48c34b993047453a4ba0974b6642999c76c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:06 +0000

    Add package ai-distributions/0.1/ai_distributions-0.1.tar.gz

---
+++ packages/ai-distributions/ai_distributions-0.1.tar.gz/ai_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ai_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author='Asif Iqbal',
+      author_email='iqmazif@gmail.com',
+      zip_safe=False)
