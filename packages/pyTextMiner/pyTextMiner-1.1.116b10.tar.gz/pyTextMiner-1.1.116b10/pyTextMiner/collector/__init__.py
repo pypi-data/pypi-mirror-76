e8e746b13f59d056e22e483d033fc09489b7201f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:31 +0000

    Add package pyTextMiner/1.1.116b10/pyTextMiner-1.1.116b10.tar.gz

---
+++ packages/pyTextMiner/pyTextMiner-1.1.116b10.tar.gz/pyTextMiner-1.1.116b10/pyTextMiner/collector/__init__.py
@@ -0,0 +1,7 @@
+from pyTextMiner.collector.colBasic import *
+from pyTextMiner.collector.colNaverNews import *
+from pyTextMiner.collector.colChosun import *
+from pyTextMiner.collector.colDonga import *
+from pyTextMiner.collector.colJoongang import *
+from pyTextMiner.collector.colBigkinds import *
+from pyTextMiner.collector.colNaverBlog import *
