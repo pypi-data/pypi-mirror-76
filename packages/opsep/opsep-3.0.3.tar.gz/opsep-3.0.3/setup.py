b83d4b9fd1a2677dc0ff8c76d2883769679272d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:14 +0000

    Add package opsep/3.0.3/opsep-3.0.3.tar.gz

---
+++ packages/opsep/opsep-3.0.3.tar.gz/opsep-3.0.3/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+from io import open
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# https://stackoverflow.com/a/15341042/1754586
+REQUIREMENTS = [i.strip() for i in open("requirements.in").readlines()]
+
+setup(
+    name='opsep',
+    author='Michael Flaxman',
+    author_email='opsep-python@michaelflaxman.com',
+    description='OpSep Python Library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/opsep/opsep-python/',
+    version='3.0.3',
+    install_requires=REQUIREMENTS,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
+    include_package_data=True,
+    package_data={"": ["LICENSE"]},
+)
+
---
+++ packages/opsep/opsep-3.0.3.tar.gz/opsep-3.0.3/tests/utils.py
@@ -0,0 +1,15 @@
+import requests
+from pathlib import Path
+import os
+
+# TODO: move test to own directory?
+key_dir = Path(__file__).absolute().parent.parent
+
+with open(os.path.join(key_dir, "insecureprivkey.pem"), "r") as f:
+    PRIVKEY_STR = f.read()
+with open(os.path.join(key_dir, "insecurepubkey.crt"), "r") as f:
+    PUBKEY_STR = f.read()
+
+
+def _fetch_testing_pubkey(url="http://localhost:8080/"):
+    return requests.get(url).json()['rsaPubKey']
