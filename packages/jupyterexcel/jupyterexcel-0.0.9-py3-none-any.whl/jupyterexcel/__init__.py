513abf8c4e6188d5cfc0d646b053c4e0af6cfe32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:24 +0000

    Add package jupyterexcel/0.0.9/jupyterexcel-0.0.9-py3-none-any.whl

---
+++ packages/jupyterexcel/jupyterexcel-0.0.9-py3-none-any.whl/jupyterexcel/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+name = "jupyterexcel"
+
+__version__ = '0.0.8'
+
+
+# Jupyter Extension points
+def _jupyter_nbextension_paths():
+    return [dict(
+        section="notebook",
+        src="",
+        dest="jupyterexcel")]
+
+def _jupyter_server_extension_paths():
+    return [{"module":"jupyterexcel.server_extension"}]
