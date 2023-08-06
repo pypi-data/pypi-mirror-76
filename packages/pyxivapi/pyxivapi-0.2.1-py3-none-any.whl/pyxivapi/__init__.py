770d6d84a221be8c6a790b749265192abab4c02a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:21 +0000

    Add package pyxivapi/0.2.1/pyxivapi-0.2.1-py3-none-any.whl

---
+++ packages/pyxivapi/pyxivapi-0.2.1-py3-none-any.whl/pyxivapi/__init__.py
@@ -0,0 +1,8 @@
+__title__ = 'pyxivapi'
+__author__ = 'Lethys'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 (c) Lethys'
+__version__ = '0.2.1'
+
+from .client import XIVAPIClient
+from .exceptions import *
