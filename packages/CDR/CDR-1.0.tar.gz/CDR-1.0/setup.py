6513618f90bdb3b9409291f8d958344682ba4850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:44 +0000

    Add package CDR/1.0/CDR-1.0.tar.gz

---
+++ packages/CDR/CDR-1.0.tar.gz/CDR-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name ="CDR",
+      version ="1.0",
+      description ="这是第一个对外发布的模块",
+      author ="cccc",
+      author_email ="2668754446@qq.com",
+      py_modules =["CDR.modu1","CDR.modu2"]
+      )
