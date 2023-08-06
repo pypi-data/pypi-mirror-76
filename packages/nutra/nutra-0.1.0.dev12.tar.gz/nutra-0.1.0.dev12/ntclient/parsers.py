5695ff6eab7e8fc427fa65c657df25f0ba3b2d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:57 +0000

    Add package nutra/0.1.0.dev12/nutra-0.1.0.dev12.tar.gz

---
+++ packages/nutra/nutra-0.1.0.dev12.tar.gz/nutra-0.1.0.dev12/ntclient/parsers.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 16:13:45 2020
+
+@author: shane
+
+This file is part of nutra, a nutrient analysis program.
+    https://github.com/nutratech/cli
+    https://pypi.org/project/nutra/
+
+nutra is an extensible nutrient analysis and composition application.
+Copyright (C) 2018-2020  Shane Jaroch
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os
+
+from .analyze import day_analyze, foods_analyze
+from .recipe import parse_recipes, recipe_analyze, recipes_overview
+from .search import search_results
+from .usda import (
+    list_nutrients,
+    sort_foods_by_kcal_nutrient_id,
+    sort_foods_by_nutrient_id,
+)
+
+
+def nutrients(args, arg_parser=None, **kwargs):
+    return list_nutrients()
+
+
+def search(args, arg_parser=None, subparsers=None):
+    """ Searches all dbs, foods, recipes, recents and favorites. """
+    if args.terms:
+        return search_results(words=args.terms)
+    else:
+        subparsers["search"].print_help()
+
+
+def sort(args, arg_parser=None, subparsers=None):
+    nutr_id = args.nutr_id
+    if not nutr_id:
+        subparsers["sort"].print_help()
+    elif args.kcal:
+        return sort_foods_by_kcal_nutrient_id(nutr_id)
+    else:
+        return sort_foods_by_nutrient_id(nutr_id)
+
+
+def analyze(args, arg_parser=None, subparsers=None):
+    food_ids = args.food_id
+    grams = args.grams
+
+    if not food_ids:
+        subparsers["anl"].print_help()
+    else:
+        return foods_analyze(food_ids, grams)
+
+
+def recipe(args, arg_parser=None, subparsers=None):
+    if args.recipe_id:
+        return recipe_analyze(args.recipe_id)
+    else:
+        return recipes_overview()
+
+
+def day(args, arg_parser=None, subparsers=None):
+    day_csv_paths = args.food_log
+    day_csv_paths = [os.path.expanduser(x) for x in day_csv_paths]
+    if args.rda:
+        rda_csv_path = os.path.expanduser(args.rda)
+
+    if not day_csv_paths:
+        subparsers["day"].print_help()
+    elif not args.rda:
+        return day_analyze(day_csv_paths)
+    else:
+        return day_analyze(day_csv_paths, rda_csv_path=rda_csv_path)
