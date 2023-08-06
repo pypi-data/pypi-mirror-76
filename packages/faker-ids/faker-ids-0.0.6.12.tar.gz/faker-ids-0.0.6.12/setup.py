18ca33399f4e5150854c83a28152d44ed9bda10b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:21 +0000

    Add package faker-ids/0.0.6.12/faker-ids-0.0.6.12.tar.gz

---
+++ packages/faker-ids/faker-ids-0.0.6.12.tar.gz/faker-ids-0.0.6.12/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='faker-ids',
+  version='0.0.6.12',
+  description='Fake id generator. Fetches upto 5 MILLION fake details like firstname, lastnam, emailid and mobile number.',
+  long_description=open('readme.txt').read(),# + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Balaji Betadur',
+  author_email='balajibetadur@gmail.com',
+#   classifiers=classifiers,
+  keywords=['fakeid','indians','indian fake ids','faker','fakes','fake ids','python','fake-ids python','fake indian ids python'],
+  license='MIT', 
+  packages=['faker_ids'], # find_packages(),
+  package_dir={'faker_ids': 'faker_ids'},
+  package_data={'faker_ids': ['data/*']},
+  include_package_data=True,
+  install_requires=['pandas'] 
+) 
