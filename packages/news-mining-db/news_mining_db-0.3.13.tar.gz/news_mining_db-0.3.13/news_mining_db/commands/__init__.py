31cde66370ff8c67697aa3152b9fc780f70b3cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:31 +0000

    Add package news-mining-db/0.3.13/news_mining_db-0.3.13.tar.gz

---
+++ packages/news-mining-db/news_mining_db-0.3.13.tar.gz/news_mining_db-0.3.13/news_mining_db/commands/__init__.py
@@ -0,0 +1,46 @@
+def news_mining_db(command: str):
+    if command == 'init':
+        _init()
+    elif command == 'upgrade':
+        _upgrade()
+    else:
+        print(f'no command {command}')
+
+
+def _init():
+    f = open('db_config.json', 'w')
+
+    template = '{' + ', '.join([f'"{config_key}": null' for config_key in _config_keys]) + '}'
+
+    f.write(template)
+
+    f.close()
+
+
+def _upgrade():
+    import os
+    import json
+    from alembic.config import Config
+    from alembic.command import upgrade
+
+    with open('db_config.json') as config_file:
+        config_data = json.load(config_file)
+
+        config = {}
+        for key in _config_keys:
+            if key in config_data and config_data[key]:
+                config[key] = config_data[key]
+
+    if 'db_uri' not in config.keys():
+        print('no db_uri supported.')
+        return
+
+    ini_file_path = os.path.join(os.path.split(os.path.dirname(os.path.abspath(__file__)))[0], 'alembic.ini')
+    config = Config(file_=ini_file_path)
+    config.set_main_option('sqlalchemy.url', config['db_uri'])
+    upgrade(config, 'head')
+
+
+_config_keys = [
+    'db_uri',
+]
