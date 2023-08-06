7abc1ad72d8807a3c15f8fb7b62641247b9ec76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:46 +0000

    Add package datacpp/0.6/datacpp-0.6-py3-none-any.whl

---
+++ packages/datacpp/datacpp-0.6-py3-none-any.whl/datacpp/dataloader.py
@@ -0,0 +1,12 @@
+import datacpp.pipeline as pipeline
+import datacpp.libdatacpp as data
+
+class data_batch:
+    def __init__(self,file_name,label_name,image_dir,label_dir,batch_size,label_type,heatmap=False):
+        self.label_type=label_type
+        self.heatmap=heatmap
+        self.pipe=pipeline.pipeline(batch_size,file_name,label_name,image_dir,label_dir,self.label_type,heatmap=self.heatmap)
+    def init_batch(self):
+        self.pipe.producer()
+    def next_batch(self,i):
+        return self.pipe.consumer(i)
