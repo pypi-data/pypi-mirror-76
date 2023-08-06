0ca23703c64a10843ed088b0002d1fc8f54d432a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:52 +0000

    Add package googling/0.0.2/googling-0.0.2-py3-none-any.whl

---
+++ packages/googling/googling-0.0.2-py3-none-any.whl/googling/googling.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   googling.py
+@Time    :   2020/08/10 13:35:31
+@Author  :   LCB 
+@Version :   1.0
+@Contact :   2281181505@qq.com
+@From    :   
+@function :  
+'''
+# here put the import lib
+
+def description():
+    print('thank you for installing our package!')
