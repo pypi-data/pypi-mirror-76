84af949762f4125eee82de43254cf30c9792e6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:36 +0000

    Add package fuzzycat/0.1.1/fuzzycat-0.1.1.tar.gz

---
+++ packages/fuzzycat/fuzzycat-0.1.1.tar.gz/fuzzycat-0.1.1/fuzzycat/main.py
@@ -0,0 +1,4 @@
+from fuzzycat import __version__
+
+def main():
+    print("hello fuzzycat {}".format(__version__))
---
+++ packages/fuzzycat/fuzzycat-0.1.1.tar.gz/fuzzycat-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+from fuzzycat import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+    setuptools.setup(
+        name="fuzzycat",
+        version=__version__,
+        author="Martin Czygan",
+        author_email="martin@archive.org",
+        description="Fuzzy matching utilities for scholarly metadata",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/miku/fuzzycat",
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6",
+        zip_safe=False,
+        entry_points={"console_scripts": ["fuzzycat=fuzzycat.main:main",],},
+        install_requires=[],
+        extras_require={"dev": ["black", "twine"],},
+    )
