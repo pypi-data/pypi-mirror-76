c6583b0ea43e212fed5097afac45e55a28a66705
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:02:05 +0000

    Add package itk-simpleitkfilters/0.1.0/itk_simpleitkfilters-0.1.0-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/itk-simpleitkfilters/itk_simpleitkfilters-0.1.0-cp35-cp35m-manylinux1_x86_64.whl/itk/Configuration/SimpleITKFiltersConfig.py
@@ -0,0 +1,18 @@
+depends = ('ITKPyBase', 'ITKImageFeature', 'ITKCommon', )
+templates = (
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterISS2', True, 'itk::Image< signed short,2 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterISS3', True, 'itk::Image< signed short,3 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterIUC2', True, 'itk::Image< unsigned char,2 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterIUC3', True, 'itk::Image< unsigned char,3 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterIUS2', True, 'itk::Image< unsigned short,2 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterIUS3', True, 'itk::Image< unsigned short,3 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterIF2', True, 'itk::Image< float,2 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterIF3', True, 'itk::Image< float,3 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterID2', True, 'itk::Image< double,2 >'),
+  ('HessianImageFilter', 'itk::HessianImageFilter', 'itkHessianImageFilterID3', True, 'itk::Image< double,3 >'),
+  ('ObjectnessMeasureImageFilter', 'itk::ObjectnessMeasureImageFilter', 'itkObjectnessMeasureImageFilterIF2IF2', True, 'itk::Image< float,2 >, itk::Image< float,2 >'),
+  ('ObjectnessMeasureImageFilter', 'itk::ObjectnessMeasureImageFilter', 'itkObjectnessMeasureImageFilterIF3IF3', True, 'itk::Image< float,3 >, itk::Image< float,3 >'),
+  ('ObjectnessMeasureImageFilter', 'itk::ObjectnessMeasureImageFilter', 'itkObjectnessMeasureImageFilterID2ID2', True, 'itk::Image< double,2 >, itk::Image< double,2 >'),
+  ('ObjectnessMeasureImageFilter', 'itk::ObjectnessMeasureImageFilter', 'itkObjectnessMeasureImageFilterID3ID3', True, 'itk::Image< double,3 >, itk::Image< double,3 >'),
+)
+snake_case_functions = ('hessian_image_filter', 'objectness_measure_image_filter', )
