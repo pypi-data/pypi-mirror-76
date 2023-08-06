95be0302c2a0172c72f3537a7aeca5a5b7560402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:11 +0000

    Add package sk-commonutils/0.1/sk_commonutils-0.1.tar.gz

---
+++ packages/sk-commonutils/sk_commonutils-0.1.tar.gz/sk_commonutils-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sk_commonutils',
+      version='0.1',
+      description='Simple Logging Utility',
+      packages=['sk_commonutils'],
+      author_email='sk.karthik315@gmail.com',
+      zip_safe=False)
---
+++ packages/sk-commonutils/sk_commonutils-0.1.tar.gz/sk_commonutils-0.1/sk_commonutils/logger.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+def logConsole(logType, msg):
+    timeStamp = datetime.now().strftime("%d.%m.%Y_%H:%M:%S")
+    print("[{0}] [{1:^7s}] - {2:s}".format(timeStamp, logType, msg))
+
+SUCCESS="SUCCESS"
+INFO="INFO"
+DEBUG="DEBUG"
+WARN="WARN"
+ERROR="ERROR"
+FATAL="FATAL"
+
+# logConsole("INFO", "Executed Successfully")
+# logConsole("WARN", "Your brain may be malfunctioning.")
+# logConsole("ERROR", "Your existance is PAIN")
+# logConsole("SUCCESS", "You've made it.... somehow....")
+# logConsole("FATAL", "Get lost!")
