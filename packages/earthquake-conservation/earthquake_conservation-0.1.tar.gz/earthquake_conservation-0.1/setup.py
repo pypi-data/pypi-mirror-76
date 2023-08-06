ee0db722f08062c6b7823e96697f64eaf3643394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:31 +0000

    Add package earthquake-conservation/0.1/earthquake_conservation-0.1.tar.gz

---
+++ packages/earthquake-conservation/earthquake_conservation-0.1.tar.gz/earthquake_conservation-0.1/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='earthquake_conservation', version='0.1', description='Saving earthquake data in a csv file', packages=['src'], author_email='clashofclansgamerarmoo@gmail.com', zip_safe=False)
