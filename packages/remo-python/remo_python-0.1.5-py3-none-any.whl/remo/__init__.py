a64b2b959ab72a68e98d3d65da8df89f74633657
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:23 +0000

    Add package remo-python/0.1.5/remo_python-0.1.5-py3-none-any.whl

---
+++ packages/remo-python/remo_python-0.1.5-py3-none-any.whl/remo/__init__.py
@@ -0,0 +1,39 @@
+from .domain import task, class_encodings, Dataset, Image, Annotation, AnnotationSet, Bbox, Segment
+from .sdk import SDK
+from .version import __version__
+
+_sdk = None
+
+
+def connect(server: str = '', email: str = '', password: str = '', viewer: str = 'browser'):
+    """
+    Connect to a remo server.
+    If no parameters are passed, it connects to a local running remo server. To connect to a remote remo, specify connection details.
+    
+    Args:
+        server: address where remo is running
+        email:  email address used for authentication
+        password: password used for authentication
+        (optional) viewer: viewer to use, one between 'browser', 'electron' and 'jupyter'
+    """
+    if not (server and email and password):
+        from .config import Config
+        config = Config.load()
+        server, email, password, viewer = config.server_url(), config.user_email, config.user_password, config.viewer
+
+    global _sdk
+    _sdk = SDK(server, email, password, viewer)
+    # set access to public SDK methods
+    import sys
+
+    is_public_sdk_method = lambda name: not name.startswith('_') and callable(getattr(_sdk, name))
+    functions = filter(is_public_sdk_method, dir(_sdk))
+    for name in functions:
+        setattr(sys.modules[__name__], name, getattr(_sdk, name))
+
+try:
+    connect()
+except:
+    print("""Warning: Can't find a running remo app. 
+To start a local server: 'python -m remo_app' and then run remo.connect(). 
+You can also use 'remo.connect()' to connect to a remote server""")
