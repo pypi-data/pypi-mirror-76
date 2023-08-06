22ef721c8197a335440d3e69634743ac57f12bad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:25 +0000

    Add package pygstuff/0.0.2/pygstuff-0.0.2.tar.gz

---
+++ packages/pygstuff/pygstuff-0.0.2.tar.gz/pygstuff-0.0.2/pygstuff/__init__.py
@@ -0,0 +1,10 @@
+'''Support for developing with pygame.
+
+Import pygstuff as pygs (pronounced "pigs").
+'''
+
+from .window import Window
+from .clock  import Clock
+from . import user
+from . import plot
+from .colors import HEX,RGB
