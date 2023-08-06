1f1c01c8bec1fc02d041aa09262e5c27d48b8c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:41 +0000

    Add package quizmake/0.1.0/quizmake-0.1.0.tar.gz

---
+++ packages/quizmake/quizmake-0.1.0.tar.gz/quizmake-0.1.0/quizmake/__init__.py
@@ -0,0 +1,16 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Initiate the file."""
+__author__ = "Jason Nguyen"
+__copyright__ = "Copyright 2020, Jason Nguyen"
+__credits__ = ["Jason Nguyen"]
+__license__ = "MIT"
+__maintainer__ = "Jason Nguyen"
+__email__ = "jnguye21@uoguelph.ca"
+__status__ = "Planning"
+__version__ = "0.1.0"
+
+# from .core import main
+# from .corpus import Corpus, corn
+# __all__ = ["Corpus", "corn", "main"]
