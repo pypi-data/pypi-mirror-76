40a1d7a1fcaaf00da26888d8fecb50b174b9c930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:15 +0000

    Add package amazon-braket-schemas/1.0.0/amazon-braket-schemas-1.0.0.tar.gz

---
+++ packages/amazon-braket-schemas/amazon-braket-schemas-1.0.0.tar.gz/amazon-braket-schemas-1.0.0/setup.py
@@ -0,0 +1,43 @@
+# Copyright 2019-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from setuptools import find_namespace_packages, setup
+
+with open("src/braket/_schemas/_version.py") as f:
+    version = f.readlines()[-1].split()[-1].strip("\"'")
+
+setup(
+    name="amazon-braket-schemas",
+    version=version,
+    license="Apache License 2.0",
+    python_requires=">= 3.7",
+    packages=find_namespace_packages(where="src", exclude=("test",)),
+    package_dir={"": "src"},
+    install_requires=["pydantic"],
+    extras_require={
+        "test": [
+            "black",
+            "flake8",
+            "isort",
+            "pre-commit",
+            "pylint",
+            "pytest",
+            "pytest-cov",
+            "pytest-rerunfailures",
+            "pytest-xdist",
+            "sphinx < 3.0.0",
+            "sphinx-rtd-theme",
+            "tox",
+        ]
+    },
+)
