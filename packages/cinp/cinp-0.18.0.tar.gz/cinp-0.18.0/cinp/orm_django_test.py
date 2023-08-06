20b9d48515900eeea44c12434596f39ddf9a9751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:19 +0000

    Add package cinp/0.18.0/cinp-0.18.0.tar.gz

---
+++ packages/cinp/cinp-0.18.0.tar.gz/cinp-0.18.0/cinp/orm_django_test.py
@@ -0,0 +1,8 @@
+
+#  The checkAuth lambda/eval mess has a way of trying to use the last checkAuth in
+#  for everything (or everything in that model.py), make sure there is a test to guard
+#  aginst that
+
+
+def test_django_stuff():
+  pass
