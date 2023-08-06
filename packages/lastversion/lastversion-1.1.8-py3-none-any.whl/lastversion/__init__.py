22a5a6eeed5c376f2928cb126538e421a694e0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:55 +0000

    Add package lastversion/1.1.8/lastversion-1.1.8-py3-none-any.whl

---
+++ packages/lastversion/lastversion-1.1.8-py3-none-any.whl/lastversion/__init__.py
@@ -0,0 +1,12 @@
+"""
+lastversion
+==========
+License: BSD, see LICENSE for more details.
+"""
+
+__author__ = "Danila Vershinin"
+
+from .lastversion import main
+from .__about__ import (
+    __version__,
+)
