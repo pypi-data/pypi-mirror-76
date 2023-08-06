56c4057c894c1038775564cfd56fa0b007827ad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:35 +0000

    Add package bxa/3.4.2/bxa-3.4.2.tar.gz

---
+++ packages/bxa/bxa-3.4.2.tar.gz/bxa-3.4.2/README.rst
@@ -0,0 +1,14 @@
+BXA - Bayesian X-ray Analysis
+==============================
+
+BXA connects the nested sampling algorithm MultiNest to the 
+X-ray spectral analysis environments Xspec/Sherpa 
+for **Bayesian Parameter Estimation** and **Model comparison**.
+
+Documentation
+----------------
+
+See the `documentation <http://johannesbuchner.github.io/BXA/>`_ at http://johannesbuchner.github.io/BXA/
+
+
+
---
+++ packages/bxa/bxa-3.4.2.tar.gz/bxa-3.4.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import setuptools
+from distutils.core import setup
+
+try:
+	with open('README.rst') as f:
+		long_description = f.read()
+except IOError:
+	long_description = ''
+
+setup(name='bxa',
+	version='3.4.2',
+	author='Johannes Buchner',
+	url='https://github.com/JohannesBuchner/BXA/',
+	author_email='johannes.buchner.acad@gmx.com',
+	description='Bayesian X-ray spectral analysis',
+	long_description=long_description,
+	packages=['bxa.xspec', 'bxa.sherpa', 'bxa.sherpa.background', 'bxa'],
+	requires=['pymultinest','numpy', 'tqdm', 'corner'],
+	include_package_data=True,
+)
+
