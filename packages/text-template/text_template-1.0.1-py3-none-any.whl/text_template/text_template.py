3d91f6a5c084094c3b7a6610b6518d98a2318825
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:56 +0000

    Add package text-template/1.0.1/text_template-1.0.1-py3-none-any.whl

---
+++ packages/text-template/text_template-1.0.1-py3-none-any.whl/text_template/text_template.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+import os
+from string import Template
+
+from ansi_colours import AnsiColours
+
+
+class TextTemplate(AnsiColours):
+    @classmethod
+    def render(cls, template, **kwargs):
+        if not os.path.isfile(template):
+            raise OSError("Template was not found: " + template)
+        with open(template, 'r') as content_file:
+            content = content_file.read()
+            t = Template(content)
+            kwargs.update(AnsiColours.colours)
+            return t.substitute(**kwargs)
