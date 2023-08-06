0aa7de91d5e6bb44d820d5b8cab3e13d10dc14ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:20 +0000

    Add package aioextensions/20.8.11546/aioextensions-20.8.11546.tar.gz

---
+++ packages/aioextensions/aioextensions-20.8.11546.tar.gz/aioextensions-20.8.11546/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['aioextensions']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'aioextensions',
+    'version': '20.8.11546',
+    'description': '',
+    'long_description': '# AIO-utils\n\n[![PyPI](https://img.shields.io/pypi/v/aioextensions)](https://pypi.org/project/aioextensions)\n[![Status](https://img.shields.io/pypi/status/aioextensions)](https://pypi.org/project/aioextensions)\n[![Downloads](https://img.shields.io/pypi/dm/aioextensions)](https://pypi.org/project/aioextensions)\n[![License](https://img.shields.io/pypi/l/aioextensions)](../LICENSE)\n',
+    'author': 'Kevin Amado',
+    'author_email': 'kamadorueda@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/kamadorueda/aioextensions',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
