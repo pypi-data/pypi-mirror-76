17b477ff9b35dcbc7505ac45d6419067d11316c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:27 +0000

    Add package yaml2jsonnet/0.6.0/yaml2jsonnet-0.6.0-py3-none-any.whl

---
+++ packages/yaml2jsonnet/yaml2jsonnet-0.6.0-py3-none-any.whl/yaml2jsonnet/cli.py
@@ -0,0 +1,28 @@
+"""Functions for helping run from the command line"""
+
+import argparse
+import logging
+import sys
+from typing import Sequence
+
+from yaml2jsonnet.yaml2jsonnet import convert_yaml
+
+log = logging.getLogger(__name__)
+
+
+def parse_args(args: Sequence[str] = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        prog="yaml2jsonnet",
+        description="Convert a YAML file to a JSONNET file, preserving comments",
+    )
+    parser.add_argument("yaml", type=argparse.FileType("r"))
+    parser.add_argument("--out", default=sys.stdout, type=argparse.FileType("w"))
+    parser.add_argument("-v", action="count", default=0)
+
+    return parser.parse_args(args)
+
+
+def run(args: argparse.Namespace) -> None:
+    log.debug("Read yaml into memory")
+    yaml_data = args.yaml.read()
+    convert_yaml(yaml_data, args.out)
