99e3db21cde6047aace9023208b783a5d883db3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package pytablewriter/0.56.1/pytablewriter-0.56.1.tar.gz

---
+++ packages/pytablewriter/pytablewriter-0.56.1.tar.gz/pytablewriter-0.56.1/test/test_table_format.py
@@ -0,0 +1,58 @@
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+import pytest
+
+from pytablewriter import FormatAttr, TableFormat
+
+
+class Test_TableFormat_search_table_format:
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            [
+                FormatAttr.TEXT,
+                [
+                    TableFormat.BOLD_UNICODE,
+                    TableFormat.BORDERLESS,
+                    TableFormat.CSS,
+                    TableFormat.CSV,
+                    TableFormat.HTML,
+                    TableFormat.JAVASCRIPT,
+                    TableFormat.JSON,
+                    TableFormat.JSON_LINES,
+                    TableFormat.LATEX_MATRIX,
+                    TableFormat.LATEX_TABLE,
+                    TableFormat.LTSV,
+                    TableFormat.MARKDOWN,
+                    TableFormat.MEDIAWIKI,
+                    TableFormat.NUMPY,
+                    TableFormat.PANDAS,
+                    TableFormat.PYTHON,
+                    TableFormat.RST_CSV_TABLE,
+                    TableFormat.RST_GRID_TABLE,
+                    TableFormat.RST_SIMPLE_TABLE,
+                    TableFormat.SPACE_ALIGNED,
+                    TableFormat.TOML,
+                    TableFormat.TSV,
+                    TableFormat.UNICODE,
+                    TableFormat.YAML,
+                ],
+            ],
+            [FormatAttr.BIN, [TableFormat.EXCEL_XLS, TableFormat.EXCEL_XLSX, TableFormat.SQLITE]],
+            [FormatAttr.API, [TableFormat.ELASTICSEARCH]],
+            [0, []],
+        ],
+    )
+    def test_normal(self, value, expected):
+        assert set(TableFormat.find_all_attr(value)) == set(expected)
+
+
+class Test_TableFormat_from_name:
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [["csv", TableFormat.CSV], ["CSV", TableFormat.CSV], ["excel", TableFormat.EXCEL_XLSX],],
+    )
+    def test_normal(self, value, expected):
+        assert TableFormat.from_name(value) == expected
