5bdefd322d4316cc0094bdea26969d2c3f6d2d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:11 +0000

    Add package wikiarticle/0.0.2/wikiarticle-0.0.2-py3-none-any.whl

---
+++ packages/wikiarticle/wikiarticle-0.0.2-py3-none-any.whl/wikiarticle/__init__.py
@@ -0,0 +1,7 @@
+from .wikiarticle import WikiArticle
+
+from .models.wiki_text import WikiText
+from .models.wiki_quote import WikiQuote
+from .models.wiki_image import WikiImage
+
+from .models.text_type import TextType
