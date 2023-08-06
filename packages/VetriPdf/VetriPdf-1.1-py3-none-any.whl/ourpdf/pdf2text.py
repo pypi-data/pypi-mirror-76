ce47d1d8abbab0108ff13e69f47879ede079c28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:14 +0000

    Add package VetriPdf/1.1/VetriPdf-1.1-py3-none-any.whl

---
+++ packages/VetriPdf/VetriPdf-1.1-py3-none-any.whl/ourpdf/pdf2text.py
@@ -0,0 +1,17 @@
+""" This module provides a function to conver PDF to TEXT. """
+
+
+class Converter:
+    """ This simple converter class converts PDF to Text """
+
+    def convert(self, path):
+        """ 
+        Covert given PDF to TEXT.
+
+            Parameter:
+            path(str) : The path to the PDF File.
+
+            Returns:
+            str  : The content of the PDF file in Text.
+        """
+        print("Pdf2Text")
