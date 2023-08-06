b0943a077bdf3b6158a4aae9ebb85f7ead091dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:55 +0000

    Add package fwiffos/0.0.8/fwiffos-0.0.8-py3-none-any.whl

---
+++ packages/fwiffos/fwiffos-0.0.8-py3-none-any.whl/fwiffos/core/shell_capabilities.py
@@ -0,0 +1,34 @@
+from . import version
+
+class Calculator(object):
+
+    def add(self, x, y):
+      return x + y
+
+    def multiply(self, x, y):
+      return x * y
+
+class FS(object):
+
+    def __init__(self):
+        self.filesystem = {'/': ['README']}
+
+    def ls(self, cwd='/'):
+        print(self.filesystem[cwd])
+
+    def commands(self):
+        return self._commands
+
+class Commands(object):
+    def __init__(self):
+        fs = FS()
+        calc = Calculator()
+        self._version = version
+        self.__commands = {
+            'ls': fs.ls,
+            'add': calc.add,
+            'mult': calc.multiply,
+        }
+
+    def exec(self):
+        return self.__commands
