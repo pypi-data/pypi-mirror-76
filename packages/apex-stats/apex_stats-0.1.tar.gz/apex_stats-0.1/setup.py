965c4ec67da879a1dde18ffedea878df751d8708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:30:14 +0000

    Add package apex-stats/0.1/apex_stats-0.1.tar.gz

---
+++ packages/apex-stats/apex_stats-0.1.tar.gz/apex_stats-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='apex_stats',
+      version='0.1',
+      description='Tracker.gg api wrapper',
+      url='http://github.com/yamozha/apex-stats',
+      author='yamozha',
+      author_email='yamozha16@protonmail.ch',
+      license='MIT',
+      packages=['apex_stats'],
+      zip_safe=False)
