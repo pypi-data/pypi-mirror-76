3964c347c3175bf8ea0af1923457526c7c324677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:25 +0000

    Add package vedant-mehta-package/1.0.0/vedant_mehta_package-1.0.0.tar.gz

---
+++ packages/vedant-mehta-package/vedant_mehta_package-1.0.0.tar.gz/vedant_mehta_package-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="vedant_mehta_package",
+    version="1.0.0",
+    author="Vedant Mehta",
+    author_email="veds.mehta@gmail.com",
+    description="first package. Nothing to see here...",
+    long_description="Hello, World. This is my first python package. Again, nothing to see here",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
