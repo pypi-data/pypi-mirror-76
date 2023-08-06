337015a1cca649f811186b7a779732a1ccfd5a33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:36 +0000

    Add package prohosting24api/0.2/prohosting24api-0.2.tar.gz

---
+++ packages/prohosting24api/prohosting24api-0.2.tar.gz/prohosting24api-0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+setuptools.setup(
+    name="prohosting24api",
+    version="0.2",
+    author="AdriBloober",
+    author_email="adrianbloober@gmail.com",
+    description="Communicate with the ProHosting24 internal api.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AdriBloober/Python_ProHosting24API",
+    packages=["prohosting24"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
