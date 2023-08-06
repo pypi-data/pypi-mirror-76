0472555b49d06adbcaa751f7b25558e3734bca92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:07 +0000

    Add package tapdance/0.9.0.172.dev0/tapdance-0.9.0.172.dev0.tar.gz

---
+++ packages/tapdance/tapdance-0.9.0.172.dev0.tar.gz/tapdance-0.9.0.172.dev0/tapdance/cli.py
@@ -0,0 +1,27 @@
+"""tapdance.cli - Defines the command-line interface (CLI) for tapdance."""
+
+import fire
+
+from tapdance.config import print_version
+from tapdance.docker import build_all_images, build_image
+from tapdance.install_helper import install
+from tapdance.plans import plan
+from tapdance.syncs import sync
+
+
+def main():
+    """Run the tapdance CLI."""
+    fire.Fire(
+        {
+            "plan": plan,
+            "sync": sync,
+            "install": install,
+            "build_all_images": build_all_images,
+            "build_image": build_image,
+            "--version": print_version,
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
