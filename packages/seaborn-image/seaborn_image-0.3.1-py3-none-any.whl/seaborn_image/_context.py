b9a8cc1d6ce108482ec3e694e9d8a4ef7db7f8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:32 +0000

    Add package seaborn-image/0.3.1/seaborn_image-0.3.1-py3-none-any.whl

---
+++ packages/seaborn-image/seaborn_image-0.3.1-py3-none-any.whl/seaborn_image/_context.py
@@ -0,0 +1,135 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.cm import register_cmap
+
+from ._colormap import _CMAP_QUAL
+
+# TODO implement a set() fuction for all underlying set_*
+
+
+def set_context(mode="talk", fontfamily="arial", fontweight="bold", rc=None):
+    """
+    Set context for images with mode, fontfamily and fontweight. Additional,
+    rc params can also be passed as dict
+
+    Args:
+        mode (str, optional): context mode. Options are 'paper', 'notebook',
+            'presentation', 'talk' and 'poster'. Defaults to "talk".
+        fontfamily (str, optional): font-family to use. Defaults to "arial".
+        fontweight (str, optional): font-weight to use. Options include 'normal'
+            and 'bold'. Defaults to "bold".
+        rc (dict, optional): additional rc params to be passed to matplotlib.
+            Defaults to None.
+
+    Example:
+        >>> import seaborn_image as isns
+        >>> isns.set_context(mode="poster", fontfamily="sans-serif")
+        >>> isns.set_context(rc={"axes.edgecolor": "red"})
+
+    """
+    # plt.rc("axes.spines", left=False, right=False, top=False, bottom=False)
+    if mode == "paper":
+        plt.rc("axes", linewidth=1.5)
+        plt.rc("axes", titlesize=15, titleweight=fontweight)
+        plt.rc("axes", labelsize=15, labelweight=fontweight)
+        mpl.rcParams.update({"figure.constrained_layout.wspace": 0.2})
+        font = {"family": fontfamily, "weight": fontweight, "size": 10}
+
+    if mode == "notebook" or mode == "presentation" or mode == "talk":
+        plt.rc("axes", linewidth=2.5)
+        plt.rc("axes", titlesize=20, titleweight=fontweight)
+        plt.rc("axes", labelsize=20, labelweight=fontweight)
+        mpl.rcParams.update({"figure.constrained_layout.wspace": 0.3})
+        font = {"family": fontfamily, "weight": fontweight, "size": 15}
+
+    if mode == "poster":
+        plt.rc("axes", linewidth=3.5)
+        plt.rc("axes", titlesize=25, titleweight=fontweight)
+        plt.rc("axes", labelsize=25, labelweight=fontweight)
+        mpl.rcParams.update({"figure.constrained_layout.wspace": 0.4})
+        font = {"family": fontfamily, "weight": fontweight, "size": 20}
+
+    plt.rc("font", **font)
+
+    if rc is not None:
+        assert isinstance(rc, dict), "'rc' must be a dict"
+        mpl.rcParams.update(rc)
+
+
+def set_save_context(dpi=300):
+    """
+    Set dpi for saving figures to disk
+
+    Args:
+        dpi (int, optional): image dpi. Defaults to 300.
+
+    Example:
+        >>> import seaborn_image as isns
+        >>> isns.set_save_context(dpi=200)
+
+    """
+    plt.rc("savefig", dpi=dpi, bbox="tight")
+
+
+def reset_defaults():
+    """
+    Reset rcParams to matplotlib defaults
+
+    Example:
+        >>> import seaborn_image as isns
+        >>> isns.reset_deafults()
+
+    """
+    mpl.rcParams.update(mpl.rcParamsDefault)
+
+
+def set_image(cmap="ice", origin="lower", interpolation="nearest"):
+    """
+    Set deaults for plotting images
+
+    Args:
+        cmap (str, optional): Colormap to use accross images.
+            Defaults to "viridis".
+        origin (str, optional): image origin - same as in matplotlib imshow.
+            Defaults to "lower".
+        interpolation (str, optional): image interpolation - same as in matplotlib imshow.
+            Defaults to "nearest".
+
+    Example:
+        >>> import seaborn_image as isns
+        >>> isns.set_image(cmap="inferno", interpolation="bicubic")
+
+    """
+
+    if cmap in _CMAP_QUAL.keys():  # doesn't work currently
+        cmap_mpl = _CMAP_QUAL.get(cmap).mpl_colormap
+        register_cmap(name=cmap, cmap=cmap_mpl)
+
+    plt.rc("image", cmap=cmap, origin=origin, interpolation=interpolation)
+
+
+def set_scalebar(rc=None):
+    """Set scalebar properties such as color, scale_loc,
+    height_fraction, length_fraction, box_alpha, etc
+
+    Args:
+        rc (dict, optional): dictionary of scalebar properties to be set.
+            Defaults to None.
+
+    Example:
+        >>> import seaborn_image as isns
+        >>> isns.set_scalebar({"color":"red"})
+        >>> isns.set_scalebar({"scale_loc":"bottom"})
+
+    """
+    mpl.rcParams.update({"scalebar.color": "white"})
+    mpl.rcParams.update({"scalebar.height_fraction": 0.05})
+    mpl.rcParams.update({"scalebar.length_fraction": 0.3})
+    mpl.rcParams.update({"scalebar.scale_loc": "top"})
+    mpl.rcParams.update({"scalebar.location": "lower right"})
+    mpl.rcParams.update({"scalebar.box_alpha": 0})
+
+    if rc is not None:
+        assert isinstance(rc, dict), "'rc' must be a dict"
+        for key, value in rc.items():
+            mpl.rcParams.update({f"scalebar.{key}": value})
---
+++ packages/seaborn-image/seaborn_image-0.3.1-py3-none-any.whl/seaborn_image/_general.py
@@ -0,0 +1,288 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.stats as ss
+from matplotlib import gridspec
+from matplotlib.axes import Axes
+from matplotlib.cm import get_cmap
+from matplotlib.colors import Colormap
+
+from ._colormap import _CMAP_QUAL
+from ._core import _SetupImage
+
+
+def imgplot(
+    data,
+    ax=None,
+    cmap=None,
+    vmin=None,
+    vmax=None,
+    dx=None,
+    units=None,
+    dimension=None,
+    describe=True,
+    cbar=True,
+    cbar_label=None,
+    cbar_fontdict=None,
+    cbar_ticks=None,
+    showticks=False,
+    title=None,
+    title_fontdict=None,
+):
+    """
+
+    Plot data as a 2-D image with options to add scalebar, colorbar, title.
+
+    Args:
+        data: Image data (array-like). Supported array shapes are all
+            `matplotlib.pyplot.imshow` array shapes
+        ax (`matplotlib.axes.Axes`, optional): Matplotlib axes to plot image on.
+            If None, figure and axes are auto-generated. Defaults to None.
+        cmap (str or `matplotlib.colors.Colormap`, optional): Colormap for image.
+            Can be a seaborn-image colormap or default matplotlib colormaps or
+            any other colormap converted to a matplotlib colormap. Defaults to None.
+        vmin (float, optional): Minimum data value that colormap covers. Defaults to None.
+        vmax (float, optional): Maximum data value that colormap covers. Defaults to None.
+        dx (float, optional): Size per pixel of the image data. If scalebar
+            is required, `dx` and `units` must be sepcified. Defaults to None.
+        units (str, optional): Units of `dx`. Defaults to None.
+        dimension (str, optional): dimension of `dx` and `units`.
+            Options include (similar to `matplotlib_scalebar`):
+                - "si" : scale bar showing km, m, cm, etc.
+                - "imperial" : scale bar showing in, ft, yd, mi, etc.
+                - "si-reciprocal" : scale bar showing 1/m, 1/cm, etc.
+                - "angle" : scale bar showing °, ʹ (minute of arc) or ʹʹ (second of arc).
+                - "pixel" : scale bar showing px, kpx, Mpx, etc.
+            Defaults to None.
+        describe (bool, optional): Brief statistical description of the data.
+            Defaults to True.
+        cbar (bool, optional): Specify if a colorbar is required or not.
+            Defaults to True.
+        cbar_label (str, optional): Colorbar label. Defaults to None.
+        cbar_fontdict (dict, optional): Font specifications for colorbar label - `cbar_label`.
+            Defaults to None.
+        cbar_ticks (list, optional): List of colorbar ticks. If None, min and max of
+            the data are used. If `vmin` and `vmax` are specified, `vmin` and `vmax` values
+            are used for colorbar ticks. Defaults to None.
+        showticks (bool, optional): Show image x-y axis ticks. Defaults to False.
+        title (str, optional): Image title. Defaults to None.
+        title_fontdict (dict, optional): [Font specifications for `title`. Defaults to None.
+
+    Raises:
+        TypeError: if `cmap` is not str or `matplotlib.colors.Colormap`
+        TypeError: if `ax` is not `matplotlib.axes.Axes`
+        TypeError: if `cbar` is not bool
+        TypeError: if `cbar_label` is not str
+        TypeError: if `cbar_fontdict` is not dict
+        TypeError: if `showticks` is not bool
+        TypeError: if `title` is not str
+        TypeError: if `title_fontdict` is not dict
+
+    Returns:
+        (tuple): tuple containing:
+            (`matplotlib.figure.Figure`): Matplotlib figure.
+            (`matplotlib.axes.Axes`): Matplotlib axes where the image is drawn.
+            (`matplotlib.axes.Axes`): Colorbar axes
+
+    Example:
+        >>> import seaborn_image as isns
+        >>> isns.imgplot(data)
+        >>> isns.imgplot(data, dx=2, units="nm") # add a scalebar
+        >>> isns.imgplot(data, cmap="deep") # specify a colormap
+
+
+    """
+
+    # add vmin, vmax, dx, units to checks
+    if cmap is not None:
+        if not isinstance(cmap, (str, Colormap)):
+            raise TypeError
+    if ax is not None:
+        if not isinstance(ax, Axes):
+            raise TypeError
+    if not isinstance(describe, bool):
+        raise TypeError
+    if not isinstance(cbar, bool):
+        raise TypeError
+    if cbar_label is not None:
+        if not isinstance(cbar_label, str):
+            raise TypeError
+    if cbar_fontdict is not None:
+        if not isinstance(cbar_fontdict, dict):
+            raise TypeError
+    if not isinstance(showticks, bool):
+        raise TypeError
+    if title is not None:
+        if not isinstance(title, str):
+            raise TypeError
+    if title_fontdict is not None:
+        if not isinstance(title_fontdict, dict):
+            raise TypeError
+
+    img_plotter = _SetupImage(
+        data=data,
+        ax=ax,
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+        dx=dx,
+        units=units,
+        dimension=dimension,
+        cbar=cbar,
+        cbar_label=cbar_label,
+        cbar_fontdict=cbar_fontdict,
+        cbar_ticks=cbar_ticks,
+        showticks=showticks,
+        title=title,
+        fontdict=title_fontdict,
+    )
+
+    f, ax, cax = img_plotter.plot()
+
+    if describe:
+        result = ss.describe(data.flatten())
+        print(f"No. of Obs. : {result.nobs}")
+        print(f"Min. Value : {result.minmax[0]}")
+        print(f"Max. Value : {result.minmax[1]}")
+        print(f"Mean : {result.mean}")
+        print(f"Variance : {result.variance}")
+        print(f"Skewness : {result.skewness}")
+
+    return f, ax, cax
+
+
+def imghist(
+    data,
+    cmap=None,
+    bins=None,
+    vmin=None,
+    vmax=None,
+    dx=None,
+    units=None,
+    dimension=None,
+    describe=True,
+    cbar=True,
+    cbar_label=None,
+    cbar_fontdict=None,
+    cbar_ticks=None,
+    showticks=False,
+    title=None,
+    title_fontdict=None,
+):
+    """Plot data as a 2-D image with histogram showing the distribution of
+    the data. Options to add scalebar, colorbar, title.
+
+    Args:
+        data: Image data (array-like). Supported array shapes are all
+            `matplotlib.pyplot.imshow` array shapes
+        cmap (str or `matplotlib.colors.Colormap`, optional): Colormap for image.
+            Can be a seaborn-image colormap or default matplotlib colormaps or
+            any other colormap converted to a matplotlib colormap. Defaults to None.
+        bins (int, optional): Number of histogram bins. Defaults to None. If None, 'auto'
+            is used.
+        vmin (float, optional): Minimum data value that colormap covers. Defaults to None.
+        vmax (float, optional): Maximum data value that colormap covers. Defaults to None.
+        dx (float, optional): Size per pixel of the image data. If scalebar
+            is required, `dx` and `units` must be sepcified. Defaults to None.
+        units (str, optional): Units of `dx`. Defaults to None.
+        dimension (str, optional): dimension of `dx` and `units`.
+            Options include :
+                - "si" : scale bar showing km, m, cm, etc.
+                - "imperial" : scale bar showing in, ft, yd, mi, etc.
+                - "si-reciprocal" : scale bar showing 1/m, 1/cm, etc.
+                - "angle" : scale bar showing °, ʹ (minute of arc) or ʹʹ (second of arc).
+                - "pixel" : scale bar showing px, kpx, Mpx, etc.
+            Defaults to None.
+        describe (bool, optional): Brief statistical description of the data.
+            Defaults to True.
+        cbar (bool, optional): Specify if a colorbar is required or not.
+            Defaults to True.
+        cbar_label (str, optional): Colorbar label. Defaults to None.
+        cbar_fontdict (dict, optional): Font specifications for colorbar label - `cbar_label`.
+            Defaults to None.
+        cbar_ticks (list, optional): List of colorbar ticks. If None, min and max of
+            the data are used. If `vmin` and `vmax` are specified, `vmin` and `vmax` values
+            are used for colorbar ticks. Defaults to None.
+        showticks (bool, optional): Show image x-y axis ticks. Defaults to False.
+        title (str, optional): Image title. Defaults to None.
+        title_fontdict (dict, optional): [Font specifications for `title`. Defaults to None.
+
+    Raises:
+        TypeError: if `bins` is not int
+
+    Returns:
+        (tuple): tuple containing:
+            (`matplotlib.figure.Figure`): Matplotlib figure.
+            (tuple): tuple containing:
+                (`matplotlib.axes.Axes`): Matplotlib axes where the image is drawn.
+                (`matplotlib.axes.Axes`): Matplotlib axes where the histogram is drawn.
+            (`matplotlib.axes.Axes`): Colorbar axes
+
+    Example:
+        >>> import seaborn_image as isns
+        >>> isns.imghist(data)
+        >>> isns.imghist(data, bins=300) # specify the number of histogram bins
+        >>> isns.imghist(data, dx=2, units="nm") # add a scalebar
+        >>> isns.imghist(data, cmap="deep") # specify a colormap
+    """
+
+    if bins is None:
+        bins = "auto"
+    else:
+        if not isinstance(bins, int):
+            raise TypeError("'bins' must be a positive integer")
+        if not bins > 0:
+            raise ValueError("'bins' must be a positive integer")
+
+    f = plt.figure(figsize=(10, 6))  # TODO make figsize user defined
+    gs = gridspec.GridSpec(1, 2, width_ratios=[5, 1], figure=f)
+
+    ax1 = f.add_subplot(gs[0])
+
+    f, ax1, cax = imgplot(
+        data,
+        ax=ax1,
+        cmap=cmap,
+        vmin=vmin,
+        vmax=vmax,
+        dx=dx,
+        units=units,
+        dimension=dimension,
+        describe=describe,
+        cbar=cbar,
+        cbar_label=cbar_label,
+        cbar_fontdict=cbar_fontdict,
+        cbar_ticks=cbar_ticks,
+        showticks=showticks,
+        title=title,
+        title_fontdict=title_fontdict,
+    )
+
+    ax2 = f.add_subplot(gs[1], sharey=cax)
+
+    n, bins, patches = ax2.hist(
+        data.ravel(), bins=bins, density=True, orientation="horizontal"
+    )
+
+    ax2.get_xaxis().set_visible(False)
+    ax2.get_yaxis().set_visible(False)
+    ax2.set_frame_on(False)
+
+    if cmap is None:
+        # cm = _CMAP_QUAL.get("deep").mpl_colormap
+        cm = get_cmap()
+    else:
+        if cmap in _CMAP_QUAL.keys():
+            cm = _CMAP_QUAL.get(cmap).mpl_colormap
+        else:
+            cm = plt.cm.get_cmap(cmap)
+
+    bin_centers = bins[:-1] + bins[1:]
+
+    # scale values to interval [0,1]
+    col = bin_centers - np.min(bin_centers)
+    col /= np.max(col)
+
+    for c, p in zip(col, patches):
+        plt.setp(p, "facecolor", cm(c))
+
+    return f, (ax1, ax2), cax
