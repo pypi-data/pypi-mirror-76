49bbc090bc1b90ff18dc75a885ed809df55b2b94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:47 +0000

    Add package cloudformation-cli/0.1.7/cloudformation_cli-0.1.7-py3-none-any.whl

---
+++ packages/cloudformation-cli/cloudformation_cli-0.1.7-py3-none-any.whl/rpdk/core/contract/suite/handler_update.py
@@ -0,0 +1,54 @@
+# fixture and parameter have the same name
+# pylint: disable=redefined-outer-name
+
+import pytest
+
+# WARNING: contract tests should use fully qualified imports to avoid issues
+# when being loaded by pytest
+from rpdk.core.contract.interface import Action, OperationStatus
+from rpdk.core.contract.suite.handler_commons import (
+    test_model_in_list,
+    test_read_success,
+)
+
+
+@pytest.fixture(scope="module")
+def updated_resource(resource_client):
+    create_request = model = resource_client.generate_create_example()
+    try:
+        _status, response, _error = resource_client.call_and_assert(
+            Action.CREATE, OperationStatus.SUCCESS, create_request
+        )
+        created_model = model = response["resourceModel"]
+        update_request = resource_client.generate_update_example(created_model)
+        _status, response, _error = resource_client.call_and_assert(
+            Action.UPDATE, OperationStatus.SUCCESS, update_request, created_model
+        )
+        updated_model = response["resourceModel"]
+        yield create_request, created_model, update_request, updated_model
+    finally:
+        resource_client.call_and_assert(Action.DELETE, OperationStatus.SUCCESS, model)
+
+
+@pytest.mark.update
+@pytest.mark.read
+def contract_update_read_success(updated_resource, resource_client):
+    # should be able to use the created model
+    # to read since physical resource id is immutable
+    _create_request, _created_model, _update_request, updated_model = updated_resource
+    assert resource_client.is_primary_identifier_equal(
+        resource_client.primary_identifier_paths, _created_model, updated_model
+    )
+    test_read_success(resource_client, updated_model)
+
+
+@pytest.mark.update
+@pytest.mark.list
+def contract_update_list_success(updated_resource, resource_client):
+    # should be able to use the created model
+    # to read since physical resource id is immutable
+    _create_request, _created_model, _update_request, updated_model = updated_resource
+    assert resource_client.is_primary_identifier_equal(
+        resource_client.primary_identifier_paths, _created_model, updated_model
+    )
+    assert test_model_in_list(resource_client, updated_model)
