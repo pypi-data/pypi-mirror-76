4a332b8fe0dc1e4951963ab794ef3cefdd54156d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:20 +0000

    Add package manage-fastapi/0.1.1/manage_fastapi-0.1.1-py3-none-any.whl

---
+++ packages/manage-fastapi/manage_fastapi-0.1.1-py3-none-any.whl/manage_fastapi/main.py
@@ -0,0 +1,16 @@
+import typer
+from .utils import start_app, start_project
+
+app = typer.Typer(add_completion=False, help="Managing FastAPI projects made easy!")
+
+
+@app.command(help="Creates a project with the given name.")
+def startproject(projectname: str = typer.Argument(...)):
+    start_project(projectname)
+    typer.echo(f"Project {projectname} created successfully!")
+
+
+@app.command(help="Creates a app with the given name.")
+def startapp(appname: str = typer.Argument(...)):
+    start_app(appname)
+    typer.echo(f"Application {appname} created successfully!")
