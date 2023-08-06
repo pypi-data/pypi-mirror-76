f2346c8113e4e5426ce8fd2aead14e9a99a547f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:10 +0000

    Add package django-otp-agents/1.0.0/django-otp-agents-1.0.0.tar.gz

---
+++ packages/django-otp-agents/django-otp-agents-1.0.0.tar.gz/django-otp-agents-1.0.0/src/otp_agents/views.py
@@ -0,0 +1,17 @@
+import django_otp.views
+
+from otp_agents.forms import OTPAuthenticationForm, OTPTokenForm
+
+
+class LoginView(django_otp.views.LoginView):
+    """
+    This is just like :class:`django_otp.views.LoginView` except that it uses
+    our agent-trust-enabled forms.
+
+    """
+    otp_authentication_form = OTPAuthenticationForm
+    otp_token_form = OTPTokenForm
+
+
+# Backwards compatibility.
+login = LoginView.as_view()
