5e40341df5d8171ed9127a0302d3a744fb8c588f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:02 +0000

    Add package one-two-scrape/0.0.1/one-two-scrape-0.0.1.tar.gz

---
+++ packages/one-two-scrape/one-two-scrape-0.0.1.tar.gz/one-two-scrape-0.0.1/otscrape/__init__.py
@@ -0,0 +1,6 @@
+from otscrape.core.base.attribute import attribute, Attribute
+from otscrape.core.attribute import RequestText, RequestStatusCode, JSON, DictPath, XPath, ZipDict
+
+from otscrape.core.io import SimpleRequestLoader, DummyLoader
+
+from otscrape.core.base.page import Page
