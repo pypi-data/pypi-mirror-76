56f42c8c49d407694832b03d3eb94863f9a240aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:25 +0000

    Add package NDBD-distributions/0.1/NDBD_distributions-0.1.tar.gz

---
+++ packages/NDBD-distributions/NDBD_distributions-0.1.tar.gz/NDBD_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='NDBD_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['NDBD_distributions'],
+      author= ' Tanvi Tiwari',
+      author_email = "tanvitiwari1700@gmail.com",
+      zip_safe=False)
