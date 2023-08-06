68f0fdbb93746ae6cd6a70e40e339354d9be9404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:43 +0000

    Add package hitanshmultiply/1.0/hitanshmultiply-1.0-py3-none-any.whl

---
+++ packages/hitanshmultiply/hitanshmultiply-1.0-py3-none-any.whl/hitanshmultiply/mult2num.py
@@ -0,0 +1,15 @@
+""" This function helps you to multiply two integers."""
+
+
+class Multiplication:
+    """ This class has a function which helps you to multiply two numbers."""
+
+    def mult2num(self, a, b):
+        """Multiplication of two numbers
+
+        Parameters:
+        two(int): Send two integer parameters
+        Returns:
+        int: As an output."""
+
+        return a*b
---
+++ packages/hitanshmultiply/hitanshmultiply-1.0-py3-none-any.whl/hitanshmultiply/mult3num.py
@@ -0,0 +1,15 @@
+""" This function helps you to multiply three integers."""
+
+
+class Multiplication:
+    """ This class has a function which helps you to multiply three numbers."""
+
+    def mult2num(self, a, b, c):
+        """Multiplication of three numbers
+
+        Parameters:
+        two(int): Send three integer parameters
+        Returns:
+        int: As an output."""
+
+        return a*b*c
