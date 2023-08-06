61ee050a5e9e6f387196748efc37029c017acae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:16 +0000

    Add package udica/0.2.2/udica-0.2.2-py3.7.egg

---
+++ packages/udica/udica-0.2.2-py3.7.egg/udica/parse.py
@@ -0,0 +1,212 @@
+# Copyright (C) 2018 Lukas Vrabec, <lvrabec@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import json
+
+#: Constant for the podman engine
+ENGINE_PODMAN = "podman"
+
+#: Constant for the cri-o engine
+ENGINE_CRIO = "CRI-O"
+
+#: Constant for the docker engine
+ENGINE_DOCKER = "docker"
+
+#: All supported engines
+ENGINE_ALL = [ENGINE_PODMAN, ENGINE_CRIO, ENGINE_DOCKER]
+
+
+def json_is_podman_or_docker_format(json_rep):
+    """Check if the inspected file is in a format from docker or podman.
+
+    We know this because the type in the inspect command will be a list
+    """
+    return isinstance(json_rep, list)
+
+
+def json_is_podman_format(json_rep):
+    """Check if the inspected file is in a format from podman. """
+    return isinstance(json_rep, list) and (
+        "container=oci" in json_rep[0]["Config"]["Env"]
+        or "container=podman" in json_rep[0]["Config"]["Env"]
+    )
+
+
+def get_engine_helper(data, ContainerEngine):
+    engine = validate_container_engine(ContainerEngine)
+    if engine == "-":
+        json_rep = json.loads(data)
+        if json_is_podman_or_docker_format(json_rep):
+            if json_is_podman_format(json_rep):
+                return PodmanHelper()
+            return DockerHelper()
+        return CrioHelper()
+    else:
+        if engine == ENGINE_DOCKER:
+            return DockerHelper()
+        elif engine == ENGINE_PODMAN:
+            return PodmanHelper()
+        elif engine == ENGINE_CRIO:
+            return CrioHelper()
+        raise RuntimeError("Unkown engine")
+
+
+class EngineHelper(object):
+    def __init__(self, container_engine):
+        self.container_engine = container_engine
+
+    def parse_inspect(self, data):
+        return json.loads(data)
+
+    def get_mounts(self, data):
+        raise Exception(
+            "Error getting mounts from unknown format %s" % self.container_engine
+        )
+
+    def get_ports(self, data):
+        raise Exception(
+            "Error getting ports from unknown format %s" % self.container_engine
+        )
+
+    def get_caps(self, data, opts):
+        if opts["Caps"]:
+            if opts["Caps"] == "None":
+                return []
+            return opts["Caps"].split(",")
+        return []
+
+
+class PodmanDockerHelper(EngineHelper):
+    def get_mounts(self, data):
+        return data[0]["Mounts"]
+
+    def get_ports(self, data):
+        ports = []
+        for key, value in data[0]["NetworkSettings"]["Ports"].items():
+            container_port = str(key).split("/")
+            new_port = {
+                "portNumber": int(container_port[0]),
+                "protocol": container_port[1],
+            }
+            ports.append(new_port)
+        return ports
+
+
+class PodmanHelper(PodmanDockerHelper):
+    def __init__(self):
+        super().__init__(ENGINE_PODMAN)
+
+    def get_caps(self, data, opts):
+        if not opts["Caps"]:
+            return data[0]["EffectiveCaps"]
+        return []
+
+
+class DockerHelper(PodmanDockerHelper):
+    def __init__(self):
+        super().__init__(ENGINE_DOCKER)
+
+    def parse_inspect(self, data):
+        json_rep = super().parse_inspect(data)
+        self.adjust_json_from_docker(json_rep)
+        return json_rep
+
+    def adjust_json_from_docker(self, json_rep):
+        """If the json comes from a docker call, we need to adjust it to make use
+        of it. """
+
+        if not isinstance(json_rep[0]["NetworkSettings"]["Ports"], dict):
+            raise Exception(
+                "Error parsing docker engine inspection JSON structure, try to specify container engine using '--container-engine' parameter"
+            )
+
+        for item in json_rep[0]["Mounts"]:
+            item["source"] = item["Source"]
+            if item["Mode"] == "rw":
+                item["options"] = "rw"
+            if item["Mode"] == "ro":
+                item["options"] = "ro"
+
+
+class CrioHelper(EngineHelper):
+    def __init__(self):
+        super().__init__(ENGINE_CRIO)
+
+    def get_mounts(self, data):
+        return data["status"]["mounts"]
+
+    def get_ports(self, data):
+        # Not applicable in the CRI-O case, since this is handled by the
+        # kube-proxy/CNI.
+        return []
+
+
+def parse_cap(data):
+    return data.decode().split("\n")[1].split(",")
+
+
+def context_to_type(context):
+    return context.split("=")[1].split(":")[2]
+
+
+def remove_dupe_perms(string):
+    perms = string.split()
+    return " ".join(sorted(set(perms), key=perms.index))
+
+
+def parse_avc_file(data):
+    append_rules = []
+
+    for avc in data.splitlines():
+        new_rule = []
+        items = avc.split(" ")
+
+        if "type=AVC" not in items[0]:
+            continue
+
+        for item in items:
+            if "scontext" in item:
+                new_rule.append(context_to_type(item))
+            if "tcontext" in item:
+                new_rule.append(context_to_type(item))
+            if "tclass" in item:
+                new_rule.append(item.split("=")[1])
+
+        open_bracket = items.index("{")
+        perm = open_bracket + 1
+        new_rule.append(items[perm])
+
+        for rule in append_rules:
+            if (
+                rule[0] == new_rule[0]
+                and rule[1] == new_rule[1]
+                and rule[2] == new_rule[2]
+            ):
+                new_rule[3] = rule[3] + " " + new_rule[3]
+                append_rules.remove(rule)
+        new_rule[3] = remove_dupe_perms(new_rule[3])
+        append_rules.append(new_rule)
+
+    return append_rules
+
+
+def validate_container_engine(ContainerEngine):
+    if ContainerEngine in ENGINE_ALL + ["CRIO", "-"]:
+        # Fix CRIO reference to use ENGINE_CRIO
+        if ContainerEngine == "CRIO":
+            return ENGINE_CRIO
+        return ContainerEngine
+    else:
+        raise Exception("Container Engine %s is not supported." % ContainerEngine)
---
+++ packages/udica/udica-0.2.2-py3.7.egg/udica/policy.py
@@ -0,0 +1,434 @@
+# Copyright (C) 2018 Lukas Vrabec, <lvrabec@redhat.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from shutil import copy
+from os import chdir, getcwd, remove
+from os.path import exists
+import tarfile
+
+import selinux
+import semanage
+
+import udica.perms as perms
+
+# Check if templates are present in current directory
+# if yes, the templates directory is used instead of system one.
+TEMPLATES_STORE = (
+    "./templates" if exists("./templates") else "/usr/share/udica/templates"
+)
+
+CONFIG_CONTAINER = "/etc"
+HOME_CONTAINER = "/home"
+LOG_CONTAINER = "/var/log"
+TMP_CONTAINER = "/tmp"
+
+TEMPLATE_PLAYBOOK = "/usr/share/udica/ansible/deploy-module.yml"
+VARIABLE_FILE_NAME = "variables-deploy-module.yml"
+
+templates_to_load = []
+
+
+def add_template(template):
+    templates_to_load.append(template)
+
+
+def list_templates_to_string(templates_to_load):
+    return ".cil,".join(map(str, templates_to_load)) + ".cil"
+
+
+def list_contexts(directory):
+    directory_len = len(directory)
+
+    handle = semanage.semanage_handle_create()
+    semanage.semanage_connect(handle)
+
+    (rc, fclist) = semanage.semanage_fcontext_list(handle)
+    (rc, fclocal) = semanage.semanage_fcontext_list_local(handle)
+    (rc, fchome) = semanage.semanage_fcontext_list_homedirs(handle)
+
+    contexts = []
+    for fcontext in fclist + fclocal + fchome:
+        expression = semanage.semanage_fcontext_get_expr(fcontext)
+        if expression[0:directory_len] == directory:
+            context = semanage.semanage_fcontext_get_con(fcontext)
+            if context:
+                contexts.append(semanage.semanage_context_get_type(context))
+
+    selabel = selinux.selabel_open(selinux.SELABEL_CTX_FILE, None, 0)
+    (rc, context) = selinux.selabel_lookup(selabel, directory, 0)
+    if context == None:
+        if exists(directory) == False:
+            exit(1)
+        context = selinux.getfilecon(directory)[1]
+    contexts.append(context.split(":")[2])
+    return contexts
+
+
+def list_ports(port_number, port_proto):
+
+    handle = semanage.semanage_handle_create()
+    semanage.semanage_connect(handle)
+
+    (rc, plist) = semanage.semanage_port_list(handle)
+    (rc, plocal) = semanage.semanage_port_list_local(handle)
+
+    for port in plocal + plist:
+        con = semanage.semanage_port_get_con(port)
+        ctype = semanage.semanage_context_get_type(con)
+        proto = semanage.semanage_port_get_proto(port)
+        proto_str = semanage.semanage_port_get_proto_str(proto)
+        low = semanage.semanage_port_get_low(port)
+        high = semanage.semanage_port_get_high(port)
+        if low <= port_number <= high and port_proto == proto_str:
+            return ctype
+
+
+def create_policy(opts, capabilities, mounts, ports, append_rules, inspect_format):
+    policy = open(opts["ContainerName"] + ".cil", "w")
+    policy.write("(block " + opts["ContainerName"] + "\n")
+    policy.write("    (blockinherit container)\n")
+    add_template("base_container")
+
+    if opts["FullNetworkAccess"]:
+        policy.write("    (blockinherit net_container)\n")
+        add_template("net_container")
+
+    if opts["VirtAccess"]:
+        policy.write("    (blockinherit virt_container)\n")
+        add_template("virt_container")
+
+    if opts["XAccess"]:
+        policy.write("    (blockinherit x_container)\n")
+        add_template("x_container")
+
+    if opts["TtyAccess"]:
+        policy.write("    (blockinherit tty_container)\n")
+        add_template("tty_container")
+
+    if ports:
+        policy.write("    (blockinherit restricted_net_container)\n")
+        add_template("net_container")
+
+    if opts["StreamConnect"]:
+        policy.write(
+            "    (allow process "
+            + opts["StreamConnect"]
+            + ".process ( unix_stream_socket ( connectto ))) \n"
+        )
+        policy.write(
+            "    (allow process "
+            + opts["StreamConnect"]
+            + ".socket ( sock_file ( getattr write open append ))) \n"
+        )
+
+    # capabilities
+    if capabilities:
+        caps = ""
+        for item in capabilities:
+            # Capabilities parsed from podman inspection JSON file have prefix "CAP_", this should be removed
+            if "CAP_" in item:
+                caps = caps + perms.cap[item[4:]]
+            else:
+                caps = caps + perms.cap[item]
+
+        policy.write("    (allow process process ( capability ( " + caps + "))) \n")
+        policy.write("\n")
+
+    # ports
+    for item in ports:
+        if "portNumber" in item:
+            policy.write(
+                "    (allow process "
+                + list_ports(item["portNumber"], item["protocol"])
+                + " ( "
+                + perms.socket[item["protocol"]]
+                + " (  name_bind ))) \n"
+            )
+
+    # mounts
+    if inspect_format == "CRI-O":
+        write_policy_for_crio_mounts(mounts, policy)
+    else:
+        write_policy_for_podman_mounts(mounts, policy)
+
+    if append_rules != None:
+        for rule in append_rules:
+            if opts["ContainerName"] in rule[0]:
+                policy.write(
+                    "    (allow process "
+                    + rule[1]
+                    + " ( "
+                    + rule[2]
+                    + " ( "
+                    + rule[3]
+                    + " ))) \n"
+                )
+            else:
+                print(
+                    "WARNING: process type: "
+                    + rule[0]
+                    + " seems to be unrelated to this container policy. Skipping allow rule."
+                )
+
+    policy.write(")")
+    policy.close()
+
+
+def write_policy_for_crio_mounts(mounts, policy):
+    for item in mounts:
+        if item["hostPath"].startswith("/var/lib/kubelet"):
+            # These should already have the right context
+            continue
+        if item["hostPath"] == LOG_CONTAINER:
+            if item["readonly"]:
+                policy.write("    (blockinherit log_container)\n")
+            else:
+                policy.write("    (blockinherit log_rw_container)\n")
+            add_template("log_container")
+            continue
+
+        if item["hostPath"] == HOME_CONTAINER:
+            if item["readonly"]:
+                policy.write("    (blockinherit home_container)\n")
+            else:
+                policy.write("    (blockinherit home_rw_container)\n")
+            add_template("home_container")
+            continue
+
+        if item["hostPath"] == TMP_CONTAINER:
+            if item["readonly"]:
+                policy.write("    (blockinherit tmp_container)\n")
+            else:
+                policy.write("    (blockinherit tmp_rw_container)\n")
+            add_template("tmp_container")
+            continue
+
+        if item["hostPath"] == CONFIG_CONTAINER:
+            if item["readonly"]:
+                policy.write("    (blockinherit config_container)\n")
+            else:
+                policy.write("    (blockinherit config_rw_container)\n")
+            add_template("config_container")
+            continue
+
+        # TODO(jaosorior): Add prefix-dir to path. This way we could call this
+        # from a container in kubernetes
+        contexts = list_contexts(item["hostPath"])
+        for context in contexts:
+            if item["readonly"] is False:
+                policy.write(
+                    "    (allow process "
+                    + context
+                    + " ( dir ( "
+                    + perms.perm["drw"]
+                    + " ))) \n"
+                )
+                policy.write(
+                    "    (allow process "
+                    + context
+                    + " ( file ( "
+                    + perms.perm["frw"]
+                    + " ))) \n"
+                )
+                policy.write(
+                    "    (allow process "
+                    + context
+                    + " ( sock_file ( "
+                    + perms.perm["srw"]
+                    + " ))) \n"
+                )
+            else:
+                policy.write(
+                    "    (allow process "
+                    + context
+                    + " ( dir ( "
+                    + perms.perm["dro"]
+                    + " ))) \n"
+                )
+                policy.write(
+                    "    (allow process "
+                    + context
+                    + " ( file ( "
+                    + perms.perm["fro"]
+                    + " ))) \n"
+                )
+                policy.write(
+                    "    (allow process "
+                    + context
+                    + " ( sock_file ( "
+                    + perms.perm["sro"]
+                    + " ))) \n"
+                )
+
+
+def write_policy_for_podman_mounts(mounts, policy):
+    for item in mounts:
+        if not item["Source"].find("/"):
+            if item["Source"] == LOG_CONTAINER and item["RW"] is False:
+                policy.write("    (blockinherit log_container)\n")
+                add_template("log_container")
+                continue
+
+            if item["Source"] == LOG_CONTAINER and item["RW"] is True:
+                policy.write("    (blockinherit log_rw_container)\n")
+                add_template("log_container")
+                continue
+
+            if item["Source"] == HOME_CONTAINER and item["RW"] is False:
+                policy.write("    (blockinherit home_container)\n")
+                add_template("home_container")
+                continue
+
+            if item["Source"] == HOME_CONTAINER and item["RW"] is True:
+                policy.write("    (blockinherit home_rw_container)\n")
+                add_template("home_container")
+                continue
+
+            if item["Source"] == TMP_CONTAINER and item["RW"] is False:
+                policy.write("    (blockinherit tmp_container)\n")
+                add_template("tmp_container")
+                continue
+
+            if item["Source"] == TMP_CONTAINER and item["RW"] is True:
+                policy.write("    (blockinherit tmp_rw_container)\n")
+                add_template("tmp_container")
+                continue
+
+            if item["Source"] == CONFIG_CONTAINER and item["RW"] is False:
+                policy.write("    (blockinherit config_container)\n")
+                add_template("config_container")
+                continue
+
+            if item["Source"] == CONFIG_CONTAINER and item["RW"] is True:
+                policy.write("    (blockinherit config_rw_container)\n")
+                add_template("config_container")
+                continue
+
+            contexts = list_contexts(item["Source"])
+            for context in contexts:
+                if item["RW"] is True:
+                    policy.write(
+                        "    (allow process "
+                        + context
+                        + " ( dir ( "
+                        + perms.perm["drw"]
+                        + " ))) \n"
+                    )
+                    policy.write(
+                        "    (allow process "
+                        + context
+                        + " ( file ( "
+                        + perms.perm["frw"]
+                        + " ))) \n"
+                    )
+                    policy.write(
+                        "    (allow process "
+                        + context
+                        + " ( sock_file ( "
+                        + perms.perm["srw"]
+                        + " ))) \n"
+                    )
+                if item["RW"] is False:
+                    policy.write(
+                        "    (allow process "
+                        + context
+                        + " ( dir ( "
+                        + perms.perm["dro"]
+                        + " ))) \n"
+                    )
+                    policy.write(
+                        "    (allow process "
+                        + context
+                        + " ( file ( "
+                        + perms.perm["fro"]
+                        + " ))) \n"
+                    )
+                    policy.write(
+                        "    (allow process "
+                        + context
+                        + " ( sock_file ( "
+                        + perms.perm["sro"]
+                        + " ))) \n"
+                    )
+
+
+def load_policy(opts):
+    PWD = getcwd()
+    chdir(TEMPLATES_STORE)
+
+    if opts["LoadModules"]:
+        handle = semanage.semanage_handle_create()
+        semanage.semanage_connect(handle)
+
+        for template in templates_to_load:
+            semanage.semanage_module_install_file(handle, template + ".cil")
+
+        chdir(PWD)
+
+        semanage.semanage_module_install_file(handle, opts["ContainerName"] + ".cil")
+
+        semanage.semanage_commit(handle)
+    else:
+        templates = list_templates_to_string(templates_to_load)
+        if len(templates_to_load) > 1:
+            print(
+                "\nPlease load these modules using: \n# semodule -i "
+                + opts["ContainerName"]
+                + ".cil "
+                + TEMPLATES_STORE
+                + "/{"
+                + templates
+                + "}"
+            )
+        else:
+            print(
+                "\nPlease load these modules using: \n# semodule -i "
+                + opts["ContainerName"]
+                + ".cil "
+                + TEMPLATES_STORE
+                + "/"
+                + templates
+                + ""
+            )
+
+        chdir(PWD)
+
+
+def generate_playbook(opts):
+    src = TEMPLATE_PLAYBOOK
+    dst = "./"
+    copy(src, dst)
+
+    with open(VARIABLE_FILE_NAME, "w") as varsfile:
+        varsfile.write("archive: " + opts["ContainerName"] + "-policy.tar.gz\n")
+        varsfile.write(
+            "policy: "
+            + opts["ContainerName"]
+            + ".cil "
+            + list_templates_to_string(templates_to_load).replace(",", " ")
+            + "\n"
+        )
+        varsfile.write("final_policy: " + opts["ContainerName"] + ".cil")
+
+    with tarfile.open(opts["ContainerName"] + "-policy.tar.gz", "w:gz") as tar:
+        for template in templates_to_load:
+            tar.add(TEMPLATES_STORE + "/" + template + ".cil", template + ".cil")
+        tar.add(opts["ContainerName"] + ".cil")
+        remove(opts["ContainerName"] + ".cil")
+
+    print(
+        "\nAnsible playbook and archive with udica policies generated! \n"
+        + "Please run ansible play to deploy the policy."
+    )
