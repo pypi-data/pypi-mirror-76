c7dbe4de73ace846ee31dad8e7b50811d09abebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package allen-ytconcate/0.1.1/allen_ytconcate-0.1.1-py3-none-any.whl

---
+++ packages/allen-ytconcate/allen_ytconcate-0.1.1-py3-none-any.whl/yt_concate/main.py
@@ -0,0 +1,48 @@
+from yt_concate.pipeline.steps.preflight import Preflight
+from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.initialize_yt import InitializeYT
+from yt_concate.pipeline.steps.download_captions import DownloadCaptions
+from yt_concate.pipeline.steps.read_caption import ReadCaption
+from yt_concate.pipeline.steps.search import Search
+from yt_concate.pipeline.steps.download_videos import DownloadVideos
+from yt_concate.pipeline.steps.edit_video import EditVideo
+from yt_concate.pipeline.steps.postflight import Postflight
+from yt_concate.pipeline.steps.step import StepException
+from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.utils import Utils
+
+CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
+
+def config_logger():
+
+    return logger
+
+def main():
+    inputs = {
+        'channel_id': CHANNEL_ID,
+        'search_word': 'incredible',
+        'limit': 20,
+        'cleanup': False,
+        'logging_level': WARNING
+    }
+
+
+    steps = [
+        Preflight(),
+        GetVideoList(),
+        InitializeYT(),
+        DownloadCaptions(),
+        ReadCaption(),
+        Search(),
+        DownloadVideos(),
+        EditVideo(),
+        Postflight(),
+        ]
+    config_logger()
+    utils = Utils()
+    p = Pipeline(steps)
+    p.run(inputs, utils)
+
+
+if __name__ == '__main__':
+    main()
