b76b970bddff061f7ddbcd5f911489fff7c2471c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:09 +0000

    Add package growattServer/0.0.5/growattServer-0.0.5.tar.gz

---
+++ packages/growattServer/growattServer-0.0.5.tar.gz/growattServer-0.0.5/growattServer/__init__.py
@@ -0,0 +1,196 @@
+name = "growattServer"
+
+import datetime
+from enum import IntEnum
+import hashlib
+import json
+import requests
+import warnings
+
+def hash_password(password):
+    """
+    Normal MD5, except add c if a byte of the digest is less than 10.
+    """
+    password_md5 = hashlib.md5(password.encode('utf-8')).hexdigest()
+    for i in range(0, len(password_md5), 2):
+        if password_md5[i] == '0':
+            password_md5 = password_md5[0:i] + 'c' + password_md5[i + 1:]
+    return password_md5
+
+class Timespan(IntEnum):
+    day = 1
+    month = 2
+
+
+class GrowattApi:
+    server_url = 'http://server.growatt.com/'
+
+    def __init__(self):
+        self.session = requests.Session()
+
+    def get_url(self, page):
+        """
+        Simple helper function to get the page url/
+        """
+        return self.server_url + page
+
+    def login(self, username, password):
+        """
+        Log the user in.
+        """
+        password_md5 = hash_password(password)
+        response = self.session.post(self.get_url('LoginAPI.do'), data={
+            'userName': username,
+            'password': password_md5
+        })
+        data = json.loads(response.content.decode('utf-8'))
+        return data['back']
+
+    def plant_list(self, user_id):
+        """
+        Get a list of plants connected to this account.
+        """
+        response = self.session.get(self.get_url('PlantListAPI.do'),
+                                    params={'userId': user_id},
+                                    allow_redirects=False)
+        if response.status_code != 200:
+            raise RuntimeError("Request failed: %s", response)
+        data = json.loads(response.content.decode('utf-8'))
+        return data['back']
+
+    def plant_detail(self, plant_id, timespan, date):
+        """
+        Get plant details for specified timespan.
+        """
+        assert timespan in Timespan
+        if timespan == Timespan.day:
+            date_str = date.strftime('%Y-%m-%d')
+        elif timespan == Timespan.month:
+            date_str = date.strftime('%Y-%m')
+
+        response = self.session.get(self.get_url('PlantDetailAPI.do'), params={
+            'plantId': plant_id,
+            'type': timespan.value,
+            'date': date_str
+        })
+        data = json.loads(response.content.decode('utf-8'))
+        return data['back']
+
+    def inverter_data(self, inverter_id, date):
+        """
+        Get inverter data for specified date or today.
+        """
+        if date is None:
+            date = datetime.date.today()
+        date_str = date.strftime('%Y-%m-%d')
+        response = self.session.get(self.get_url('newInverterAPI.do'), params={
+            'op': 'getInverterData',
+            'id': inverter_id,
+            'type': 1,
+            'date': date_str
+        })
+        data = json.loads(response.content.decode('utf-8'))
+        return data
+
+    def inverter_detail(self, inverter_id):
+        """
+        Get "All parameters" from PV inverter.
+        """
+        response = self.session.get(self.get_url('newInverterAPI.do'), params={
+            'op': 'getInverterDetailData_two',
+            'inverterId': inverter_id
+        })
+
+        data = json.loads(response.content.decode('utf-8'))
+        return data
+
+    def tlx_data(self, tlx_id, date):
+        """
+        Get inverter data for specified date or today.
+        """
+        if date is None:
+            date = datetime.date.today()
+        date_str = date.strftime('%Y-%m-%d')
+        response = self.session.get(self.get_url('newTlxApi.do'), params={
+            'op': 'getTlxData',
+            'id': tlx_id,
+            'type': 1,
+            'date': date_str
+        })
+        data = json.loads(response.content.decode('utf-8'))
+        return data
+		
+    def tlx_detail(self, tlx_id):
+        """
+        Get "All parameters" from PV inverter.
+        """
+        response = self.session.get(self.get_url('newTlxApi.do'), params={
+            'op': 'getTlxDetailData',
+            'id': tlx_id
+        })
+
+        data = json.loads(response.content.decode('utf-8'))
+        return data
+    
+    def storage_detail(self, storage_id):
+        """
+        Get "All parameters" from battery storage.
+        """
+        response = self.session.get(self.get_url('newStorageAPI.do'), params={
+            'op': 'getStorageInfo_sacolar',
+            'storageId': storage_id
+        })
+
+        data = json.loads(response.content.decode('utf-8'))
+        return data
+
+    def storage_params(self, storage_id):
+        """
+        Get much more detail from battery storage.
+        """
+        response = self.session.get(self.get_url('newStorageAPI.do'), params={
+            'op': 'getStorageParams_sacolar',
+            'storageId': storage_id
+        })
+
+        data = json.loads(response.content.decode('utf-8'))
+        return data
+
+    def storage_energy_overview(self, plant_id, storage_id):
+        """
+        Get some energy/generation overview data.
+        """
+        response = self.session.post(self.get_url('newStorageAPI.do?op=getEnergyOverviewData_sacolar'), params={
+            'plantId': plant_id,
+            'storageSn': storage_id
+        })
+
+        data = json.loads(response.content.decode('utf-8'))
+        return data['obj']
+
+    def inverter_list(self, plant_id):
+        """
+        Use device_list, it's more descriptive since the list contains more than inverters.
+        """
+        warnings.warn("This function may be deprecated in the future because naming is not correct, use device_list instead", DeprecationWarning)
+        return self.device_list(plant_id)
+
+    def device_list(self, plant_id):
+        """
+        Get a list of all devices connected to plant.
+        """
+        return self.plant_info(plant_id)['deviceList']
+
+    def plant_info(self, plant_id):
+        """
+        Get basic plant information with device list.
+        """
+        response = self.session.get(self.get_url('newPlantAPI.do'), params={
+            'op': 'getAllDeviceListThree',
+            'plantId': plant_id,
+            'pageNum': 1,
+            'pageSize': 1
+        })
+
+        data = json.loads(response.content.decode('utf-8'))
+        return data
