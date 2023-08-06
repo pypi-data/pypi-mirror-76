53e9e4f68990b57445081d2fc928ad930256caa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:43 +0000

    Add package federal-register/0.0.1/federal-register-0.0.1.tar.gz

---
+++ packages/federal-register/federal-register-0.0.1.tar.gz/federal-register-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from setuptools import find_packages
+from setuptools import find_namespace_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='federal-register',
+    author='Alex Reed',
+    author_email='coding.sigma@gmail.com',
+    version='0.0.1',
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='',
+    install_requires=[
+        'requests'
+    ],
+    packages=find_namespace_packages(include=['federal_register']),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3'
+    ],
+    python_requires='>3.7'
+)
