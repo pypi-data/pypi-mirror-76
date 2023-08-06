9c148b42366a83c1bad2b519b7578aad8858c5a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:51 +0000

    Add package unsplashbot/0.0.1/unsplashbot-0.0.1.tar.gz

---
+++ packages/unsplashbot/unsplashbot-0.0.1.tar.gz/unsplashbot-0.0.1/unsplashbot/__init__.py
@@ -0,0 +1,10 @@
+print('Thank you for trying unsplashbot')
+
+"""
+A collection of selenium automation scripts for
+searching and downloading stock images
+for developing and designing.
+
+Originally wrritten by Manoj A.P 
+:print: http://github.com/manojap
+"""
