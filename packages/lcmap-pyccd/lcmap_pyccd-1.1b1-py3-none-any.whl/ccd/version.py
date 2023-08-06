7ab41b2e73093ff76f635c7f97e8d7c0f96e2b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:44 +0000

    Add package lcmap-pyccd/1.1b1/lcmap_pyccd-1.1b1-py3-none-any.whl

---
+++ packages/lcmap-pyccd/lcmap_pyccd-1.1b1-py3-none-any.whl/ccd/version.py
@@ -0,0 +1,9 @@
+""" holds algorithm name & version information.  Information is needed in setup.py 
+as well as ccd/__init__.py.  If vals were all defined in __init__.py install would fail.
+"""
+
+__name = 'lcmap-pyccd'
+__version = '1.1-b1'
+
+#__algorithm = ':'.join([__name, __version])
+
