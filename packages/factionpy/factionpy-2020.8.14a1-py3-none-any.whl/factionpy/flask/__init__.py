801d563d75d9c3682d7f144820842a6b17cbc0b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:55 +0000

    Add package factionpy/2020.8.14a1/factionpy-2020.8.14a1-py3-none-any.whl

---
+++ packages/factionpy/factionpy-2020.8.14a1-py3-none-any.whl/factionpy/flask/__init__.py
@@ -0,0 +1,75 @@
+from flask import request, jsonify
+from factionpy.logger import log
+from functools import wraps
+from factionpy.services import validate_authorization_header
+
+standard_read = [
+    'admin',
+    'operator',
+    'read-only'
+]
+
+standard_write = [
+    'admin',
+    'operator'
+]
+
+
+class authorized_groups(object):
+    def __init__(self, groups: list):
+        self.groups = groups
+
+    def __call__(self, func):
+
+        @wraps(func)
+        def callable(*args, **kwargs):
+            log("factionpy:authorized_groups", f"Checking if authenticated..")
+            user_data = None
+            authorized = False
+            groups = self.groups
+            try:
+                auth_header = request.headers.get("Authorization", None)
+                log("factionpy:authorized_groups", f"got auth_header: {auth_header}")
+                if auth_header:
+                    verified_header = validate_authorization_header(auth_header)
+                    log("factionpy:authorized_groups", f"got verfied_header: {verified_header}")
+                    if verified_header["success"] == "True":
+                        user_data = verified_header["results"]
+            except Exception as e:
+                log("factionpy:authorized_groups", f"Could not verify Authorization header. Error: {e}")
+                return jsonify({
+                    "success": "false",
+                    "message": f"Could not verify Authorization header. Error: {e}"
+                }), 401
+
+            if user_data:
+                try:
+                    log("factionpy:authorized_groups", f"got user_data: {user_data}")
+                    # Replace meta group names with contents of meta group
+                    if 'standard_read' in groups:
+                        groups.remove('standard_read')
+                        groups.extend(standard_read)
+
+                    if 'standard_write' in groups:
+                        groups.remove('standard_write')
+                        groups.extend(standard_write)
+
+                    if 'all' in groups:
+                        authorized = True
+
+                    # Iterate through valid groups, checking if the user is in there.
+                    if user_data['role'] in groups:
+                        log("factionpy:authorized_groups", f"user authorized. returning results of function.")
+                        return func(*args, **kwargs)
+                    else:
+                        log("factionpy:authorized_groups", f"User {user_data['username']} is not in the following "
+                              f"groups: {groups}")
+                except Exception as e:
+                    log("factionpy:authorized_groups", f"Could not verify user_data. Error: {e}")
+                    pass
+            return jsonify({
+                "success": "false",
+                "message": f"Invalid API key provided or you do not have permission to perform this action."
+            }), 401
+
+        return callable
