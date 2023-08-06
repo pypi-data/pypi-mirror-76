3ec61f179a86bb38415e6df831428f41d14857c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:56 +0000

    Add package hello-image/0.0.2/hello_image-0.0.2.tar.gz

---
+++ packages/hello-image/hello_image-0.0.2.tar.gz/hello_image-0.0.2/README.md
@@ -0,0 +1,31 @@
+# Flask-Share
+
+Create social share component in Jinja2 template based
+on [share.js](https://github.com/overtrue/share.js/).
+
+## Links
+
+* [Documentation](https://flask-share.readthedocs.io/en/latest/)
+* [PyPI](https://pypi.org/project/Flask-Share/)
+
+## Screenshot
+
+![flask-share demo](images/demo.png)
+
+## Screenshot2
+.. image::  https://raw.githubusercontent.com/hadi-muhammad/hello_image/master/images/pyplatform.png
+
+## Screenshot3
+![pyplatform](https://raw.githubusercontent.com/hadi-muhammad/hello_image/master/images/pyplatform.png)
+
+## Test Image
+![test image](images/pyplatform.png)
+## License
+
+This project is licensed under the MIT License (see the
+`LICENSE` file for details).
+
+## Test Image5
+<img src="https://raw.githubusercontent.com/hadi-muhammad/hello_image/master/images/pyplatform.png"
+     alt="Markdown Monster icon"
+     style="float: left; margin-right: 10px;" />
---
+++ packages/hello-image/hello_image-0.0.2.tar.gz/hello_image-0.0.2/setup.py
@@ -0,0 +1,57 @@
+"""
+    
+"""
+from os import path
+from codecs import open
+from setuptools import setup
+
+basedir = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(basedir, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='hello_image',
+    version='0.0.2',
+    url='https://github.com/hadi-muhammad/hello_image',
+    license='MIT',
+    author='Me',
+    author_email='',
+    description='test image for pypi docs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    platforms='any',
+    packages=['hello_image'],
+    zip_safe=False,
+    test_suite='test_image',
+    include_package_data=True,
+    install_requires=[
+        'Flask'
+    ],
+    extras_require={
+        'dev': [
+            'coverage',
+            'flake8',
+            'tox',
+        ],
+     },
+    keywords='flask extension development',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
